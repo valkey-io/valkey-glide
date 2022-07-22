4ed98e7d709aee6a3975845448cbe1e318d16cc8
Shachar Langbeheim <shachlan@amazon.com>
2022-07-22 08:40:37 +0000

    Add support for pipeline in Python.

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,9 +1,10 @@
 use pyo3::prelude::*;
+use pyo3::types::PyString;
 use redis::aio::{MultiplexedConnection};
 use redis::AsyncCommands;
 
 #[pyclass]
-struct Client{
+struct Client {
     multiplexer: MultiplexedConnection
 }
 
@@ -34,6 +35,49 @@ impl Client {
         pyo3_asyncio::tokio::future_into_py(py, async move {
             let _:() = connection.set(key, value).await.unwrap();
             Ok(Python::with_gil(|py| py.None()))
+        })
+    }
+
+    fn create_pipeline(&self) -> Pipeline{
+        Pipeline::new(self.multiplexer.clone())
+    }
+}
+
+#[pyclass]
+struct Pipeline {
+    internal_pipeline: redis::Pipeline,
+    multiplexer: MultiplexedConnection
+}
+
+impl Pipeline {
+    fn new(multiplexer: MultiplexedConnection) -> Self {
+        Pipeline { internal_pipeline: redis::Pipeline::new(), multiplexer }
+    }
+}
+
+#[pymethods]
+impl Pipeline {
+    fn get<'a>(this: &'a PyCell<Self>, key:String) -> &'a PyCell<Self> {
+        let mut pipeline = this.borrow_mut();
+        pipeline.internal_pipeline.get(key);
+        this
+    }
+
+    fn set<'a>(this: &'a PyCell<Self>, key: String, value: String) -> &'a PyCell<Self> {
+        let mut pipeline = this.borrow_mut();
+        pipeline.internal_pipeline.set(key, value);
+        this
+    }
+
+    fn execute<'a>(&self, py: Python<'a>) -> PyResult<&'a PyAny> {
+        let mut connection = self.multiplexer.clone();
+        let pipeline = self.internal_pipeline.clone();
+        pyo3_asyncio::tokio::future_into_py(py, async move {
+            let result: Result<Vec<String>, redis::RedisError> = pipeline.query_async(&mut connection).await;
+            match result {
+                Ok(results) => Ok(Python::with_gil(|py| results.into_py(py))),
+                Err(err) => Err(PyErr::new::<PyString, _>(err.to_string())),
+            }
         })
     }
 }
--- python/tests/babushka_test.py
+++ python/tests/babushka_test.py
@@ -11,3 +11,25 @@ async def test_set_get():
     await client.set("key", time_str)
     result = await client.get("key")
     assert result == time_str
+
+
+async def test_set_get_pipeline():
+    client = await babushka.Client.new(
+        "redis://localhost:6379"
+    )  # replace weith your Redis server
+    pipeline = client.create_pipeline()
+    time_str = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    pipeline.set("pipeline_key", time_str)
+    pipeline.get("pipeline_key")
+    result = await pipeline.execute()
+    assert result == ["OK", time_str]
+
+
+async def test_set_get_pipeline_chained_requests():
+    client = await babushka.Client.new(
+        "redis://localhost:6379"
+    )  # replace weith your Redis server
+    pipeline = client.create_pipeline()
+    time_str = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    result = await pipeline.set("pipeline_key", time_str).get("pipeline_key").execute()
+    assert result == ["OK", time_str]
