e93d2e4b582ca79314e55d9100f995fedd32fb89
Shachar Langbeheim <98546660+shachlanAmazon@users.noreply.github.com>
2022-07-26 10:38:54 +0300

    Add support for pipeline in Python. (#5)
    * Add vscode settings that conform to linters.
    
    * Add .gitignore file for Node.
    
    * Add support for pipeline in Python.
    
    * Rename types to async.
    
    * Add option to ignore set result.

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,20 +1,21 @@
 use pyo3::prelude::*;
+use pyo3::types::PyString;
 use redis::aio::{MultiplexedConnection};
 use redis::AsyncCommands;
 
 #[pyclass]
-struct Client{
+struct AsyncClient {
     multiplexer: MultiplexedConnection
 }
 
 #[pymethods]
-impl Client {
+impl AsyncClient {
     #[staticmethod]
     fn new<'a>(address: String, py: Python<'a>) -> PyResult<&'a PyAny> {
         pyo3_asyncio::tokio::future_into_py(py, async move {
             let client = redis::Client::open(address).unwrap();
             let multiplexer = client.get_multiplexed_async_connection().await.unwrap();
-            let client = Client {
+            let client = AsyncClient {
                 multiplexer
             };
             Ok(Python::with_gil(|py| client.into_py(py)))
@@ -36,11 +37,58 @@ impl Client {
             Ok(Python::with_gil(|py| py.None()))
         })
     }
+
+    fn create_pipeline(&self) -> AsyncPipeline{
+        AsyncPipeline::new(self.multiplexer.clone())
+    }
+}
+
+#[pyclass]
+struct AsyncPipeline {
+    internal_pipeline: redis::Pipeline,
+    multiplexer: MultiplexedConnection
+}
+
+impl AsyncPipeline {
+    fn new(multiplexer: MultiplexedConnection) -> Self {
+        AsyncPipeline { internal_pipeline: redis::Pipeline::new(), multiplexer }
+    }
+}
+
+#[pymethods]
+impl AsyncPipeline {
+    fn get<'a>(this: &'a PyCell<Self>, key:String) -> &'a PyCell<Self> {
+        let mut pipeline = this.borrow_mut();
+        pipeline.internal_pipeline.get(key);
+        this
+    }
+
+    #[args(ignore_result=false)]
+    fn set<'a>(this: &'a PyCell<Self>, key: String, value: String, ignore_result: bool) -> &'a PyCell<Self> {
+        let mut pipeline = this.borrow_mut();
+        pipeline.internal_pipeline.set(key, value);
+        if ignore_result { 
+          pipeline.internal_pipeline.ignore();
+        }
+        this
+    }
+
+    fn execute<'a>(&self, py: Python<'a>) -> PyResult<&'a PyAny> {
+        let mut connection = self.multiplexer.clone();
+        let pipeline = self.internal_pipeline.clone();
+        pyo3_asyncio::tokio::future_into_py(py, async move {
+            let result: Result<Vec<String>, redis::RedisError> = pipeline.query_async(&mut connection).await;
+            match result {
+                Ok(results) => Ok(Python::with_gil(|py| results.into_py(py))),
+                Err(err) => Err(PyErr::new::<PyString, _>(err.to_string())),
+            }
+        })
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn babushka(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Client>()?;
+    m.add_class::<AsyncClient>()?;
     Ok(())
 }
