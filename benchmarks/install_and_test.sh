b5aaf1ca00ade0e8254ed770d4a714f740f46183
Bar Shaul <barshaul@amazon.com>
2022-08-01 10:21:52 +0000

    rebasing install_and_test.sh script

--- benchmarks/install_and_test.sh
+++ benchmarks/install_and_test.sh
@@ -2,17 +2,22 @@
 
 set -x
 
+function runPythonBenchmark(){
+  cd ${PYTHON_FOLDER}
+  python -m venv .env
+  source .env/bin/activate
+  pip install --upgrade --quiet pip
+  pip install --quiet -r requirements.txt
+  maturin develop
+  echo "Starting Python benchmarks"
+  python ${BENCH_FOLDER}/python_benchmark.py
+  # exit python virtualenv
+  deactivate
+}
+
+
 script=`pwd`/${BASH_SOURCE[0]}
 HERE=`dirname ${script}`
 BENCH_FOLDER=`realpath ${HERE}`
 PYTHON_FOLDER="${BENCH_FOLDER}/../python"
-
-cd ${PYTHON_FOLDER}
-
-python -m venv .env
-source .env/bin/activate
-pip install --upgrade --quiet pip
-pip install --quiet -r requirements.txt
-maturin develop
-echo "Starting Python benchmarks"
-python ${BENCH_FOLDER}/python_benchmark.py
+runPythonBenchmark
