fa90d9ded66f22a42ebba450baa204d937205587
Shachar Langbeheim <shachlan@amazon.com>
2022-08-11 17:30:22 +0000

    Fix Clippy's comments.
    Because the socket listener was behind a feature flag, Clippy didn't check
    our additions.
    The vast changes to the .yml files are mostly indentation changes.
    The main change is the addition of optional arguments to the clippy linter,
    used to tell it which features to check.

--- redis-rs/src/socket_listener/mod.rs
+++ redis-rs/src/socket_listener/mod.rs
@@ -1,5 +1,5 @@
 /// Contains information that determines how the request and response headers are shaped.
 pub mod headers;
 mod rotating_buffer;
-/// Contains the functions that create a socket listener.
-pub mod socket_listener;
+mod socket_listener_impl;
+pub use socket_listener_impl::*;
