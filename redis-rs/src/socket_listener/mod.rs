791e7838098f590d9574219b82df353029e13ea3
Shachar Langbeheim <shachlan@amazon.com>
2022-08-11 15:44:53 +0000

    Add missing documentation.

--- redis-rs/src/socket_listener/mod.rs
+++ redis-rs/src/socket_listener/mod.rs
@@ -1,3 +1,5 @@
-mod headers;
+/// Contains information that determines how the request and response headers are shaped.
+pub mod headers;
 mod rotating_buffer;
+/// Contains the functions that create a socket listener.
 pub mod socket_listener;
