79dce610ccde1e99c443db6392b0238ba6ae099f
Shachar Langbeheim <shachlan@amazon.com>
2022-09-08 11:16:22 +0000

    Move Messages outisde of AsyncClient.
    This allows AsyncSocketClient to use messages, too.

---
+++ csharp/lib/MessageContainer.cs
@@ -0,0 +1,53 @@
+using System.Collections.Concurrent;
+
+namespace babushka
+{
+
+    internal class MessageContainer
+    {
+        internal Message<string?> GetMessage(int index)
+        {
+            return messages[index];
+        }
+
+        internal (Message<string?>, Task<string?>) GetMessageForCall(string key, string? value)
+        {
+            var message = GetFreeMessage();
+            var task = message.CreateTask(key, value, this).ContinueWith(result =>
+            {
+                ReturnFreeMessage(message);
+                return result.Result;
+            });
+            return (message, task);
+        }
+
+        private Message<string?> GetFreeMessage()
+        {
+            if (!availableMessages.TryDequeue(out var message))
+            {
+                lock (messages)
+                {
+                    var index = messages.Count;
+                    message = new Message<string?>(index);
+                    messages.Add(message);
+                }
+            }
+            return message;
+        }
+
+        private void ReturnFreeMessage(Message<string?> message)
+        {
+            availableMessages.Enqueue(message);
+        }
+
+        /// This list allows us random-access to the message in each index,
+        /// which means that once we receive a callback with an index, we can
+        /// find the message to resolve in constant time.
+        private List<Message<string?>> messages = new();
+
+        /// This queue contains the messages that were created and are currently unused by any task,
+        /// so they can be reused y new tasks instead of allocating new messages.
+        private ConcurrentQueue<Message<string?>> availableMessages = new();
+    }
+
+}
