1245f55ffef122d364e28390b7aae3d2bb881c27
Shachar Langbeheim <shachlan@amazon.com>
2022-10-02 16:50:35 +0000

    Benchmarks: Separate gets to set and unset values.
    This will allow us to compare the latency of get calls that return a
    value, to those that don't.

--- benchmarks/node/node_benchmark.ts
+++ benchmarks/node/node_benchmark.ts
@@ -5,10 +5,17 @@ import { AsyncClient, SocketConnection } from "babushka-rs";
 import commandLineArgs from "command-line-args";
 import { writeFileSync } from "fs";
 
+enum ChosenAction {
+    GET_NON_EXISTING,
+    GET_EXISTING,
+    SET,
+}
+
 const HOST = "localhost";
 const PORT = 6379;
 const ADDRESS = `redis://${HOST}:${PORT}`;
 const PROB_GET = 0.8;
+const PROB_GET_EXISTING_KEY = 0.8;
 const SIZE_GET_KEYSPACE = 3750000; // 3.75 million
 const SIZE_SET_KEYSPACE = 3000000; // 3 million
 
@@ -16,8 +23,11 @@ let counter = 0;
 const running_tasks: Promise<void>[] = [];
 const bench_str_results: string[] = [];
 const bench_json_results: {}[] = [];
-const get_latency: Record<string, number[]> = {};
-const set_latency: Record<string, number[]> = {};
+const action_latencies: Record<ChosenAction, Record<string, number[]>> = {
+    [ChosenAction.SET]: {},
+    [ChosenAction.GET_NON_EXISTING]: {},
+    [ChosenAction.GET_EXISTING]: {},
+};
 
 interface IAsyncClient {
     set: (key: string, value: string) => Promise<any>;
@@ -32,11 +42,18 @@ function generate_key_set(): string {
     return (Math.floor(Math.random() * SIZE_SET_KEYSPACE) + 1).toString();
 }
 function generate_key_get(): string {
-    return Math.floor(Math.random() * SIZE_GET_KEYSPACE + 1).toString();
+    const range = SIZE_GET_KEYSPACE - SIZE_SET_KEYSPACE;
+    return Math.floor(Math.random() * range + SIZE_SET_KEYSPACE + 1).toString();
 }
 
-function should_get(): boolean {
-    return Math.random() < PROB_GET;
+function choose_action(): ChosenAction {
+    if (Math.random() > PROB_GET) {
+        return ChosenAction.SET;
+    }
+    if (Math.random() > PROB_GET_EXISTING_KEY) {
+        return ChosenAction.GET_NON_EXISTING;
+    }
+    return ChosenAction.GET_EXISTING;
 }
 
 function calculate_latency(latency_list: number[], percentile_point: number) {
@@ -62,15 +79,21 @@ async function redis_benchmark(
     data: string
 ) {
     while (counter < total_commands) {
-        let use_get = should_get();
+        const chosen_action = choose_action();
         let tic = process.hrtime();
-        if (use_get) {
-            await client.get(generate_key_get());
-        } else {
-            await client.set(generate_key_set(), data);
+        switch (chosen_action) {
+            case ChosenAction.GET_EXISTING:
+                await client.get(generate_key_set());
+                break;
+            case ChosenAction.GET_NON_EXISTING:
+                await client.get(generate_key_get());
+                break;
+            case ChosenAction.SET:
+                await client.set(generate_key_set(), data);
+                break;
         }
         let toc = process.hrtime(tic);
-        const latency_list = (use_get ? get_latency : set_latency)[client_name];
+        const latency_list = action_latencies[chosen_action][client_name];
         latency_list.push(toc[0] * 1000 + toc[1] / 1000000);
         counter += 1;
     }
@@ -84,8 +107,9 @@ async function create_bench_tasks(
     data: string
 ) {
     counter = 0;
-    get_latency[client_name] = [];
-    set_latency[client_name] = [];
+    action_latencies[ChosenAction.GET_EXISTING][client_name] = [];
+    action_latencies[ChosenAction.GET_NON_EXISTING][client_name] = [];
+    action_latencies[ChosenAction.SET][client_name] = [];
     let tic = process.hrtime();
     for (let i = 0; i < num_of_concurrent_tasks; i++) {
         running_tasks.push(
@@ -113,23 +137,59 @@ async function run_client(
         data
     );
     const tps = Math.round(counter / time);
-    const get_p50_latency = calculate_latency(get_latency[client_name], 50);
-    const get_p90_latency = calculate_latency(get_latency[client_name], 90);
-    const get_p99_latency = calculate_latency(get_latency[client_name], 99);
-    const get_std_dev = stdev(get_latency[client_name]);
+
+    const get_non_existing_latency =
+        action_latencies[ChosenAction.GET_NON_EXISTING];
+    const get_non_existing_p50_latency = calculate_latency(
+        get_non_existing_latency[client_name],
+        50
+    );
+    const get_non_existing_p90_latency = calculate_latency(
+        get_non_existing_latency[client_name],
+        90
+    );
+    const get_non_existing_p99_latency = calculate_latency(
+        get_non_existing_latency[client_name],
+        99
+    );
+    const get_non_existing_std_dev = stdev(
+        get_non_existing_latency[client_name]
+    );
+
+    const get_existing_latency = action_latencies[ChosenAction.GET_EXISTING];
+    const get_existing_p50_latency = calculate_latency(
+        get_existing_latency[client_name],
+        50
+    );
+    const get_existing_p90_latency = calculate_latency(
+        get_existing_latency[client_name],
+        90
+    );
+    const get_existing_p99_latency = calculate_latency(
+        get_existing_latency[client_name],
+        99
+    );
+    const get_existing_std_dev = stdev(get_existing_latency[client_name]);
+
+    const set_latency = action_latencies[ChosenAction.SET];
     const set_p50_latency = calculate_latency(set_latency[client_name], 50);
     const set_p90_latency = calculate_latency(set_latency[client_name], 90);
     const set_p99_latency = calculate_latency(set_latency[client_name], 99);
     const set_std_dev = stdev(set_latency[client_name]);
+
     const json_res = {
         client: client_name,
         num_of_tasks: num_of_concurrent_tasks,
         data_size,
         tps,
-        get_p50_latency,
-        get_p90_latency,
-        get_p99_latency,
-        get_std_dev,
+        get_non_existing_p50_latency,
+        get_non_existing_p90_latency,
+        get_non_existing_p99_latency,
+        get_non_existing_std_dev,
+        get_existing_p50_latency,
+        get_existing_p90_latency,
+        get_existing_p99_latency,
+        get_existing_std_dev,
         set_p50_latency,
         set_p90_latency,
         set_p99_latency,
@@ -138,7 +198,8 @@ async function run_client(
     bench_json_results.push(json_res);
     bench_str_results.push(
         `client: ${client_name}, concurrent_tasks: ${num_of_concurrent_tasks}, data_size: ${data_size}, TPS: ${tps}, ` +
-            `get_p50: ${get_p50_latency}, get_p90: ${get_p90_latency}, get_p99: ${get_p99_latency}, get_std_dev: ${get_std_dev}, ` +
+            `get_non_existing_p50: ${get_non_existing_p50_latency}, get_non_existing_p90: ${get_non_existing_p90_latency}, get_non_existing_p99: ${get_non_existing_p99_latency}, get_non_existing_std_dev: ${get_non_existing_std_dev}, ` +
+            `get_existing_p50: ${get_existing_p50_latency}, get_existing_p90: ${get_existing_p90_latency}, get_existing_p99: ${get_existing_p99_latency}, get_existing_std_dev: ${get_existing_std_dev}, ` +
             `set_p50: ${set_p50_latency}, set_p90: ${set_p90_latency}, set_p99: ${set_p99_latency}, set_std_dev: ${set_std_dev}`
     );
 }
