8c5d00977d27da5f0a10d93a679938bd46be9df1
Shachar Langbeheim <shachlan@amazon.com>
2023-01-08 14:29:07 +0000

    Remove redis-rs folder.

--- redis-rs/src/socket_listener/mod.rs
+++
@@ -1,5 +0,0 @@
-/// Contains information that determines how the request and response headers are shaped.
-pub mod headers;
-mod rotating_buffer;
-mod socket_listener_impl;
-pub use socket_listener_impl::*;
