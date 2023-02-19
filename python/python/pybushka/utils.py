95a5bbe3750bd752348451fb31843012732b1190
Shachar Langbeheim <shachlan@amazon.com>
2023-02-19 13:52:29 +0000

    Benchmarks: Add TLS support.

--- python/python/pybushka/utils.py
+++ python/python/pybushka/utils.py
@@ -1,5 +1,5 @@
-def to_url(host, port=6379, user="", password="", tls=False, *args, **kwargs):
-    protocol = "rediss" if tls else "redis"
+def to_url(host, port=6379, user="", password="", tls_enabled=False, *args, **kwargs):
+    protocol = "rediss" if tls_enabled else "redis"
     auth = ""
     if user or password:
         auth = f"{user}{':' if user else ''}"
