987294440b40206392eb43eb5e20c5e765bd6181
Shachar Langbeheim <shachlan@amazon.com>
2023-03-10 14:09:23 +0000

    Remove the unnecessary headers file.

--- babushka-core/src/headers.rs
+++
@@ -1,8 +0,0 @@
-use lifeguard::RcRecycled;
-use std::rc::Rc;
-include!(concat!(env!("OUT_DIR"), "/protobuf/mod.rs"));
-
-pub(super) type Buffer = RcRecycled<Vec<u8>>;
-/// Buffer needs to be wrapped in Rc, because RcRecycled's clone implementation
-/// involves copying the array.
-pub(super) type SharedBuffer = Rc<Buffer>;
