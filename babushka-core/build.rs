aac45ce4d137fc73c31bffac8b58a30cb75e1041
Shachar Langbeheim <shachlan@amazon.com>
2023-03-13 15:05:45 +0000

    Split protobuf configuration into separate files.

--- babushka-core/build.rs
+++ babushka-core/build.rs
@@ -2,6 +2,8 @@ fn main() {
     protobuf_codegen::Codegen::new()
         .cargo_out_dir("protobuf")
         .include("src")
-        .input("src/protobuf/pb_message.proto")
+        .input("src/protobuf/redis_request.proto")
+        .input("src/protobuf/response.proto")
+        .input("src/protobuf/connection_request.proto")
         .run_from_script();
 }
