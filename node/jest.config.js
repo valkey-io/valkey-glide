de3bd50bceed94b80caabe10cec4cd718de45d64
Shachar Langbeheim <shachlan@amazon.com>
2023-04-19 11:14:01 +0000

    Move tests to `tests` folder.
    This is done to align with the naming convention throughout the repo.

--- node/jest.config.js
+++ node/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     preset: "ts-jest",
     transform: { "^.+\\.ts?$": "ts-jest" },
     testEnvironment: "node",
-    testRegex: "/test_module/.*\\.(test|spec)?\\.(ts|tsx)$",
+    testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
