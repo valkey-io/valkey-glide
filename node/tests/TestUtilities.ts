39d82997ee0715319a45ecc63c59b9df970d524a
Shachar Langbeheim <shachlan@amazon.com>
2023-04-27 12:56:03 +0000

    Add cluster support in node.

--- node/tests/TestUtilities.ts
+++ node/tests/TestUtilities.ts
@@ -1,6 +1,6 @@
-import { setLoggerConfig } from "..";
+import { beforeAll, expect, it } from "@jest/globals";
 import { v4 as uuidv4 } from "uuid";
-import { expect, beforeAll, it } from "@jest/globals";
+import { setLoggerConfig } from "..";
 
 beforeAll(() => {
     setLoggerConfig("info");
@@ -23,7 +23,8 @@ export async function GetAndSetRandomValue(client: Client) {
 
 export function runCommonTests<Context>(config: {
     init: () => Promise<{ context: Context; client: Client }>;
-    close: (context: Context) => Promise<void>;
+    close: (context: Context) => void;
+    timeout?: number;
 }) {
     const runTest = async (test: (client: Client) => Promise<void>) => {
         const { context, client } = await config.init();
@@ -35,75 +36,99 @@ export function runCommonTests<Context>(config: {
         }
     };
 
-    it("set and get flow works", async () => {
-        await runTest((client: Client) => GetAndSetRandomValue(client));
-    });
+    it(
+        "set and get flow works",
+        async () => {
+            await runTest((client: Client) => GetAndSetRandomValue(client));
+        },
+        config.timeout
+    );
 
-    it("can handle non-ASCII unicode", async () => {
-        await runTest(async (client: Client) => {
-            const key = uuidv4();
-            const value = "שלום hello 汉字";
-            await client.set(key, value);
-            const result = await client.get(key);
-            expect(result).toEqual(value);
-        });
-    });
+    it(
+        "can handle non-ASCII unicode",
+        async () => {
+            await runTest(async (client: Client) => {
+                const key = uuidv4();
+                const value = "שלום hello 汉字";
+                await client.set(key, value);
+                const result = await client.get(key);
+                expect(result).toEqual(value);
+            });
+        },
+        config.timeout
+    );
 
-    it("get for missing key returns null", async () => {
-        await runTest(async (client: Client) => {
-            const result = await client.get(uuidv4());
+    it(
+        "get for missing key returns null",
+        async () => {
+            await runTest(async (client: Client) => {
+                const result = await client.get(uuidv4());
 
-            expect(result).toEqual(null);
-        });
-    });
+                expect(result).toEqual(null);
+            });
+        },
+        config.timeout
+    );
 
-    it("get for empty string", async () => {
-        await runTest(async (client: Client) => {
-            const key = uuidv4();
-            await client.set(key, "");
-            const result = await client.get(key);
+    it(
+        "get for empty string",
+        async () => {
+            await runTest(async (client: Client) => {
+                const key = uuidv4();
+                await client.set(key, "");
+                const result = await client.get(key);
 
-            expect(result).toEqual("");
-        });
-    });
+                expect(result).toEqual("");
+            });
+        },
+        config.timeout
+    );
 
-    it("send very large values", async () => {
-        await runTest(async (client: Client) => {
-            const WANTED_LENGTH = Math.pow(2, 16);
-            const getLongUUID = () => {
-                let id = uuidv4();
-                while (id.length < WANTED_LENGTH) {
-                    id += uuidv4();
+    it(
+        "send very large values",
+        async () => {
+            await runTest(async (client: Client) => {
+                const WANTED_LENGTH = Math.pow(2, 16);
+                const getLongUUID = () => {
+                    let id = uuidv4();
+                    while (id.length < WANTED_LENGTH) {
+                        id += uuidv4();
+                    }
+                    return id;
+                };
+                const key = getLongUUID();
+                const value = getLongUUID();
+                await client.set(key, value);
+                const result = await client.get(key);
+
+                expect(result).toEqual(value);
+            });
+        },
+        config.timeout
+    );
+
+    it(
+        "can handle concurrent operations",
+        async () => {
+            await runTest(async (client: Client) => {
+                const singleOp = async (index: number) => {
+                    if (index % 2 === 0) {
+                        await GetAndSetRandomValue(client);
+                    } else {
+                        const result = await client.get(uuidv4());
+                        expect(result).toEqual(null);
+                    }
+                };
+
+                const operations: Promise<void>[] = [];
+
+                for (let i = 0; i < 100; ++i) {
+                    operations.push(singleOp(i));
                 }
-                return id;
-            };
-            const key = getLongUUID();
-            const value = getLongUUID();
-            await client.set(key, value);
-            const result = await client.get(key);
 
-            expect(result).toEqual(value);
-        });
-    });
-
-    it("can handle concurrent operations", async () => {
-        await runTest(async (client: Client) => {
-            const singleOp = async (index: number) => {
-                if (index % 2 === 0) {
-                    await GetAndSetRandomValue(client);
-                } else {
-                    const result = await client.get(uuidv4());
-                    expect(result).toEqual(null);
-                }
-            };
-
-            const operations: Promise<void>[] = [];
-
-            for (let i = 0; i < 100; ++i) {
-                operations.push(singleOp(i));
-            }
-
-            await Promise.all(operations);
-        });
-    });
+                await Promise.all(operations);
+            });
+        },
+        config.timeout
+    );
 }
