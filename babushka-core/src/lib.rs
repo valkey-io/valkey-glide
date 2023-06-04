f31b48fa8e84182035fb4d5fe3ff2452be67e6a9
Shachar Langbeheim <shachlan@amazon.com>
2023-06-04 07:41:55 +0000

    Fix CI issues.
    New clippy warning requires the removal of the glob export of the
    legacy socket listener.
    -Z unstable options are no longer allowed outside of nightly compiler
    for cargo test.

--- babushka-core/src/lib.rs
+++ babushka-core/src/lib.rs
@@ -7,4 +7,4 @@ mod rotating_buffer_legacy;
 mod socket_listener;
 mod socket_listener_legacy;
 pub use socket_listener::*;
-pub use socket_listener_legacy::*;
+pub use socket_listener_legacy::start_legacy_socket_listener;
