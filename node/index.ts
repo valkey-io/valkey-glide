fb164bffdd215138da1ff05ffa12fd842f77511f
Shachar Langbeheim <shachlan@amazon.com>
2023-06-19 13:07:03 +0000

    Add transaction support to node.

--- node/index.ts
+++ node/index.ts
@@ -1,4 +1,10 @@
 export { AsyncClient } from "babushka-rs-internal";
 export { ClusterSocketConnection } from "./src/ClusterSocketConnection";
+export { SetOptions } from "./src/Commands";
 export { setLoggerConfig } from "./src/Logger";
-export { ConnectionOptions, SocketConnection } from "./src/SocketConnection";
+export {
+    ConnectionOptions,
+    ReturnType,
+    SocketConnection,
+} from "./src/SocketConnection";
+export { Transaction } from "./src/Transaction";
