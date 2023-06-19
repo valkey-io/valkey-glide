597199cab2f0e547723ab880d66308df15c5105d
Shachar Langbeheim <shachlan@amazon.com>
2023-06-19 12:10:42 +0000

    Update imports and exports.
    index.ts shouldn't export types that are meant for internal use.

--- node/src/Logger.ts
+++ node/src/Logger.ts
@@ -1,15 +1,15 @@
-import { BabushkaInternal } from "../";
+import { InitInternalLogger, Level, log } from "babushka-rs-internal";
 
-const LEVEL: Map<levelOptions | undefined, BabushkaInternal.Level | undefined> =
+const LEVEL: Map<LevelOptions | undefined, Level | undefined> =
     new Map([
-        ["error", BabushkaInternal.Level.Error],
-        ["warn", BabushkaInternal.Level.Warn],
-        ["info", BabushkaInternal.Level.Info],
-        ["debug", BabushkaInternal.Level.Debug],
-        ["trace", BabushkaInternal.Level.Trace],
+        ["error", Level.Error],
+        ["warn", Level.Warn],
+        ["info", Level.Info],
+        ["debug", Level.Debug],
+        ["trace", Level.Trace],
         [undefined, undefined],
     ]);
-type levelOptions = "error" | "warn" | "info" | "debug" | "trace";
+type LevelOptions = "error" | "warn" | "info" | "debug" | "trace";
 
 /*
 A class that allows logging which is consistent with logs from the internal rust core.
@@ -23,8 +23,8 @@ export class Logger {
     private static _instance: Logger;
     private static logger_level = 0;
 
-    private constructor(level?: levelOptions, fileName?: string) {
-        Logger.logger_level = BabushkaInternal.InitInternalLogger(
+    private constructor(level?: LevelOptions, fileName?: string) {
+        Logger.logger_level = InitInternalLogger(
             LEVEL.get(level),
             fileName
         );
@@ -33,7 +33,7 @@ export class Logger {
     // Initialize a logger instance if none were initialized before - this method is meant to be used when there is no intention to replace an existing logger.
     // The logger will filter all logs with a level lower than the given level,
     // If given a fileName argument, will write the logs to files postfixed with fileName. If fileName isn't provided, the logs will be written to the console.
-    public static init(level?: levelOptions, fileName?: string) {
+    public static init(level?: LevelOptions, fileName?: string) {
         return this._instance || (this._instance = new this(level, fileName));
     }
 
@@ -48,7 +48,7 @@ export class Logger {
     // 2. external user want to set the logger and we don't want to return to him the logger itself, just config it
     // the level argument is the level of the logs you want the system to provide (error logs, warn logs, etc.)
     // the filename argument is optional - if provided the target of the logs will be the file mentioned, else will be the console
-    public static setConfig(level: levelOptions, fileName?: string) {
+    public static setConfig(level: LevelOptions, fileName?: string) {
         this._instance = new this(level, fileName);
         return;
     }
@@ -58,18 +58,18 @@ export class Logger {
     // if a logger instance doesn't exist, create new one with default mode (decided by rust core, normally - level: error, target: console)
     // logIdentifier arg is a string contain data that suppose to give the log a context and make it easier to find certain type of logs.
     // when the log is connect to certain task the identifier should be the task id, when the log is not part of specific task the identifier should give a context to the log - for example, "socket connection".
-    public log(logLevel: levelOptions, logIdentifier: string, message: string) {
+    public log(logLevel: LevelOptions, logIdentifier: string, message: string) {
         if (!Logger._instance) {
             new Logger();
         }
         const level = LEVEL.get(logLevel) || 0;
         if (!(level <= Logger.logger_level)) return;
-        BabushkaInternal.log(level, logIdentifier, message);
+        log(level, logIdentifier, message);
     }
 }
 
 // This function is the interface for logging that will be provided to external user
 // for more details see setConfig function above
-export function setLoggerConfig(level: levelOptions, fileName?: string) {
+export function setLoggerConfig(level: LevelOptions, fileName?: string) {
     Logger.setConfig(level, fileName);
 }
