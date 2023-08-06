e15bb2d55f143bcf3521492e9a011649c0a7ffb9
barshaul <barshaul@amazon.com>
2023-08-06 16:07:17 +0000

    Added python support in command routing

---
+++ python/python/pybushka/async_commands/cme_commands.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+
+from pybushka.async_commands.core import CoreCommands
+from pybushka.constants import TResult
+from pybushka.protobuf.redis_request_pb2 import RequestType
+from pybushka.routes import TRoute
+
+
+class CMECommands(CoreCommands):
+    async def custom_command(
+        self, command_args: List[str], route: Optional[TRoute] = None
+    ) -> TResult:
+        """Executes a single command, without checking inputs.
+            @example - Return a list of all pub/sub clients from all nodes:
+
+                connection.customCommand(["CLIENT", "LIST","TYPE", "PUBSUB"], AllNodes())
+        Args:
+            command_args (List[str]): List of strings of the command's arguements.
+            Every part of the command, including the command name and subcommands, should be added as a separate value in args.
+            route (Optional[TRoute], optional): The command will be routed automatically, unless `route` is provided, in which
+            case the client will initially try to route the command to the nodes defined by `route`. Defaults to None.
+
+        Returns:
+            TResult: The returning value depends on the executed command and the route
+        """
+        return await self._execute_command(
+            RequestType.CustomCommand, command_args, route
+        )
