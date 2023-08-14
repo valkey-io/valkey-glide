14a3a6a17c2b0d1784b38e3381c3faee76cb794c
barshaul <barshaul@amazon.com>
2023-08-14 12:26:00 +0000

    Added python example

---
+++ examples/python/build_client.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+script=`pwd`/${BASH_SOURCE[0]}
+RELATIVE_BUILD_PATH=`dirname ${script}`
+export PYTHON_EXAMPLE_FOLDER=`realpath ${RELATIVE_BUILD_PATH}`
+export BABUSHKA_HOME_FOLDER="${PYTHON_EXAMPLE_FOLDER}/../.."
+export PYTHON_FOLDER="${BABUSHKA_HOME_FOLDER}/python"
+
+# Generate protobuf files
+protoc -Iprotobuf=${BABUSHKA_HOME_FOLDER}/babushka-core/src/protobuf/ --python_out=${PYTHON_FOLDER}/python/pybushka ${BABUSHKA_HOME_FOLDER}/babushka-core/src/protobuf/*.proto
+cd ${PYTHON_EXAMPLE_FOLDER}
+# Create a virtual environment 
+python3 -m pip install --user virtualenv
+python3 -m venv .env
+source .env/bin/activate
+cd ${PYTHON_FOLDER}
+# Build the client
+pip install --upgrade --quiet pip
+pip install --quiet -r requirements.txt
+maturin develop --release
+echo "To activate the created virtual environment, run: source ${PYTHON_EXAMPLE_FOLDER}/.env/bin/activate"
