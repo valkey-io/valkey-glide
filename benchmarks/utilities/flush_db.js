77d6e1fa7b88f7f452991061e41199cb79dd4cf4
Shachar Langbeheim <shachlan@amazon.com>
2023-09-18 14:02:50 +0000

    Allow choosing port in benchmark.
    This will allow us to test vs. local clusters that aren't promised to
    use port 6379.

--- benchmarks/utilities/flush_db.js
+++ benchmarks/utilities/flush_db.js
@@ -2,15 +2,15 @@ import commandLineArgs from "command-line-args";
 import { createClient, createCluster } from "redis";
 
 const PORT = 6379;
-function getAddress(host, tls) {
+function getAddress(host, tls, port) {
     const protocol = tls ? "rediss" : "redis";
-    return `${protocol}://${host}:${PORT}`;
+    return `${protocol}://${host}:${port ?? PORT}`;
 }
 
-function createRedisClient(host, isCluster, tls) {
+function createRedisClient(host, isCluster, tls, port) {
     return isCluster
         ? createCluster({
-              rootNodes: [{ socket: { host, port: PORT, tls } }],
+              rootNodes: [{ socket: { host, port: port ?? PORT, tls } }],
               defaults: {
                   socket: {
                       tls,
@@ -19,18 +19,18 @@ function createRedisClient(host, isCluster, tls) {
               useReplicas: true,
           })
         : createClient({
-              url: getAddress(host, tls),
+              url: getAddress(host, tls, port),
           });
 }
 
-async function flush_database(host, isCluster, tls) {
-    const client = await createRedisClient(host, isCluster, tls);
+async function flush_database(host, isCluster, tls, port) {
+    const client = await createRedisClient(host, isCluster, tls, port);
     await client.connect();
     if (isCluster) {
         // since the cluster client doesn't support fan-out commands, we need to create a client to each node, and send the flush command there.
         await Promise.all(
             client.getMasters().map((master) => {
-                return flush_database(master.host, false, tls);
+                return flush_database(master.host, false, tls, master.port);
             })
         );
     } else {
@@ -43,6 +43,7 @@ const optionDefinitions = [
     { name: "host", type: String },
     { name: "tls", type: Boolean },
     { name: "clusterModeEnabled", type: Boolean },
+    { name: "port", type: Number },
 ];
 const receivedOptions = commandLineArgs(optionDefinitions);
 
@@ -52,7 +53,8 @@ Promise.resolve()
         await flush_database(
             receivedOptions.host,
             receivedOptions.clusterModeEnabled,
-            receivedOptions.tls
+            receivedOptions.tls,
+            receivedOptions.port
         );
     })
     .then(() => {
