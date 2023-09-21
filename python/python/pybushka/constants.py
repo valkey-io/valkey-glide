fa0cdce5171e0d2475269dfeb8162cb64ce2bbe1
Shoham Elias <shohame@amazon.com>
2023-09-21 16:02:26 +0000

    Python - cluster commands return type

--- python/python/pybushka/constants.py
+++ python/python/pybushka/constants.py
@@ -1,4 +1,4 @@
-from typing import List, Literal, Union
+from typing import Dict, List, Literal, TypeVar, Union
 
 from pybushka.protobuf.connection_request_pb2 import ConnectionRequest
 from pybushka.protobuf.redis_request_pb2 import RedisRequest
@@ -6,7 +6,10 @@ from pybushka.protobuf.redis_request_pb2 import RedisRequest
 OK: str = "OK"
 DEFAULT_READ_BYTES_SIZE: int = pow(2, 16)
 # Typing
-
+T = TypeVar("T")
 TOK = Literal["OK"]
 TResult = Union[TOK, str, List[str], List[List[str]], int, None]
 TRequest = Union[RedisRequest, ConnectionRequest]
+# When routing to a single node, response will be T
+# Otherwise, response will be : {Address : response , ... } with type of Dict[str, T].
+TClusterResponse = Union[T, Dict[str, T]]
