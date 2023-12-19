524c0842b572712386faf963c25351616957e84f
Yury-Fridlyand <yury.fridlyand@improving.com>
2023-12-19 12:13:54 -0800

    Add java client connection layer. (#670)
    * Add java client connection layer.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

---
+++ java/client/src/main/java/babushka/connectors/handlers/ChannelHandler.java
@@ -0,0 +1,78 @@
+package babushka.connectors.handlers;
+
+import babushka.connectors.resources.Platform;
+import connection_request.ConnectionRequestOuterClass.ConnectionRequest;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.unix.DomainSocketAddress;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+import redis_request.RedisRequestOuterClass.RedisRequest;
+import response.ResponseOuterClass.Response;
+
+/**
+ * Class responsible for handling calls to/from a netty.io {@link Channel}.<br>
+ * Uses a {@link CallbackDispatcher} to record callbacks of every request sent.
+ */
+public class ChannelHandler {
+
+  private static final String THREAD_POOL_NAME = "babushka-channel";
+
+  private final Channel channel;
+  private final CallbackDispatcher callbackDispatcher;
+
+  /** Open a new channel for a new client. */
+  public ChannelHandler(CallbackDispatcher callbackDispatcher, String socketPath) {
+    channel =
+        new Bootstrap()
+            // TODO let user specify the thread pool or pool size as an option
+            .group(Platform.createNettyThreadPool(THREAD_POOL_NAME, Optional.empty()))
+            .channel(Platform.getClientUdsNettyChannelType())
+            .handler(new ProtobufSocketChannelInitializer(callbackDispatcher))
+            .connect(new DomainSocketAddress(socketPath))
+            // TODO call here .sync() if needed or remove this comment
+            .channel();
+    this.callbackDispatcher = callbackDispatcher;
+  }
+
+  /**
+   * Complete a protobuf message and write it to the channel (to UDS).
+   *
+   * @param request Incomplete request, function completes it by setting callback ID
+   * @param flush True to flush immediately
+   * @return A response promise
+   */
+  public CompletableFuture<Response> write(RedisRequest.Builder request, boolean flush) {
+    var commandId = callbackDispatcher.registerRequest();
+    request.setCallbackIdx(commandId.getKey());
+
+    if (flush) {
+      channel.writeAndFlush(request.build());
+    } else {
+      channel.write(request.build());
+    }
+    return commandId.getValue();
+  }
+
+  /**
+   * Write a protobuf message to the channel (to UDS).
+   *
+   * @param request A connection request
+   * @return A connection promise
+   */
+  public CompletableFuture<Response> connect(ConnectionRequest request) {
+    channel.writeAndFlush(request);
+    return callbackDispatcher.registerConnection();
+  }
+
+  private final AtomicBoolean closed = new AtomicBoolean(false);
+
+  /** Closes the UDS connection and frees corresponding resources. */
+  public void close() {
+    if (closed.compareAndSet(false, true)) {
+      channel.close();
+      callbackDispatcher.shutdownGracefully();
+    }
+  }
+}
