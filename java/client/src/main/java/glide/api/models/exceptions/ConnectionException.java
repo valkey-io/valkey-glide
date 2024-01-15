e8da8e5b631a43dff4f1fda007cfc8066f8abcd1
Andrew Carbonetto <andrewc@bitquilltech.com>
2024-01-15 09:32:37 -0800

    Spotless
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>

--- java/client/src/main/java/glide/api/models/exceptions/ConnectionException.java
+++ java/client/src/main/java/glide/api/models/exceptions/ConnectionException.java
@@ -1,7 +1,8 @@
 package glide.api.models.exceptions;
 
 /**
- * Redis client error: Errors that are thrown when a connection disconnects. These errors can be temporary, as the client will attempt to reconnect.
+ * Redis client error: Errors that are thrown when a connection disconnects. These errors can be
+ * temporary, as the client will attempt to reconnect.
  */
 public class ConnectionException extends RedisException {
   public ConnectionException(String message) {
