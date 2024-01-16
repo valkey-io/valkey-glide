1911ce18448699b88bb4285d87d552bd368f436c
Andrew Carbonetto <andrewc@bitquilltech.com>
2024-01-15 18:42:50 -0800

    Java - Add RedisException return to connection/command response handl… (#788)
    * Java - Add RedisException return to connection/command response handling (#57)
    
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>

---
+++ java/client/src/main/java/glide/api/models/exceptions/RedisException.java
@@ -0,0 +1,8 @@
+package glide.api.models.exceptions;
+
+/** Redis client error: Base class for errors. */
+public class RedisException extends RuntimeException {
+  public RedisException(String message) {
+    super(message);
+  }
+}
