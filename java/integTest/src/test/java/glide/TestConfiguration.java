7e31d6dbc775d2f78dbc9e620087fab9e616397f
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-01-31 09:47:14 -0800

    Java: Add IT framework (#863)
    * Add IT framework (#63)
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

---
+++ java/integTest/src/test/java/glide/TestConfiguration.java
@@ -0,0 +1,15 @@
+package glide;
+
+import java.util.Arrays;
+
+public final class TestConfiguration {
+    // All redis servers are hosted on localhost
+    public static final int[] STANDALONE_PORTS = getPortsFromProperty("test.redis.standalone.ports");
+    public static final int[] CLUSTER_PORTS = getPortsFromProperty("test.redis.cluster.ports");
+
+    private static int[] getPortsFromProperty(String propName) {
+        return Arrays.stream(System.getProperty(propName).split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+}
