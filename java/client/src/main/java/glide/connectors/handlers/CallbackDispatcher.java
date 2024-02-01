25e6be2f1217e50883133fa53a4e5bbe7dc4dacf
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-02-01 10:42:22 -0800

    test
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/glide/connectors/handlers/CallbackDispatcher.java
+++ java/client/src/main/java/glide/connectors/handlers/CallbackDispatcher.java
@@ -58,7 +58,7 @@ public class CallbackDispatcher {
             callbackId = nextAvailableRequestId.getAndIncrement();
         }
         responses.put(callbackId, future);
-        System.out.printf("=== registerRequest %d%n", callbackId);
+        System.out.printf("=== %d registerRequest %d%n", System.nanoTime(), callbackId);
         return Pair.of(callbackId, future);
     }
 
@@ -82,7 +82,7 @@ public class CallbackDispatcher {
         // Complete and return the response at callbackId
         // free up the callback ID in the freeRequestIds list
         int callbackId = response.getCallbackIdx();
-        System.out.printf("=== completeRequest %d%n", callbackId);
+        System.out.printf("=== %d completeRequest %d%n", System.nanoTime(), callbackId);
         CompletableFuture<Response> future = responses.remove(callbackId);
         if (future != null) {
             freeRequestIds.add(callbackId);
