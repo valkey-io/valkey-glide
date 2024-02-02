827f8db5c40a2a6c59004707dbbb964205aed9a6
SanHalacogluImproving <144171266+SanHalacogluImproving@users.noreply.github.com>
2024-02-02 15:39:39 -0800

    Implementing Customizable Event Loop Groups and Domain Socket Channels for Netty Channel Configuration (#861)
    * Implementing Customizable Event Loop Groups and Domain Socket Channels for Netty Channel Configuration (#61)
    * Added configuration for ThreadPoolResource, helps in the setup for EventloopGroup and channel configuration for Netty.
    * Removed tests as they are platform specific.
    * Documentation fixes.
    * Spotless.
    * Added exception handling for interrupted exceptions in RedisClient.
    * Small Code Enhancements: Added Documentation, Updated Unit Tests, Updated code and Refactored Access Modifiers
    * Rebased and fixed tests.
    
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>
    Co-authored-by: Andrew Carbonetto <andrewc@bitquilltech.com>

---
+++ java/client/src/main/java/glide/connectors/resources/KQueuePoolResource.java
@@ -0,0 +1,25 @@
+/** Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0 */
+package glide.connectors.resources;
+
+import io.netty.channel.kqueue.KQueueDomainSocketChannel;
+import io.netty.channel.kqueue.KQueueEventLoopGroup;
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+/**
+ * Implementation of ThreadPoolResource for Kqueue-based systems. Enabling custom/default
+ * configurations.
+ */
+public class KQueuePoolResource extends ThreadPoolResource {
+    private static final String KQUEUE_EVENT_LOOP_IDENTIFIER = "glide-channel-kqueue-elg";
+
+    public KQueuePoolResource() {
+        this(
+                new KQueueEventLoopGroup(
+                        Runtime.getRuntime().availableProcessors(),
+                        new DefaultThreadFactory(KQUEUE_EVENT_LOOP_IDENTIFIER, true)));
+    }
+
+    public KQueuePoolResource(KQueueEventLoopGroup eventLoopGroup) {
+        super(eventLoopGroup, KQueueDomainSocketChannel.class);
+    }
+}
---
+++ java/client/src/test/java/glide/connectors/resources/ThreadPoolResourceAllocatorTest.java
@@ -0,0 +1,72 @@
+/** Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0 */
+package glide.connectors.resources;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+import io.netty.channel.EventLoop;
+import io.netty.channel.EventLoopGroup;
+import java.util.function.Supplier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ThreadPoolResourceAllocatorTest {
+
+    ThreadPoolResourceAllocator service;
+
+    @BeforeEach
+    public void init() {
+        var threadPoolResource = ThreadPoolResourceAllocator.getOrCreate(() -> null);
+        if (threadPoolResource != null) {
+            threadPoolResource.getEventLoopGroup().shutdownGracefully();
+            ThreadPoolResourceAllocator.getOrCreate(() -> null);
+        }
+    }
+
+    @Test
+    public void getOrCreate_returns_default_after_repeated_calls() {
+        ThreadPoolResource mockedThreadPoolResource = mock(ThreadPoolResource.class);
+        EventLoopGroup mockedEventLoopGroup = mock(EventLoop.class);
+        Supplier<ThreadPoolResource> threadPoolSupplier = mock(Supplier.class);
+
+        when(mockedThreadPoolResource.getEventLoopGroup()).thenReturn(mockedEventLoopGroup);
+        when(mockedEventLoopGroup.isShuttingDown()).thenReturn(false);
+        when(threadPoolSupplier.get()).thenReturn(mockedThreadPoolResource);
+
+        ThreadPoolResource theResource = service.getOrCreate(threadPoolSupplier);
+        assertEquals(mockedThreadPoolResource, theResource);
+
+        // Ensure that supplier only is invoked once to set up the shared resource
+        ThreadPoolResource theSameResource = service.getOrCreate(threadPoolSupplier);
+        assertEquals(mockedThreadPoolResource, theSameResource);
+        verify(threadPoolSupplier, times(1)).get();
+
+        // teardown
+        when(mockedEventLoopGroup.isShuttingDown()).thenReturn(true);
+    }
+
+    @Test
+    public void getOrCreate_returns_new_thread_pool_after_shutdown() {
+        ThreadPoolResource mockedThreadPoolResource = mock(ThreadPoolResource.class);
+        EventLoopGroup mockedEventLoopGroup = mock(EventLoop.class);
+        Supplier<ThreadPoolResource> threadPoolSupplier = mock(Supplier.class);
+
+        when(mockedThreadPoolResource.getEventLoopGroup()).thenReturn(mockedEventLoopGroup);
+        when(mockedEventLoopGroup.isShuttingDown()).thenReturn(true);
+        when(threadPoolSupplier.get()).thenReturn(mockedThreadPoolResource);
+
+        ThreadPoolResource theResource = service.getOrCreate(threadPoolSupplier);
+        assertEquals(mockedThreadPoolResource, theResource);
+
+        // Ensure that supplier only is invoked once to set up the shared resource
+        ThreadPoolResource theSameResource = service.getOrCreate(threadPoolSupplier);
+        assertEquals(mockedThreadPoolResource, theSameResource);
+        verify(threadPoolSupplier, times(2)).get();
+
+        // teardown
+        when(mockedEventLoopGroup.isShuttingDown()).thenReturn(true);
+    }
+}
