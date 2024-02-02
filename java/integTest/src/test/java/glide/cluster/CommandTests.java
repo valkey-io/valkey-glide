2abfc004b8ee23f3f57bca45304bcbe55fa38d84
Andrew Carbonetto <andrewc@bitquilltech.com>
2024-02-02 13:20:50 -0800

    Add IT tests
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>

--- java/integTest/src/test/java/glide/cluster/CommandTests.java
+++ java/integTest/src/test/java/glide/cluster/CommandTests.java
@@ -1,13 +1,22 @@
 /** Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0 */
 package glide.cluster;
 
+import static glide.api.models.commands.InfoOptions.Section.CLUSTER;
+import static glide.api.models.commands.InfoOptions.Section.CPU;
+import static glide.api.models.commands.InfoOptions.Section.EVERYTHING;
+import static glide.api.models.commands.InfoOptions.Section.MEMORY;
+import static glide.api.models.configuration.RequestRoutingConfiguration.SimpleRoute.RANDOM;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import glide.TestConfiguration;
 import glide.api.RedisClusterClient;
+import glide.api.models.ClusterValue;
+import glide.api.models.commands.InfoOptions;
 import glide.api.models.configuration.NodeAddress;
 import glide.api.models.configuration.RedisClusterClientConfiguration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
@@ -27,19 +36,20 @@ public class CommandTests {
                                         .address(NodeAddress.builder().port(TestConfiguration.CLUSTER_PORTS[0]).build())
                                         .requestTimeout(5000)
                                         .build())
-                        .get(10, TimeUnit.SECONDS);
+                        .get(10, SECONDS);
     }
 
     @AfterAll
     @SneakyThrows
-    public static void deinit() {
+    public static void teardown() {
         clusterClient.close();
     }
 
     @Test
     @SneakyThrows
     public void custom_command_info() {
-        var data = clusterClient.customCommand(new String[] {"info"}).get(10, TimeUnit.SECONDS);
+        ClusterValue<Object> data = clusterClient.customCommand(new String[] {"info"}).get(10, SECONDS);
+        assertTrue(data.hasMultiData());
         for (var info : data.getMultiValue().values()) {
             assertTrue(((String) info).contains("# Stats"));
         }
@@ -50,5 +60,82 @@ public class CommandTests {
     public void custom_command_ping() {
         var data = clusterClient.customCommand(new String[] {"ping"}).get(10, TimeUnit.SECONDS);
         assertEquals("PONG", data.getSingleValue());
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_without_options() {
+        ClusterValue<String> data = clusterClient.info().get(10, SECONDS);
+        assertTrue(data.hasMultiData());
+        for (var info : data.getMultiValue().values()) {
+            for (var section : List.of("Server", "Clients", "Memory", "Persistence", "Stats", "Replication", "CPU", "Modules", "Errorstats", "Cluster", "Keyspace")) {
+                assertTrue(info.contains("# " + section), "Section " + section + " is missing");
+            }
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_with_route() {
+        ClusterValue<String> data = clusterClient.info(RANDOM).get(10, SECONDS);
+        assertTrue(data.hasSingleData());
+        String infoData = data.getSingleValue();
+        for (var section : List.of("Server", "Clients", "Memory", "Persistence", "Stats", "Replication", "CPU", "Modules", "Errorstats", "Cluster", "Keyspace")) {
+            assertTrue(infoData.contains("# " + section), "Section " + section + " is missing");
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_with_multiple_options() {
+        InfoOptions options = InfoOptions.builder().section(CLUSTER).section(CPU).section(MEMORY).build();
+        ClusterValue<String> data = clusterClient.info(options).get(10, SECONDS);
+        assertTrue(data.hasMultiData());
+        for (var info : data.getMultiValue().values()) {
+
+            for (var section :  options.toArgs()) {
+                assertTrue(info.toLowerCase().contains("# " + section.toLowerCase()), "Section " + section + " is missing");
+            }
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_with_everything_option() {
+        InfoOptions options = InfoOptions.builder().section(EVERYTHING).build();
+        ClusterValue<String> data = clusterClient.info(options).get(10, SECONDS);
+        assertTrue(data.hasMultiData());
+        for (var info : data.getMultiValue().values()) {
+
+            for (var section :  List.of("Server", "Clients", "Memory", "Persistence", "Stats", "Replication", "CPU", "Modules", "Commandstats", "Errorstats", "Latencystats", "Cluster", "Keyspace")) {
+                assertTrue(info.contains("# " + section), "Section " + section + " is missing");
+            }
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_with_everything_option_and_route() {
+        InfoOptions options = InfoOptions.builder().section(EVERYTHING).build();
+        ClusterValue<String> data = clusterClient.info(options, RANDOM).get(10, SECONDS);
+        assertTrue(data.hasSingleData());
+        String infoData = data.getSingleValue();
+        for (var section :  List.of("Server", "Clients", "Memory", "Persistence", "Stats", "Replication", "CPU", "Modules", "Commandstats", "Errorstats", "Latencystats", "Cluster", "Keyspace")) {
+            assertTrue(infoData.contains("# " + section), "Section " + section + " is missing");
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void ping() {
+        String data = clusterClient.ping().get(10, SECONDS);
+        assertEquals("PONG", data);
+    }
+
+    @Test
+    @SneakyThrows
+    public void ping_with_message() {
+        String data = clusterClient.ping("H3LL0").get(10, SECONDS);
+        assertEquals("H3LL0", data);
     }
 }
--- java/integTest/src/test/java/glide/standalone/CommandTests.java
+++ java/integTest/src/test/java/glide/standalone/CommandTests.java
@@ -1,21 +1,35 @@
 /** Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0 */
 package glide.standalone;
 
+import static glide.api.models.commands.InfoOptions.Section.CLUSTER;
+import static glide.api.models.commands.InfoOptions.Section.CPU;
+import static glide.api.models.commands.InfoOptions.Section.EVERYTHING;
+import static glide.api.models.commands.InfoOptions.Section.MEMORY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import glide.TestConfiguration;
 import glide.api.RedisClient;
+import glide.api.models.Ok;
+import glide.api.models.commands.InfoOptions;
+import glide.api.models.commands.SetOptions;
 import glide.api.models.configuration.NodeAddress;
 import glide.api.models.configuration.RedisClientConfiguration;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.Random;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CommandTests {
-
     private static RedisClient regularClient = null;
+
+    private static final String KEY_NAME = "key";
+    private static final String INITIAL_VALUE = "VALUE";
+    private static final String ANOTHER_VALUE = "VALUE2";
 
     @BeforeAll
     @SneakyThrows
@@ -26,19 +40,101 @@ public class CommandTests {
                                         .address(
                                                 NodeAddress.builder().port(TestConfiguration.STANDALONE_PORTS[0]).build())
                                         .build())
-                        .get(10, TimeUnit.SECONDS);
+                        .get(10, SECONDS);
     }
 
     @AfterAll
     @SneakyThrows
-    public static void deinit() {
+    public static void teardown() {
         regularClient.close();
     }
 
     @Test
     @SneakyThrows
     public void custom_command_info() {
-        var data = regularClient.customCommand(new String[] {"info"}).get(10, TimeUnit.SECONDS);
+        var data = regularClient.customCommand(new String[] {"info"}).get(10, SECONDS);
         assertTrue(((String) data).contains("# Stats"));
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_without_options() {
+        String data = regularClient.info().get(10, SECONDS);
+        for (var section : List.of("Server", "Clients", "Memory", "Persistence", "Stats", "Replication", "CPU", "Modules", "Errorstats", "Cluster", "Keyspace")) {
+            assertTrue(data.contains("# " + section), "Section " + section + " is missing");
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_with_multiple_options() {
+        InfoOptions options = InfoOptions.builder().section(CLUSTER).section(CPU).section(MEMORY).build();
+        String data = regularClient.info(options).get(10, SECONDS);
+        for (var section :  options.toArgs()) {
+            assertTrue(data.toLowerCase().contains("# " + section.toLowerCase()), "Section " + section + " is missing");
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void info_with_everything_option() {
+        InfoOptions options = InfoOptions.builder().section(EVERYTHING).build();
+        String data = regularClient.info(options).get(10, SECONDS);
+        for (var section :  List.of("Server", "Clients", "Memory", "Persistence", "Stats", "Replication", "CPU", "Modules", "Commandstats", "Errorstats", "Latencystats", "Cluster", "Keyspace")) {
+            assertTrue(data.contains("# " + section), "Section " + section + " is missing");
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void set_and_get_without_options() {
+        Ok ok = regularClient.set(KEY_NAME, INITIAL_VALUE).get(10, SECONDS);
+        assertEquals(Ok.INSTANCE, ok);
+
+        String data = regularClient.get(KEY_NAME).get(10, SECONDS);
+        assertEquals(INITIAL_VALUE, data);
+    }
+
+    @Test
+    @SneakyThrows
+    public void get_missing_value() {
+        var data = regularClient.get("invalid").get(10, SECONDS);
+        assertNull(data);
+    }
+
+    @Test
+    @SneakyThrows
+    public void set_overwrite_value_and_returnOldValue_returns_string() {
+        var ok = regularClient.set(KEY_NAME, INITIAL_VALUE).get(10, SECONDS);
+        assertEquals(Ok.INSTANCE, ok);
+
+        var options = SetOptions.builder().returnOldValue(true).build();
+        var data = regularClient.set(KEY_NAME, ANOTHER_VALUE, options).get(10, SECONDS);
+        assertEquals(INITIAL_VALUE, data);
+    }
+
+    @Test
+    @SneakyThrows
+    public void set_missing_value_and_returnOldValue_is_null() {
+        var ok = regularClient.set(KEY_NAME, INITIAL_VALUE).get(10, SECONDS);
+        assertEquals(Ok.INSTANCE, ok);
+
+        var options = SetOptions.builder().returnOldValue(true).build();
+        var data = regularClient.set("another", ANOTHER_VALUE, options).get(10, SECONDS);
+        assertNull(data);
+    }
+
+    @Test
+    @SneakyThrows
+    public void ping() {
+        var data = regularClient.ping().get(10, SECONDS);
+        assertEquals("PONG", data);
+    }
+
+    @Test
+    @SneakyThrows
+    public void ping_with_message() {
+        var data = regularClient.ping("H3LL0").get(10, SECONDS);
+        assertEquals("H3LL0", data);
     }
 }
