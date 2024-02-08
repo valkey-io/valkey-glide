472da0aada001f1725e3186872ce4215558ef081
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-02-08 10:35:08 -0800

    Java: Rename BaseCommands and BaseClusterCommands (#917)
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/commands/GenericCommands.java
+++ java/client/src/main/java/glide/api/commands/GenericCommands.java
@@ -3,24 +3,25 @@ package glide.api.commands;
 
 import java.util.concurrent.CompletableFuture;
 
-/** Base Commands interface to handle generic command and transaction requests. */
-public interface BaseCommands {
+/** Generic Commands interface to handle generic command and transaction requests. */
+public interface GenericCommands {
 
     /**
      * Executes a single command, without checking inputs. Every part of the command, including
-     * subcommands, should be added as a separate value in {@code args}.
+     * subcommands, should be added as a separate value in args.
      *
      * @remarks This function should only be used for single-response commands. Commands that don't
      *     return response (such as <em>SUBSCRIBE</em>), or that return potentially more than a single
      *     response (such as <em>XREAD</em>), or that change the client's behavior (such as entering
      *     <em>pub</em>/<em>sub</em> mode on <em>RESP2</em> connections) shouldn't be called using
      *     this function.
-     * @example Returns a list of all <em>pub</em>/<em>sub</em> clients:
-     *     <p><code>
+     * @example Returns a list of all pub/sub clients:
+     *     <pre>
      * Object result = client.customCommand(new String[]{ "CLIENT", "LIST", "TYPE", "PUBSUB" }).get();
-     * </code>
-     * @param args Arguments for the custom command including the command name
-     * @return A <em>CompletableFuture</em> with response result from Redis
+     * </pre>
+     *
+     * @param args Arguments for the custom command.
+     * @return Response from Redis containing an <code>Object</code>.
      */
     CompletableFuture<Object> customCommand(String[] args);
 }
