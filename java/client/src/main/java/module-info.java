bb748628c2f91fc8189226dc3bac114636f99b0d
Jonathan Louie <jonathanl@bitquilltech.com>
2024-02-09 14:45:27 -0800

    Add module-info for glide client and update jar name

---
+++ java/client/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module glide.api {
+    exports glide.api;
+    exports glide.api.commands;
+    exports glide.api.models;
+    exports glide.api.models.configuration;
+    exports glide.api.models.exceptions;
+    requires com.google.protobuf;
+    requires io.netty.transport;
+    requires lombok;
+}
