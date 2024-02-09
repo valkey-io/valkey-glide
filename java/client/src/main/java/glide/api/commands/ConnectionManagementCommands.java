0b073aba249ca710a99ca0d0fd9c59fda67a8caa
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-02-08 22:26:37 -0800

    Java: Add PING command (#914)
    * Java: Add PING command
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Remove unnecessary handlers
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Remove OK string
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Clean up import
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Spotless
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Add @NonNull annotation
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Ping documentation update
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Add sharedtests to IT
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Fix IT
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Use genertic response handler
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update handleRedisResponse
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Use @Timeout annotation
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Remove unnecessary nonnull
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Allow for null response in customCommand
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

---
+++ java/client/src/main/java/glide/api/commands/ConnectionManagementCommands.java
@@ -0,0 +1,30 @@
+/** Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0 */
+package glide.api.commands;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Connection Management Commands interface.
+ *
+ * @see: <a href="https://redis.io/commands/?group=connection">Connection Management Commands</a>
+ */
+public interface ConnectionManagementCommands {
+
+    /**
+     * Ping the Redis server.
+     *
+     * @see <a href="https://redis.io/commands/ping/">redis.io</a> for details.
+     * @return Response from Redis containing a <code>String</code> with "PONG".
+     */
+    CompletableFuture<String> ping();
+
+    /**
+     * Ping the Redis server.
+     *
+     * @see <a href="https://redis.io/commands/ping/">redis.io</a> for details.
+     * @param str The ping argument that will be returned.
+     * @return Response from Redis containing a <code>String</code> with a copy of the argument <code>
+     *     str</code>.
+     */
+    CompletableFuture<String> ping(String str);
+}
