2259992f095ba4185cbbffcde38edc665c17b01e
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-02-12 23:03:37 -0800

    Combine clients for benchmarking
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/benchmarks/src/main/java/glide/benchmarks/clients/glide/GlideAsyncClient.java
+++ java/benchmarks/src/main/java/glide/benchmarks/clients/glide/GlideAsyncClient.java
@@ -1,9 +1,12 @@
 /** Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0 */
 package glide.benchmarks.clients.glide;
 
+import glide.api.BaseClient;
 import glide.api.RedisClient;
+import glide.api.RedisClusterClient;
 import glide.api.models.configuration.NodeAddress;
 import glide.api.models.configuration.RedisClientConfiguration;
+import glide.api.models.configuration.RedisClusterClientConfiguration;
 import glide.benchmarks.clients.AsyncClient;
 import glide.benchmarks.utils.ConnectionSettings;
 import java.util.concurrent.CompletableFuture;
@@ -11,38 +14,54 @@ import java.util.concurrent.ExecutionException;
 
 /** A Glide client with async capabilities */
 public class GlideAsyncClient implements AsyncClient<String> {
-    private RedisClient redisClient;
+    private BaseClient redisClient;
 
     @Override
     public void connectToRedis(ConnectionSettings connectionSettings) {
-        if (connectionSettings.clusterMode) {
-            throw new RuntimeException("Use client GlideAsyncClusterClient");
-        }
-        RedisClientConfiguration config =
-                RedisClientConfiguration.builder()
-                        .address(
-                                NodeAddress.builder()
-                                        .host(connectionSettings.host)
-                                        .port(connectionSettings.port)
-                                        .build())
-                        .useTLS(connectionSettings.useSsl)
-                        .build();
 
-        try {
-            redisClient = RedisClient.CreateClient(config).get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
+        if (connectionSettings.clusterMode) {
+            RedisClusterClientConfiguration config =
+                    RedisClusterClientConfiguration.builder()
+                            .address(
+                                    NodeAddress.builder()
+                                            .host(connectionSettings.host)
+                                            .port(connectionSettings.port)
+                                            .build())
+                            .useTLS(connectionSettings.useSsl)
+                            .build();
+            try {
+                redisClient = RedisClusterClient.CreateClient(config).get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+
+        } else {
+            RedisClientConfiguration config =
+                    RedisClientConfiguration.builder()
+                            .address(
+                                    NodeAddress.builder()
+                                            .host(connectionSettings.host)
+                                            .port(connectionSettings.port)
+                                            .build())
+                            .useTLS(connectionSettings.useSsl)
+                            .build();
+
+            try {
+                redisClient = RedisClient.CreateClient(config).get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     @Override
     public CompletableFuture<String> asyncSet(String key, String value) {
-        return redisClient.customCommand(new String[] {"Set", key, value}).thenApply(r -> (String) r);
+        return redisClient.set(key, value);
     }
 
     @Override
     public CompletableFuture<String> asyncGet(String key) {
-        return redisClient.customCommand(new String[] {"Get", key}).thenApply(r -> (String) r);
+        return redisClient.get(key);
     }
 
     @Override
