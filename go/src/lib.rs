c306c0d2475386d73a2b601026f99657d2130e0f
Aaron <69273634+aaron-congo@users.noreply.github.com>
2024-02-14 09:23:27 -0800

    Go: Add CI (#946)
    * Add CI for Go
    
    * Add command to install wget to amazon linux workflow
    
    * Remove sudo from the amazon linux tar command
    
    * Add command to install tar on amazon linux
    
    * Change command so that the Go path is updated for all amazon linux steps
    
    * Remove command to list Go version
    
    * Fix missing steps, go mod command
    
    * Remove redundant step
    
    * Add Go bin to path, remove patch version from go mod command
    
    * Update amazon linux test name
    
    * Increase timeout
    
    * Specify minimum go version as 1.18, remove dependency requiring Go 1.21, fix yml formatting, fix setup-go cache

---
+++ go/src/lib.rs
@@ -0,0 +1,9 @@
+/**
+ * Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
+ */
+use std::ffi::c_void;
+
+#[no_mangle]
+pub extern "C" fn create_connection() -> *const c_void {
+    todo!()
+}
