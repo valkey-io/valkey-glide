4542eef9ec9dabc233b9d7f816673874f7285544
Aaron <69273634+aaron-congo@users.noreply.github.com>
2024-02-14 09:23:27 -0800

    Go: Add CI (#946)
    * Add CI for Go
    
    * Add command to install wget to amazon linux workflow
    
    * Remove sudo from the amazon linux tar command
    
    * Add command to install tar on amazon linux
    
    * Change command so that the Go path is updated for all amazon linux steps
    
    * Remove command to list Go version
    
    * Fix missing steps, go mod command
    
    * Remove redundant step
    
    * Add Go bin to path, remove patch version from go mod command
    
    * Update amazon linux test name
    
    * Increase timeout
    
    * Specify minimum go version as 1.18, remove dependency requiring Go 1.21, fix yml formatting, fix setup-go cache

---
+++ go/tests/glide_test.go
@@ -0,0 +1,13 @@
+package tests
+
+import (
+	"testing"
+)
+
+// TODO: Replace this test with real tests when glide client implementation is started
+func TestArbitraryLogic(t *testing.T) {
+	someVar := true
+	if !someVar {
+		t.Fatalf("Expected someVar to be true, but was false.")
+	}
+}
