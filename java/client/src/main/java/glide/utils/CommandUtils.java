1ad0d068ea0cd41508c140eff113d0bce03c430a
SanHalacogluImproving <san.halacoglu@improving.com>
2024-02-14 16:32:48 -0800

    Added documentation for CommandUtils.

--- java/client/src/main/java/glide/utils/CommandUtils.java
+++ java/client/src/main/java/glide/utils/CommandUtils.java
@@ -7,12 +7,27 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public class CommandUtils {
+
+    /**
+     * Converts a map to an array of strings with alternating keys and values.
+     *
+     * @param args Map of string pairs to convert.
+     * @return Array of strings [key1, value1, key2, value2, ...].
+     */
     public static String[] convertMapToArgArray(Map<String, String> args) {
         return args.entrySet().stream()
                 .flatMap(entry -> Stream.of(entry.getKey(), entry.getValue()))
                 .toArray(String[]::new);
     }
 
+    /**
+     * Casts an array of objects to an array of a specific type.
+     *
+     * @param objectArr Array of objects to cast.
+     * @param clazz The class of the array elements to cast to.
+     * @return An array of type T, containing the elements from the input array.
+     * @param <T> The type to which the elements are cast.
+     */
     @SuppressWarnings("unchecked")
     public static <T> T[] objectArrayToTypedArray(Object[] objectArr, Class<T> clazz) {
         return Arrays.stream(objectArr)
