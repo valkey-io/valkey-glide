db7ecfb265abb2c99fa46fd7eee642f896a272ea
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-02-16 13:38:46 -0800

    Spotless
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/BaseClient.java
+++ java/client/src/main/java/glide/api/BaseClient.java
@@ -49,7 +49,6 @@ import glide.managers.CommandManager;
 import glide.managers.ConnectionManager;
 import java.util.Map;
 import java.util.Set;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.function.BiFunction;
