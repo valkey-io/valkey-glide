0dce8924d28dbbdd5103c48c831f78e4253e46a3
SanHalacogluImproving <san.halacoglu@improving.com>
2024-02-22 11:10:37 -0800

    Minor update to TransactionTestUtilities.

--- java/integTest/src/test/java/glide/TransactionTestUtilities.java
+++ java/integTest/src/test/java/glide/TransactionTestUtilities.java
@@ -44,8 +44,8 @@ public class TransactionTestUtilities {
 
         baseTransaction.hset(key4, Map.of(field1, value1, field2, value2));
         baseTransaction.hget(key4, field1);
-        baseTransaction.hmget(key4, new String[] {field1, "non_existing_field", field2});
         baseTransaction.hexists(key4, field2);
+        baseTransaction.hmget(key4, new String[] {field1, "non_existing_field", field2});
         baseTransaction.hdel(key4, new String[] {field1});
 
         baseTransaction.sadd(key5, new String[] {"baz", "foo"});
@@ -74,8 +74,8 @@ public class TransactionTestUtilities {
             0.5,
             2L,
             value1,
-            new String[] {value1, null, value2},
             true,
+            new String[] {value1, null, value2},
             1L,
             2L,
             1L,
