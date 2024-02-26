fbe4f5007ec6824f0cd14420501876dfd28a2e83
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-02-26 11:51:35 -0800

    Java: Move ping() commands out of baseclient (#1029)
    * Java: Move ping() commands out of baseclient (#110)
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    Co-authored-by: SanHalacogluImproving <san.halacoglu@improving.com>

--- java/client/src/main/java/glide/api/RedisClient.java
+++ java/client/src/main/java/glide/api/RedisClient.java
@@ -3,8 +3,10 @@ package glide.api;
 
 import static redis_request.RedisRequestOuterClass.RequestType.CustomCommand;
 import static redis_request.RedisRequestOuterClass.RequestType.Info;
+import static redis_request.RedisRequestOuterClass.RequestType.Ping;
 import static redis_request.RedisRequestOuterClass.RequestType.Select;
 
+import glide.api.commands.ConnectionManagementCommands;
 import glide.api.commands.GenericCommands;
 import glide.api.commands.ServerManagementCommands;
 import glide.api.models.Transaction;
@@ -19,7 +21,8 @@ import lombok.NonNull;
  * Async (non-blocking) client for Redis in Standalone mode. Use {@link #CreateClient} to request a
  * client to Redis.
  */
-public class RedisClient extends BaseClient implements GenericCommands, ServerManagementCommands {
+public class RedisClient extends BaseClient
+        implements GenericCommands, ServerManagementCommands, ConnectionManagementCommands {
 
     protected RedisClient(ConnectionManager connectionManager, CommandManager commandManager) {
         super(connectionManager, commandManager);
@@ -44,6 +47,17 @@ public class RedisClient extends BaseClient implements GenericCommands, ServerMa
     @Override
     public CompletableFuture<Object[]> exec(Transaction transaction) {
         return commandManager.submitNewCommand(transaction, this::handleArrayOrNullResponse);
+    }
+
+    @Override
+    public CompletableFuture<String> ping() {
+        return commandManager.submitNewCommand(Ping, new String[0], this::handleStringResponse);
+    }
+
+    @Override
+    public CompletableFuture<String> ping(@NonNull String message) {
+        return commandManager.submitNewCommand(
+                Ping, new String[] {message}, this::handleStringResponse);
     }
 
     @Override
