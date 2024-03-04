b514eb20a7ba6a766c9962ddddfda6372d7113eb
barshaul <barshaul@amazon.com>
2024-03-04 11:42:06 +0200

    Node: Added testTimeout config to jest to fix macos timeout failures

--- node/jest.config.js
+++ node/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
     testEnvironment: "node",
     testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testTimeout: 20000,
 };
