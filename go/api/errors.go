01579ea4a537be96901fdc1ff4b76c36b958fa72
aaron-congo <aaron.congo@improving.com>
2024-03-04 16:58:23 -0800

    Remove unused errors.go file

--- go/api/errors.go
+++
@@ -1,9 +0,0 @@
-// Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
-
-package api
-
-type RedisError struct {
-	msg string
-}
-
-func (e *RedisError) Error() string { return e.msg }
