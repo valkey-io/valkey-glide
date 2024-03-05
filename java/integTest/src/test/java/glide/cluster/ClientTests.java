ad58a89f751abc0cbde97fe10d853b57cfe59d7e
SanHalacogluImproving <144171266+SanHalacogluImproving@users.noreply.github.com>
2024-03-05 12:09:09 -0800

    Java: Refactor channel closure handling (#118) (#1074)
    * Refactor channel closure handling (#118)
    
    * Ensure resources complete with Closing Error when client closes. New command submissions on closed clients now also return a future with a ClosingException Set. (Consistent with Py and Node clients)
    
    * Added a private boolean that saves the state of channel handler.
    
    * Changed Boolean to Atomic Boolean added IT tests and modified UT tests.
    
    * Fix IT
    
    * Minor refactor for ClientTests.

--- java/integTest/src/test/java/glide/cluster/ClientTests.java
+++ java/integTest/src/test/java/glide/cluster/ClientTests.java
@@ -2,15 +2,18 @@
 package glide.cluster;
 
 import static glide.TestConfiguration.CLUSTER_PORTS;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import glide.api.RedisClusterClient;
 import glide.api.models.configuration.NodeAddress;
 import glide.api.models.configuration.RedisClusterClientConfiguration;
+import glide.api.models.exceptions.ClosingException;
+import java.util.concurrent.ExecutionException;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
-public class ClientTest {
+public class ClientTests {
     @Test
     @SneakyThrows
     public void custom_command_info() {
@@ -25,5 +28,21 @@ public class ClientTest {
         String clientInfo =
                 (String) client.customCommand(new String[] {"CLIENT", "INFO"}).get().getSingleValue();
         assertTrue(clientInfo.contains("name=TEST_CLIENT_NAME"));
+    }
+
+    @Test
+    @SneakyThrows
+    public void close_client_throws_ExecutionException_with_ClosingException_cause() {
+        RedisClusterClient client =
+                RedisClusterClient.CreateClient(
+                                RedisClusterClientConfiguration.builder()
+                                        .address(NodeAddress.builder().port(CLUSTER_PORTS[0]).build())
+                                        .build())
+                        .get();
+
+        client.close();
+        ExecutionException executionException =
+                assertThrows(ExecutionException.class, () -> client.set("foo", "bar").get());
+        assertTrue(executionException.getCause() instanceof ClosingException);
     }
 }
