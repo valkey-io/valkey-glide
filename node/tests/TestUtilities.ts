ed1de0632a67ad4faaf60be7a43fe99a32477923
Adan <adanwat@amazon.com>
2024-03-06 09:02:01 +0000

    Node: added hvals command.

--- node/tests/TestUtilities.ts
+++ node/tests/TestUtilities.ts
@@ -86,6 +86,8 @@ export function transactionTest(
     args.push(1);
     baseTransaction.hlen(key4);
     args.push(1);
+    baseTransaction.hvals(key4);
+    args.push([value]);
     baseTransaction.hget(key4, field);
     args.push(value);
     baseTransaction.hgetall(key4);
