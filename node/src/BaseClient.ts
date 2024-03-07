abda4860f5409f41fb35332776d5d1c3e5d0dd7b
Adan Wattad <adanwat@amazon.com>
2024-03-07 12:36:56 +0200

    Add zremRangeByRank command in Node. (#924)

--- node/src/BaseClient.ts
+++ node/src/BaseClient.ts
@@ -65,6 +65,7 @@ import {
     createZpopmax,
     createZpopmin,
     createZrem,
+    createZremRangeByRank,
     createZscore,
 } from "./Commands";
 import {
@@ -1169,6 +1170,23 @@ export class BaseClient {
      */
     public pttl(key: string): Promise<number> {
         return this.createWritePromise(createPttl(key));
+    }
+
+    /** Removes all elements in the sorted set stored at `key` with rank between `start` and `end`.
+     * Both `start` and `end` are zero-based indexes with 0 being the element with the lowest score.
+     * These indexes can be negative numbers, where they indicate offsets starting at the element with the highest score.
+     * See https://redis.io/commands/zremrangebyrank/ for more details.
+     * 
+     * @param key - The key of the sorted set.
+     * @param start - The starting point of the range.
+     * @param end - The end of the range.
+     * @returns The number of members removed.
+     * If `start` exceeds the end of the sorted set, or if `start` is greater than `end`, 0 returned.
+     * If `end` exceeds the actual end of the sorted set, the range will stop at the actual end of the sorted set.
+     * If `key` does not exist 0 will be returned.
+     */
+    public zremRangeByRank(key: string, start: number, end: number): Promise<number> {
+        return this.createWritePromise(createZremRangeByRank(key, start, end));
     }
 
     private readonly MAP_READ_FROM_STRATEGY: Record<
