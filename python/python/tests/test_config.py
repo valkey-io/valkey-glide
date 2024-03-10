1a25a7ad253e39553a08feca6d86693633ca31ba
barshaul <barshaul@amazon.com>
2024-03-10 12:36:56 +0000

    Core + Python: Enable cluster periodic checks by default, add python configuration

--- python/python/tests/test_config.py
+++ python/python/tests/test_config.py
@@ -1,6 +1,13 @@
 # Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
 
-from glide.config import BaseClientConfiguration, NodeAddress, ReadFrom
+from glide.config import (
+    BaseClientConfiguration,
+    ClusterClientConfiguration,
+    NodeAddress,
+    PeriodicChecksManualInterval,
+    PeriodicChecksStatus,
+    ReadFrom,
+)
 from glide.protobuf.connection_request_pb2 import ConnectionRequest
 from glide.protobuf.connection_request_pb2 import ReadFrom as ProtobufReadFrom
 from glide.protobuf.connection_request_pb2 import TlsMode
@@ -28,3 +35,20 @@ def test_convert_to_protobuf():
     assert request.tls_mode is TlsMode.SecureTls
     assert request.read_from == ProtobufReadFrom.PreferReplica
     assert request.client_name == "TEST_CLIENT_NAME"
+
+
+def test_periodic_checks_interval_to_protobuf():
+    config = ClusterClientConfiguration(
+        [NodeAddress("127.0.0.1")],
+    )
+    request = config._create_a_protobuf_conn_request(cluster_mode=True)
+    assert not request.HasField("periodic_checks_disabled")
+    assert not request.HasField("periodic_checks_manual_interval")
+
+    config.periodic_checks = PeriodicChecksStatus.DISABLED
+    request = config._create_a_protobuf_conn_request(cluster_mode=True)
+    assert request.HasField("periodic_checks_disabled")
+
+    config.periodic_checks = PeriodicChecksManualInterval(30)
+    request = config._create_a_protobuf_conn_request(cluster_mode=True)
+    assert request.periodic_checks_manual_interval.duration_in_sec == 30
