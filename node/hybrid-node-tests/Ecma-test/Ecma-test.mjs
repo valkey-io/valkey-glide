30a1a51db73d26700c50c4abd4859c13c2c2de0d
avifenesh <aviarchi1994@gmail.com>
2024-03-17 16:29:31 +0000

    Added a hybrid moudole for node

---
+++ node/hybrid-node-tests/Ecma-test/Ecma-test.mjs
@@ -0,0 +1,45 @@
+import { exec } from "child_process";
+import findFreePorts from "find-free-ports";
+import { AsyncClient } from "glide-rs";
+import RedisServer from "redis-server";
+
+const PORT_NUMBER = 4000;
+let server;
+let port;
+
+export function flushallOnPort(port) {
+    return new Promise((resolve, reject) => {
+        exec(`redis-cli -p ${port} FLUSHALL`, (error, _, stderr) => {
+            if (error) {
+                console.error(stderr);
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+port = await findFreePorts(PORT_NUMBER).then(([free_port]) => free_port);
+server = await new Promise((resolve, reject) => {
+    const server = new RedisServer(port);
+    server.open(async (err) => {
+        if (err) {
+            reject(err);
+        }
+
+        resolve(server);
+    });
+});
+const client = AsyncClient.CreateConnection("redis://localhost:" + port);
+await client.set("test", "test");
+let result = await client.get("test");
+
+if (result !== "test") {
+    throw new Error("Ecma Test failed");
+} else {
+    console.log("Ecma Test passed");
+}
+
+await flushallOnPort(port);
+await server.close();
