35b715fe9bb43ec981cf0582385f782af26fd0ad
SanHalacogluImproving <144171266+SanHalacogluImproving@users.noreply.github.com>
2024-03-18 12:43:28 -0700

    Java: Added Missing Client Tests. (#127) (#1120)
    * Java: Added Missing Client Tests. (#127)
    
    * Minor update.
    
    * Refactored to unicode litterals.

--- java/integTest/src/test/java/glide/TestUtilities.java
+++ java/integTest/src/test/java/glide/TestUtilities.java
@@ -1,9 +1,18 @@
 /** Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0 */
 package glide;
 
+import static glide.TestConfiguration.CLUSTER_PORTS;
+import static glide.TestConfiguration.STANDALONE_PORTS;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import glide.api.models.ClusterValue;
+import glide.api.models.configuration.NodeAddress;
+import glide.api.models.configuration.RedisClientConfiguration;
+import glide.api.models.configuration.RedisClusterClientConfiguration;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -22,5 +31,49 @@ public class TestUtilities {
     /** Extract first value from {@link ClusterValue} assuming it contains a multi-value. */
     public static <T> T getFirstEntryFromMultiValue(ClusterValue<T> data) {
         return data.getMultiValue().get(data.getMultiValue().keySet().toArray(String[]::new)[0]);
+    }
+
+    /** Generates a random string of a specified length using ASCII letters. */
+    public static String getRandomString(int length) {
+        String asciiLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(asciiLetters.length());
+            char randomChar = asciiLetters.charAt(index);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Transforms server info string into a Map, using lines with ":" to create key-value pairs,
+     * replacing duplicates with the last encountered value.
+     */
+    public static Map<String, String> parseInfoResponseToMap(String serverInfo) {
+        return serverInfo
+                .lines()
+                .filter(line -> line.contains(":"))
+                .map(line -> line.split(":", 2))
+                .collect(
+                        Collectors.toMap(
+                                parts -> parts[0],
+                                parts -> parts[1],
+                                (existingValue, newValue) -> newValue,
+                                HashMap::new));
+    }
+
+    public static RedisClientConfiguration.RedisClientConfigurationBuilder<?, ?>
+            commonClientConfig() {
+        return RedisClientConfiguration.builder()
+                .address(NodeAddress.builder().port(STANDALONE_PORTS[0]).build());
+    }
+
+    public static RedisClusterClientConfiguration.RedisClusterClientConfigurationBuilder<?, ?>
+            commonClusterClientConfig() {
+        return RedisClusterClientConfiguration.builder()
+                .address(NodeAddress.builder().port(CLUSTER_PORTS[0]).build());
     }
 }
