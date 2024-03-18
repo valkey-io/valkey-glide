dac491d92c378075c79665c916d69598f32c23a3
Avi Fenesh <55848801+avifenesh@users.noreply.github.com>
2024-03-18 15:15:21 +0200

    Added a hybrid module for node (#1132)
    * Added a hybrid module for node
    
    * Added explanation to DEVELOPER.md about the hybrid build process
    
    * PR fixes - DEVELOPER.md wordings, Script naming, CI structure, tsconfig usage in npm folder, enter to change log
    
    * added tsconfig property to jest
    
    * Added comment to fixup script file

---
+++ node/hybrid-node-tests/ecmascript-test/ecmascript-test.mjs
@@ -0,0 +1,49 @@
+import { exec } from "child_process";
+import findFreePorts from "find-free-ports";
+import { AsyncClient } from "glide-rs";
+import RedisServer from "redis-server";
+
+const PORT_NUMBER = 4001;
+let server;
+let port;
+
+export function flushallOnPort(port) {
+    return new Promise((resolve, reject) => {
+        exec(`redis-cli -p ${port} FLUSHALL`, (error, _, stderr) => {
+            if (error) {
+                console.error(stderr);
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+port = await findFreePorts(PORT_NUMBER).then(([free_port]) => free_port);
+server = await new Promise((resolve, reject) => {
+    const server = new RedisServer(port);
+    server.open(async (err) => {
+        if (err) {
+            reject(err);
+        }
+
+        resolve(server);
+    });
+});
+const client = AsyncClient.CreateConnection("redis://localhost:" + port);
+await client.set("test", "test");
+let result = await client.get("test");
+
+if (result !== "test") {
+    throw new Error("Ecma Test failed");
+} else {
+    console.log("Ecma Test passed");
+}
+
+await flushallOnPort(port).then(() => {
+    console.log("db flushed");
+});
+await server.close().then(() => {
+    console.log("server closed");
+});
