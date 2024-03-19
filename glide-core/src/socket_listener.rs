96f78c2d7f875226dfd1ad12e404de30c2f086c1
Shoham Elias <116083498+shohamazon@users.noreply.github.com>
2024-03-19 18:27:50 +0200

    Python: modify json module to use custom_command (#1139)

--- glide-core/src/socket_listener.rs
+++ glide-core/src/socket_listener.rs
@@ -359,8 +359,6 @@ fn get_command(request: &Command) -> Option<Cmd> {
         RequestType::PTTL => Some(cmd("PTTL")),
         RequestType::ZRemRangeByRank => Some(cmd("ZREMRANGEBYRANK")),
         RequestType::Persist => Some(cmd("PERSIST")),
-        RequestType::JsonSet => Some(cmd("JSON.SET")),
-        RequestType::JsonGet => Some(cmd("JSON.GET")),
         RequestType::ZRemRangeByScore => Some(cmd("ZREMRANGEBYSCORE")),
         RequestType::Time => Some(cmd("TIME")),
         RequestType::Zrank => Some(cmd("ZRANK")),
