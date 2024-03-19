cdfce234df6ebc5e0ad17221869f292a4e68003f
Shoham Elias <116083498+shohamazon@users.noreply.github.com>
2024-03-19 14:21:21 +0200

    Python: adds HVALS command (#1130)

--- python/python/glide/async_commands/core.py
+++ python/python/glide/async_commands/core.py
@@ -634,6 +634,24 @@ class CoreCommands(Protocol):
         """
         return cast(int, await self._execute_command(RequestType.HLen, [key]))
 
+    async def hvals(self, key: str) -> List[str]:
+        """
+        Returns all values in the hash stored at `key`.
+
+        See https://redis.io/commands/hvals/ for more details.
+
+        Args:
+            key (str): The key of the hash.
+
+        Returns:
+            List[str]: A list of values in the hash, or an empty list when the key does not exist.
+
+         Examples:
+            >>> await client.hvals("my_hash")
+                ["value1", "value2", "value3"]  # Returns all the values stored in the hash "my_hash".
+        """
+        return cast(List[str], await self._execute_command(RequestType.Hvals, [key]))
+
     async def lpush(self, key: str, elements: List[str]) -> int:
         """
         Insert all the specified values at the head of the list stored at `key`.
