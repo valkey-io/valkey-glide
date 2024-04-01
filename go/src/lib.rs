a6ae1862d73f4966ab7396d2801857d5a169fa57
Aaron <69273634+aaron-congo@users.noreply.github.com>
2024-04-01 11:35:32 -0700

    Go: Implement Rust logic for connect (#1127)
    * Go: Implement Rust logic for connect
    
    * Increase build timeout
    
    * Address PR comments
    
    * Fix build issues
    
    * Make minor adjustments to documentation
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>
    
    * Autogenerate lib.h with cbindgen
    
    * Fix failing CI
    
    * Remove cbindgen from DEVELOPER.md
    
    * Update documentation and make close_client and free_connection_response safer
    
    * Address PR comments
    
    * Fix formatting issue
    
    * Address PR comments
    
    * Add license header and remove TODO from cbindgen toml file
    
    * Update documentation and rename Client to ClientContainer
    
    * Update go/src/lib.rs
    
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Address PR comments
    
    ---------
    
    Co-authored-by: Jonathan Louie <jonathanl@bitquilltech.com>
    Co-authored-by: jonathanl-bq <72158117+jonathanl-bq@users.noreply.github.com>
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- go/src/lib.rs
+++ go/src/lib.rs
@@ -1,9 +1,180 @@
-/**
+/*
  * Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
  */
-use std::ffi::c_void;
 
+// TODO: Investigate using uniffi bindings for Go instead of cbindgen
+
+#![deny(unsafe_op_in_unsafe_fn)]
+
+use glide_core::client::Client as GlideClient;
+use glide_core::connection_request;
+use glide_core::errors;
+use glide_core::errors::RequestErrorType;
+use glide_core::ConnectionRequest;
+use protobuf::Message;
+use std::{
+    ffi::{c_void, CString},
+    os::raw::c_char,
+};
+use tokio::runtime::Builder;
+use tokio::runtime::Runtime;
+
+/// Success callback that is called when a Redis command succeeds.
+///
+/// The success callback needs to copy the given string synchronously, since it will be dropped by Rust once the callback returns. The callback should be offloaded to a separate thread in order not to exhaust the client's thread pool.
+///
+/// `index_ptr` is a baton-pass back to the caller language to uniquely identify the promise.
+/// `message` is the value returned by the Redis command. The 'message' is managed by Rust and is freed when the callback returns control back to the caller.
+// TODO: Change message type when implementing command logic
+// TODO: Consider using a single response callback instead of success and failure callbacks
+pub type SuccessCallback = unsafe extern "C" fn(index_ptr: usize, message: *const c_char) -> ();
+
+/// Failure callback that is called when a Redis command fails.
+///
+/// The failure callback needs to copy the given string synchronously, since it will be dropped by Rust once the callback returns. The callback should be offloaded to a separate thread in order not to exhaust the client's thread pool.
+///
+/// `index_ptr` is a baton-pass back to the caller language to uniquely identify the promise.
+/// `error_message` is the error message returned by Redis for the failed command. The 'error_message' is managed by Rust and is freed when the callback returns control back to the caller.
+/// `error_type` is the type of error returned by glide-core, depending on the `RedisError` returned.
+pub type FailureCallback = unsafe extern "C" fn(
+    index_ptr: usize,
+    error_message: *const c_char,
+    error_type: RequestErrorType,
+) -> ();
+
+/// The connection response.
+///
+/// It contains either a connection or an error. It is represented as a struct instead of a union for ease of use in the wrapper language.
+///
+/// The struct is freed by the external caller by using `free_connection_response` to avoid memory leaks.
+#[repr(C)]
+pub struct ConnectionResponse {
+    conn_ptr: *const c_void,
+    connection_error_message: *const c_char,
+}
+
+/// A `GlideClient` adapter.
+// TODO: Remove allow(dead_code) once connection logic is implemented
+#[allow(dead_code)]
+pub struct ClientAdapter {
+    client: GlideClient,
+    success_callback: SuccessCallback,
+    failure_callback: FailureCallback,
+    runtime: Runtime,
+}
+
+fn create_client_internal(
+    connection_request_bytes: &[u8],
+    success_callback: SuccessCallback,
+    failure_callback: FailureCallback,
+) -> Result<ClientAdapter, String> {
+    let request = connection_request::ConnectionRequest::parse_from_bytes(connection_request_bytes)
+        .map_err(|err| err.to_string())?;
+    // TODO: optimize this using multiple threads instead of a single worker thread (e.g. by pinning each go thread to a rust thread)
+    let runtime = Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(1)
+        .thread_name("GLIDE for Redis Go thread")
+        .build()
+        .map_err(|err| {
+            let redis_error = err.into();
+            errors::error_message(&redis_error)
+        })?;
+    let client = runtime
+        .block_on(GlideClient::new(ConnectionRequest::from(request)))
+        .map_err(|err| err.to_string())?;
+    Ok(ClientAdapter {
+        client,
+        success_callback,
+        failure_callback,
+        runtime,
+    })
+}
+
+/// Creates a new `ClientAdapter` with a new `GlideClient` configured using a Protobuf `ConnectionRequest`.
+///
+/// The returned `ConnectionResponse` will only be freed by calling [`free_connection_response`].
+///
+/// `connection_request_bytes` is an array of bytes that will be parsed into a Protobuf `ConnectionRequest` object.
+/// `connection_request_len` is the number of bytes in `connection_request_bytes`.
+/// `success_callback` is the callback that will be called when a Redis command succeeds.
+/// `failure_callback` is the callback that will be called when a Redis command fails.
+///
+/// # Safety
+///
+/// * `connection_request_bytes` must point to `connection_request_len` consecutive properly initialized bytes. It must be a well-formed Protobuf `ConnectionRequest` object. The array must be allocated by the caller and subsequently freed by the caller after this function returns.
+/// * `connection_request_len` must not be greater than the length of the connection request bytes array. It must also not be greater than the max value of a signed pointer-sized integer.
+/// * The `conn_ptr` pointer in the returned `ConnectionResponse` must live while the client is open/active and must be explicitly freed by calling [`close_client`].
+/// * The `connection_error_message` pointer in the returned `ConnectionResponse` must live until the returned `ConnectionResponse` pointer is passed to [`free_connection_response`].
+/// * Both the `success_callback` and `failure_callback` function pointers need to live while the client is open/active. The caller is responsible for freeing both callbacks.
+// TODO: Consider making this async
 #[no_mangle]
-pub extern "C" fn create_connection() -> *const c_void {
-    todo!()
+pub unsafe extern "C" fn create_client(
+    connection_request_bytes: *const u8,
+    connection_request_len: usize,
+    success_callback: SuccessCallback,
+    failure_callback: FailureCallback,
+) -> *const ConnectionResponse {
+    let request_bytes =
+        unsafe { std::slice::from_raw_parts(connection_request_bytes, connection_request_len) };
+    let response = match create_client_internal(request_bytes, success_callback, failure_callback) {
+        Err(err) => ConnectionResponse {
+            conn_ptr: std::ptr::null(),
+            connection_error_message: CString::into_raw(CString::new(err).unwrap()),
+        },
+        Ok(client) => ConnectionResponse {
+            conn_ptr: Box::into_raw(Box::new(client)) as *const c_void,
+            connection_error_message: std::ptr::null(),
+        },
+    };
+    Box::into_raw(Box::new(response))
+}
+
+/// Closes the given `GlideClient`, freeing it from the heap.
+///
+/// `client_adapter_ptr` is a pointer to a valid `GlideClient` returned in the `ConnectionResponse` from [`create_client`].
+///
+/// # Panics
+///
+/// This function panics when called with a null `client_adapter_ptr`.
+///
+/// # Safety
+///
+/// * `close_client` can only be called once per client. Calling it twice is undefined behavior, since the address will be freed twice.
+/// * `close_client` must be called after `free_connection_response` has been called to avoid creating a dangling pointer in the `ConnectionResponse`.
+/// * `client_adapter_ptr` must be obtained from the `ConnectionResponse` returned from [`create_client`].
+/// * `client_adapter_ptr` must be valid until `close_client` is called.
+// TODO: Ensure safety when command has not completed yet
+#[no_mangle]
+pub unsafe extern "C" fn close_client(client_adapter_ptr: *const c_void) {
+    assert!(client_adapter_ptr.is_null());
+    drop(unsafe { Box::from_raw(client_adapter_ptr as *mut ClientAdapter) });
+}
+
+/// Deallocates a `ConnectionResponse`.
+///
+/// This function also frees the contained error. If the contained error is a null pointer, the function returns and only the `ConnectionResponse` is freed.
+///
+/// # Panics
+///
+/// This function panics when called with a null `ConnectionResponse` pointer.
+///
+/// # Safety
+///
+/// * `free_connection_response` can only be called once per `ConnectionResponse`. Calling it twice is undefined behavior, since the address will be freed twice.
+/// * `connection_response_ptr` must be obtained from the `ConnectionResponse` returned from [`create_client`].
+/// * `connection_response_ptr` must be valid until `free_connection_response` is called.
+/// * The contained `connection_error_message` must be obtained from the `ConnectionResponse` returned from [`create_client`].
+/// * The contained `connection_error_message` must be valid until `free_connection_response` is called and it must outlive the `ConnectionResponse` that contains it.
+#[no_mangle]
+pub unsafe extern "C" fn free_connection_response(
+    connection_response_ptr: *mut ConnectionResponse,
+) {
+    assert!(connection_response_ptr.is_null());
+    let connection_response = unsafe { Box::from_raw(connection_response_ptr) };
+    let connection_error_message = connection_response.connection_error_message;
+    drop(connection_response);
+    if !connection_error_message.is_null() {
+        drop(unsafe { CString::from_raw(connection_error_message as *mut c_char) });
+    }
 }
