1efc8d4983f1ee8218ef8f995287d205b3a402a7
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-04-03 13:11:44 -0700

    Javadoc fixes.  (#1185)
    * Javadoc fixes.
    
    ---------
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/glide/api/commands/HashBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/HashBaseCommands.java
@@ -59,7 +59,7 @@ public interface HashBaseCommands {
      *     If <code>key</code> does not exist, it is treated as an empty hash and it returns 0.<br>
      * @example
      *     <pre>{@code
-     * Long num = client.hdel("my_hash", new String[] {}).get("field1", "field2");
+     * Long num = client.hdel("my_hash", new String[] {"field1", "field2"}).get();
      * assert num == 2L; //Indicates that two fields were successfully removed from the hash.
      * }</pre>
      */
@@ -142,7 +142,7 @@ public interface HashBaseCommands {
     CompletableFuture<Long> hincrBy(String key, String field, long amount);
 
     /**
-     * Increment the string representing a floating point number stored at <code>field</code> in the
+     * Increments the string representing a floating point number stored at <code>field</code> in the
      * hash stored at <code>key</code> by increment. By using a negative increment value, the value
      * stored at <code>field</code> in the hash stored at <code>key</code> is decremented. If <code>
      * field</code> or <code>key</code> does not exist, it is set to 0 before performing the
@@ -154,7 +154,7 @@ public interface HashBaseCommands {
      *     value.
      * @param amount The amount by which to increment or decrement the field's value. Use a negative
      *     value to decrement.
-     * @returns The value of <code>field</code> in the hash stored at <code>key</code> after the
+     * @return The value of <code>field</code> in the hash stored at <code>key</code> after the
      *     increment or decrement.
      * @example
      *     <pre>{@code
