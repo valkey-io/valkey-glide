4341eb2388e72c32a87af00ca0a183a596356279
Shoham Elias <116083498+shohamazon@users.noreply.github.com>
2024-04-07 15:39:45 +0300

    Update node/src/BaseClient.ts
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- node/src/BaseClient.ts
+++ node/src/BaseClient.ts
@@ -1626,7 +1626,7 @@ export class BaseClient {
      * @example
      * ```typescript
      * // Example usage of the zcount method to count members in a sorted set within a score range
-     * const result = await client.zcount("my_sorted_set", \{ bound: 5.0, isInclusive: true \}, \{ bound: 10.0, isInclusive: false \});
+     * const result = await client.zcount("my_sorted_set", { bound: 5.0, isInclusive: true }, { bound: 10.0, isInclusive: false });
      * console.log(result); // Output: 1 - Indicates that there is one member with score between 5.0 (inclusive) and 10.0 (exclusive) in the sorted set "my_sorted_set".
      * ```
      */
