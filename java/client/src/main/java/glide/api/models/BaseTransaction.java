d3987811a22757135e53afc9015a5c97d0aef735
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-04-08 15:06:07 -0700

    Move xadd command
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/models/BaseTransaction.java
+++ java/client/src/main/java/glide/api/models/BaseTransaction.java
@@ -1480,42 +1480,6 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
     }
 
     /**
-     * Adds an entry to the specified stream stored at <code>key</code>.<br>
-     * If the <code>key</code> doesn't exist, the stream is created.
-     *
-     * @see <a href="https://redis.io/commands/xadd/">redis.io</a> for details.
-     * @param key The key of the stream.
-     * @param values Field-value pairs to be added to the entry.
-     * @return Command Response - The id of the added entry.
-     */
-    public T xadd(@NonNull String key, @NonNull Map<String, String> values) {
-        this.xadd(key, values, StreamAddOptions.builder().build());
-        return getThis();
-    }
-
-    /**
-     * Adds an entry to the specified stream stored at <code>key</code>.<br>
-     * If the <code>key</code> doesn't exist, the stream is created.
-     *
-     * @see <a href="https://redis.io/commands/xadd/">redis.io</a> for details.
-     * @param key The key of the stream.
-     * @param values Field-value pairs to be added to the entry.
-     * @param options Stream add options.
-     * @return Command Response - The id of the added entry, or <code>null</code> if {@link
-     *     StreamAddOptions#makeStream} is set to <code>false</code> and no stream with the matching
-     *     <code>key</code> exists.
-     */
-    public T xadd(
-            @NonNull String key, @NonNull Map<String, String> values, @NonNull StreamAddOptions options) {
-        String[] arguments =
-                ArrayUtils.addAll(
-                        ArrayUtils.addFirst(options.toArgs(), key), convertMapToKeyValueStringArray(values));
-        ArgsArray commandArgs = buildArgs(arguments);
-        protobufTransaction.addCommands(buildCommand(XAdd, commandArgs));
-        return getThis();
-    }
-
-    /**
      * Returns the rank of <code>member</code> in the sorted set stored at <code>key</code>, with
      * scores ordered from low to high.<br>
      * To get the rank of <code>member</code> with it's score, see <code>zrankWithScore</code>.
@@ -1569,7 +1533,8 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
     }
 
     /**
-     * Adds an entry to the specified stream.
+     * Adds an entry to the specified stream stored at <code>key</code>.<br>
+     * If the <code>key</code> doesn't exist, the stream is created.
      *
      * @see <a href="https://redis.io/commands/xadd/">redis.io</a> for details.
      * @param key The key of the stream.
@@ -1582,7 +1547,8 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
     }
 
     /**
-     * Adds an entry to the specified stream.
+     * Adds an entry to the specified stream stored at <code>key</code>.<br>
+     * If the <code>key</code> doesn't exist, the stream is created.
      *
      * @see <a href="https://redis.io/commands/xadd/">redis.io</a> for details.
      * @param key The key of the stream.
@@ -1593,10 +1559,10 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
      *     <code>key</code> exists.
      */
     public T xadd(
-            @NonNull String key, @NonNull Map<String, String> values, @NonNull StreamAddOptions options) {
+        @NonNull String key, @NonNull Map<String, String> values, @NonNull StreamAddOptions options) {
         String[] arguments =
-                ArrayUtils.addAll(
-                        ArrayUtils.addFirst(options.toArgs(), key), convertMapToKeyValueStringArray(values));
+            ArrayUtils.addAll(
+                ArrayUtils.addFirst(options.toArgs(), key), convertMapToKeyValueStringArray(values));
         ArgsArray commandArgs = buildArgs(arguments);
         protobufTransaction.addCommands(buildCommand(XAdd, commandArgs));
         return getThis();
