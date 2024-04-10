d42acd6e9a0dd558a89bdfb74e663f25bb5d0b08
Jonathan Louie <jonathanl@bitquilltech.com>
2024-04-10 14:13:45 -0700

    Update module-info.java

--- java/client/src/main/java/module-info.java
+++ java/client/src/main/java/module-info.java
@@ -6,7 +6,10 @@ module glide.api {
     exports glide.api.models.configuration;
     exports glide.api.models.exceptions;
 
+    requires org.apache.commons.lang3;
     requires com.google.protobuf;
+    requires io.netty.common;
     requires io.netty.transport;
+    requires io.netty.transport.classes.kqueue;
     requires lombok;
 }
