820c3c56990b6b294ca593ad8b1febef96bccd01
Gilboab <97948000+GilboaAWS@users.noreply.github.com>
2024-04-11 16:12:14 +0300

    Update python/python/glide/async_commands/transaction.py
    Co-authored-by: Shoham Elias <116083498+shohamazon@users.noreply.github.com>

--- python/python/glide/async_commands/transaction.py
+++ python/python/glide/async_commands/transaction.py
@@ -812,7 +812,7 @@ class BaseTransaction:
 
         Commands response:
             Set[str]: A set of popped elements will be returned depending on the set's length.
-                  If `key` does not exist, empty set will be returned.
+                  If `key` does not exist, an empty set will be returned.
 
         """
         return self.append_command(RequestType.Spop, [key, str(count)])
