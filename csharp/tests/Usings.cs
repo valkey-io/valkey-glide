ad43da61d47bf617d0cf23410dc6ee3108353a0b
Guriy Samarin <guriysamarin@outlook.com>
2024-04-12 18:29:56 +0100

    C#: Strict rules (#1136)
    enable strict linting
    
    Co-authored-by: Guriy Samarin <guriy@amazon.com>

--- csharp/tests/Usings.cs
+++ csharp/tests/Usings.cs
@@ -1,5 +1,3 @@
-/**
- * Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
- */
+// Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
 
 global using NUnit.Framework;
