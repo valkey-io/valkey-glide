701cf90ee40b16674d5f5111f75d21e036838edf
SanHalacogluImproving <144171266+SanHalacogluImproving@users.noreply.github.com>
2024-04-12 15:03:47 -0700

    Java: Add `Zremrangebyscore` command. (Sorted Set Group) (#1269)
    * Java: Add `Zremrangebyscore` command. (Sorted Set Group) (#211)
    
    * Added checks for elements in IT tests.
    
    * Minor ordering fix.
    
    * Rebase + Spotless.

--- java/client/src/test/java/glide/api/models/TransactionTests.java
+++ java/client/src/test/java/glide/api/models/TransactionTests.java
@@ -86,6 +86,7 @@ import static redis_request.RedisRequestOuterClass.RequestType.ZPopMax;
 import static redis_request.RedisRequestOuterClass.RequestType.ZPopMin;
 import static redis_request.RedisRequestOuterClass.RequestType.ZRemRangeByLex;
 import static redis_request.RedisRequestOuterClass.RequestType.ZRemRangeByRank;
+import static redis_request.RedisRequestOuterClass.RequestType.ZRemRangeByScore;
 import static redis_request.RedisRequestOuterClass.RequestType.ZScore;
 import static redis_request.RedisRequestOuterClass.RequestType.Zadd;
 import static redis_request.RedisRequestOuterClass.RequestType.Zcard;
@@ -95,6 +96,7 @@ import static redis_request.RedisRequestOuterClass.RequestType.Zrank;
 import static redis_request.RedisRequestOuterClass.RequestType.Zrem;
 
 import glide.api.models.commands.InfoOptions;
+import glide.api.models.commands.RangeOptions;
 import glide.api.models.commands.RangeOptions.InfLexBound;
 import glide.api.models.commands.RangeOptions.InfScoreBound;
 import glide.api.models.commands.RangeOptions.LexBoundary;
@@ -386,6 +388,10 @@ public class TransactionTests {
 
         transaction.zremrangebylex("key", new LexBoundary("a", false), InfLexBound.POSITIVE_INFINITY);
         results.add(Pair.of(ZRemRangeByLex, buildArgs("key", "(a", "+")));
+
+        transaction.zremrangebyscore(
+                "key", new ScoreBoundary(5, false), RangeOptions.InfScoreBound.POSITIVE_INFINITY);
+        results.add(Pair.of(ZRemRangeByScore, buildArgs("key", "(5.0", "+inf")));
 
         transaction.xadd("key", Map.of("field1", "foo1"));
         results.add(Pair.of(XAdd, buildArgs("key", "*", "field1", "foo1")));
--- java/integTest/src/test/java/glide/TransactionTestUtilities.java
+++ java/integTest/src/test/java/glide/TransactionTestUtilities.java
@@ -117,6 +117,7 @@ public class TransactionTestUtilities {
         baseTransaction.zpopmax(key8);
         baseTransaction.zremrangebyrank(key8, 5, 10);
         baseTransaction.zremrangebylex(key8, new LexBoundary("j"), InfLexBound.POSITIVE_INFINITY);
+        baseTransaction.zremrangebyscore(key8, new ScoreBoundary(5), InfScoreBound.POSITIVE_INFINITY);
         baseTransaction.zdiffstore(key8, new String[] {key8, key8});
 
         baseTransaction.zadd(zSetKey2, Map.of("one", 1.0, "two", 2.0));
@@ -217,6 +218,7 @@ public class TransactionTestUtilities {
             Map.of("three", 3.0), // zpopmax(key8)
             0L, // zremrangebyrank(key8, 5, 10)
             0L, // zremrangebylex(key8, new LexBoundary("j"), InfLexBound.POSITIVE_INFINITY)
+            0L, // zremrangebyscore(key8, new ScoreBoundary(5), InfScoreBound.POSITIVE_INFINITY)
             0L, // zdiffstore(key8, new String[] {key8, key8})
             2L, // zadd(zSetKey2, Map.of("one", 1.0, "two", 2.0))
             new String[] {"one", "two"}, // zdiff(new String[] {zSetKey2, key8})
