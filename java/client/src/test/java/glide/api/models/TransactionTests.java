4fc5f0827e3c22ba43cc8f91f0b4c54369da3c93
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-04-12 12:47:45 -0700

    Java: Add `SUNIONSTORE` command. (#203)
    * Add `SUNIONSTORE` command.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * PR comments.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    ---------
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/test/java/glide/api/models/TransactionTests.java
+++ java/client/src/test/java/glide/api/models/TransactionTests.java
@@ -77,6 +77,7 @@ import static redis_request.RedisRequestOuterClass.RequestType.SMembers;
 import static redis_request.RedisRequestOuterClass.RequestType.SMove;
 import static redis_request.RedisRequestOuterClass.RequestType.SRem;
 import static redis_request.RedisRequestOuterClass.RequestType.SetRange;
+import static redis_request.RedisRequestOuterClass.RequestType.SUnionStore;
 import static redis_request.RedisRequestOuterClass.RequestType.SetString;
 import static redis_request.RedisRequestOuterClass.RequestType.Strlen;
 import static redis_request.RedisRequestOuterClass.RequestType.TTL;
@@ -284,6 +285,12 @@ public class TransactionTests {
 
         transaction.sinterstore("key", new String[] {"set1", "set2"});
         results.add(Pair.of(SInterStore, buildArgs("key", "set1", "set2")));
+
+        transaction.sunionstore("key", new String[] {"set1", "set2"});
+        results.add(
+                Pair.of(
+                        SUnionStore,
+                        ArgsArray.newBuilder().addArgs("key").addArgs("set1").addArgs("set2").build()));
 
         transaction.exists(new String[] {"key1", "key2"});
         results.add(Pair.of(Exists, buildArgs("key1", "key2")));
