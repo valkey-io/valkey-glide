250a07cff8285bddf3226286205bbe882c91e9aa
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-04-16 17:21:46 -0700

    Java: Add `SDIFFSTORE` command (#1288)
    * Add `SDIFFSTORE` command. (#205)
    
    * Add `SDIFFSTORE` command.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * PR comments.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    ---------
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Typo fix.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Remove TODO.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * PR comments.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * I HATE YOU SPOTLESS
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Fix IT.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Fix the merge.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * PR comments.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * PR comments.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * update doc.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    ---------
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/glide/api/models/BaseTransaction.java
+++ java/client/src/main/java/glide/api/models/BaseTransaction.java
@@ -64,6 +64,7 @@ import static redis_request.RedisRequestOuterClass.RequestType.RPush;
 import static redis_request.RedisRequestOuterClass.RequestType.RPushX;
 import static redis_request.RedisRequestOuterClass.RequestType.SAdd;
 import static redis_request.RedisRequestOuterClass.RequestType.SCard;
+import static redis_request.RedisRequestOuterClass.RequestType.SDiffStore;
 import static redis_request.RedisRequestOuterClass.RequestType.SInterStore;
 import static redis_request.RedisRequestOuterClass.RequestType.SIsMember;
 import static redis_request.RedisRequestOuterClass.RequestType.SMembers;
@@ -924,6 +925,21 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
     public T scard(@NonNull String key) {
         ArgsArray commandArgs = buildArgs(key);
         protobufTransaction.addCommands(buildCommand(SCard, commandArgs));
+        return getThis();
+    }
+
+    /**
+     * Stores the difference between the first set and all the successive sets in <code>keys</code>
+     * into a new set at <code>destination</code>.
+     *
+     * @see <a href="https://redis.io/commands/sdiffstore/">redis.io</a> for details.
+     * @param destination The key of the destination set.
+     * @param keys The keys of the sets to diff.
+     * @return Command Response - The number of elements in the resulting set.
+     */
+    public T sdiffstore(@NonNull String destination, @NonNull String[] keys) {
+        ArgsArray commandArgs = buildArgs(ArrayUtils.addFirst(keys, destination));
+        protobufTransaction.addCommands(buildCommand(SDiffStore, commandArgs));
         return getThis();
     }
 
