e92ff302f6edbce5b00b9c7efa528e48532d7d90
jonathanl-bq <72158117+jonathanl-bq@users.noreply.github.com>
2024-04-25 13:03:23 -0700

    Java: Implement OBJECT IDLETIME command (#1328)
    * Implement OBJECT IDLETIME command
    
    * Address PR comment
    
    * Fix build and SPOTLESS
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Fix nit comment and run spotless
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/integTest/src/test/java/glide/SharedCommandTests.java
+++ java/integTest/src/test/java/glide/SharedCommandTests.java
@@ -2787,6 +2787,24 @@ public class SharedCommandTests {
     @SneakyThrows
     @ParameterizedTest(autoCloseArguments = false)
     @MethodSource("getClients")
+    public void objectIdletime_returns_null(BaseClient client) {
+        String nonExistingKey = UUID.randomUUID().toString();
+        assertNull(client.objectIdletime(nonExistingKey).get());
+    }
+
+    @SneakyThrows
+    @ParameterizedTest(autoCloseArguments = false)
+    @MethodSource("getClients")
+    public void objectIdletime(BaseClient client) {
+        String key = UUID.randomUUID().toString();
+        assertEquals(OK, client.set(key, "").get());
+        Thread.sleep(1000);
+        assertTrue(client.objectIdletime(key).get() > 0L);
+    }
+
+    @SneakyThrows
+    @ParameterizedTest(autoCloseArguments = false)
+    @MethodSource("getClients")
     public void objectRefcount_returns_null(BaseClient client) {
         String nonExistingKey = UUID.randomUUID().toString();
         assertNull(client.objectRefcount(nonExistingKey).get());
