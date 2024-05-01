405d337792d01d63d5df9c03092ae5871e9aab23
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-05-01 14:30:14 -0700

    Restore IT.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/integTest/src/test/java/glide/TransactionTestUtilities.java
+++ java/integTest/src/test/java/glide/TransactionTestUtilities.java
@@ -3,6 +3,7 @@ package glide;
 
 import static glide.TestConfiguration.REDIS_VERSION;
 import static glide.api.BaseClient.OK;
+import static glide.api.models.commands.FlushOption.ASYNC;
 import static glide.api.models.commands.LInsertOptions.InsertPosition.AFTER;
 
 import glide.api.models.BaseTransaction;
@@ -188,6 +189,9 @@ public class TransactionTestUtilities {
                 .pfmerge(hllKey3, new String[] {hllKey1, hllKey2})
                 .pfcount(new String[] {hllKey3});
 
+        // keep it last - it deletes all the keys
+        baseTransaction.flushall().flushall(ASYNC);
+
         return baseTransaction;
     }
 
@@ -298,6 +302,8 @@ public class TransactionTestUtilities {
             3L, // pfcount(new String[] { hllKey1, hllKey2 });;
             OK, // pfmerge(hllKey3, new String[] {hllKey1, hllKey2})
             3L, // pfcount(new String[] { hllKey3 })
+            OK, // flushall()
+            OK, // flushall(ASYNC)
         };
     }
 }
