01c78b13c8d1a512c4f89d1c51b680c96d8b10d9
Guian Gumpac <guian.gumpac@improving.com>
2024-05-10 19:41:24 +0000

    Java: Add `BITCOUNT` command (#1384)
    * Java: Add command `BITCOUNT` (#263)
    
    * Implemented Bitcount command for java client
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Removed unrelated change
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Removed unnecessary constructor
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    ---------
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Resolved conflicts
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Renamed interface for consistency
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Address PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Added missed javadoc
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Update java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>
    
    * Update java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>
    
    * addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Removed wrong version check
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Rebased and resolved conflicts
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    ---------
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>

--- java/client/src/main/java/glide/api/BaseClient.java
+++ java/client/src/main/java/glide/api/BaseClient.java
@@ -12,6 +12,7 @@ import static redis_request.RedisRequestOuterClass.RequestType.BLPop;
 import static redis_request.RedisRequestOuterClass.RequestType.BRPop;
 import static redis_request.RedisRequestOuterClass.RequestType.BZPopMax;
 import static redis_request.RedisRequestOuterClass.RequestType.BZPopMin;
+import static redis_request.RedisRequestOuterClass.RequestType.Bitcount;
 import static redis_request.RedisRequestOuterClass.RequestType.Decr;
 import static redis_request.RedisRequestOuterClass.RequestType.DecrBy;
 import static redis_request.RedisRequestOuterClass.RequestType.Del;
@@ -107,6 +108,7 @@ import static redis_request.RedisRequestOuterClass.RequestType.ZScore;
 import static redis_request.RedisRequestOuterClass.RequestType.ZUnion;
 import static redis_request.RedisRequestOuterClass.RequestType.ZUnionStore;
 
+import glide.api.commands.BitmapBaseCommands;
 import glide.api.commands.GenericBaseCommands;
 import glide.api.commands.GeospatialIndicesBaseCommands;
 import glide.api.commands.HashBaseCommands;
@@ -117,6 +119,7 @@ import glide.api.commands.SortedSetBaseCommands;
 import glide.api.commands.StreamBaseCommands;
 import glide.api.commands.StringBaseCommands;
 import glide.api.models.Script;
+import glide.api.models.commands.BitmapIndexType;
 import glide.api.models.commands.ExpireOptions;
 import glide.api.models.commands.LInsertOptions.InsertPosition;
 import glide.api.models.commands.RangeOptions;
@@ -160,6 +163,7 @@ import response.ResponseOuterClass.Response;
 @AllArgsConstructor
 public abstract class BaseClient
         implements AutoCloseable,
+                BitmapBaseCommands,
                 GenericBaseCommands,
                 StringBaseCommands,
                 HashBaseCommands,
@@ -1211,5 +1215,26 @@ public abstract class BaseClient
                 GeoPos,
                 arguments,
                 response -> castArrayofArrays(handleArrayResponse(response), Double.class));
+    }
+
+    @Override
+    public CompletableFuture<Long> bitcount(@NonNull String key) {
+        return commandManager.submitNewCommand(Bitcount, new String[] {key}, this::handleLongResponse);
+    }
+
+    @Override
+    public CompletableFuture<Long> bitcount(@NonNull String key, long start, long end) {
+        return commandManager.submitNewCommand(
+                Bitcount,
+                new String[] {key, Long.toString(start), Long.toString(end)},
+                this::handleLongResponse);
+    }
+
+    @Override
+    public CompletableFuture<Long> bitcount(
+            @NonNull String key, long start, long end, @NonNull BitmapIndexType options) {
+        String[] arguments =
+                new String[] {key, Long.toString(start), Long.toString(end), options.toString()};
+        return commandManager.submitNewCommand(Bitcount, arguments, this::handleLongResponse);
     }
 }
