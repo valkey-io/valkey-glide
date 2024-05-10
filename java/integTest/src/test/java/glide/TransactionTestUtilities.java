4884e54f77328d09270771bc275ae58a033539a6
Guian Gumpac <guian.gumpac@improving.com>
2024-05-10 19:41:24 +0000

    Java: Add `BITCOUNT` command (#1384)
    * Java: Add command `BITCOUNT` (#263)
    
    * Implemented Bitcount command for java client
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Removed unrelated change
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Removed unnecessary constructor
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    ---------
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Resolved conflicts
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Renamed interface for consistency
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Address PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Added missed javadoc
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Update java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>
    
    * Update java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>
    
    * addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Addressed PR comments
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Removed wrong version check
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Rebased and resolved conflicts
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    
    ---------
    
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>

--- java/integTest/src/test/java/glide/TransactionTestUtilities.java
+++ java/integTest/src/test/java/glide/TransactionTestUtilities.java
@@ -8,6 +8,7 @@ import static glide.api.models.commands.LInsertOptions.InsertPosition.AFTER;
 import static glide.utils.ArrayTransformUtils.concatenateArrays;
 
 import glide.api.models.BaseTransaction;
+import glide.api.models.commands.BitmapIndexType;
 import glide.api.models.commands.ExpireOptions;
 import glide.api.models.commands.RangeOptions.InfLexBound;
 import glide.api.models.commands.RangeOptions.InfScoreBound;
@@ -62,7 +63,9 @@ public class TransactionTestUtilities {
                         (TransactionBuilder) TransactionTestUtilities::connectionManagementCommands),
                 Arguments.of(
                         "Geospatial Commands",
-                        (TransactionBuilder) TransactionTestUtilities::geospatialCommands));
+                        (TransactionBuilder) TransactionTestUtilities::geospatialCommands),
+                Arguments.of(
+                        "Bitmap Commands", (TransactionBuilder) TransactionTestUtilities::bitmapCommands));
     }
 
     /** Generate test samples for parametrized tests. Could be routed to primary nodes only. */
@@ -461,5 +464,31 @@ public class TransactionTestUtilities {
                 {15.08726745843887329, 37.50266842333162032},
             }, // geopos(new String[]{"Palermo", "Catania"})
         };
+    }
+
+    private static Object[] bitmapCommands(BaseTransaction<?> transaction) {
+        String key = "{key-" + UUID.randomUUID();
+
+        transaction.set(key, "foobar").bitcount(key).bitcount(key, 1, 1);
+
+        if (REDIS_VERSION.isGreaterThanOrEqualTo("7.0.0")) {
+            transaction.bitcount(key, 5, 30, BitmapIndexType.BIT);
+        }
+
+        var expectedResults =
+                new Object[] {
+                    OK, // set(key, "foobar")
+                    26L, // bitcount(key)
+                    6L, // bitcount(key, 1, 1)
+                };
+
+        if (REDIS_VERSION.isGreaterThanOrEqualTo("7.0.0")) {
+            return concatenateArrays(
+                    expectedResults,
+                    new Object[] {
+                        17L, // bitcount(key, 5, 30, BitmapIndexType.BIT)
+                    });
+        }
+        return expectedResults;
     }
 }
