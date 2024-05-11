60ab21cc1fa7a062d372bf27e6fd23fdcab6fd1e
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-05-10 17:29:59 -0700

    I HATE YOU SPOTLESS
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
@@ -852,8 +852,8 @@ public interface SortedSetBaseCommands {
      * stores the result in <code>destination</code>. If <code>destination</code> already exists, it
      * is overwritten. Otherwise, a new sorted set will be created.
      *
-     * @apiNote When in cluster mode, <code>destination</code> and all <code>keysOrWeightedKeys</code> must map to
-     *     the same hash slot.
+     * @apiNote When in cluster mode, <code>destination</code> and all <code>keysOrWeightedKeys</code>
+     *     must map to the same hash slot.
      * @see <a href="https://redis.io/commands/zunionstore/">redis.io</a> for more details.
      * @param destination The key of the destination sorted set.
      * @param keysOrWeightedKeys The keys of the sorted sets with possible formats:
@@ -880,8 +880,8 @@ public interface SortedSetBaseCommands {
      * stores the result in <code>destination</code>. If <code>destination</code> already exists, it
      * is overwritten. Otherwise, a new sorted set will be created.
      *
-     * @apiNote When in cluster mode, <code>destination</code> and all <code>keysOrWeightedKeys</code> must map to
-     *     the same hash slot.
+     * @apiNote When in cluster mode, <code>destination</code> and all <code>keysOrWeightedKeys</code>
+     *     must map to the same hash slot.
      * @see <a href="https://redis.io/commands/zunionstore/">redis.io</a> for more details.
      * @param destination The key of the destination sorted set.
      * @param keysOrWeightedKeys The keys of the sorted sets with possible formats:
