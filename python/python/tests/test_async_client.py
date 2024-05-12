ed61fa02e853ec1d5c08bbf4ed90510df1f982cc
Shoham Elias <shohame@amazon.com>
2024-05-12 12:53:59 +0000

    Allow endpoint specification for Python and Node test

--- python/python/tests/test_async_client.py
+++ python/python/tests/test_async_client.py
@@ -262,7 +262,8 @@ class TestRedisClients:
             # Delete this user
             await redis_client.custom_command(["ACL", "DELUSER", username])
 
-    async def test_select_standalone_database_id(self, request):
+    @pytest.mark.parametrize("cluster_mode", [False])
+    async def test_select_standalone_database_id(self, request, cluster_mode):
         redis_client = await create_client(request, cluster_mode=False, database_id=4)
         client_info = await redis_client.custom_command(["CLIENT", "INFO"])
         assert "db=4" in client_info
