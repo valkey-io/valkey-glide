9fe8f3ed657ed04791c6ed305bb23813e99c4421
Guian Gumpac <guian.gumpac@improving.com>
2024-05-13 17:31:52 -0700

    Update java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
    Co-authored-by: jonathanl-bq <72158117+jonathanl-bq@users.noreply.github.com>

--- java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
@@ -100,7 +100,7 @@ public interface BitmapBaseCommands {
      * Returns the bit value at <code>offset</code> in the string value stored at <code>key</code>.
      *
      * @see <a href="https://redis.io/commands/getbit/">redis.io</a> for details.
-     * @param key The key for the string to get the bit at offset of.
+     * @param key The key for the string to get the bit at <code>offset</code> of.
      * @param offset The index of the bit to return.
      * @return The bit at offset of the string. Returns zero if the key is missing as it is treated as
      *     an empty string. Returns zero if the positive offset exceeds the length of the string as it
