0d674ba110b2c110f01b9a9bd45484a8225f1200
Gilboab <97948000+GilboaAWS@users.noreply.github.com>
2024-05-16 10:19:21 +0300

    Python: Added ZINTERSTORE and ZUNIONSTORE commands (#1388)
    Added ZINTERSTORE and ZUNIONSTORE commands to python
    
    Co-authored-by: Shoham Elias 
    Co-authored-by: Yury-Fridlyand

--- python/python/glide/async_commands/transaction.py
+++ python/python/glide/async_commands/transaction.py
@@ -16,12 +16,14 @@ from glide.async_commands.core import (
     UpdateOptions,
 )
 from glide.async_commands.sorted_set import (
+    AggregationType,
     InfBound,
     LexBoundary,
     RangeByIndex,
     RangeByLex,
     RangeByScore,
     ScoreBoundary,
+    _create_z_cmd_store_args,
     _create_zrange_args,
     _create_zrangestore_args,
 )
@@ -2046,6 +2048,62 @@ class BaseTransaction:
         return self.append_command(
             RequestType.ZDiffStore, [destination, str(len(keys))] + keys
         )
+
+    def zinterstore(
+        self: TTransaction,
+        destination: str,
+        keys: Union[List[str], List[Tuple[str, float]]],
+        aggregation_type: Optional[AggregationType] = None,
+    ) -> TTransaction:
+        """
+        Computes the intersection of sorted sets given by the specified `keys` and stores the result in `destination`.
+        If `destination` already exists, it is overwritten. Otherwise, a new sorted set will be created.
+
+        When in cluster mode, `destination` and all keys in `keys` must map to the same hash slot.
+
+        See https://valkey.io/commands/zinterstore/ for more details.
+
+        Args:
+            destination (str): The key of the destination sorted set.
+            keys (Union[List[str], List[Tuple[str, float]]]): The keys of the sorted sets with possible formats:
+                List[str] - for keys only.
+                List[Tuple[str, float]]] - for weighted keys with score multipliers.
+            aggregation_type (Optional[AggregationType]): Specifies the aggregation strategy to apply
+                when combining the scores of elements. See `AggregationType`.
+
+        Command response:
+            int: The number of elements in the resulting sorted set stored at `destination`.
+        """
+        args = _create_z_cmd_store_args(destination, keys, aggregation_type)
+        return self.append_command(RequestType.ZInterStore, args)
+
+    def zunionstore(
+        self: TTransaction,
+        destination: str,
+        keys: Union[List[str], List[Tuple[str, float]]],
+        aggregation_type: Optional[AggregationType] = None,
+    ) -> TTransaction:
+        """
+        Computes the union of sorted sets given by the specified `keys` and stores the result in `destination`.
+        If `destination` already exists, it is overwritten. Otherwise, a new sorted set will be created.
+
+        When in cluster mode, `destination` and all keys in `keys` must map to the same hash slot.
+
+        see https://valkey.io/commands/zunionstore/ for more details.
+
+        Args:
+            destination (str): The key of the destination sorted set.
+            keys (Union[List[str], List[Tuple[str, float]]]): The keys of the sorted sets with possible formats:
+                List[str] - for keys only.
+                List[Tuple[str, float]]] - for weighted keys with score multipliers.
+            aggregation_type (Optional[AggregationType]): Specifies the aggregation strategy to apply
+                when combining the scores of elements. See `AggregationType`.
+
+        Command response:
+            int: The number of elements in the resulting sorted set stored at `destination`.
+        """
+        args = _create_z_cmd_store_args(destination, keys, aggregation_type)
+        return self.append_command(RequestType.ZUnionStore, args)
 
     def dbsize(self: TTransaction) -> TTransaction:
         """
