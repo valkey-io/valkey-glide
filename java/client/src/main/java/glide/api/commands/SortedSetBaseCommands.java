8616ea4fa218916b79f5b6e37c9d80c4d48a00ac
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-05-15 21:19:24 -0700

    Apply suggestions from code review
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    Co-authored-by: Shoham Elias <116083498+shohamazon@users.noreply.github.com>

--- java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
@@ -859,7 +859,7 @@ public interface SortedSetBaseCommands {
      * stores the result in <code>destination</code>. If <code>destination</code> already exists, it
      * is overwritten. Otherwise, a new sorted set will be created.
      *
-     * @apiNote When in cluster mode, <code>destination</code> and all <code>keysOrWeightedKeys</code>
+     * @apiNote When in cluster mode, <code>destination</code> and all keys in <code>keysOrWeightedKeys</code>
      *     must map to the same hash slot.
      * @see <a href="https://redis.io/commands/zunionstore/">redis.io</a> for more details.
      * @param destination The key of the destination sorted set.
