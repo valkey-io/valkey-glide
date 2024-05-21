435ffb1ab8e08dce4ad3d00678a4f65434fbdf0e
Guian Gumpac <guian.gumpac@improving.com>
2024-05-21 10:42:42 -0700

    Spotless

--- java/client/src/main/java/glide/api/BaseClient.java
+++ java/client/src/main/java/glide/api/BaseClient.java
@@ -23,7 +23,6 @@ import static redis_request.RedisRequestOuterClass.RequestType.ExpireAt;
 import static redis_request.RedisRequestOuterClass.RequestType.GeoAdd;
 import static redis_request.RedisRequestOuterClass.RequestType.GeoDist;
 import static redis_request.RedisRequestOuterClass.RequestType.GeoHash;
-import static redis_request.RedisRequestOuterClass.RequestType.GetBit;
 import static redis_request.RedisRequestOuterClass.RequestType.GeoPos;
 import static redis_request.RedisRequestOuterClass.RequestType.Get;
 import static redis_request.RedisRequestOuterClass.RequestType.GetBit;
