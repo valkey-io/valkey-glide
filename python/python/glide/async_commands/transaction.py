3d11c3458119bd537ace84610735a8ab1038e6f5
Aaron <69273634+aaron-congo@users.noreply.github.com>
2024-05-22 08:15:10 -0700

    Python: fix minor error in zmpop documentation (#1435)

--- python/python/glide/async_commands/transaction.py
+++ python/python/glide/async_commands/transaction.py
@@ -2226,7 +2226,7 @@ class BaseTransaction:
         Command response:
             Optional[List[Union[str, Mapping[str, float]]]]: A two-element list containing the key name of the set from
                 which elements were popped, and a member-score mapping of the popped elements. If no members could be
-                popped and the timeout expired, returns None.
+                popped, returns None.
 
         Since: Redis version 7.0.0.
         """
