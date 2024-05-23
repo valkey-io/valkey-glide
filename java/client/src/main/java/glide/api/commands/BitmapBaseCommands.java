3e8c084145a815d5ed416160a0ba38afb2238a01
Guian Gumpac <guian.gumpac@improving.com>
2024-05-23 20:14:29 +0000

    Java: Add `BITPOS` command  (#1445)
    * Java: Add `BITPOS` command (#284)
    
    * Added Bitpos command in Java
    
    * Added javadocs
    
    * Addressed PR comments
    
    * Addressed comments
    
    * Fixed typo
    
    * Resolved conflicts
    
    * Addressed PR comments
    
    * Addressed comments

--- java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
@@ -113,4 +113,104 @@ public interface BitmapBaseCommands {
      * }</pre>
      */
     CompletableFuture<Long> getbit(String key, long offset);
+
+    /**
+     * Returns the position of the first bit matching the given <code>bit</code> value.
+     *
+     * @see <a href="https://redis.io/commands/bitpos/">redis.io</a> for details.
+     * @param key The key of the string.
+     * @param bit The bit value to match. The value must be <code>0</code> or <code>1</code>.
+     * @return The position of the first occurrence matching <code>bit</code> in the binary value of
+     *     the string held at <code>key</code>. If <code>bit</code> is not found, a <code>-1</code> is
+     *     returned.
+     * @example
+     *     <pre>{@code
+     * Long payload = client.bitpos("myKey1", 0).get();
+     * // Indicates that the first occurrence of a 0 bit value is the fourth bit of the binary value
+     * // of the string stored at "myKey1".
+     * assert payload == 3L;
+     * }</pre>
+     */
+    CompletableFuture<Long> bitpos(String key, long bit);
+
+    /**
+     * Returns the position of the first bit matching the given <code>bit</code> value. The offset
+     * <code>start</code> is a zero-based index, with <code>0</code> being the first byte of the list,
+     * <code>1</code> being the next byte and so on. These offsets can also be negative numbers
+     * indicating offsets starting at the end of the list, with <code>-1</code> being the last byte of
+     * the list, <code>-2</code> being the penultimate, and so on.
+     *
+     * @see <a href="https://redis.io/commands/bitpos/">redis.io</a> for details.
+     * @param key The key of the string.
+     * @param bit The bit value to match. The value must be <code>0</code> or <code>1</code>.
+     * @param start The starting offset.
+     * @return The position of the first occurrence beginning at the <code>start</code> offset of the
+     *     <code>bit</code> in the binary value of the string held at <code>key</code>. If <code>bit
+     *     </code> is not found, a <code>-1</code> is returned.
+     * @example
+     *     <pre>{@code
+     * Long payload = client.bitpos("myKey1", 1, 4).get();
+     * // Indicates that the first occurrence of a 1 bit value starting from fifth byte is the 34th
+     * // bit of the binary value of the string stored at "myKey1".
+     * assert payload == 33L;
+     * }</pre>
+     */
+    CompletableFuture<Long> bitpos(String key, long bit, long start);
+
+    /**
+     * Returns the position of the first bit matching the given <code>bit</code> value. The offsets
+     * <code>start</code> and <code>end</code> are zero-based indexes, with <code>0</code> being the
+     * first byte of the list, <code>1</code> being the next byte and so on. These offsets can also be
+     * negative numbers indicating offsets starting at the end of the list, with <code>-1</code> being
+     * the last byte of the list, <code>-2</code> being the penultimate, and so on.
+     *
+     * @see <a href="https://redis.io/commands/bitpos/">redis.io</a> for details.
+     * @param key The key of the string.
+     * @param bit The bit value to match. The value must be <code>0</code> or <code>1</code>.
+     * @param start The starting offset.
+     * @param end The ending offset.
+     * @return The position of the first occurrence from the <code>start</code> to the <code>end
+     *     </code> offsets of the <code>bit</code> in the binary value of the string held at <code>key
+     *     </code>. If <code>bit</code> is not found, a <code>-1</code> is returned.
+     * @example
+     *     <pre>{@code
+     * Long payload = client.bitpos("myKey1", 1, 4, 6).get();
+     * // Indicates that the first occurrence of a 1 bit value starting from the fifth to seventh
+     * // bytes is the 34th bit of the binary value of the string stored at "myKey1".
+     * assert payload == 33L;
+     * }</pre>
+     */
+    CompletableFuture<Long> bitpos(String key, long bit, long start, long end);
+
+    /**
+     * Returns the position of the first bit matching the given <code>bit</code> value. The offset
+     * <code>offsetType</code> specifies whether the offset is a BIT or BYTE. If BIT is specified,
+     * <code>start==0</code> and <code>end==2</code> means to look at the first three bits. If BYTE is
+     * specified, <code>start==0</code> and <code>end==2</code> means to look at the first three bytes
+     * The offsets are zero-based indexes, with <code>0</code> being the first element of the list,
+     * <code>1</code> being the next, and so on. These offsets can also be negative numbers indicating
+     * offsets starting at the end of the list, with <code>-1</code> being the last element of the
+     * list, <code>-2</code> being the penultimate, and so on.
+     *
+     * @since Redis 7.0 and above.
+     * @see <a href="https://redis.io/commands/bitpos/">redis.io</a> for details.
+     * @param key The key of the string.
+     * @param bit The bit value to match. The value must be <code>0</code> or <code>1</code>.
+     * @param start The starting offset.
+     * @param end The ending offset.
+     * @param offsetType The index offset type. Could be either {@link BitmapIndexType#BIT} or {@link
+     *     BitmapIndexType#BYTE}.
+     * @return The position of the first occurrence from the <code>start</code> to the <code>end
+     *     </code> offsets of the <code>bit</code> in the binary value of the string held at <code>key
+     *     </code>. If <code>bit</code> is not found, a <code>-1</code> is returned.
+     * @example
+     *     <pre>{@code
+     * Long payload = client.bitpos("myKey1", 1, 4, 6, BIT).get();
+     * // Indicates that the first occurrence of a 1 bit value starting from the fifth to seventh
+     * // bits is the sixth bit of the binary value of the string stored at "myKey1".
+     * assert payload == 5L;
+     * }</pre>
+     */
+    CompletableFuture<Long> bitpos(
+            String key, long bit, long start, long end, BitmapIndexType offsetType);
 }
