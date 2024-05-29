f83e519702bad3d0f1b08761bc608037d4f562f3
tjzhang-BQ <111323543+tjzhang-BQ@users.noreply.github.com>
2024-05-29 15:53:20 -0700

    Java: Adding command `LMove` (#1491)
    * Adding command `LMove` (#319)
    
    * Adding command LMove
    
    ---------
    
    Co-authored-by: TJ Zhang <tj.zhang@improving.com>
    
    * addressing review comments
    
    * undo renaming of the lmpop arguments
    
    * spotless
    
    ---------
    
    Co-authored-by: TJ Zhang <tj.zhang@improving.com>

--- java/integTest/src/test/java/glide/SharedCommandTests.java
+++ java/integTest/src/test/java/glide/SharedCommandTests.java
@@ -33,7 +33,7 @@ import glide.api.RedisClusterClient;
 import glide.api.models.Script;
 import glide.api.models.commands.ConditionalChange;
 import glide.api.models.commands.ExpireOptions;
-import glide.api.models.commands.PopDirection;
+import glide.api.models.commands.ListDirection;
 import glide.api.models.commands.RangeOptions.InfLexBound;
 import glide.api.models.commands.RangeOptions.InfScoreBound;
 import glide.api.models.commands.RangeOptions.LexBoundary;
@@ -4141,19 +4141,19 @@ public class SharedCommandTests {
         Map<String, String[]> expected2 = Map.of(key2, new String[] {"one", "two"});
 
         // nothing to be popped
-        assertNull(client.lmpop(singleKeyArray, PopDirection.LEFT).get());
-        assertNull(client.lmpop(singleKeyArray, PopDirection.LEFT, count).get());
+        assertNull(client.lmpop(singleKeyArray, ListDirection.LEFT).get());
+        assertNull(client.lmpop(singleKeyArray, ListDirection.LEFT, count).get());
 
         // pushing to the arrays to be popped
         assertEquals(arraySize, client.lpush(key1, lpushArgs).get());
         assertEquals(arraySize, client.lpush(key2, lpushArgs).get());
 
         // assert correct result from popping
-        Map<String, String[]> result = client.lmpop(singleKeyArray, PopDirection.LEFT).get();
+        Map<String, String[]> result = client.lmpop(singleKeyArray, ListDirection.LEFT).get();
         assertDeepEquals(result, expected);
 
         // assert popping multiple elements from the right
-        Map<String, String[]> result2 = client.lmpop(multiKeyArray, PopDirection.RIGHT, 2L).get();
+        Map<String, String[]> result2 = client.lmpop(multiKeyArray, ListDirection.RIGHT, 2L).get();
         assertDeepEquals(result2, expected2);
 
         // key exists but is not a list type key
@@ -4161,7 +4161,7 @@ public class SharedCommandTests {
         ExecutionException executionException =
                 assertThrows(
                         ExecutionException.class,
-                        () -> client.lmpop(new String[] {nonListKey}, PopDirection.LEFT).get());
+                        () -> client.lmpop(new String[] {nonListKey}, ListDirection.LEFT).get());
         assertInstanceOf(RequestException.class, executionException.getCause());
     }
 
@@ -4183,19 +4183,20 @@ public class SharedCommandTests {
         Map<String, String[]> expected2 = Map.of(key2, new String[] {"one", "two"});
 
         // nothing to be popped
-        assertNull(client.blmpop(singleKeyArray, PopDirection.LEFT, 0.1).get());
-        assertNull(client.blmpop(singleKeyArray, PopDirection.LEFT, count, 0.1).get());
+        assertNull(client.blmpop(singleKeyArray, ListDirection.LEFT, 0.1).get());
+        assertNull(client.blmpop(singleKeyArray, ListDirection.LEFT, count, 0.1).get());
 
         // pushing to the arrays to be popped
         assertEquals(arraySize, client.lpush(key1, lpushArgs).get());
         assertEquals(arraySize, client.lpush(key2, lpushArgs).get());
 
         // assert correct result from popping
-        Map<String, String[]> result = client.blmpop(singleKeyArray, PopDirection.LEFT, 0.1).get();
+        Map<String, String[]> result = client.blmpop(singleKeyArray, ListDirection.LEFT, 0.1).get();
         assertDeepEquals(result, expected);
 
         // assert popping multiple elements from the right
-        Map<String, String[]> result2 = client.blmpop(multiKeyArray, PopDirection.RIGHT, 2L, 0.1).get();
+        Map<String, String[]> result2 =
+                client.blmpop(multiKeyArray, ListDirection.RIGHT, 2L, 0.1).get();
         assertDeepEquals(result2, expected2);
 
         // key exists but is not a list type key
@@ -4203,7 +4204,7 @@ public class SharedCommandTests {
         ExecutionException executionException =
                 assertThrows(
                         ExecutionException.class,
-                        () -> client.blmpop(new String[] {nonListKey}, PopDirection.LEFT, 0.1).get());
+                        () -> client.blmpop(new String[] {nonListKey}, ListDirection.LEFT, 0.1).get());
         assertInstanceOf(RequestException.class, executionException.getCause());
     }
 
@@ -4220,14 +4221,16 @@ public class SharedCommandTests {
                         : RedisClusterClient.CreateClient(commonClusterClientConfig().build()).get()) {
 
             // ensure that commands doesn't time out even if timeout > request timeout
-            assertNull(testClient.blmpop(new String[] {key}, PopDirection.LEFT, 1).get());
+            assertNull(testClient.blmpop(new String[] {key}, ListDirection.LEFT, 1).get());
 
             // with 0 timeout (no timeout) should never time out,
             // but we wrap the test with timeout to avoid test failing or stuck forever
             assertThrows(
                     TimeoutException.class, // <- future timeout, not command timeout
                     () ->
-                            testClient.blmpop(new String[] {key}, PopDirection.LEFT, 0).get(3, TimeUnit.SECONDS));
+                            testClient
+                                    .blmpop(new String[] {key}, ListDirection.LEFT, 0)
+                                    .get(3, TimeUnit.SECONDS));
         }
     }
 
@@ -4271,5 +4274,55 @@ public class SharedCommandTests {
         assertEquals(OK, response2);
         String[] updatedList2 = client.lrange(key, 0, -1).get();
         assertArrayEquals(updatedList2, expectedList2);
+    }
+
+    @SneakyThrows
+    @ParameterizedTest(autoCloseArguments = false)
+    @MethodSource("getClients")
+    public void lmove(BaseClient client) {
+        assumeTrue(REDIS_VERSION.isGreaterThanOrEqualTo("6.2.0"), "This feature added in redis 6.2.0");
+        // setup
+        String key1 = "{key}-1" + UUID.randomUUID();
+        String key2 = "{key}-2" + UUID.randomUUID();
+        String nonExistingKey = "{key}-3" + UUID.randomUUID();
+        String nonListKey = "{key}-4" + UUID.randomUUID();
+        String[] lpushArgs1 = {"four", "three", "two", "one"};
+        String[] lpushArgs2 = {"six", "five", "four"};
+
+        // source does not exist or is empty
+        assertNull(client.lmove(key1, key2, ListDirection.LEFT, ListDirection.RIGHT).get());
+
+        // only source exists, only source elements gets popped, creates a list at nonExistingKey
+        assertEquals(lpushArgs1.length, client.lpush(key1, lpushArgs1).get());
+        assertEquals(
+                "four", client.lmove(key1, nonExistingKey, ListDirection.RIGHT, ListDirection.LEFT).get());
+        assertArrayEquals(new String[] {"one", "two", "three"}, client.lrange(key1, 0, -1).get());
+
+        // source and destination are the same, performing list rotation, "three" gets popped and added
+        // back
+        assertEquals("one", client.lmove(key1, key1, ListDirection.LEFT, ListDirection.LEFT).get());
+        assertArrayEquals(new String[] {"one", "two", "three"}, client.lrange(key1, 0, -1).get());
+
+        // normal use case, "three" gets popped and added to the left of destination
+        assertEquals(lpushArgs2.length, client.lpush(key2, lpushArgs2).get());
+        assertEquals("three", client.lmove(key1, key2, ListDirection.RIGHT, ListDirection.LEFT).get());
+        assertArrayEquals(new String[] {"one", "two"}, client.lrange(key1, 0, -1).get());
+        assertArrayEquals(
+                new String[] {"three", "four", "five", "six"}, client.lrange(key2, 0, -1).get());
+
+        // source exists but is not a list type key
+        assertEquals(OK, client.set(nonListKey, "NotAList").get());
+        ExecutionException executionException =
+                assertThrows(
+                        ExecutionException.class,
+                        () -> client.lmove(nonListKey, key1, ListDirection.LEFT, ListDirection.LEFT).get());
+        assertInstanceOf(RequestException.class, executionException.getCause());
+
+        // destination exists but is not a list type key
+        ExecutionException executionException2 =
+                assertThrows(
+                        ExecutionException.class,
+                        () -> client.lmove(key1, nonListKey, ListDirection.LEFT, ListDirection.LEFT).get());
+        assertInstanceOf(RequestException.class, executionException2.getCause());
     }
 }
--- java/integTest/src/test/java/glide/cluster/CommandTests.java
+++ java/integTest/src/test/java/glide/cluster/CommandTests.java
@@ -36,7 +36,7 @@ import glide.api.RedisClusterClient;
 import glide.api.models.ClusterValue;
 import glide.api.models.commands.FlushMode;
 import glide.api.models.commands.InfoOptions;
-import glide.api.models.commands.PopDirection;
+import glide.api.models.commands.ListDirection;
 import glide.api.models.commands.RangeOptions.RangeByIndex;
 import glide.api.models.commands.WeightAggregateOptions.KeyArray;
 import glide.api.models.commands.bitmap.BitwiseOperation;
@@ -698,7 +698,7 @@ public class CommandTests {
                 Arguments.of(
                         "lmpop",
                         "7.0.0",
-                        clusterClient.lmpop(new String[] {"abc", "def"}, PopDirection.LEFT, 1L)),
+                        clusterClient.lmpop(new String[] {"abc", "def"}, ListDirection.LEFT, 1L)),
                 Arguments.of(
                         "bitop",
                         null,
@@ -706,7 +706,11 @@ public class CommandTests {
                 Arguments.of(
                         "blmpop",
                         "7.0.0",
-                        clusterClient.blmpop(new String[] {"abc", "def"}, PopDirection.LEFT, 1L, 0.1)));
+                        clusterClient.blmpop(new String[] {"abc", "def"}, ListDirection.LEFT, 1L, 0.1)),
+                Arguments.of(
+                        "lmove",
+                        "6.2.0",
+                        clusterClient.lmove("abc", "def", ListDirection.LEFT, ListDirection.LEFT)));
     }
 
     @SneakyThrows
