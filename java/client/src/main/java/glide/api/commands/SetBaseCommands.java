89aa3739c544375ed26562ed4907aeff5303c095
tjzhang-BQ <111323543+tjzhang-BQ@users.noreply.github.com>
2024-05-31 15:18:16 -0700

    Java: Adding command `SRandMember` (#1504)
    * Java: Adding command SRandMember
    
    ---------
    
    Co-authored-by: TJ Zhang <tj.zhang@improving.com>

--- java/client/src/main/java/glide/api/commands/SetBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/SetBaseCommands.java
@@ -221,4 +221,38 @@ public interface SetBaseCommands {
      * }</pre>
      */
     CompletableFuture<Long> sunionstore(String destination, String[] keys);
+
+    /**
+     * Returns a random element from the set value stored at <code>key</code>.
+     *
+     * @see <a href="https://redis.io/commands/srandmember/">redis.io</a> for details.
+     * @param key The key from which to retrieve the set member.
+     * @return A random element from the set, or <code>null</code> if <code>key</code> does not exist.
+     * @example
+     *     <pre>{@code
+     * client.sadd("test", new String[] {"one"}).get();
+     * String response = client.srandmember("test").get();
+     * assertEquals("one", response);
+     * }</pre>
+     */
+    CompletableFuture<String> srandmember(String key);
+
+    /**
+     * Returns one or more random elements from the set value stored at <code>key</code>.
+     *
+     * @see <a href="https://redis.io/commands/srandmember/">redis.io</a> for details.
+     * @param key The key from which to retrieve the set members.
+     * @param count The number of elements to return.<br>
+     *     If <code>count</code> is positive, returns unique elements.<br>
+     *     If negative, allows for duplicates.<br>
+     * @return An <code>array</code> of elements from the set, or an empty <code>array</code> if
+     *     <code>key</code> does not exist.
+     * @example
+     *     <pre>{@code
+     * client.sadd("test", new String[] {"one"}).get();
+     * String[] response = client.srandmember("test", -2).get();
+     * assertArrayEquals(new String[] {"one", "one"}, response);
+     * }</pre>
+     */
+    CompletableFuture<String[]> srandmember(String key, long count);
 }
