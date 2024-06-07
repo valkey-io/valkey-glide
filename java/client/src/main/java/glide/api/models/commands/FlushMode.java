e54ba891687b947830c2c93c3118a23648526432
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-06-07 11:56:04 -0700

    Java: Add `FUNCTION FLUSH` command. (#1533)
    * java: Add `FUNCTION FLUSH` command. (#288)
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/models/commands/FlushMode.java
+++ java/client/src/main/java/glide/api/models/commands/FlushMode.java
@@ -4,17 +4,27 @@ package glide.api.models.commands;
 import glide.api.RedisClient;
 import glide.api.RedisClusterClient;
 import glide.api.models.configuration.RequestRoutingConfiguration.Route;
+import glide.api.models.configuration.RequestRoutingConfiguration.SingleNodeRoute;
 
+// TODO add links to script flush
 /**
- * Defines flushing mode for <code>FLUSHALL</code> command implemented by {@link
- * RedisClient#flushall(FlushMode)}, {@link RedisClusterClient#flushall(FlushMode)}, and {@link
- * RedisClusterClient#flushall(FlushMode, Route)}.
+ * Defines flushing mode for:
  *
- * @see <a href="https://valkey.io/commands/flushall/">valkey.io</a>
+ * <ul>
+ *   <li><code>FLUSHALL</code> command implemented by {@link RedisClient#flushall(FlushMode)},
+ *       {@link RedisClusterClient#flushall(FlushMode)}, and {@link
+ *       RedisClusterClient#flushall(FlushMode, SingleNodeRoute)}.
+ *   <li><code>FUNCTION FLUSH</code> command implemented by {@link
+ *       RedisClient#functionFlush(FlushMode)}, {@link RedisClusterClient#functionFlush(FlushMode)},
+ *       and {@link RedisClusterClient#functionFlush(FlushMode, Route)}.
+ * </ul>
+ *
+ * @see <a href="https://valkey.io/commands/flushall/">valkey.io</a> and <a
+ *     href="https://valkey.io/commands/function-flush/">valkey.io</a>
  */
 public enum FlushMode {
-    /** Flushes the databases synchronously. */
+    /** Flushes synchronously. */
     SYNC,
-    /** Flushes the databases asynchronously. */
+    /** Flushes asynchronously. */
     ASYNC
 }
