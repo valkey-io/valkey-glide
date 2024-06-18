a4cfa510cb1c7c142d583ad12a3680cf549c66ea
ikolomi <ikolomin@amazon.com>
2024-06-18 22:13:07 +0300

    Pubsub implementation in glide-core and with Python wrapper. Works both in standalone and cluster modes. Pubsub configuration is provided in client creation params.

--- glide-core/tests/test_standalone_client.rs
+++ glide-core/tests/test_standalone_client.rs
@@ -199,7 +199,7 @@ mod standalone_client_tests {
         connection_request.read_from = config.read_from.into();
 
         block_on_all(async {
-            let mut client = StandaloneClient::create_client(connection_request.into())
+            let mut client = StandaloneClient::create_client(connection_request.into(), None)
                 .await
                 .unwrap();
             for mock in mocks.drain(1..config.number_of_replicas_dropped_after_connection + 1) {
@@ -305,7 +305,7 @@ mod standalone_client_tests {
         let connection_request =
             create_connection_request(addresses.as_slice(), &Default::default());
         block_on_all(async {
-            let client_res = StandaloneClient::create_client(connection_request.into())
+            let client_res = StandaloneClient::create_client(connection_request.into(), None)
                 .await
                 .map_err(ConnectionError::Standalone);
             assert!(client_res.is_err());
@@ -344,7 +344,7 @@ mod standalone_client_tests {
             create_connection_request(addresses.as_slice(), &Default::default());
 
         block_on_all(async {
-            let mut client = StandaloneClient::create_client(connection_request.into())
+            let mut client = StandaloneClient::create_client(connection_request.into(), None)
                 .await
                 .unwrap();
 
