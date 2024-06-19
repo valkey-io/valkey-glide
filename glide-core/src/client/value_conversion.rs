04a163ef8e446469ae26a4d9883ac2f090e13c6c
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-06-18 18:20:01 -0700

    Java: Add commands `XGROUP CREATECONSUMER` and `XGROUP DELCONSUMER` (#1599)
    * Java: Add commands `XGROUP CREATECONSUMER` and `XGROUP DELCONSUMER` (#360)
    
    * Add XGROUP CreateConsumer, DelConsumer
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update Xgroup createconsumer docs
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Fix doc
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- glide-core/src/client/value_conversion.rs
+++ glide-core/src/client/value_conversion.rs
@@ -860,9 +860,21 @@ pub(crate) fn expected_type_for_cmd(cmd: &Cmd) -> Option<ExpectedReturnType> {
             }),
         }),
         b"INCRBYFLOAT" | b"HINCRBYFLOAT" | b"ZINCRBY" => Some(ExpectedReturnType::Double),
-        b"HEXISTS" | b"HSETNX" | b"EXPIRE" | b"EXPIREAT" | b"PEXPIRE" | b"PEXPIREAT"
-        | b"SISMEMBER" | b"PERSIST" | b"SMOVE" | b"RENAMENX" | b"MOVE" | b"COPY"
-        | b"XGROUP DESTROY" | b"MSETNX" => Some(ExpectedReturnType::Boolean),
+        b"HEXISTS"
+        | b"HSETNX"
+        | b"EXPIRE"
+        | b"EXPIREAT"
+        | b"PEXPIRE"
+        | b"PEXPIREAT"
+        | b"SISMEMBER"
+        | b"PERSIST"
+        | b"SMOVE"
+        | b"RENAMENX"
+        | b"MOVE"
+        | b"COPY"
+        | b"MSETNX"
+        | b"XGROUP DESTROY"
+        | b"XGROUP CREATECONSUMER" => Some(ExpectedReturnType::Boolean),
         b"SMISMEMBER" => Some(ExpectedReturnType::ArrayOfBools),
         b"SMEMBERS" | b"SINTER" | b"SDIFF" | b"SUNION" => Some(ExpectedReturnType::Set),
         b"ZSCORE" | b"GEODIST" => Some(ExpectedReturnType::DoubleOrNull),
