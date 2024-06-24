d6c3ba684771a89e80ceee1e8738e8bf22cecc12
Alon Arenberg <93711356+alon-arenberg@users.noreply.github.com>
2024-06-24 14:32:02 +0300

    support bitcount with GlideString (#1627)
    support bitcount with GlideString

--- java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/BitmapBaseCommands.java
@@ -4,6 +4,7 @@ package glide.api.commands;
 import static glide.api.models.commands.bitmap.BitFieldOptions.BitFieldReadOnlySubCommands;
 import static glide.api.models.commands.bitmap.BitFieldOptions.BitFieldSubCommands;
 
+import glide.api.models.GlideString;
 import glide.api.models.commands.bitmap.BitFieldOptions.BitFieldGet;
 import glide.api.models.commands.bitmap.BitFieldOptions.BitFieldIncrby;
 import glide.api.models.commands.bitmap.BitFieldOptions.BitFieldOverflow;
@@ -38,6 +39,21 @@ public interface BitmapBaseCommands {
     CompletableFuture<Long> bitcount(String key);
 
     /**
+     * Counts the number of set bits (population counting) in a string stored at <code>key</code>.
+     *
+     * @see <a href="https://valkey.io/commands/bitcount/">valkey.io</a> for details.
+     * @param key The key for the string to count the set bits of.
+     * @return The number of set bits in the string. Returns zero if the key is missing as it is
+     *     treated as an empty string.
+     * @example
+     *     <pre>{@code
+     * Long payload = client.bitcount(gs("myKey1")).get();
+     * assert payload == 2L; // The string stored at "myKey1" contains 2 set bits.
+     * }</pre>
+     */
+    CompletableFuture<Long> bitcount(GlideString key);
+
+    /**
      * Counts the number of set bits (population counting) in a string stored at <code>key</code>. The
      * offsets <code>start</code> and <code>end</code> are zero-based indexes, with <code>0</code>
      * being the first element of the list, <code>1</code> being the next element and so on. These
@@ -67,6 +83,28 @@ public interface BitmapBaseCommands {
      * <code>-1</code> being the last element of the list, <code>-2</code> being the penultimate, and
      * so on.
      *
+     * @see <a href="https://valkey.io/commands/bitcount/">valkey.io</a> for details.
+     * @param key The key for the string to count the set bits of.
+     * @param start The starting offset byte index.
+     * @param end The ending offset byte index.
+     * @return The number of set bits in the string byte interval specified by <code>start</code> and
+     *     <code>end</code>. Returns zero if the key is missing as it is treated as an empty string.
+     * @example
+     *     <pre>{@code
+     * Long payload = client.bitcount(gs("myKey1"), 1, 3).get();
+     * assert payload == 2L; // The second to fourth bytes of the string stored at "myKey1" contains 2 set bits.
+     * }</pre>
+     */
+    CompletableFuture<Long> bitcount(GlideString key, long start, long end);
+
+    /**
+     * Counts the number of set bits (population counting) in a string stored at <code>key</code>. The
+     * offsets <code>start</code> and <code>end</code> are zero-based indexes, with <code>0</code>
+     * being the first element of the list, <code>1</code> being the next element and so on. These
+     * offsets can also be negative numbers indicating offsets starting at the end of the list, with
+     * <code>-1</code> being the last element of the list, <code>-2</code> being the penultimate, and
+     * so on.
+     *
      * @since Redis 7.0 and above
      * @see <a href="https://valkey.io/commands/bitcount/">valkey.io</a> for details.
      * @param key The key for the string to count the set bits of.
@@ -84,6 +122,32 @@ public interface BitmapBaseCommands {
      * }</pre>
      */
     CompletableFuture<Long> bitcount(String key, long start, long end, BitmapIndexType options);
+
+    /**
+     * Counts the number of set bits (population counting) in a string stored at <code>key</code>. The
+     * offsets <code>start</code> and <code>end</code> are zero-based indexes, with <code>0</code>
+     * being the first element of the list, <code>1</code> being the next element and so on. These
+     * offsets can also be negative numbers indicating offsets starting at the end of the list, with
+     * <code>-1</code> being the last element of the list, <code>-2</code> being the penultimate, and
+     * so on.
+     *
+     * @since Redis 7.0 and above
+     * @see <a href="https://valkey.io/commands/bitcount/">valkey.io</a> for details.
+     * @param key The key for the string to count the set bits of.
+     * @param start The starting offset.
+     * @param end The ending offset.
+     * @param options The index offset type. Could be either {@link BitmapIndexType#BIT} or {@link
+     *     BitmapIndexType#BYTE}.
+     * @return The number of set bits in the string interval specified by <code>start</code>, <code>
+     *     end</code>, and <code>options</code>. Returns zero if the key is missing as it is treated
+     *     as an empty string.
+     * @example
+     *     <pre>{@code
+     * Long payload = client.bitcount(gs("myKey1"), 1, 1, BIT).get();
+     * assert payload == 1L; // Indicates that the second bit of the string stored at "myKey1" is set.
+     * }</pre>
+     */
+    CompletableFuture<Long> bitcount(GlideString key, long start, long end, BitmapIndexType options);
 
     /**
      * Sets or clears the bit at <code>offset</code> in the string value stored at <code>key</code>.
