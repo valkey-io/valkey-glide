1a32c686e30e74168e9b8409d9d00fd40338fd64
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-06-27 11:08:19 -0700

    Java: Update some Stream docs (#1682)
    * Update some Stream docs
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update some Stream docs
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Doc updates for review comments
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update test comments
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/models/BaseTransaction.java
+++ java/client/src/main/java/glide/api/models/BaseTransaction.java
@@ -3051,7 +3051,7 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
      *
      * @see <a href="https://valkey.io/commands/xgroup-destroy/">valkey.io</a> for details.
      * @param key The key of the stream.
-     * @param groupname The newly created consumer group name.
+     * @param groupname The consumer group name to delete.
      * @return Command Response - <code>true</code> if the consumer group is destroyed. Otherwise,
      *     <code>false</code>.
      */
@@ -3084,7 +3084,7 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
      * @see <a href="https://valkey.io/commands/xgroup-delconsumer/">valkey.io</a> for details.
      * @param key The key of the stream.
      * @param group The consumer group name.
-     * @param consumer The newly created consumer.
+     * @param consumer The consumer to delete.
      * @return Command Response - The number of pending messages the <code>consumer</code> had before
      *     it was deleted.
      */
@@ -3105,12 +3105,11 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
      *     will be read. Use the special id of <code>{@literal Map<String, Map<String, String[][]>>}
      *     </code> to receive only new messages.
      * @param group The consumer group name.
-     * @param consumer The newly created consumer.
+     * @param consumer The consumer name.
      * @return Command Response - A <code>{@literal Map<String, Map<String, String[][]>>}</code> with
      *     stream keys, to <code>Map</code> of stream-ids, to an array of pairings with format <code>
      *     [[field, entry], [field, entry], ...]<code>.
-     *     Returns <code>null</code> if the consumer group does not exist. Returns a <code>Map</code>
-     *     with a value of code>null</code> if the stream is empty.
+     *     Returns <code>null</code> if there is no stream that can be served.
      */
     public T xreadgroup(
             @NonNull Map<String, String> keysAndIds, @NonNull String group, @NonNull String consumer) {
@@ -3128,13 +3127,12 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
      *     will be read. Use the special id of <code>{@literal Map<String, Map<String, String[][]>>}
      *     </code> to receive only new messages.
      * @param group The consumer group name.
-     * @param consumer The newly created consumer.
+     * @param consumer The consumer name.
      * @param options Options detailing how to read the stream {@link StreamReadGroupOptions}.
      * @return Command Response - A <code>{@literal Map<String, Map<String, String[][]>>}</code> with
      *     stream keys, to <code>Map</code> of stream-ids, to an array of pairings with format <code>
      *     [[field, entry], [field, entry], ...]<code>.
-     *     Returns <code>null</code> if the consumer group does not exist. Returns a <code>Map</code>
-     *     with a value of code>null</code> if the stream is empty.
+     *     Returns <code>null</code> if the {@link StreamReadGroupOptions#block} option is given and a timeout occurs, or if there is no stream that can be served.
      */
     public T xreadgroup(
             @NonNull Map<String, String> keysAndIds,
