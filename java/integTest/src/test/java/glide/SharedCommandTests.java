b908a0b7438a8166bbfd18348344bac6ca3a80df
James Duong <james.duong@improving.com>
2024-06-28 00:04:36 -0700

    Experiment with unsigned 64-bit cursors

--- java/integTest/src/test/java/glide/SharedCommandTests.java
+++ java/integTest/src/test/java/glide/SharedCommandTests.java
@@ -7766,9 +7766,6 @@ public class SharedCommandTests {
 
         // Result contains the whole set
         assertEquals(charMembers.length, client.zadd(key1, charMap).get());
-        // Sleep after zadd() for eventual consistency.
-        // TODO: Replace sleep with WAIT request to enforce strong consistency.
-        Thread.sleep(4200);
         result = client.zscan(key1, initialCursor).get();
         assertEquals(initialCursor, result[resultCursorIndex]);
         assertEquals(
