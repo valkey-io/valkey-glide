e1bed7cbfceef887d3d317491fbaaa3ed5ce82cb
James Duong <james.duong@improving.com>
2024-06-27 23:10:10 -0700

    Longer sleeps

--- java/integTest/src/test/java/glide/SharedCommandTests.java
+++ java/integTest/src/test/java/glide/SharedCommandTests.java
@@ -7768,10 +7768,7 @@ public class SharedCommandTests {
         assertEquals(charMembers.length, client.zadd(key1, charMap).get());
         // Sleep after zadd() for eventual consistency.
         // TODO: Replace sleep with WAIT request to enforce strong consistency.
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-        }
+        Thread.sleep(5000);
         result = client.zscan(key1, initialCursor).get();
         assertEquals(initialCursor, result[resultCursorIndex]);
         assertEquals(
