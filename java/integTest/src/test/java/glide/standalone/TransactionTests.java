abc209fb1cdb8536301ee3bb072f0035c407a588
James Duong <james.duong@improving.com>
2024-06-28 02:36:39 -0700

    Add large value tests

--- java/integTest/src/test/java/glide/standalone/TransactionTests.java
+++ java/integTest/src/test/java/glide/standalone/TransactionTests.java
@@ -119,6 +119,10 @@ public class TransactionTests {
 
     @SneakyThrows
     @Test
+    public void test_transaction_large_values() {}
+
+    @SneakyThrows
+    @Test
     public void test_standalone_transaction() {
         String key = UUID.randomUUID().toString();
         String value = UUID.randomUUID().toString();
