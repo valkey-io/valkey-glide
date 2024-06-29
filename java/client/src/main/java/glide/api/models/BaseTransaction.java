f677c605f8c65ad20cf512e2a215655274d59fdb
James Duong <duong.james@gmail.com>
2024-06-29 10:24:27 -0700

    Update java/client/src/main/java/glide/api/models/BaseTransaction.java
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/models/BaseTransaction.java
+++ java/client/src/main/java/glide/api/models/BaseTransaction.java
@@ -5543,7 +5543,7 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
      * Iterates incrementally over a sorted set.
      *
      * @see <a href="https://valkey.io/commands/zscan">valkey.io</a> for details.
-     * @param key The key of the set.
+     * @param key The key of the sorted set.
      * @param cursor The cursor that points to the next iteration of results.
      * @return Command Response - An <code>Array</code> of <code>Objects</code>. The first element is
      *     always the <code>cursor</code> for the next iteration of results. <code>0</code> will be
