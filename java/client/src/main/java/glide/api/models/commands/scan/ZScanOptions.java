0d0f5a464ca057fc58b792641ffb05bfe503582a
James Duong <duong.james@gmail.com>
2024-06-29 11:47:56 -0700

    Java: Add SSCAN and ZSCAN commands (#1705)
    * Java: Add `SSCAN` command (#394)
    
    * Add ScanOptions base class for scan-family options.
    * Expose the cursor as a String to support unsigned 64-bit cursor values.
    
    Co-authored-by: James Duong <james.duong@improving.com>
    
    * Java: Add `ZSCAN` command (#397)
    
    ---------
    
    Co-authored-by: James Duong <james.duong@improving.com>
    
    * WIP TODO: support transactions, docs, and more IT
    
    * Added more tests
    
    * Added tests and javadocs
    
    * Improved examples and tests
    
    * Correct use of SScanOptions instead of ScanOptions for SScan
    
    * Remove plumbing for SCAN command
    
    * Sleep after sadd() calls before sscan() calls
    
    Due to eventual consistency
    
    * Change sscan cursor to be a String
    
    Also fix bug in SharedCommandTests
    
    * WIP with todos
    
    # Conflicts:
    #	glide-core/src/protobuf/redis_request.proto
    #	glide-core/src/request_type.rs
    #	java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
    
    * Add ZScan to TransactionTestUtilities
    
    * Spotless cleanup
    
    * Test fixes
    
    * Cleanup test code
    
    * Apply IntelliJ suggestions
    * Use String.valueOf() instead of concatenating empty string
    
    * Added better error info for set comparison failures
    
    * More logging for test failures
    
    * Add sleeps after zadd() calls
    
    To help make sure data is consistent without WAIT
    
    * Longer sleeps
    
    * Reduce wait time
    
    * Experiment with unsigned 64-bit cursors
    
    * Fix rebase error
    
    * WIP TODO: support transactions, docs, and more IT
    
    * Added more tests
    
    * Added tests and javadocs
    
    * Improved examples and tests
    
    * Apply PR comments
    
    * Fix method ordering in BaseTransaction
    * Fix broken line breaks within code tags in ScanOptions
    * More thoroughly test results in SharedCommandTests
    
    * Add better logging for set comparisons
    
    * Spotless
    
    * Sleep after sadd() calls before sscan() calls
    
    Due to eventual consistency
    
    * Change sscan cursor to be a String
    
    Also fix bug in SharedCommandTests
    
    * Update java/integTest/src/test/java/glide/SharedCommandTests.java
    
    Co-authored-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Update java/integTest/src/test/java/glide/SharedCommandTests.java
    
    Co-authored-by: Guian Gumpac <guian.gumpac@improving.com>
    
    * Fix rebase conflicts
    
    * Fix another rebase conflict
    
    * Spotless
    
    * Update java/client/src/main/java/glide/api/models/BaseTransaction.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update java/client/src/main/java/glide/api/models/BaseTransaction.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update java/client/src/main/java/glide/api/models/BaseTransaction.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update java/client/src/main/java/glide/api/models/BaseTransaction.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Correctly use constants in TransactionTests
    
    * Rename ScanOptions to BaseScanOptions
    
    * Doc PR fixes
    
    * Treat end of cursor as failure
    
    * Spotless
    
    * Fixes
    
    * Update java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Update java/client/src/main/java/glide/api/commands/SortedSetBaseCommands.java
    
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>
    
    * Minor doc changes
    
    ---------
    
    Co-authored-by: Guian Gumpac <guian.gumpac@improving.com>
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

---
+++ java/client/src/main/java/glide/api/models/commands/scan/ZScanOptions.java
@@ -0,0 +1,13 @@
+/** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
+package glide.api.models.commands.scan;
+
+import glide.api.commands.SortedSetBaseCommands;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Optional arguments for {@link SortedSetBaseCommands#zscan(String, String, ZScanOptions)}.
+ *
+ * @see <a href="https://valkey.io/commands/zscan/">valkey.io</a>
+ */
+@SuperBuilder
+public class ZScanOptions extends BaseScanOptions {}
