241ef45b890f3f9cceb7ce460f4d36c913d0e867
James Duong <james.duong@improving.com>
2024-06-29 06:20:19 -0700

    Rename ScanOptions to BaseScanOptions

--- java/client/src/main/java/glide/api/models/commands/scan/HScanOptions.java
+++ java/client/src/main/java/glide/api/models/commands/scan/HScanOptions.java
@@ -10,4 +10,4 @@ import lombok.experimental.SuperBuilder;
  * @see <a href="https://valkey.io/commands/hscan/">valkey.io</a>
  */
 @SuperBuilder
-public class HScanOptions extends ScanOptions {}
+public class HScanOptions extends BaseScanOptions {}
