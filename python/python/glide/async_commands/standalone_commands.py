cbb6294d2162c08a6dd1a7923f593814479d6a6c
tjzhang-BQ <111323543+tjzhang-BQ@users.noreply.github.com>
2024-06-30 15:47:26 -0700

    Python: command WAIT (#1710)
    * Python: command WAIT
    
    * Python: command WAIT
    
    * changelog
    
    * doc & test changes
    
    * linter
    
    * doc update
    
    * update example
    
    * addressing commends rd2
    
    ---------
    
    Co-authored-by: TJ Zhang <tj.zhang@improving.com>

--- python/python/glide/async_commands/standalone_commands.py
+++ python/python/glide/async_commands/standalone_commands.py
@@ -665,3 +665,33 @@ class StandaloneCommands(CoreCommands):
             Optional[str],
             await self._execute_command(RequestType.RandomKey, []),
         )
+
+    async def wait(
+        self,
+        numreplicas: int,
+        timeout: int,
+    ) -> int:
+        """
+        Blocks the current client until all the previous write commands are successfully transferred
+        and acknowledged by at least `numreplicas` of replicas. If `timeout` is
+        reached, the command returns even if the specified number of replicas were not yet reached.
+
+        See https://valkey.io/commands/wait for more details.
+
+        Args:
+            numreplicas (int): The number of replicas to reach.
+            timeout (int): The timeout value specified in milliseconds. A value of 0 will block indefinitely.
+
+        Returns:
+            int: The number of replicas reached by all the writes performed in the context of the current connection.
+
+        Examples:
+            >>> await client.set("key", "value");
+            >>> await client.wait(1, 1000);
+            // return 1 when a replica is reached or 0 if 1000ms is reached.
+        """
+        args = [str(numreplicas), str(timeout)]
+        return cast(
+            int,
+            await self._execute_command(RequestType.Wait, args),
+        )
