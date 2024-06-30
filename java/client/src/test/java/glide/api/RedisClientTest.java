2af5d1bb16507b531ae18e03bd85f71937345682
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-06-30 15:27:47 -0700

    Fix merge conflicts
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/test/java/glide/api/RedisClientTest.java
+++ java/client/src/test/java/glide/api/RedisClientTest.java
@@ -1894,29 +1894,6 @@ public class RedisClientTest {
 
     @SneakyThrows
     @Test
-    public void hget_binary_success() {
-        // setup
-        GlideString key = gs("testKey");
-        GlideString field = gs("field");
-        GlideString[] args = new GlideString[] {key, field};
-        GlideString value = gs("value");
-
-        CompletableFuture<GlideString> testResponse = new CompletableFuture<>();
-        testResponse.complete(value);
-        when(commandManager.<GlideString>submitNewCommand(eq(HGet), eq(args), any()))
-                .thenReturn(testResponse);
-
-        // exercise
-        CompletableFuture<GlideString> response = service.hget(key, field);
-        GlideString payload = response.get();
-
-        // verify
-        assertEquals(testResponse, response);
-        assertEquals(value, payload);
-    }
-
-    @SneakyThrows
-    @Test
     public void hset_success() {
         // setup
         String key = "testKey";
@@ -1924,31 +1901,6 @@ public class RedisClientTest {
         fieldValueMap.put("field1", "value1");
         fieldValueMap.put("field2", "value2");
         String[] args = new String[] {key, "field1", "value1", "field2", "value2"};
-        Long value = 2L;
-
-        CompletableFuture<Long> testResponse = new CompletableFuture<>();
-        testResponse.complete(value);
-        when(commandManager.<Long>submitNewCommand(eq(HSet), eq(args), any())).thenReturn(testResponse);
-
-        // exercise
-        CompletableFuture<Long> response = service.hset(key, fieldValueMap);
-        Long payload = response.get();
-
-        // verify
-        assertEquals(testResponse, response);
-        assertEquals(value, payload);
-    }
-
-    @SneakyThrows
-    @Test
-    public void hset_binary_success() {
-        // setup
-        GlideString key = gs("testKey");
-        Map<GlideString, GlideString> fieldValueMap = new LinkedHashMap<>();
-        fieldValueMap.put(gs("field1"), gs("value1"));
-        fieldValueMap.put(gs("field2"), gs("value2"));
-        GlideString[] args =
-                new GlideString[] {key, gs("field1"), gs("value1"), gs("field2"), gs("value2")};
         Long value = 2L;
 
         CompletableFuture<Long> testResponse = new CompletableFuture<>();
@@ -2069,29 +2021,6 @@ public class RedisClientTest {
         // setup
         String key = "testKey";
         String[] args = {key};
-        Long value = 2L;
-
-        CompletableFuture<Long> testResponse = new CompletableFuture<>();
-        testResponse.complete(value);
-
-        // match on protobuf request
-        when(commandManager.<Long>submitNewCommand(eq(HLen), eq(args), any())).thenReturn(testResponse);
-
-        // exercise
-        CompletableFuture<Long> response = service.hlen(key);
-        Long payload = response.get();
-
-        // verify
-        assertEquals(testResponse, response);
-        assertEquals(value, payload);
-    }
-
-    @SneakyThrows
-    @Test
-    public void hlen_binary_success() {
-        // setup
-        GlideString key = gs("testKey");
-        GlideString[] args = {key};
         Long value = 2L;
 
         CompletableFuture<Long> testResponse = new CompletableFuture<>();
@@ -2755,31 +2684,6 @@ public class RedisClientTest {
 
     @SneakyThrows
     @Test
-    public void lindex_binary_returns_success() {
-        // setup
-        GlideString key = gs("testKey");
-        long index = 2;
-        GlideString[] args = new GlideString[] {key, gs(Long.toString(index))};
-        GlideString value = gs("value");
-
-        CompletableFuture<GlideString> testResponse = new CompletableFuture<>();
-        testResponse.complete(value);
-
-        // match on protobuf request
-        when(commandManager.<GlideString>submitNewCommand(eq(LIndex), eq(args), any()))
-                .thenReturn(testResponse);
-
-        // exercise
-        CompletableFuture<GlideString> response = service.lindex(key, index);
-        GlideString payload = response.get();
-
-        // verify
-        assertEquals(testResponse, response);
-        assertEquals(value, payload);
-    }
-
-    @SneakyThrows
-    @Test
     public void ltrim_returns_success() {
         // setup
         String key = "testKey";
@@ -3270,31 +3174,6 @@ public class RedisClientTest {
         String key = "testKey";
         String[] members = {"1", "2"};
         String[] arguments = {"testKey", "1", "2"};
-        Boolean[] value = {true, false};
-
-        CompletableFuture<Boolean[]> testResponse = new CompletableFuture<>();
-        testResponse.complete(value);
-
-        // match on protobuf request
-        when(commandManager.<Boolean[]>submitNewCommand(eq(SMIsMember), eq(arguments), any()))
-                .thenReturn(testResponse);
-
-        // exercise
-        CompletableFuture<Boolean[]> response = service.smismember(key, members);
-        Boolean[] payload = response.get();
-
-        // verify
-        assertEquals(testResponse, response);
-        assertEquals(value, payload);
-    }
-
-    @SneakyThrows
-    @Test
-    public void smismember_binary_returns_success() {
-        // setup
-        GlideString key = gs("testKey");
-        GlideString[] members = {gs("1"), gs("2")};
-        GlideString[] arguments = {gs("testKey"), gs("1"), gs("2")};
         Boolean[] value = {true, false};
 
         CompletableFuture<Boolean[]> testResponse = new CompletableFuture<>();
@@ -6893,33 +6772,6 @@ public class RedisClientTest {
 
     @SneakyThrows
     @Test
-    public void linsert_binary_returns_success() {
-        // setup
-        GlideString key = gs("testKey");
-        var position = BEFORE;
-        GlideString pivot = gs("pivot");
-        GlideString elem = gs("elem");
-        GlideString[] arguments = new GlideString[] {key, gs(position.toString()), pivot, elem};
-        long value = 42;
-
-        CompletableFuture<Long> testResponse = new CompletableFuture<>();
-        testResponse.complete(value);
-
-        // match on protobuf request
-        when(commandManager.<Long>submitNewCommand(eq(LInsert), eq(arguments), any()))
-                .thenReturn(testResponse);
-
-        // exercise
-        CompletableFuture<Long> response = service.linsert(key, position, pivot, elem);
-        long payload = response.get();
-
-        // verify
-        assertEquals(testResponse, response);
-        assertEquals(value, payload);
-    }
-
-    @SneakyThrows
-    @Test
     public void blpop_returns_success() {
         // setup
         String key = "key";
@@ -8641,34 +8493,6 @@ public class RedisClientTest {
         // exercise
         CompletableFuture<String> response = service.blmove(key1, key2, wherefrom, whereto, 0.1);
         String payload = response.get();
-
-        // verify
-        assertEquals(testResponse, response);
-        assertEquals(value, payload);
-    }
-
-    @SneakyThrows
-    @Test
-    public void blmove_binary_returns_success() {
-        // setup
-        GlideString key1 = gs("testKey");
-        GlideString key2 = gs("testKey2");
-        ListDirection wherefrom = ListDirection.LEFT;
-        ListDirection whereto = ListDirection.RIGHT;
-        GlideString[] arguments =
-                new GlideString[] {key1, key2, gs(wherefrom.toString()), gs(whereto.toString()), gs("0.1")};
-        GlideString value = gs("one");
-
-        CompletableFuture<GlideString> testResponse = new CompletableFuture<>();
-        testResponse.complete(value);
-
-        // match on protobuf request
-        when(commandManager.<GlideString>submitNewCommand(eq(BLMove), eq(arguments), any()))
-                .thenReturn(testResponse);
-
-        // exercise
-        CompletableFuture<GlideString> response = service.blmove(key1, key2, wherefrom, whereto, 0.1);
-        GlideString payload = response.get();
 
         // verify
         assertEquals(testResponse, response);
