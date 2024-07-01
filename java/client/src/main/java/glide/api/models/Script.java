81df50af49c5afb5b17422fbb4e60e3bdb2cfeeb
James Duong <james.duong@improving.com>
2024-06-30 17:10:07 -0700

    Unrelated: avoid double-dropping of scripts
    The same hash could potentially be reused.

--- java/client/src/main/java/glide/api/models/Script.java
+++ java/client/src/main/java/glide/api/models/Script.java
@@ -17,6 +17,7 @@ public class Script implements AutoCloseable {
 
     /** Hash string representing the code. */
     @Getter private final String hash;
+    private boolean isDropped = false;
 
     /** Indicatoin if script invocation output can return binary data. */
     @Getter private final Boolean binarySafeOutput;
@@ -35,7 +36,11 @@ public class Script implements AutoCloseable {
     /** Drop the linked script from glide-rs <code>code</code>. */
     @Override
     public void close() throws Exception {
-        dropScript(hash);
+
+        if (!isDropped) {
+            dropScript(hash);
+            isDropped = true;
+        }
     }
 
     @Override
