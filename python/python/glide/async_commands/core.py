b96716964dd20eaa114c9e536023ee6a5e46d2fe
James Xin <126831592+jamesx-improving@users.noreply.github.com>
2024-06-30 20:58:01 -0700

    Python: add WATCH and UNWATCH commands (#1736)
    * Python: add WATCH and UNWATCH commands
    
    * isort
    
    * update redis_client type for test_unwatch_with_route()
    
    * add watch to the correct cluster test
    
    * add cluster mode note
    
    * address comments from GumpacG
    
    * Python: added XAUTOCLAIM command (#1718)
    
    * Python: add XAUTOCLAIM command
    
    * minor doc update
    
    * Minor doc update
    
    * PR suggestions
    
    * Update test assertions with string conversions
    
    * PR suggestions
    
    * Add clarifying comments
    
    * Python: Add ZSCAN and HSCAN commands (#1732)
    
    * Added zscan command
    
    * Added HSCAN command
    
    * Fix tests
    
    * Fix tests
    
    * Fix tests
    
    * Fix tests
    
    * Fix tests
    
    * Fix tests
    
    * Debug tests
    
    * Debug tests
    
    * Debug tests
    
    * Debug tests
    
    * Debug tests
    
    * Debug tests
    
    * Debug tests
    
    * Debug tests
    
    * Debug tests
    
    * Fixed tests and addressed comments
    
    * Update python/python/tests/test_async_client.py
    
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    ---------
    
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Python: add WATCH and UNWATCH commands
    
    * Python: add WATCH and UNWATCH commands
    
    * add changelog
    
    * fix broken syntax after solving conflicts
    
    ---------
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>
    Co-authored-by: Guian Gumpac <guian.gumpac@improving.com>
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- python/python/glide/async_commands/core.py
+++ python/python/glide/async_commands/core.py
@@ -5743,6 +5743,44 @@ class CoreCommands(Protocol):
             await self._execute_command(RequestType.FCallReadOnly, args),
         )
 
+    async def watch(self, keys: List[str]) -> TOK:
+        """
+        Marks the given keys to be watched for conditional execution of a transaction. Transactions
+        will only execute commands if the watched keys are not modified before execution of the
+        transaction.
+
+        See https://valkey.io/commands/watch for more details.
+
+        Note:
+            When in cluster mode, the command may route to multiple nodes when `keys` map to different hash slots.
+
+        Args:
+            keys (List[str]): The keys to watch.
+
+        Returns:
+            TOK: A simple "OK" response.
+
+        Examples:
+            >>> await client.watch("sampleKey")
+                'OK'
+            >>> transaction.set("sampleKey", "foobar")
+            >>> await redis_client.exec(transaction)
+                'OK' # Executes successfully and keys are unwatched.
+
+            >>> await client.watch("sampleKey")
+                'OK'
+            >>> transaction.set("sampleKey", "foobar")
+            >>> await client.set("sampleKey", "hello world")
+                'OK'
+            >>> await redis_client.exec(transaction)
+                None  # None is returned when the watched key is modified before transaction execution.
+        """
+
+        return cast(
+            TOK,
+            await self._execute_command(RequestType.Watch, keys),
+        )
+
     @dataclass
     class PubSubMsg:
         """
