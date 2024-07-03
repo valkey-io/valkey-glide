f62ba4e460fd1521c91d422a5c0c3407eec25a1a
tjzhang-BQ <111323543+tjzhang-BQ@users.noreply.github.com>
2024-07-03 10:22:08 -0700

    Java: type migration for entries_read (#1767)
    * Java: type migration for entries_read
    
    * Update java/client/src/main/java/glide/api/BaseClient.java
    
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * small fix variable name
    
    * renaming argument for xgroupSetId
    
    * changing to using primitive long and applying checkstyle
    
    * Update java/client/src/main/java/glide/api/models/BaseTransaction.java
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>
    
    * change entriesread type for BaseTransaction
    
    ---------
    
    Co-authored-by: TJ Zhang <tj.zhang@improving.com>
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>

--- java/client/src/main/java/glide/api/commands/StreamBaseCommands.java
+++ java/client/src/main/java/glide/api/commands/StreamBaseCommands.java
@@ -682,9 +682,7 @@ public interface StreamBaseCommands {
      * @param groupName The consumer group name.
      * @param id The stream entry ID that should be set as the last delivered ID for the consumer
      *     group.
-     * @param entriesReadId An arbitrary ID (that isn't the first ID, last ID, or the zero ID (<code>
-     *     "0-0"</code>)) used to find out how many entries are between the arbitrary ID (excluding
-     *     it) and the stream's last entry.
+     * @param entriesRead A value representing the number of stream entries already read by the group.
      * @return <code>OK</code>.
      * @example
      *     <pre>{@code
@@ -692,8 +690,7 @@ public interface StreamBaseCommands {
      * assert client.xgroupSetId("mystream", "mygroup", "0", "1-1").get().equals("OK");
      * }</pre>
      */
-    CompletableFuture<String> xgroupSetId(
-            String key, String groupName, String id, String entriesReadId);
+    CompletableFuture<String> xgroupSetId(String key, String groupName, String id, long entriesRead);
 
     /**
      * Reads entries from the given streams owned by a consumer group.
