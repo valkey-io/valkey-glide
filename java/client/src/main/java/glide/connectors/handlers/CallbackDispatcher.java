c6f7c9c81beb6bedbcd4ba318239d2aeb6792b0d
James Duong <james.duong@improving.com>
2024-07-05 11:40:53 -0700

    Java: Fix critical spotbugs findings (#1826)
    * Critical spotbugs issues
    
    * Fix important P2s

--- java/client/src/main/java/glide/connectors/handlers/CallbackDispatcher.java
+++ java/client/src/main/java/glide/connectors/handlers/CallbackDispatcher.java
@@ -102,15 +102,19 @@ public class CallbackDispatcher {
                     case Unspecified:
                         // Unspecified error on Redis service-side
                         future.completeExceptionally(new RequestException(msg));
+                        break;
                     case ExecAbort:
                         // Transactional error on Redis service-side
                         future.completeExceptionally(new ExecAbortException(msg));
+                        break;
                     case Timeout:
                         // Timeout from Glide to Redis service
                         future.completeExceptionally(new TimeoutException(msg));
+                        break;
                     case Disconnect:
                         // Connection problem between Glide and Redis
                         future.completeExceptionally(new ConnectionException(msg));
+                        break;
                     default:
                         // Request or command error from Redis
                         future.completeExceptionally(new RequestException(msg));
