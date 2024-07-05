c4620f88d4d5b69340e2f80e86ed87785ac78ac8
TJ Zhang <tj.zhang@improving.com>
2024-07-05 14:40:19 -0700

    add back missing doc in revert

--- java/client/src/main/java/glide/api/models/BaseTransaction.java
+++ java/client/src/main/java/glide/api/models/BaseTransaction.java
@@ -3717,6 +3717,10 @@ public abstract class BaseTransaction<T extends BaseTransaction<T>> {
      * @see <a href="https://valkey.io/commands/xreadgroup/">valkey.io</a> for
      *     details.
      *     </code> to receive only new messages.
+     * @param keysAndIds A <code>Map</code> of keys and entry ids to read from.
+     *     The <code> Map</code> is composed of a stream's key and the id of the
+     *     entry after which the stream will be read. Use the special id of
+     *     <code>{@literal Map<String, Map<String, String[][]>>}
      * @param group The consumer group name.
      * @param consumer The newly created consumer.
      * @return Command Response - A <code>{@literal Map<String, Map<String,
