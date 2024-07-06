7e6361c0e680c65150ba9f933f0ec6938e8eb4e6
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-07-05 17:54:18 -0700

    spootless
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/commands/GenericCommands.java
+++ java/client/src/main/java/glide/api/commands/GenericCommands.java
@@ -395,7 +395,8 @@ public interface GenericCommands {
      * @param cursor The cursor that points to the next iteration of results. A value of <code>gs("0")
      *     </code> indicates the start of the search.
      * @return An <code>Array</code> of <code>Objects</code>. The first element is always the <code>
-     *     cursor</code> for the next iteration of results. <code>gs("0")</code> will be the <code>cursor
+     *     cursor</code> for the next iteration of results. <code>gs("0")</code> will be the <code>
+     *     cursor
      *     </code> returned on the last iteration of the scan.<br>
      *     The second element is always an <code>Array</code> of matched keys from the database.
      * @example
@@ -455,7 +456,8 @@ public interface GenericCommands {
      *     </code> indicates the start of the search.
      * @param options The {@link ScanOptions}.
      * @return An <code>Array</code> of <code>Objects</code>. The first element is always the <code>
-     *     cursor</code> for the next iteration of results. <code>gs("0")</code> will be the <code>cursor
+     *     cursor</code> for the next iteration of results. <code>gs("0")</code> will be the <code>
+     *     cursor
      *     </code> returned on the last iteration of the scan.<br>
      *     The second element is always an <code>Array</code> of matched keys from the database.
      * @example
