766464629e07cfe25113449063585df514b9dd83
Chloe Yip <chloe.yip@improving.com>
2024-07-12 12:40:53 -0700

    ran linter
    Signed-off-by: Chloe Yip <chloe.yip@improving.com>

--- node/src/Transaction.ts
+++ node/src/Transaction.ts
@@ -587,7 +587,7 @@ export class BaseTransaction<T extends BaseTransaction<T>> {
      * The index is zero-based, so 0 means the first element,1 the second element and so on.
      * Negative indices can be used to designate elements starting at the tail of
      * the list. Here, -1 means the last element, -2 means the penultimate and so forth.
-     * 
+     *
      * See https://valkey.io/commands/lset/ for details.
      *
      * @param key The key of the list.
