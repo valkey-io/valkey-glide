e9470841bfbfba352fa1fd9475643f36b55e2b67
Chloe Yip <chloe.yip@improving.com>
2024-07-26 17:16:31 -0700

    remove extra line
    Signed-off-by: Chloe Yip <chloe.yip@improving.com>

--- node/src/BaseClient.ts
+++ node/src/BaseClient.ts
@@ -1609,7 +1609,6 @@ export class BaseClient {
      * @param timeout - The number of seconds to wait for a blocking operation to complete. A value of `0` will block indefinitely.
      * @returns The popped element, or `null` if `source` does not exist or if the operation timed-out.
      *
-     *
      * Since: Valkey version 6.2.0.
      *
      * @example
