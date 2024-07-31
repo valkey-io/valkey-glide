9363150bdb2e6918801bdbf64e15c3f048092237
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-07-31 10:28:25 -0700

    Node: Add `LCS` command. (#2049)
    * Add `LCS` command.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- node/src/Commands.ts
+++ node/src/Commands.ts
@@ -2776,3 +2776,27 @@ export function createZRandMember(
 
     return createCommand(RequestType.ZRandMember, args);
 }
+
+/** @internal */
+export function createLCS(
+    key1: string,
+    key2: string,
+    options?: {
+        len?: boolean;
+        idx?: { withMatchLen?: boolean; minMatchLen?: number };
+    },
+): command_request.Command {
+    const args = [key1, key2];
+
+    if (options) {
+        if (options.len) args.push("LEN");
+        else if (options.idx) {
+            args.push("IDX");
+            if (options.idx.withMatchLen) args.push("WITHMATCHLEN");
+            if (options.idx.minMatchLen !== undefined)
+                args.push("MINMATCHLEN", options.idx.minMatchLen.toString());
+        }
+    }
+
+    return createCommand(RequestType.LCS, args);
+}
