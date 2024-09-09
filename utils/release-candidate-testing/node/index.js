455bf3d51548770e923d4059a7e911f056212944
Adar Ovadia <adarov@amazon.com>
2024-09-09 10:35:13 +0000

    fix class name error
    Signed-off-by: Adar Ovadia <adarov@amazon.com>

--- utils/release-candidate-testing/node/index.js
+++ utils/release-candidate-testing/node/index.js
@@ -66,7 +66,7 @@ async function clusterTests() {
     try {
         console.log("Testing cluster");
         console.log("Creating cluster");
-        let glideCluster = await GlideClient.createCluster(true,
+        let glideCluster = await glideCluster.createCluster(true,
             3,
             1,
         );
