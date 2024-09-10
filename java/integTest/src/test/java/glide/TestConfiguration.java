699bed7d0701ed8d23424df250739bce54d9e298
Prateek Kumar <prateek.kumar@improving.com>
2024-09-09 18:02:52 -0700

    Get server version code moved to TestUtils.java
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- java/integTest/src/test/java/glide/TestConfiguration.java
+++ java/integTest/src/test/java/glide/TestConfiguration.java
@@ -1,58 +1,37 @@
-/**
- * Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
- */
+/** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
 package glide;
+
+import static glide.TestUtilities.commonClientConfig;
 
 import com.vdurmont.semver4j.Semver;
 import glide.api.GlideClient;
-import glide.api.models.commands.InfoOptions;
-
 import java.util.Arrays;
-
-import static glide.TestUtilities.commonClientConfig;
 
 public final class TestConfiguration {
     // All servers are hosted on localhost
     public static final int[] STANDALONE_PORTS = getPortsFromProperty("test.server.standalone.ports");
     public static final int[] CLUSTER_PORTS = getPortsFromProperty("test.server.cluster.ports");
-    private static final GlideClient standaloneClient;
     public static final Semver SERVER_VERSION;
 
     static {
         try {
-            standaloneClient = GlideClient.createClient(commonClientConfig().build()).get();
-            String infoResponse = standaloneClient.info(
-                InfoOptions.builder()
-                    .section(InfoOptions.Section.SERVER)
-                    .build()
-            ).get();
-            String serverVersion = null;
-            String[] serverSectionArray = infoResponse.split("\n");
-
-            for (int i = 0; i < serverSectionArray.length; i++) {
-                if (serverSectionArray[i].contains("redis_version")) {
-                    String[] versionKeyValue = serverSectionArray[i].split(":");
-                    if (versionKeyValue.length > 1) {
-                        serverVersion = versionKeyValue[1];
-                    }
-                    break;
-                }
-            }
+            String serverVersion =
+                    TestUtilities.getServerVersion(
+                            GlideClient.createClient(commonClientConfig().build()).get());
             if (serverVersion != null) {
                 SERVER_VERSION = new Semver(serverVersion);
             } else {
-                throw new Exception("Error in getting server version");
+                throw new Exception("Failed to get server version");
             }
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to get server version", e);
         }
-
     }
 
     private static int[] getPortsFromProperty(String propName) {
         return Arrays.stream(System.getProperty(propName).split(","))
-            .mapToInt(Integer::parseInt)
-            .toArray();
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
