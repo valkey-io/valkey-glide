f80f8461dbc907031cd1784b7c671122b6566bf4
Adar Ovadia <adarov@amazon.com>
2024-09-18 09:01:31 +0000

    fix version server
    Signed-off-by: Adar Ovadia <adarov@amazon.com>

--- utils/release-candidate-testing/node/index.js
+++ utils/release-candidate-testing/node/index.js
@@ -63,7 +63,10 @@ async function closeClientAndCluster(client, Cluster) {
 }
 
 async function clusterTests() {
-    const getServerVersion = new Promise((resolve)=>resolve("255.255.255"));
+    const getServerVersion = async (addresses, clusterMode) => {
+        // General version for those tests
+        return "255.255.255";
+    };
     try {
         console.log("Testing cluster");
         console.log("Creating cluster");
@@ -95,7 +98,10 @@ async function clusterTests() {
 }
 
 async function standaloneTests() {
-    const getServerVersion = new Promise((resolve)=>resolve("255.255.255"));
+    const getServerVersion = async (addresses, clusterMode) => {
+        // General version for those tests
+        return "255.255.255";
+    };
     try {
         console.log("Testing standalone Cluster")
         console.log("Creating Cluster");
