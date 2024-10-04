8ef623073c032a7cb84e7aaa729f27d2d7733373
Prateek Kumar <prateek.kumar@improving.com>
2024-10-04 01:16:50 -0700

    Command constants documented
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- python/python/glide/async_commands/server_modules/search_constants.py
+++ python/python/glide/async_commands/server_modules/search_constants.py
@@ -1,10 +1,18 @@
 class CommandNames:
+    """
+    Command name constants for vector search.
+    """
+
     FT_CREATE = "FT.CREATE"
     FT_SEARCH = "FT.SEARCH"
     INFO = "INFO"
     FT_DROPINDEX = "FT.DROPINDEX"
 
 class FtCreateKeywords:
+    """
+    Keywords used in the [FT.CREATE] command statment.
+    """
+
     SCHEMA = "SCHEMA"
     AS = "AS"
     SORTABLE = "SORTABLE"
@@ -14,6 +22,10 @@ class FtCreateKeywords:
     PREFIX = "PREFIX"
 
 class FtSearchKeywords:
+    """
+    Keywords used in the [FT.SEARCH] command statement.
+    """
+
     RETURN = "RETURN"
     AS = "AS"
     LIMIT = "LIMIT"
@@ -22,4 +34,8 @@ class FtSearchKeywords:
     COUNT = "COUNT"
 
 class FtDropIndexKeywords:
+    """
+    Keywords used in the [FT.DROPINDEX] command statment.
+    """
+
     DELETE_DOCUMENT = "dd"
