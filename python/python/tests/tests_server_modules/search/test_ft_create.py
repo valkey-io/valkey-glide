5acda14d841c48b0a1d6939406abfff7397121c0
Prateek Kumar <prateek.kumar@improving.com>
2024-10-09 04:04:56 -0700

    Test sortable on memory db
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- python/python/tests/tests_server_modules/search/test_ft_create.py
+++ python/python/tests/tests_server_modules/search/test_ft_create.py
@@ -13,16 +13,13 @@ class TestVss:
     async def test_vss_create(self, glide_client: TGlideClient):
         fields: List[Field] = []
         field1: TextField = TextField(
-            "title", 
-            sortable = SortableOptions.IS_SORTABLE
+            "title"
         )
         field2: NumericField = NumericField(
-            "published_at",
-            sortable = SortableOptions.IS_SORTABLE
+            "published_at"
         )
         field3: TextField = TextField(
-            "category",
-            sortable = SortableOptions.IS_SORTABLE
+            "category"
         )
         fields.append(field1)
         fields.append(field2)
@@ -38,60 +35,60 @@ class TestVss:
         result = await search.create(glide_client, index, fields, FtCreateOptions(DataType.HASH, prefixes))
         assert result == OK
 
-        """
-        Create an index with multiple fields with JSON data type.
-        """
-        index2 = "idx2"
-        result = await search.create(glide_client, index2, fields, FtCreateOptions(DataType.JSON, prefixes))
-        assert result == OK
+        # """
+        # Create an index with multiple fields with JSON data type.
+        # """
+        # index2 = "idx2"
+        # result = await search.create(glide_client, index2, fields, FtCreateOptions(DataType.JSON, prefixes))
+        # assert result == OK
   
-        """
-        Create an index for vectors of size 2
-        FT.CREATE hash_idx1 ON HASH PREFIX 1 hash: SCHEMA vec AS VEC VECTOR HNSW 6 DIM 2 TYPE FLOAT32 DISTANCE_METRIC L2
-        """
-        index3 = "hash_idx1"
-        prefixes = []
-        prefixes.append("hash:")
-        fields = []
-        fieldInfo: VectorField = VectorField(
-            name = "vec",
-            algorithm=VectorAlgorithm.HNSW,
-            attributes=VectorFieldAttributesHnsw(
-                dim = 2,
-                distanceMetric = DistanceMetricType.L2,
-                type = VectorType.FLOAT32
-            ),
-            alias = "VEC"
-        )
-        fields.append(fieldInfo)
+        # """
+        # Create an index for vectors of size 2
+        # FT.CREATE hash_idx1 ON HASH PREFIX 1 hash: SCHEMA vec AS VEC VECTOR HNSW 6 DIM 2 TYPE FLOAT32 DISTANCE_METRIC L2
+        # """
+        # index3 = "hash_idx1"
+        # prefixes = []
+        # prefixes.append("hash:")
+        # fields = []
+        # fieldInfo: VectorField = VectorField(
+        #     name = "vec",
+        #     algorithm=VectorAlgorithm.HNSW,
+        #     attributes=VectorFieldAttributesHnsw(
+        #         dim = 2,
+        #         distanceMetric = DistanceMetricType.L2,
+        #         type = VectorType.FLOAT32
+        #     ),
+        #     alias = "VEC"
+        # )
+        # fields.append(fieldInfo)
 
-        result = await search.create(glide_client, index3, fields, FtCreateOptions(DataType.HASH, prefixes))
-        assert result == OK
+        # result = await search.create(glide_client, index3, fields, FtCreateOptions(DataType.HASH, prefixes))
+        # assert result == OK
 
-        """
-        Create a 6-dimensional JSON index using the HNSW algorithm
-        FT.CREATE json_idx1 ON JSON PREFIX 1 json: SCHEMA $.vec AS VEC VECTOR HNSW 6 DIM 6 TYPE FLOAT32 DISTANCE_METRIC L2
-        """
-        index4 = "json_idx1"
-        prefixes = []
-        prefixes.append("json:")
-        fields = []
-        fieldInfo: VectorField = VectorField(
-            name = "$.vec",
-            algorithm=VectorAlgorithm.HNSW,
-            attributes=VectorFieldAttributesHnsw(
-                dim = 6,
-                distanceMetric = DistanceMetricType.L2,
-                type = VectorType.FLOAT32
-            ),
-            alias = "VEC"
-        )
-        fields.append(fieldInfo)
+        # """
+        # Create a 6-dimensional JSON index using the HNSW algorithm
+        # FT.CREATE json_idx1 ON JSON PREFIX 1 json: SCHEMA $.vec AS VEC VECTOR HNSW 6 DIM 6 TYPE FLOAT32 DISTANCE_METRIC L2
+        # """
+        # index4 = "json_idx1"
+        # prefixes = []
+        # prefixes.append("json:")
+        # fields = []
+        # fieldInfo: VectorField = VectorField(
+        #     name = "$.vec",
+        #     algorithm=VectorAlgorithm.HNSW,
+        #     attributes=VectorFieldAttributesHnsw(
+        #         dim = 6,
+        #         distanceMetric = DistanceMetricType.L2,
+        #         type = VectorType.FLOAT32
+        #     ),
+        #     alias = "VEC"
+        # )
+        # fields.append(fieldInfo)
 
-        result = await search.create(glide_client, index4, fields, FtCreateOptions(DataType.JSON, prefixes))
-        assert result == OK
+        # result = await search.create(glide_client, index4, fields, FtCreateOptions(DataType.JSON, prefixes))
+        # assert result == OK
 
-        """
-        TO-DO:
-        Add additional tests from VSS documentation that require a combination of commands to run.
-        """
+        # """
+        # TO-DO:
+        # Add additional tests from VSS documentation that require a combination of commands to run.
+        # """
