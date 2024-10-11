efdf4aea50be13dae543537af864c2d05a129a9f
Prateek Kumar <prateek.kumar@improving.com>
2024-10-11 16:31:09 -0700

    Python [FT.DROPINDEX] Update documentation
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- python/python/glide/async_commands/server_modules/ft.py
+++ python/python/glide/async_commands/server_modules/ft.py
@@ -65,7 +65,7 @@ async def dropindex(client: TGlideClient, indexName: TEncodable):
         indexName (TEncodable): The index name for the index to be dropped.
 
     Returns:
-        If the index is successfully dropped, returns "OK".
+        If the index is successfully dropped, returns "OK" else throws an error
 
     Examples:
         For the following example to work, an index named 'idx' must be already created. If not created, you will get an error.
