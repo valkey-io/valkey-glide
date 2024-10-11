9ead166ec194d00da16a390e1c01bd9f1bf13792
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-10-11 00:32:13 -0700

    Initial commit for FT.CREATE - no tests
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

---
+++ node/src/server-modules/GlideFt.ts
@@ -0,0 +1,47 @@
+/**
+ * Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+ */
+
+import { Decoder, DecoderOption, GlideString } from "../BaseClient";
+import { GlideClient } from "../GlideClient";
+import { GlideClusterClient } from "../GlideClusterClient";
+import { Field, FtCreateOptions } from "./GlideFtOptions";
+
+/**
+ * Creates an index and initiates a backfill of that index.
+ * 
+ * @param client The client to execute the command.
+ * @param indexName The index name for the index to be created.
+ * @param schema The fields of the index schema, specifying the fields and their types.
+ * @param options Optional arguments for the `FT.CREATE` command.
+ *   
+ * @returns If the index is successfully created, returns "OK".
+ */
+export async function create(
+    client: GlideClient | GlideClusterClient,
+    indexName: GlideString,
+    schema: Field[],
+    options?: FtCreateOptions | DecoderOption,
+): Promise<"OK" | null> {
+    const args: GlideString[] = ["FT.CREATE", indexName];
+    
+    schema.forEach(f => {args.push(f.toString())});
+
+    if (options) {
+        if ("dataType" in options) {
+            args.push("ON", options.dataType);
+        }
+
+        if ("prefixes" in options && options.prefixes) {
+            args.push("PREFIX", options.prefixes.length.toString(), ...options.prefixes);
+        }
+    }
+
+    return _handleCustomCommand(client, args, {decoder: Decoder.String}) as Promise<"OK" | null>;
+}
+
+function _handleCustomCommand(client: GlideClient | GlideClusterClient, args: GlideString[], decoderOption: DecoderOption) {
+    return client instanceof GlideClient 
+        ? (client as GlideClient).customCommand(args, decoderOption)
+        : (client as GlideClusterClient).customCommand(args, decoderOption);
+}
