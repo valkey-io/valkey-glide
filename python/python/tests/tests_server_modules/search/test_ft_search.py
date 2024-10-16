d22bc1be83a4bf072f4c0fe89b66b156b58c3f24
Prateek Kumar <prateek.kumar@improving.com>
2024-10-16 14:43:37 -0700

    Update test cases
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- python/python/tests/tests_server_modules/search/test_ft_search.py
+++ python/python/tests/tests_server_modules/search/test_ft_search.py
@@ -27,9 +27,9 @@ class TestFtSearch:
     @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
     async def test_ft_search(self, glide_client: GlideClusterClient):
         prefix = "{json-search-"+str(uuid.uuid4())+"}:"
-        json_key = prefix + str(uuid.uuid4())
+        json_key1 = prefix + str(uuid.uuid4())
         json_key2 = prefix + str(uuid.uuid4())
-        json_value = {"a": 11111, "b": 2, "c": 3}
+        json_value1 = {"a": 11111, "b": 2, "c": 3}
         json_value2 = {"a": 22222, "b": 2, "c": 3}
         prefixes: List[TEncodable] = []
         prefixes.append(prefix)
@@ -52,7 +52,7 @@ class TestFtSearch:
 
         # Create a json key
         assert (
-            await json.set(glide_client, json_key, "$", OuterJson.dumps(json_value))
+            await json.set(glide_client, json_key1, "$", OuterJson.dumps(json_value1))
             == OK
         )
         assert (
@@ -63,7 +63,7 @@ class TestFtSearch:
         # Wait for index to be updated to avoid this error - ResponseError: The index is under construction.
         time.sleep(0.5)
 
-        # Search the index
+        # Search the index for string inputs
         result = await ft.search(
             glide_client,
             index,
@@ -75,17 +75,45 @@ class TestFtSearch:
                 ]
             ),
         )
-        # Check if we get the expected result
+
+        # Check if we get the expected result from ft.search for string inputs
+        self.checkExpectedSearchResult(result)
+
+        # Search the index for byte inputs
+        result = await ft.search(
+            glide_client,
+            bytes(index, "utf-8"),
+            b"*",
+            options=FtSeachOptions(
+                return_fields=[
+                    ReturnField(field_identifier=b"a", alias=b"a_new"),
+                    ReturnField(field_identifier=b"b", alias=b"b_new"),
+                ]
+            ),
+        )
+
+
+        # Check if we get the expected result from ft.search from byte inputs
+        self.checkExpectedSearchResult(result)
+
+
+
+    def checkExpectedSearchResult(result, json_key1, json_key2, json_value1, json_value2):
         assert len(result) == 2
         assert result[0] == 2
         searchResultMap: Mapping[TEncodable, Mapping[TEncodable, TEncodable]]  = result[1]
         for key, fieldsMap in searchResultMap.items():
             keyString = key.decode("utf-8")
-            assert keyString == json_key or keyString == json_key2
-            if keyString == json_key:
-                print(fieldsMap)
+            assert keyString == json_key1 or keyString == json_key2
+            if keyString == json_key1:
                 for fieldName, fieldValue in fieldsMap.items():
                     fieldNameString = fieldName.decode("utf-8")
                     fieldValueInt = int(fieldValue.decode("utf-8"))
                     assert fieldNameString == "a" or fieldNameString == "b"
-                    assert fieldValueInt == json_value.get("a") or fieldValueInt == json_value.get("b")
+                    assert fieldValueInt == json_value1.get("a") or fieldValueInt == json_value1.get("b")
+            if keyString == json_key2:
+                for fieldName, fieldValue in fieldsMap.items():
+                    fieldNameString = fieldName.decode("utf-8")
+                    fieldValueInt = int(fieldValue.decode("utf-8"))
+                    assert fieldNameString == "a" or fieldNameString == "b"
+                    assert fieldValueInt == json_value2.get("a") or fieldValueInt == json_value2.get("b")
