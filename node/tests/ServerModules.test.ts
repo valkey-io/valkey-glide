962546ac1de97845b2ef4094f9716e7496f72057
Andrew Carbonetto <andrew.carbonetto@improving.com>
2024-10-17 15:27:03 -0700

    Add node CI changes from #2472
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

---
+++ node/tests/ServerModules.test.ts
@@ -0,0 +1,64 @@
+/**
+ * Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+ */
+import {
+    afterAll,
+    afterEach,
+    beforeAll,
+    describe,
+    expect,
+    it,
+} from "@jest/globals";
+import { GlideClusterClient, InfoOptions, ProtocolVersion } from "..";
+import { ValkeyCluster } from "../../utils/TestUtils";
+import {
+    flushAndCloseClient,
+    getClientConfigurationOption,
+    getServerVersion,
+    parseCommandLineArgs,
+    parseEndpoints,
+} from "./TestUtilities";
+
+const TIMEOUT = 50000;
+describe("GlideJson", () => {
+    const testsFailed = 0;
+    let cluster: ValkeyCluster;
+    let client: GlideClusterClient;
+    beforeAll(async () => {
+        const clusterAddresses = parseCommandLineArgs()["cluster-endpoints"];
+        cluster = clusterAddresses
+            ? await ValkeyCluster.initFromExistingCluster(
+                  false,
+                  parseEndpoints(clusterAddresses),
+                  getServerVersion,
+              )
+            : await ValkeyCluster.createCluster(false, 1, 1, getServerVersion);
+    }, 20000);
+
+    afterEach(async () => {
+        await flushAndCloseClient(false, cluster.getAddresses(), client);
+    });
+
+    afterAll(async () => {
+        if (testsFailed === 0) {
+            await cluster.close();
+        }
+    }, TIMEOUT);
+
+    it.each([ProtocolVersion.RESP2, ProtocolVersion.RESP3])(
+        "ServerModules check modules loaded",
+        async (protocol) => {
+            client = await GlideClusterClient.createClient(
+                getClientConfigurationOption(cluster.getAddresses(), protocol, {
+                    useTLS: true,
+                }),
+            );
+            const info = await client.info({
+                sections: [InfoOptions.Modules],
+                route: "randomNode",
+            });
+            expect(info).toContain("Modules");
+            //expect(info).toContain("# search_index_stats");
+        },
+    );
+});
