44b5a055ae62a63a435317a700e218f1140ca655
tjzhang-BQ <111323543+tjzhang-BQ@users.noreply.github.com>
2024-10-22 11:33:15 -0700

    Node: Add command JSON.TOGGLE (#2491)
    * Node: Add command JSON.TOGGLE
    
    Signed-off-by: TJ Zhang <tj.zhang@improving.com>
    Co-authored-by: TJ Zhang <tj.zhang@improving.com>

--- node/src/server-modules/GlideJson.ts
+++ node/src/server-modules/GlideJson.ts
@@ -7,7 +7,7 @@ import { ConditionalChange } from "../Commands";
 import { GlideClient } from "../GlideClient";
 import { GlideClusterClient, RouteOption } from "../GlideClusterClient";
 
-export type ReturnTypeJson = GlideString | (GlideString | null)[];
+export type ReturnTypeJson<T> = T | (T | null)[];
 
 /**
  * Represents options for formatting JSON data, to be used in the [JSON.GET](https://valkey.io/commands/json.get/) command.
@@ -80,6 +80,7 @@ export class GlideJson {
     /**
      * Sets the JSON value at the specified `path` stored at `key`.
      *
+     * @param client The client to execute the command.
      * @param key - The key of the JSON document.
      * @param path - Represents the path within the JSON document where the value will be set.
      *      The key will be modified only if `value` is added as the last child in the specified `path`, or if the specified `path` acts as the parent of a new child being added.
@@ -123,8 +124,11 @@ export class GlideJson {
     /**
      *  Retrieves the JSON value at the specified `paths` stored at `key`.
      *
+     * @param client The client to execute the command.
      * @param key - The key of the JSON document.
-     * @param options - Options for formatting the byte representation of the JSON data. See {@link JsonGetOptions}.
+     * @param options - (Optional) Additional parameters:
+     * - (Optional) Options for formatting the byte representation of the JSON data. See {@link JsonGetOptions}.
+     * - (Optional) `decoder`: see {@link DecoderOption}.
      * @returns ReturnTypeJson:
      *   - If one path is given:
      *     - For JSONPath (path starts with `$`):
@@ -164,10 +168,10 @@ export class GlideJson {
      * ```
      */
     static async get(
-        client: GlideClient | GlideClusterClient,
+        client: BaseClient,
         key: GlideString,
         options?: JsonGetOptions & DecoderOption,
-    ): Promise<ReturnTypeJson> {
+    ): Promise<ReturnTypeJson<GlideString>> {
         const args = ["JSON.GET", key];
 
         if (options) {
@@ -175,6 +179,62 @@ export class GlideJson {
             args.push(...optionArgs);
         }
 
-        return _executeCommand<ReturnTypeJson>(client, args, options);
+        return _executeCommand<ReturnTypeJson<GlideString>>(
+            client,
+            args,
+            options,
+        );
+    }
+
+    /**
+     * Toggles a Boolean value stored at the specified `path` within the JSON document stored at `key`.
+     *
+     * @param client - The client to execute the command.
+     * @param key - The key of the JSON document.
+     * @param options - (Optional) Additional parameters:
+     * - (Optional) The JSONPath to specify. Defaults to the root if not specified.
+     * @returns - For JSONPath (`path` starts with `$`), returns a list of boolean replies for every possible path, with the toggled boolean value,
+     * or null for JSON values matching the path that are not boolean.
+     * - For legacy path (`path` doesn't starts with `$`), returns the value of the toggled boolean in `path`.
+     * - Note that when sending legacy path syntax, If `path` doesn't exist or the value at `path` isn't a boolean, an error is raised.
+     *
+     * @example
+     * ```typescript
+     * const value = {bool: true, nested: {bool: false, nested: {bool: 10}}};
+     * const jsonStr = JSON.stringify(value);
+     * const resultSet = await GlideJson.set("doc", "$", jsonStr);
+     * // Output: 'OK'
+     *
+     * const resultToggle = await.GlideJson.toggle(client, "doc", "$.bool")
+     * // Output: [false, true, null] - Indicates successful toggling of the Boolean values at path '$.bool' in the key stored at `doc`.
+     *
+     * const resultToggle = await.GlideJson.toggle(client, "doc", "bool")
+     * // Output: true - Indicates successful toggling of the Boolean value at path 'bool' in the key stored at `doc`.
+     *
+     * const resultToggle = await.GlideJson.toggle(client, "doc", "bool")
+     * // Output: true - Indicates successful toggling of the Boolean value at path 'bool' in the key stored at `doc`.
+     *
+     * const jsonGetStr = await GlideJson.get(client, "doc", "$");
+     * console.log(JSON.stringify(jsonGetStr));
+     * // Output: [{bool: true, nested: {bool: true, nested: {bool: 10}}}] - The updated JSON value in the key stored at `doc`.
+     *
+     * // Without specifying a path, the path defaults to root.
+     * console.log(await GlideJson.set(client, "doc2", ".", true)); // Output: "OK"
+     * console.log(await GlideJson.toggle(client,"doc2")); // Output: "false"
+     * console.log(await GlideJson.toggle(client, "doc2")); // Output: "true"
+     * ```
+     */
+    static async toggle(
+        client: BaseClient,
+        key: GlideString,
+        options?: { path: GlideString },
+    ): Promise<ReturnTypeJson<boolean>> {
+        const args = ["JSON.TOGGLE", key];
+
+        if (options !== undefined) {
+            args.push(options.path);
+        }
+
+        return _executeCommand<ReturnTypeJson<boolean>>(client, args);
     }
 }
