b34f7393a999b81ed1c9ee593a87281d30a7927a
Avi Fenesh <55848801+avifenesh@users.noreply.github.com>
2024-10-31 14:45:15 +0200

    fixes for CI (#2552)
    Signed-off-by: avifenesh <aviarchi1994@gmail.com>

--- java/integTest/src/test/java/glide/TestConfiguration.java
+++ java/integTest/src/test/java/glide/TestConfiguration.java
@@ -8,6 +8,7 @@ import com.vdurmont.semver4j.Semver;
 import glide.api.BaseClient;
 import glide.api.GlideClient;
 import glide.api.GlideClusterClient;
+import glide.api.logging.Logger;
 
 public final class TestConfiguration {
     // All servers are hosted on localhost
@@ -19,6 +20,8 @@ public final class TestConfiguration {
     public static final boolean TLS = Boolean.parseBoolean(System.getProperty("test.server.tls", ""));
 
     static {
+        Logger.init(Logger.Level.OFF);
+        Logger.setLoggerConfig(Logger.Level.OFF);
         try {
             BaseClient client =
                     !STANDALONE_HOSTS[0].isEmpty()
