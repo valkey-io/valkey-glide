6364e394b999eb245acc91483f901effb517613e
TJ Zhang <tj.zhang@improving.com>
2024-11-06 18:52:14 -0800

    updating example
    Signed-off-by: TJ Zhang <tj.zhang@improving.com>

--- examples/node/json_example.ts
+++ examples/node/json_example.ts
@@ -27,13 +27,13 @@ async function jsonSetAndGet(client: GlideClient | GlideClusterClient) {
     const value = '{"a": 1.0, "b":2}';
     const result = await GlideJson.set(client, "doc", "$", value);
     console.log(result); // 'OK' - Indicates successful setting of the value at path '$' in the key stored at `doc`.
-    const result2 = await GlideJson.get(client, "doc", {paths:["$"]});
+    const result2 = await GlideJson.get(client, "doc", { path: ["$"] });
     console.log(result2);
 
-    // expect(result).toBe("OK");
-    // expect(result2).toBe(value);
+    expect(result).toBe("OK");
+    expect(result2).toBe(value);
 
-    // console.log("All examples succeeded.");
+    console.log("All examples succeeded.");
 }
 
 function setFileLogger() {
