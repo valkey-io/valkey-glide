ea14d71b2d7b1d02dd53c201318fd1a68b0fbf97
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-11-07 13:29:26 -0800

    update
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/glide/api/commands/servermodules/Json.java
+++ java/client/src/main/java/glide/api/commands/servermodules/Json.java
@@ -456,8 +456,12 @@ public class Json {
      * Retrieves the JSON values at the specified <code>path</code> stored at multiple <code>keys
      * </code>.
      *
-     * @apiNote When in cluster mode, the command may route to multiple nodes when <code>keys</code>
-     *     map to different hash slots.
+     * @apiNote When in cluster mode, if keys in <code>keys</code> map to different hash slots, the
+     *     command will be split across these slots and executed separately for each. This means the
+     *     command is atomic only at the slot level. If one or more slot-specific requests fail, the
+     *     entire call will return the first encountered error, even though some requests may have
+     *     succeeded while others did not. If this behavior impacts your application logic, consider
+     *     splitting the request into sub-requests per slot to ensure atomicity.
      * @param client The client to execute the command.
      * @param keys The keys of the JSON documents.
      * @param path The path within the JSON documents.
