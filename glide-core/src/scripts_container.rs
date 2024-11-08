da4b6248e0c5202c024ca1f4585215d678ec171a
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-11-07 19:01:52 -0800

    Fix license header in core files
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- glide-core/src/scripts_container.rs
+++ glide-core/src/scripts_container.rs
@@ -1,7 +1,7 @@
-use bytes::BytesMut;
 /**
  * Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
  */
+use bytes::BytesMut;
 use logger_core::log_info;
 use once_cell::sync::Lazy;
 use sha1_smol::Sha1;
--- glide-core/tests/test_socket_listener.rs
+++ glide-core/tests/test_socket_listener.rs
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
  */
 
