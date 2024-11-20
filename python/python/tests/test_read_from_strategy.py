0c753db75b217d7bbd8ebf580414c1703e90dd1e
Adar Ovadia <adarov@amazon.com>
2024-11-20 15:55:49 +0000

    upload timeout
    Signed-off-by: Adar Ovadia <adarov@amazon.com>

--- python/python/tests/test_read_from_strategy.py
+++ python/python/tests/test_read_from_strategy.py
@@ -30,6 +30,77 @@ class TestAZAffinity:
     @pytest.mark.skip_if_version_below("8.0.0")
     @pytest.mark.parametrize("cluster_mode", [True])
     @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
+    async def test_routing_with_az_affinity_strategy_to_1_replica(
+        self,
+        request,
+        cluster_mode: bool,
+        protocol: ProtocolVersion,
+        multiple_replicas_cluster,
+    ):
+        """Test that the client with az affinity strategy will only route to the 1 replica with the same az"""
+        az = "us-east-1a"
+        GET_CALLS = 3
+        get_cmdstat = f"cmdstat_get:calls={GET_CALLS}"
+
+        client_for_config_set = await create_client(
+            request,
+            cluster_mode,
+            addresses=multiple_replicas_cluster.nodes_addr,
+            protocol=protocol,
+            timeout=2000,
+        )
+
+        # Reset the availability zone for all nodes
+        await client_for_config_set.custom_command(
+            ["CONFIG", "SET", "availability-zone", ""],
+            route=AllNodes(),
+        )
+        assert await client_for_config_set.config_resetstat() == OK
+
+        # 12182 is the slot of "foo"
+        await client_for_config_set.custom_command(
+            ["CONFIG", "SET", "availability-zone", az],
+            route=SlotIdRoute(SlotType.REPLICA, 12182),
+        )
+
+        client_for_testing_az = await create_client(
+            request,
+            cluster_mode,
+            addresses=multiple_replicas_cluster.nodes_addr,
+            protocol=protocol,
+            read_from=ReadFrom.AZ_AFFINITY,
+            timeout=2000,
+            client_az=az,
+        )
+
+        for _ in range(GET_CALLS):
+            await client_for_testing_az.get("foo")
+
+        info_result = await client_for_testing_az.info(
+            [InfoSection.SERVER, InfoSection.COMMAND_STATS], AllNodes()
+        )
+
+        # Check that only the replica with az has all the GET calls
+        matching_entries_count = sum(
+            1
+            for value in info_result.values()
+            if get_cmdstat in value.decode() and az in value.decode()
+        )
+        assert matching_entries_count == 1
+
+        # Check that the other replicas have no availability zone set
+        changed_az_count = sum(
+            1
+            for node in info_result.values()
+            if f"availability_zone:{az}" in node.decode()
+        )
+        assert changed_az_count == 1
+        await client_for_testing_az.close()
+        await client_for_config_set.close()
+
+    @pytest.mark.skip_if_version_below("8.0.0")
+    @pytest.mark.parametrize("cluster_mode", [True])
+    @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
     async def test_routing_by_slot_to_replica_with_az_affinity_strategy_to_all_replicas(
         self,
         request,
@@ -47,11 +118,10 @@ class TestAZAffinity:
             protocol=protocol,
             timeout=2000,
         )
-        await client_for_config_set.config_resetstat() == OK
+        assert await client_for_config_set.config_resetstat() == OK
         await client_for_config_set.custom_command(
             ["CONFIG", "SET", "availability-zone", az], AllNodes()
         )
-        await client_for_config_set.close()
 
         client_for_testing_az = await create_client(
             request,
@@ -95,121 +165,49 @@ class TestAZAffinity:
         )
         assert matching_entries_count == n_replicas
 
-        await client_for_testing_az.close()
-
-    @pytest.mark.skip_if_version_below("8.0.0")
-    @pytest.mark.parametrize("cluster_mode", [True])
-    @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
-    async def test_routing_with_az_affinity_strategy_to_1_replica(
-        self,
-        request,
-        cluster_mode: bool,
-        protocol: ProtocolVersion,
-        multiple_replicas_cluster,
-    ):
-        """Test that the client with az affinity strategy will only route to the 1 replica with the same az"""
-        az = "us-east-1a"
-        GET_CALLS = 3
-        get_cmdstat = f"cmdstat_get:calls={GET_CALLS}"
-
-        client_for_config_set = await create_client(
-            request,
-            cluster_mode,
-            addresses=multiple_replicas_cluster.nodes_addr,
-            protocol=protocol,
-            timeout=2000,
-        )
-
-        # Reset the availability zone for all nodes
-        await client_for_config_set.custom_command(
-            ["CONFIG", "SET", "availability-zone", ""],
-            route=AllNodes(),
-        )
-        await client_for_config_set.config_resetstat() == OK
-
-        # 12182 is the slot of "foo"
-        await client_for_config_set.custom_command(
-            ["CONFIG", "SET", "availability-zone", az],
-            route=SlotIdRoute(SlotType.REPLICA, 12182),
-        )
-
         await client_for_config_set.close()
-
-        client_for_testing_az = await create_client(
-            request,
-            cluster_mode,
-            addresses=multiple_replicas_cluster.nodes_addr,
-            protocol=protocol,
-            read_from=ReadFrom.AZ_AFFINITY,
-            timeout=2000,
-            client_az=az,
-        )
-
-        for _ in range(GET_CALLS):
-            await client_for_testing_az.get("foo")
-
-        info_result = await client_for_testing_az.info(
-            [InfoSection.SERVER, InfoSection.COMMAND_STATS], AllNodes()
-        )
-
-        # Check that only the replica with az has all the GET calls
-        matching_entries_count = sum(
-            1
-            for value in info_result.values()
-            if get_cmdstat in value.decode() and az in value.decode()
-        )
-        assert matching_entries_count == 1
-
-        # Check that the other replicas have no availability zone set
-        changed_az_count = sum(
-            1
-            for node in info_result.values()
-            if f"availability_zone:{az}" in node.decode()
-        )
-        assert changed_az_count == 1
-
         await client_for_testing_az.close()
 
-    @pytest.mark.skip_if_version_below("8.0.0")
-    @pytest.mark.parametrize("cluster_mode", [True])
-    @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
-    async def test_az_affinity_non_existing_az(
-        self,
-        request,
-        cluster_mode: bool,
-        protocol: ProtocolVersion,
-        multiple_replicas_cluster,
-    ):
-        GET_CALLS = 4
+    # @pytest.mark.skip_if_version_below("8.0.0")
+    # @pytest.mark.parametrize("cluster_mode", [True])
+    # @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
+    # async def test_az_affinity_non_existing_az(
+    #     self,
+    #     request,
+    #     cluster_mode: bool,
+    #     protocol: ProtocolVersion,
+    #     multiple_replicas_cluster,
+    # ):
+    #     GET_CALLS = 4
 
-        client_for_testing_az = await create_client(
-            request,
-            cluster_mode,
-            addresses=multiple_replicas_cluster.nodes_addr,
-            protocol=protocol,
-            read_from=ReadFrom.AZ_AFFINITY,
-            timeout=2000,
-            client_az="non-existing-az",
-        )
-        await client_for_testing_az.config_resetstat() == OK
+    #     client_for_testing_az = await create_client(
+    #         request,
+    #         cluster_mode,
+    #         addresses=multiple_replicas_cluster.nodes_addr,
+    #         protocol=protocol,
+    #         read_from=ReadFrom.AZ_AFFINITY,
+    #         timeout=2000,
+    #         client_az="non-existing-az",
+    #     )
+    #     assert await client_for_testing_az.config_resetstat() == OK
 
-        for _ in range(GET_CALLS):
-            await client_for_testing_az.get("foo")
+    #     for _ in range(GET_CALLS):
+    #         await client_for_testing_az.get("foo")
 
-        n_replicas = await self._get_num_replicas(client_for_testing_az)
-        # We expect the calls to be distributed evenly among the replicas
-        get_cmdstat = f"cmdstat_get:calls={GET_CALLS // n_replicas}"
+    #     n_replicas = await self._get_num_replicas(client_for_testing_az)
+    #     # We expect the calls to be distributed evenly among the replicas
+    #     get_cmdstat = f"cmdstat_get:calls={GET_CALLS // n_replicas}"
 
-        info_result = await client_for_testing_az.info(
-            [InfoSection.COMMAND_STATS, InfoSection.SERVER], AllNodes()
-        )
+    #     info_result = await client_for_testing_az.info(
+    #         [InfoSection.COMMAND_STATS, InfoSection.SERVER], AllNodes()
+    #     )
 
-        matching_entries_count = sum(
-            1 for value in info_result.values() if get_cmdstat in value.decode()
-        )
-        assert matching_entries_count == GET_CALLS
+    #     matching_entries_count = sum(
+    #         1 for value in info_result.values() if get_cmdstat in value.decode()
+    #     )
+    #     assert matching_entries_count == GET_CALLS
 
-        await client_for_testing_az.close()
+    #     await client_for_testing_az.close()
 
     @pytest.mark.skip_if_version_below("8.0.0")
     @pytest.mark.parametrize("cluster_mode", [True])
