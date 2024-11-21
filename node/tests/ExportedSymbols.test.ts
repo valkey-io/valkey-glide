07b96dfdc13c43130d20596ca3963d8140c528d4
Prateek Kumar <prateek.kumar@improving.com>
2024-11-21 23:26:38 +0530

    Node: code cleanup

--- node/tests/ExportedSymbols.test.ts
+++ node/tests/ExportedSymbols.test.ts
@@ -2,21 +2,15 @@ import { it } from "@jest/globals";
 
 const fs = require('fs/promises');
 var exportedSymbols = require('../index');
-var exportedSymbolsRust = require('../rust-client/index')
 let i = 0;
 let filesWithNodeCode: string[] = [];
+let getActualSymbolsList: string[] = [];
 
 describe("Exported Symbols test", () => {
     var excludedSymbols: string[] = [];
     beforeAll(() => {
         /**
          * Add Excluded symbols
-         * Example:
-         * excludedSymbols.push('convertGlideRecord');
-         */
-
-        /**
-         * Add Excluded symbols for rust client
          * Example:
          * excludedSymbols.push('convertGlideRecord');
          */
@@ -28,12 +22,6 @@ describe("Exported Symbols test", () => {
         const filteredExportedList = exportedSymbolsList.filter(symbol => excludedSymbols.includes(symbol));
         console.log("Following symbols are exported but are in the exluded list, please remove: " + filteredExportedList);
         expect(filteredExportedList.length).toBe(0);
-
-        // // Check exported symbols from rust client package.
-        // let exportedRustSymbolsList = Object.keys(exportedSymbolsRust);
-        // const filteredExportedListForRust = exportedRustSymbolsList.filter(symbol => excludedSymbols.includes(symbol));
-        // console.log("Following symbols are exported but are in the exluded list, please remove: " + filteredExportedListForRust);
-        // expect(filteredExportedListForRust.length).toBe(0);
 
         const testFolder = './';
         await getFiles(testFolder);
@@ -73,5 +61,4 @@ async function getFiles(folderName: string) {
             filesWithNodeCode.push(folderName + file.name);
         }
     }
-
 }
