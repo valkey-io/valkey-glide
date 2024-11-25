7bda107b70c0ba1114156c85ee86ad48e7a93406
Prateek Kumar <prateek.kumar@improving.com>
2024-11-25 11:54:06 -0800

    Node: find missing symbols
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- node/tests/ExportedSymbols.test.ts
+++ node/tests/ExportedSymbols.test.ts
@@ -1,10 +1,13 @@
 import { it } from "@jest/globals";
+import * as ts from "typescript";
 
 const fs = require('fs/promises');
-var exportedSymbols = require('../index');
+const f = require('fs');
+
+var exportedSymbols = require('../npm/glide/index');
 let i = 0;
 let filesWithNodeCode: string[] = [];
-let getActualSymbolsList: string[] = [];
+let getActualSymbolsList: any[] = [];
 
 describe("Exported Symbols test", () => {
     var excludedSymbolList: string[] = [];
@@ -18,20 +21,30 @@ describe("Exported Symbols test", () => {
     it("check excluded symbols are not exported", async () => {
         // Check exported symbols for valkey glide package
         let exportedSymbolsList = Object.keys(exportedSymbols);  // exportedList
-        const filteredExportedList = exportedSymbolsList.filter(symbol => excludedSymbolList.includes(symbol));
-        console.log("Following symbols are exported but are in the exlcuded list, please remove: " + filteredExportedList);
-        expect(filteredExportedList.length).toBe(0);
+        //        const filteredExportedList = exportedSymbolsList.filter(symbol => excludedSymbolList.includes(symbol));
+        //        console.log("Following symbols are exported but are in the exlcuded list, please remove: " + filteredExportedList);
+        //        expect(filteredExportedList.length).toBe(0);
 
         const testFolder = './';
         await getFiles(testFolder);
-        console.log('Total files found =' + i);
-        console.log(filesWithNodeCode);
-
-        let actualSymbolList: string[] = [];  //Actual list
-
+        console.log('Total files found =' + i); // 10
+        console.log(filesWithNodeCode); //[]
+        for (let file of filesWithNodeCode) {
+            const sourceCode = await f.readFileSync(file, 'utf8');
+            const sourceFile = await ts.createSourceFile(file, sourceCode, ts.ScriptTarget.Latest, true);
+            visit(sourceFile, 0);
+        }
+        console.log("actual exports in the source code =");
+        //console.log(getActualSymbolsList);
+        console.log("Total exported symbols in index=" + exportedSymbolsList.length);
+        console.log("Total symbols in the source code=" + getActualSymbolsList.length);
+        //        console.log(exportedSymbolsList);
+        let l = getActualSymbolsList.filter(actualSymbol => !exportedSymbolsList.includes(actualSymbol));
+        console.log("Total missed symbols=" + l.length);
+        console.log(l);
         //1. Test if actualSymbolList - exportedSymbolsList = excludedSymbolList
         //2. If actualSymbolList - exportedSymbolsList != excludedSymbolList, 
-        //   throw an error that either some symbol not exported or there is some error in the error list.
+        //   throw an error that either some symbol not exported or there is some error in the excluded symbol list.
     });
 });
 
@@ -64,6 +77,50 @@ async function getFiles(folderName: string) {
             }
             i++;
             filesWithNodeCode.push(folderName + file.name);
+        }
+    }
+}
+
+function visit(node: ts.Node, depth = 0) {
+    if (depth == 2) {
+        let name: string | undefined = "";
+        if (ts.isFunctionDeclaration(node)) {
+            name = node.name?.text;
+        } else if (ts.isVariableStatement(node)) {
+            name = "";
+        } else if (ts.isInterfaceDeclaration(node)) {
+            name = node.name?.text;
+        } else if (ts.isClassDeclaration(node)) {
+            name = node.name?.text;
+        } else if (ts.isTypeAliasDeclaration(node)) {
+            name = node.name?.text;
+        } else if (ts.isEnumDeclaration(node)) {
+            name = node.name?.text;
+        } else if (ts.isModuleDeclaration(node)) {
+            name = node.name?.text;
+        } else if (ts.SyntaxKind[node.kind] == "FirstStatement") {
+            name = node.name?.text;
+        }
+        let f = 0;
+        for (let c of node.getChildren()) {
+            if (c.getText().trim() == "export") {
+                f = 1;
+            }
+        }
+        if (f == 1) {
+            if (name == '') {
+                console.log('depth=' + depth + ", ts.SyntaxKind===" + ts.SyntaxKind[node.kind] + ", name=" + name);
+                console.log(node.getText());
+            }
+            getActualSymbolsList.push(name);
+
+        } else {
+            //            console.log('depth=' + depth + ", ts.SyntaxKind===" + ts.SyntaxKind[node.kind] + ", name=" + name + " ---not exported");
+        }
+    }
+    if (depth <= 1) {
+        for (let c of node.getChildren()) {
+            visit(c, depth + 1);
         }
     }
 }
