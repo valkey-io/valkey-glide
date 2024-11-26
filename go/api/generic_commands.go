c30630973224506c1ff0a1d244c8767826eb26dc
EdricCua <ecuartero@google.com>
2024-11-26 08:56:32 +0000

    Fix documentation and unit test Signed-off-by: EdricCua <ecuartero@google.com>

--- go/api/generic_commands.go
+++ go/api/generic_commands.go
@@ -316,7 +316,9 @@ type GenericBaseCommands interface {
 
 	// Unlink (delete) multiple keys from the database. A key is ignored if it does not exist.
 	// This command, similar to Del However, this command does not block the server
+	//
 	// Note:
+	//  When in cluster mode, the command may route to multiple nodes when keys map to different hash slots.
 	//
 	// Parameters:
 	//  keys - One or more keys to unlink.
@@ -331,18 +333,19 @@ type GenericBaseCommands interface {
 	//	}
 	//	fmt.Println(result) // Output: int
 	//
-	// [valkey.io]: Https://valkey.io/commands/touch/
+	// [valkey.io]: Https://valkey.io/commands/unlink/
 	Unlink(keys []string) (Result[int64], error)
 
 	// Alters the last access time of a key(s). A key is ignored if it does not exist.
 	//
 	// Note:
+	//  When in cluster mode, the command may route to multiple nodes when `keys` map to different hash slots.
 	//
 	// Parameters:
-	//  keys - One or more keys to touch.
+	//  The keys to update last access time.
 	//
 	// Return value:
-	//  Returns the number of keys that were touched.
+	//  The number of keys that were updated.
 	//
 	// Example:
 	//	result, err := client.Touch([]string{"key1", "key2", "key3"})
@@ -358,12 +361,13 @@ type GenericBaseCommands interface {
 	// The different types that can be returned are: string, list, set, zset, hash and stream.
 	//
 	// Note:
+	//  When in cluster mode, the command may route to multiple nodes when `keys` map to different hash slots.
 	//
 	// Parameters:
 	//  keys - string
 	//
 	// Return value:
-	//  Return the type of the stored value iun sting representation .
+	//  If the key exists, the type of the stored value is returned. Otherwise, a none" string is returned.
 	//
 	// Example:
 	//	result, err := client.Type([]string{"key"})
