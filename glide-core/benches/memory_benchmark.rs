48979482a41ce5c0156aa032bb48453402016e11
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-12-06 10:23:16 -0800

    Fix license header in core files (#2648)
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- glide-core/benches/memory_benchmark.rs
+++ glide-core/benches/memory_benchmark.rs
@@ -1,6 +1,5 @@
-/**
- * Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
- */
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
 use glide_core::{
     client::Client,
     connection_request::{ConnectionRequest, NodeAddress, TlsMode},
