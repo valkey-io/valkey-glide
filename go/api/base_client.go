36a4b863e99df672c43ab389f89be60d0aa2d6a4
Joseph Brinkman <joe.brinkman@improving.com>
2024-12-20 16:37:00 -0500

    GO: Add ZIncrBy Command (#2830)
    * Go: Add ZIncrBy command
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * reformat document to resolve linting error
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * update test with additional type check
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * add example
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * fix merge error
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    ---------
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    Signed-off-by: Joseph Brinkman <joe.brinkman@improving.com>

--- go/api/base_client.go
+++ go/api/base_client.go
@@ -48,7 +48,6 @@ func successCallback(channelPtr unsafe.Pointer, cResponse *C.struct_CommandRespo
 	resultChannel <- payload{value: response, error: nil}
 }
 
-//
 //export failureCallback
 func failureCallback(channelPtr unsafe.Pointer, cErrorMessage *C.char, cErrorType C.RequestErrorType) {
 	resultChannel := *(*chan payload)(channelPtr)
@@ -1289,4 +1288,13 @@ func (client *baseClient) ZAddIncrWithOptions(
 	}
 
 	return client.zAddIncrBase(key, incrOpts)
+}
+
+func (client *baseClient) ZIncrBy(key string, increment float64, member string) (Result[float64], error) {
+	result, err := client.executeCommand(C.ZIncrBy, []string{key, utils.FloatToString(increment), member})
+	if err != nil {
+		return CreateNilFloat64Result(), err
+	}
+
+	return handleDoubleResponse(result)
 }
