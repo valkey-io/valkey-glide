f58864f93a7e0ae7c69547e61c1376066cbcf685
Janhavi Gupta <janhavigupta@google.com>
2024-12-31 06:51:22 +0000

    Go:Fix doc comments and formatting
    Signed-off-by: Janhavi Gupta <janhavigupta@google.com>

--- go/api/generic_cluster_commands.go
+++ go/api/generic_cluster_commands.go
@@ -2,15 +2,17 @@
 
 package api
 
-// GenericCommands supports commands for the "Generic Commands" group for standalone client.
+// GenericClusterCommands supports commands for the "Generic Commands" group for cluster client.
 //
 // See [valkey.io] for details.
 //
 // [valkey.io]: https://valkey.io/commands/#generic
-type GenericCommands interface {
+type GenericClusterCommands interface {
 	// CustomCommand executes a single command, specified by args, without checking inputs. Every part of the command, including
 	// the command name and subcommands, should be added as a separate value in args. The returning value depends on the executed
 	// command.
+	//
+	// The command will be routed automatically based on the passed command's default request policy.
 	//
 	// See [Valkey GLIDE Wiki] for details on the restrictions and limitations of the custom command API.
 	//
@@ -25,9 +27,10 @@ type GenericCommands interface {
 	//	The returned value for the custom command.
 	//
 	// For example:
+	//
 	//	result, err := client.CustomCommand([]string{"ping"})
-	//	result.(string): "PONG"
+	//	result.Value().(string): "PONG"
 	//
 	// [Valkey GLIDE Wiki]: https://github.com/valkey-io/valkey-glide/wiki/General-Concepts#custom-command
-	CustomCommand(args []string) (interface{}, error)
+	CustomCommand(args []string) (ClusterValue[interface{}], error)
 }
---
+++ go/api/server_management_cluster_commands.go
@@ -0,0 +1,11 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+package api
+
+// ServerManagementClusterCommands supports commands and transactions for the "Server Management" group for a cluster client.
+//
+// See [valkey.io] for details.
+//
+// [valkey.io]: https://valkey.io/commands/#server
+type ServerManagementClusterCommands interface {
+}
