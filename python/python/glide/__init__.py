8ef529b4ddf1aca5e4795a19c8130c09c17af54c
Yi-Pin Chen <yi-pin.chen@improving.com>
2025-01-02 15:14:37 -0800

    [Merge to main] Support transactions for JSON commands (#2862)
    Java, Node, Python: Add transaction commands for JSON module
    
    ---------
    Signed-off-by: Yi-Pin Chen <yi-pin.chen@improving.com>

--- python/python/glide/__init__.py
+++ python/python/glide/__init__.py
@@ -32,7 +32,7 @@ from glide.async_commands.core import (
     InsertPosition,
     UpdateOptions,
 )
-from glide.async_commands.server_modules import ft, glide_json
+from glide.async_commands.server_modules import ft, glide_json, json_transaction
 from glide.async_commands.server_modules.ft_options.ft_aggregate_options import (
     FtAggregateApply,
     FtAggregateClause,
@@ -271,6 +271,7 @@ __all__ = [
     "PubSubMsg",
     # Json
     "glide_json",
+    "json_transaction",
     "JsonGetOptions",
     "JsonArrIndexOptions",
     "JsonArrPopOptions",
