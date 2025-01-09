6c66640be38592cc96ac6232254b5a96e7c430be
Prateek Kumar <prateek.kumar@improving.com>
2025-01-09 09:58:28 -0800

    Go: Zcount documentation updated
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- go/api/options/zcount_options.go
+++ go/api/options/zcount_options.go
@@ -4,6 +4,7 @@ package options
 
 import "github.com/valkey-io/valkey-glide/go/glide/utils"
 
+// The common interface for representing all the range type for Zcount command.
 type ScoreRange interface {
 	ToArgs() ([]string, error)
 }
@@ -19,14 +20,17 @@ const (
 	NegativeInfinity InfBoundary = "-inf"
 )
 
+// This struct represents the infinity boundary for a score range.
 type InfScoreBound struct {
 	value InfBoundary
 }
 
+// Create a new infinite score boundary
 func NewInfScoreBoundBuilder() *InfScoreBound {
 	return &InfScoreBound{}
 }
 
+// The value of the infinite score bound.
 func (infScoreBound *InfScoreBound) SetValue(value InfBoundary) *InfScoreBound {
 	infScoreBound.value = value
 	return infScoreBound
@@ -38,20 +42,24 @@ func (infScoreBound *InfScoreBound) ToArgs() ([]string, error) {
 	return args, nil
 }
 
+// This struct represents score boundary for a bound.
 type ScoreBoundary struct {
 	bound       float64
 	isInclusive bool
 }
 
+// Create a new score boundary.
 func NewScoreBoundaryBuilder() *ScoreBoundary {
 	return &ScoreBoundary{isInclusive: true}
 }
 
+// Set the bound for a score boundary.
 func (scoreBoundary *ScoreBoundary) SetBound(bound float64) *ScoreBoundary {
 	scoreBoundary.bound = bound
 	return scoreBoundary
 }
 
+// Set if the bound for a score boundary is inclusive or not inclusive in the boundary.
 func (scoreBoundary *ScoreBoundary) SetIsInclusive(isInclusive bool) *ScoreBoundary {
 	scoreBoundary.isInclusive = isInclusive
 	return scoreBoundary
@@ -67,20 +75,24 @@ func (scoreBoundary *ScoreBoundary) ToArgs() ([]string, error) {
 	return args, nil
 }
 
+// This struct represents the min and max boundary for the Zcount command.
 type ZCountRange struct {
 	min ScoreRange
 	max ScoreRange
 }
 
+// Create a new Zcount range.
 func NewZCountRangeBuilder() *ZCountRange {
 	return &ZCountRange{}
 }
 
+// Set the minimum value for the Zcount command range.
 func (zCountRange *ZCountRange) SetMin(min ScoreRange) *ZCountRange {
 	zCountRange.min = min
 	return zCountRange
 }
 
+// Set the maximum value for the Zcount command range.
 func (zCountRange *ZCountRange) SetMax(max ScoreRange) *ZCountRange {
 	zCountRange.max = max
 	return zCountRange
