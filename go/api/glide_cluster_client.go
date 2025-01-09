bef751dce91758dcf7cf18aa2ac4b784a13d10f6
janhavigupta007 <46344506+janhavigupta007@users.noreply.github.com>
2025-01-09 13:14:36 +0530

    GO: Fixing docs and interfaces (#2897)
    * Go:Fix doc comments and formatting
    
    Signed-off-by: Janhavi Gupta <janhavigupta@google.com>

--- go/api/glide_cluster_client.go
+++ go/api/glide_cluster_client.go
@@ -6,48 +6,31 @@ package api
 // #include "../lib.h"
 import "C"
 
+// GlideClusterClient interface compliance check.
+var _ GlideClusterClient = (*glideClusterClient)(nil)
+
 // GlideClusterClient is a client used for connection in cluster mode.
-type GlideClusterClient struct {
+type GlideClusterClient interface {
+	BaseClient
+	GenericClusterCommands
+}
+
+// glideClusterClient implements cluster mode operations by extending baseClient functionality.
+type glideClusterClient struct {
 	*baseClient
 }
 
 // NewGlideClusterClient creates a [GlideClusterClient] in cluster mode using the given [GlideClusterClientConfiguration].
-func NewGlideClusterClient(config *GlideClusterClientConfiguration) (*GlideClusterClient, error) {
+func NewGlideClusterClient(config *GlideClusterClientConfiguration) (GlideClusterClient, error) {
 	client, err := createClient(config)
 	if err != nil {
 		return nil, err
 	}
 
-	return &GlideClusterClient{client}, nil
+	return &glideClusterClient{client}, nil
 }
 
-// CustomCommand executes a single command, specified by args, without checking inputs. Every part of the command, including
-// the command name and subcommands, should be added as a separate value in args. The returning value depends on the executed
-// command.
-//
-// The command will be routed automatically based on the passed command's default request policy.
-//
-// See [Valkey GLIDE Wiki] for details on the restrictions and limitations of the custom command API.
-//
-// This function should only be used for single-response commands. Commands that don't return complete response and awaits
-// (such as SUBSCRIBE), or that return potentially more than a single response (such as XREAD), or that change the client's
-// behavior (such as entering pub/sub mode on RESP2 connections) shouldn't be called using this function.
-//
-// Parameters:
-//
-//	args - Arguments for the custom command including the command name.
-//
-// Return value:
-//
-//	The returned value for the custom command.
-//
-// For example:
-//
-//	result, err := client.CustomCommand([]string{"ping"})
-//	result.Value().(string): "PONG"
-//
-// [Valkey GLIDE Wiki]: https://github.com/valkey-io/valkey-glide/wiki/General-Concepts#custom-command
-func (client *GlideClusterClient) CustomCommand(args []string) (ClusterValue[interface{}], error) {
+func (client *glideClusterClient) CustomCommand(args []string) (ClusterValue[interface{}], error) {
 	res, err := client.executeCommand(C.CustomCommand, args)
 	if err != nil {
 		return CreateEmptyClusterValue(), err
