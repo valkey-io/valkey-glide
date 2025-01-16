e30993816341ed12f779bf60e9328a6ca9dbf389
Adar Ovadia <adarov@amazon.com>
2025-01-16 07:56:47 +0000

    add OpenTelemetry client configs
    Signed-off-by: Adar Ovadia <adarov@amazon.com>

--- node/src/BaseClient.ts
+++ node/src/BaseClient.ts
@@ -647,6 +647,17 @@ export interface BaseClientConfiguration {
      * ```
      */
     clientAz?: string;
+    /**
+     * Configs for OpenTelemetry if exits.
+     */
+    openTelemetryConfigs?: {
+        collectorEndPoint: string;
+        /**
+         * The duration in milliseconds the data will export to the collector.
+         * If interval isn't supplied, 500 will be used
+         */
+        spanFlushInterval?: number;
+    }
 }
 
 /**
