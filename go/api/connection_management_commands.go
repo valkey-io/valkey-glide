8b2fe205e865d3725711d1f851a7901d100db389
Edric Cuartero <ecuartero@google.com>
2025-01-18 01:57:17 +0800

    GO: Implement Echo Command (#2863)
    * Implement Echo Command
    
    Signed-off-by: EdricCua <ecuartero@google.com>

--- go/api/connection_management_commands.go
+++ go/api/connection_management_commands.go
@@ -32,4 +32,23 @@ type ConnectionManagementCommands interface {
 	//
 	// [valkey.io]: https://valkey.io/commands/ping/
 	PingWithMessage(message string) (string, error)
+
+	// Echo the provided message back.
+	// The command will be routed a random node.
+	//
+	// Parameters:
+	// 	message - The provided message.
+	//
+	// Return value:
+	//  The provided message
+	//
+	// For example:
+	//  result, err := client.Echo("Hello World")
+	//	if err != nil {
+	//	    // handle error
+	//	}
+	//	fmt.Println(result.Value()) // Output: Hello World
+	//
+	// [valkey.io]: https://valkey.io/commands/echo/
+	Echo(message string) (Result[string], error)
 }
