26c41cb02687732fa959b306412de3e699df515e
tjzhang-BQ <111323543+tjzhang-BQ@users.noreply.github.com>
2025-01-17 17:13:54 -0800

    Go: update return types & response handlers for scan commands (#2956)
    * Go: update return types & response handlers for scan commands
    
    Signed-off-by: TJ Zhang <tj.zhang@improving.com>

--- go/integTest/test_utils.go
+++ go/integTest/test_utils.go
@@ -2,28 +2,26 @@
 
 package integTest
 
-import "github.com/valkey-io/valkey-glide/go/glide/api"
-
 // check if sliceA is a subset of sliceB
-func isSubset(sliceA []api.Result[string], sliceB []api.Result[string]) bool {
-	setB := make(map[string]struct{})
+func isSubset[T comparable](sliceA []T, sliceB []T) bool {
+	setB := make(map[T]struct{})
 	for _, v := range sliceB {
-		setB[v.Value()] = struct{}{}
+		setB[v] = struct{}{}
 	}
 	for _, v := range sliceA {
-		if _, found := setB[v.Value()]; !found {
+		if _, found := setB[v]; !found {
 			return false
 		}
 	}
 	return true
 }
 
-func convertMapKeysAndValuesToResultList(m map[string]string) ([]api.Result[string], []api.Result[string]) {
-	keys := make([]api.Result[string], 0)
-	values := make([]api.Result[string], 0)
+func convertMapKeysAndValuesToLists(m map[string]string) ([]string, []string) {
+	keys := make([]string, 0)
+	values := make([]string, 0)
 	for key, value := range m {
-		keys = append(keys, api.CreateStringResult(key))
-		values = append(values, api.CreateStringResult(value))
+		keys = append(keys, key)
+		values = append(values, value)
 	}
 	return keys, values
 }
