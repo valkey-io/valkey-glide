74e490424817521204e623e6bead4996f392ddb7
Prateek Kumar <prateek.kumar@improving.com>
2025-01-20 15:16:24 -0800

    Go: Add XAck command : documentation updated
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- go/api/base_client.go
+++ go/api/base_client.go
@@ -2957,6 +2957,10 @@ func (client *baseClient) XGroupDelConsumer(
 //
 // For example:
 //
+//	//Assuming streamId1 and streamId2 already exist.
+//	xackResult, err := client.XAck("key", "groupName", []string{"streamId1", "streamId2"})
+//	fmt.Println(xackResult) // 2
+//
 // [valkey.io]: https://valkey.io/commands/xack/
 func (client *baseClient) XAck(key string, group string, ids []string) (int64, error) {
 	result, err := client.executeCommand(C.XAck, append([]string{key, group}, ids...))
