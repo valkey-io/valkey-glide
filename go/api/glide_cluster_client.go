d0b68b2480894960300baafee98fabbf910ba788
EdricCua <ecuartero@google.com>
2025-01-23 03:13:24 +0000

    Implement DBSize Server Management Cluster
    Signed-off-by: EdricCua <ecuartero@google.com>

--- go/api/glide_cluster_client.go
+++ go/api/glide_cluster_client.go
@@ -5,6 +5,7 @@ package api
 // #cgo LDFLAGS: -L../target/release -lglide_rs
 // #include "../lib.h"
 import "C"
+import "github.com/valkey-io/valkey-glide/go/glide/api/options"
 
 // GlideClusterClient interface compliance check.
 var _ GlideClusterClient = (*glideClusterClient)(nil)
@@ -40,4 +41,27 @@ func (client *glideClusterClient) CustomCommand(args []string) (ClusterValue[int
 		return CreateEmptyClusterValue(), err
 	}
 	return CreateClusterValue(data), nil
+}
+
+// Returns the number of keys in the currently selected database.
+//
+// Return value:
+//
+//	The number of keys in the currently selected database.
+//
+// Example:
+//
+//	result, err := client.DBSize()
+//	if err != nil {
+//		// handle error
+//	}
+//	fmt.Println(result) // Output: 1
+//
+// [valkey.io]: https://valkey.io/commands/dbsize/
+func (client *glideClusterClient) DBSizeWithOptions(opts *options.DBSizeOptions) (int64, error) {
+	result, err := client.executeCommandWithRoute(C.DBSize, []string{}, opts.Route)
+	if err != nil {
+		return handleIntOrNilResponse, err
+	}
+	return handleIntResponse(result)
 }
---
+++ go/api/server_management_cluster_commands.go
@@ -0,0 +1,14 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+package api
+
+import "github.com/valkey-io/valkey-glide/go/glide/api/options"
+
+// ServerManagementClusterCommands supports commands for the "Server Management Commands" group for cluster client.
+//
+// See [valkey.io] for details.
+//
+// [valkey.io]: https://valkey.io/commands/#server
+type ServerManagementClusterCommands interface {
+	DBSizeWithOptions(dbSizeOptions *options.DBSizeOptions) (int64, error)
+}
