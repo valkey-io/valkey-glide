6af0e9383c7eb6f2b8b899e14e596e57ccd093bf
jbrinkman <joe.brinkman@improving.com>
2025-01-27 11:38:33 -0500

    add initial framework examples
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

---
+++ go/api/set_commands_test.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"fmt"
+)
+
+func ExampleGlideClient_SAdd() {
+	var client *GlideClient = getExampleGlideClient() // example helper function
+	result, err := client.SAdd("my_set2", []string{"member1", "member2"})
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+	fmt.Println(result)
+
+	// Output: 2
+}
+
+func ExampleGlideClient_SRem() {
+	var client *GlideClient = getExampleGlideClient() // example helper function
+	result, err := client.SRem("my_set2", []string{"member1", "member2"})
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+	fmt.Println(result)
+
+	// Output: 2
+}
