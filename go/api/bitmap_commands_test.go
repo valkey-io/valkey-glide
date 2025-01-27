2d9e9cd516f954b2d8409316498689660e9935d2
jbrinkman <joe.brinkman@improving.com>
2025-01-27 16:30:56 -0500

    add string commands examples
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- go/api/bitmap_commands_test.go
+++ go/api/bitmap_commands_test.go
@@ -8,6 +8,7 @@ import (
 
 func ExampleGlideClient_SetBit() {
 	var client *GlideClient = getExampleGlideClient() // example helper function
+
 	result, err := client.SetBit("my_key", 1, 1)
 	if err != nil {
 		fmt.Println("Glide example failed with an error: ", err)
@@ -19,6 +20,7 @@ func ExampleGlideClient_SetBit() {
 
 func ExampleGlideClient_GetBit() {
 	var client *GlideClient = getExampleGlideClient() // example helper function
+
 	result, err := client.GetBit("my_key", 1)
 	if err != nil {
 		fmt.Println("Glide example failed with an error: ", err)
@@ -30,6 +32,7 @@ func ExampleGlideClient_GetBit() {
 
 func ExampleGlideClient_BitCount() {
 	var client *GlideClient = getExampleGlideClient() // example helper function
+
 	result, err := client.BitCount("my_key")
 	if err != nil {
 		fmt.Println("Glide example failed with an error: ", err)
@@ -41,6 +44,7 @@ func ExampleGlideClient_BitCount() {
 
 func ExampleGlideClient_BitCountWithOptions() {
 	var client *GlideClient = getExampleGlideClient() // example helper function
+
 	options := options.NewBitCountOptionsBuilder().
 		SetStart(1).
 		SetEnd(1).
