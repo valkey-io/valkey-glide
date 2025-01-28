f91f4209ad3ed8b15418f8d5adcc539aeb5505bc
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-01-28 13:23:50 -0800

    Go: `INFO`. (#2988)
    * Go: `INFO`.
    
    ---------
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- go/api/glide_client.go
+++ go/api/glide_client.go
@@ -92,7 +92,7 @@ func (client *GlideClient) CustomCommand(args []string) (interface{}, error) {
 func (client *GlideClient) ConfigSet(parameters map[string]string) (string, error) {
 	result, err := client.executeCommand(C.ConfigSet, utils.MapToString(parameters))
 	if err != nil {
-		return "", err
+		return defaultStringResponse, err
 	}
 	return handleStringResponse(result)
 }
@@ -145,7 +145,59 @@ func (client *GlideClient) ConfigGet(args []string) (map[string]string, error) {
 func (client *GlideClient) Select(index int64) (string, error) {
 	result, err := client.executeCommand(C.Select, []string{utils.IntToString(index)})
 	if err != nil {
-		return "", err
+		return defaultStringResponse, err
+	}
+
+	return handleStringResponse(result)
+}
+
+// Gets information and statistics about the server.
+//
+// See [valkey.io] for details.
+//
+// Return value:
+//
+//	A string with the information for the default sections.
+//
+// Example:
+//
+//	response, err := standaloneClient.Info(opts)
+//	if err != nil {
+//		// handle error
+//	}
+//	fmt.Println(response)
+//
+// [valkey.io]: https://valkey.io/commands/info/
+func (client *GlideClient) Info() (string, error) {
+	return client.InfoWithOptions(InfoOptions{[]Section{}})
+}
+
+// Gets information and statistics about the server.
+//
+// See [valkey.io] for details.
+//
+// Parameters:
+//
+//	options - Additional command parameters, see [InfoOptions] for more details.
+//
+// Return value:
+//
+//	A string containing the information for the sections requested.
+//
+// Example:
+//
+//	opts := api.InfoOptions{Sections: []api.Section{api.Server}}
+//	response, err := standaloneClient.InfoWithOptions(opts)
+//	if err != nil {
+//		// handle error
+//	}
+//	fmt.Println(response)
+//
+// [valkey.io]: https://valkey.io/commands/info/
+func (client *GlideClient) InfoWithOptions(options InfoOptions) (string, error) {
+	result, err := client.executeCommand(C.Info, options.toArgs())
+	if err != nil {
+		return defaultStringResponse, err
 	}
 
 	return handleStringResponse(result)
--- go/integTest/cluster_commands_test.go
+++ go/integTest/cluster_commands_test.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/valkey-io/valkey-glide/go/glide/api"
+	"github.com/valkey-io/valkey-glide/go/glide/api/config"
 )
 
 func (suite *GlideTestSuite) TestClusterCustomCommandInfo() {
@@ -14,7 +16,7 @@ func (suite *GlideTestSuite) TestClusterCustomCommandInfo() {
 
 	assert.Nil(suite.T(), err)
 	// INFO is routed to all primary nodes by default
-	for _, value := range result.Value().(map[string]interface{}) {
+	for _, value := range result.MultiValue() {
 		assert.True(suite.T(), strings.Contains(value.(string), "# Stats"))
 	}
 }
@@ -25,5 +27,82 @@ func (suite *GlideTestSuite) TestClusterCustomCommandEcho() {
 
 	assert.Nil(suite.T(), err)
 	// ECHO is routed to a single random node
-	assert.Equal(suite.T(), "GO GLIDE GO", result.Value().(string))
+	assert.Equal(suite.T(), "GO GLIDE GO", result.SingleValue().(string))
+}
+
+func (suite *GlideTestSuite) TestInfoCluster() {
+	DEFAULT_INFO_SECTIONS := []string{
+		"Server",
+		"Clients",
+		"Memory",
+		"Persistence",
+		"Stats",
+		"Replication",
+		"CPU",
+		"Modules",
+		"Errorstats",
+		"Cluster",
+		"Keyspace",
+	}
+
+	client := suite.defaultClusterClient()
+	t := suite.T()
+
+	// info without options
+	data, err := client.Info()
+	assert.NoError(t, err)
+	for _, info := range data {
+		for _, section := range DEFAULT_INFO_SECTIONS {
+			assert.Contains(t, info, "# "+section, "Section "+section+" is missing")
+		}
+	}
+
+	// info with option or with multiple options without route
+	sections := []api.Section{api.Cpu}
+	if suite.serverVersion >= "7.0.0" {
+		sections = append(sections, api.Memory)
+	}
+	opts := api.ClusterInfoOptions{
+		InfoOptions: &api.InfoOptions{Sections: sections},
+		Route:       nil,
+	}
+	response, err := client.InfoWithOptions(opts)
+	assert.NoError(t, err)
+	assert.True(t, response.IsMultiValue())
+	for _, info := range response.MultiValue() {
+		for _, section := range sections {
+			assert.Contains(t, strings.ToLower(info), strings.ToLower("# "+string(section)), "Section "+section+" is missing")
+		}
+	}
+
+	// same sections with random route
+	opts = api.ClusterInfoOptions{
+		InfoOptions: &api.InfoOptions{Sections: sections},
+		Route:       config.RandomRoute.ToPtr(),
+	}
+	response, err = client.InfoWithOptions(opts)
+	assert.NoError(t, err)
+	assert.True(t, response.IsSingleValue())
+	for _, section := range sections {
+		assert.Contains(
+			t,
+			strings.ToLower(response.SingleValue()),
+			strings.ToLower("# "+string(section)),
+			"Section "+section+" is missing",
+		)
+	}
+
+	// default sections, multi node route
+	opts = api.ClusterInfoOptions{
+		InfoOptions: nil,
+		Route:       config.AllPrimaries.ToPtr(),
+	}
+	response, err = client.InfoWithOptions(opts)
+	assert.NoError(t, err)
+	assert.True(t, response.IsMultiValue())
+	for _, info := range response.MultiValue() {
+		for _, section := range DEFAULT_INFO_SECTIONS {
+			assert.Contains(t, info, "# "+section, "Section "+section+" is missing")
+		}
+	}
 }
