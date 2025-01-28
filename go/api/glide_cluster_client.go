be05a996739003de77d54d76cb98b339d3e08483
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-01-28 13:23:50 -0800

    Go: `INFO`. (#2988)
    * Go: `INFO`.
    
    ---------
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- go/api/glide_cluster_client.go
+++ go/api/glide_cluster_client.go
@@ -13,6 +13,7 @@ var _ GlideClusterClientCommands = (*GlideClusterClient)(nil)
 type GlideClusterClientCommands interface {
 	BaseClient
 	GenericClusterCommands
+	ServerManagementClusterCommands
 }
 
 // GlideClusterClient implements cluster mode operations by extending baseClient functionality.
@@ -61,11 +62,101 @@ func NewGlideClusterClient(config *GlideClusterClientConfiguration) (GlideCluste
 func (client *GlideClusterClient) CustomCommand(args []string) (ClusterValue[interface{}], error) {
 	res, err := client.executeCommand(C.CustomCommand, args)
 	if err != nil {
-		return CreateEmptyClusterValue(), err
+		return createEmptyClusterValue[interface{}](), err
 	}
 	data, err := handleInterfaceResponse(res)
 	if err != nil {
-		return CreateEmptyClusterValue(), err
+		return createEmptyClusterValue[interface{}](), err
 	}
-	return CreateClusterValue(data), nil
+	return createClusterValue[interface{}](data), nil
+}
+
+// Gets information and statistics about the server.
+//
+// The command will be routed to all primary nodes.
+//
+// See [valkey.io] for details.
+//
+// Return value:
+//
+//	A map where each address is the key and its corresponding node response is the information for the default sections.
+//
+// Example:
+//
+//	response, err := clusterClient.Info(opts)
+//	if err != nil {
+//		// handle error
+//	}
+//	for node, data := range response {
+//		fmt.Printf("%s node returned %s\n", node, data)
+//	}
+//
+// [valkey.io]: https://valkey.io/commands/info/
+func (client *GlideClusterClient) Info() (map[string]string, error) {
+	result, err := client.executeCommand(C.Info, []string{})
+	if err != nil {
+		return nil, err
+	}
+
+	return handleStringToStringMapResponse(result)
+}
+
+// Gets information and statistics about the server.
+//
+// The command will be routed to all primary nodes, unless `route` in [ClusterInfoOptions] is provided.
+//
+// See [valkey.io] for details.
+//
+// Parameters:
+//
+//	options - Additional command parameters, see [ClusterInfoOptions] for more details.
+//
+// Return value:
+//
+//	When specifying a route other than a single node or when route is not given,
+//	it returns a map where each address is the key and its corresponding node response is the value.
+//	When a single node route is given, command returns a string containing the information for the sections requested.
+//
+// Example:
+//
+//	opts := api.ClusterInfoOptions{
+//		InfoOptions: &api.InfoOptions{Sections: []api.Section{api.Server}},
+//		Route: api.RandomRoute.ToPtr(),
+//	}
+//	response, err := clusterClient.InfoWithOptions(opts)
+//	if err != nil {
+//		// handle error
+//	}
+//	// Command sent to a single random node via RANDOM route, expecting SingleValue result as a `string`.
+//	fmt.Println(response.SingleValue())
+//
+// [valkey.io]: https://valkey.io/commands/info/
+func (client *GlideClusterClient) InfoWithOptions(options ClusterInfoOptions) (ClusterValue[string], error) {
+	if options.Route == nil {
+		response, err := client.executeCommand(C.Info, options.toArgs())
+		if err != nil {
+			return createEmptyClusterValue[string](), err
+		}
+		data, err := handleStringToStringMapResponse(response)
+		if err != nil {
+			return createEmptyClusterValue[string](), err
+		}
+		return createClusterMultiValue[string](data), nil
+	}
+	response, err := client.executeCommandWithRoute(C.Info, options.toArgs(), *options.Route)
+	if err != nil {
+		return createEmptyClusterValue[string](), err
+	}
+	if (*options.Route).IsMultiNode() {
+		data, err := handleStringToStringMapResponse(response)
+		if err != nil {
+			return createEmptyClusterValue[string](), err
+		}
+		return createClusterMultiValue[string](data), nil
+	}
+	data, err := handleStringResponse(response)
+	if err != nil {
+		return createEmptyClusterValue[string](), err
+	}
+	return createClusterSingleValue[string](data), nil
 }
