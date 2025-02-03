28edfda8a999e3b84479c74baacd831c88136523
Adar Ovadia <adarov@amazon.com>
2025-02-03 13:18:08 +0000

    fix pr comments
    Signed-off-by: Adar Ovadia <adarov@amazon.com>

--- glide-core/src/client/types.rs
+++ glide-core/src/client/types.rs
@@ -25,8 +25,8 @@ pub struct ConnectionRequest {
     pub periodic_checks: Option<PeriodicCheck>,
     pub pubsub_subscriptions: Option<redis::PubSubSubscriptionInfo>,
     pub inflight_requests_limit: Option<u32>,
-    pub open_telemetry_endpoint: Option<String>,
-    pub open_telemetry_span_interval: Option<u64>,
+    pub otel_endpoint: Option<String>,
+    pub otel_span_flush_interval_ms: Option<u64>,
 }
 
 pub struct AuthenticationInfo {
@@ -208,9 +208,8 @@ impl From<protobuf::ConnectionRequest> for ConnectionRequest {
 
         let inflight_requests_limit = none_if_zero(value.inflight_requests_limit);
 
-        let open_telemetry_endpoint =
-            chars_to_string_option(&value.opentelemetry_config.collector_end_point);
-        let open_telemetry_span_interval = value.opentelemetry_config.span_flush_interval;
+        let otel_endpoint = chars_to_string_option(&value.opentelemetry_config.collector_end_point);
+        let otel_span_flush_interval_ms = value.opentelemetry_config.span_flush_interval;
 
         ConnectionRequest {
             read_from,
@@ -227,8 +226,8 @@ impl From<protobuf::ConnectionRequest> for ConnectionRequest {
             periodic_checks,
             pubsub_subscriptions,
             inflight_requests_limit,
-            open_telemetry_endpoint,
-            open_telemetry_span_interval,
+            otel_endpoint,
+            otel_span_flush_interval_ms,
         }
     }
 }
