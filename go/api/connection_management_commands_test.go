a95b79c4b55ccc8945fe449574feac8fa8352ea1
jbrinkman <joe.brinkman@improving.com>
2025-02-03 20:21:39 -0500

    cleanup rebase issues
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- go/api/connection_management_commands_test.go
+++ go/api/connection_management_commands_test.go
@@ -15,17 +15,6 @@ func ExampleGlideClient_Ping() {
 	// Output: PONG
 }
 
-func ExampleGlideClient_PingWithMessage() {
-	var client *GlideClient = getExampleGlideClient() // example helper function
-	result, err := client.PingWithMessage("Hello")
-	if err != nil {
-		fmt.Println("Glide example failed with an error: ", err)
-	}
-	fmt.Println(result)
-
-	// Output: Hello
-}
-
 func ExampleGlideClient_Echo() {
 	var client *GlideClient = getExampleGlideClient() // example helper function
 	result, err := client.Echo("Hello World")
