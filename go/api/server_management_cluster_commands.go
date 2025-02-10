36579ababe2ca7514e3e0e7d1c0a1b5bd67a9b1e
EdricCua <ecuartero@google.com>
2025-02-10 19:38:59 +0000

    Implement COnfigResetStat
    Signed-off-by: EdricCua <ecuartero@google.com>

--- go/api/server_management_cluster_commands.go
+++ go/api/server_management_cluster_commands.go
@@ -17,4 +17,6 @@ type ServerManagementClusterCommands interface {
 	TimeWithOptions(routeOption options.RouteOption) (ClusterValue[[]string], error)
 
 	DBSizeWithOptions(routeOption options.RouteOption) (int64, error)
+
+	ConfigResetStatWithOptions(routeOption options.RouteOption) (string, error)
 }
