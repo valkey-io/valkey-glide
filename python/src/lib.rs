e0d3205bfe0d1a3e7072831d6779366283b17889
Edward Liang <76571219+edlng@users.noreply.github.com>
2025-02-26 18:07:10 -0800

    revert changes
    Signed-off-by: Edward Liang <76571219+edlng@users.noreply.github.com>

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,5 +1,4 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
-#![allow(clippy::useless_conversion)]
 
 use bytes::Bytes;
 use glide_core::client::FINISHED_SCAN_CURSOR;
