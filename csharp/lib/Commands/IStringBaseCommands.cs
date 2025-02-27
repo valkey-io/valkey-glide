3d813efc3479325e74c1762b7da10f543bffea83
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-02-26 19:53:40 -0800

    Client structure.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

---
+++ csharp/lib/Commands/IStringBaseCommands.cs
@@ -0,0 +1,39 @@
+﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+namespace Glide.Commands;
+
+/// <summary>
+/// Supports commands for the "String Commands" group for standalone and cluster clients.
+/// <br/>
+/// See more on <see href="https://valkey.io/commands/?group=string">valkey.io</see>.
+/// </summary>
+internal interface IStringBaseCommands
+{
+    /// <summary>
+    /// Sets the given <paramref name="key"/> with the given value.
+    /// </summary>
+    /// <param name="key">The <paramref name="key"/> to store.</param>
+    /// <param name="value">The value to store with the given <paramref name="key"/>.</param>
+    /// <returns>A simple <c>"OK"</c> response.</returns>
+    /// <example>
+    /// <code>
+    /// string result = await client.Set(key, value);
+    /// </code>
+    /// </example>
+    Task<string?> Set(string key, string value);
+
+    /// <summary>
+    /// Gets the value associated with the given <paramref name="key"/>, or <see langword="null"/> if no such <paramref name="key"/> exists.
+    /// </summary>
+    /// <param name="key">The <paramref name="key"/> to retrieve from the database.</param>
+    /// <returns>
+    /// If <paramref name="key"/> exists, returns the value of <paramref name="key"/> as a <see langword="string"/>.<br/>
+    /// Otherwise, returns <c>null</c>.
+    /// </returns>
+    /// <example>
+    /// <code>
+    /// string value = await client.Get(key);
+    /// </code>
+    /// </example>
+    Task<string?> Get(string key);
+}
