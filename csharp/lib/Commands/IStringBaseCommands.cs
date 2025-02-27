d24074304367b1abc96ead15b209661c3880806d
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-02-26 20:00:14 -0800

    fix
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- csharp/lib/Commands/IStringBaseCommands.cs
+++ csharp/lib/Commands/IStringBaseCommands.cs
@@ -10,30 +10,30 @@ namespace Glide.Commands;
 internal interface IStringBaseCommands
 {
     /// <summary>
-    /// Sets the given <paramref name="key"/> with the given value.
-    /// </summary>
-    /// <param name="key">The <paramref name="key"/> to store.</param>
-    /// <param name="value">The value to store with the given <paramref name="key"/>.</param>
-    /// <returns>A simple <c>"OK"</c> response.</returns>
+    /// Sets the given <paramref name="key"/> with the given <paramref name="value"/>.
     /// <example>
     /// <code>
     /// string result = await client.Set(key, value);
     /// </code>
     /// </example>
+    /// </summary>
+    /// <param name="key">The <paramref name="key"/> to store.</param>
+    /// <param name="value">The value to store with the given <paramref name="key"/>.</param>
+    /// <returns>A simple <c>"OK"</c> response.</returns>
     Task<string?> Set(string key, string value);
 
     /// <summary>
     /// Gets the value associated with the given <paramref name="key"/>, or <see langword="null"/> if no such <paramref name="key"/> exists.
-    /// </summary>
-    /// <param name="key">The <paramref name="key"/> to retrieve from the database.</param>
-    /// <returns>
-    /// If <paramref name="key"/> exists, returns the value of <paramref name="key"/> as a <see langword="string"/>.<br/>
-    /// Otherwise, returns <c>null</c>.
-    /// </returns>
     /// <example>
     /// <code>
     /// string value = await client.Get(key);
     /// </code>
     /// </example>
+    /// </summary>
+    /// <param name="key">The <paramref name="key"/> to retrieve from the database.</param>
+    /// <returns>
+    /// If <paramref name="key"/> exists, returns the value of <paramref name="key"/> as a <see langword="string"/>.<br/>
+    /// Otherwise, returns <see langword="null"/>.
+    /// </returns>
     Task<string?> Get(string key);
 }
