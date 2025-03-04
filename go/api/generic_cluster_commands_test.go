e84f3934cbe64fbae9a3bd54d1b9846e3bb8f87d
Edward Liang <76571219+edlng@users.noreply.github.com>
2025-03-04 15:38:16 -0800

    Go: Implemented Cluster Scan (#3295)
    * Added cluster scan functionality
    
    Signed-off-by: Edward Liang <76571219+edlng@users.noreply.github.com>
    
    * Added safety docs to rust code and extra memory free command
    
    Signed-off-by: Edward Liang <76571219+edlng@users.noreply.github.com>
    
    * Added documentation and fixes
    
    Signed-off-by: Edward Liang <76571219+edlng@users.noreply.github.com>
    
    * remove leftover todo
    
    Signed-off-by: Edward Liang <76571219+edlng@users.noreply.github.com>
    
    * modified types
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * included mutex changes and changelog updates
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * added some pr fixes
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * small fixes to docs
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * provide support for invalid utf8 characters
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * added better error callback
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * removed unnecessary exposed function and added safety docs
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * modified for scan to return cursor object
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * fix lint
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * fixed safety note
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * removed unnecessary safety docs
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    * removed cursor arg as a pointer
    
    Signed-off-by: Edward Liang <edward.liang@improving.com>
    
    ---------
    
    Signed-off-by: Edward Liang <76571219+edlng@users.noreply.github.com>
    Signed-off-by: Edward Liang <edward.liang@improving.com>

--- go/api/generic_cluster_commands_test.go
+++ go/api/generic_cluster_commands_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/valkey-io/valkey-glide/go/api/config"
+	"github.com/valkey-io/valkey-glide/go/api/options"
 )
 
 func ExampleGlideClusterClient_CustomCommand() {
@@ -27,4 +28,187 @@ func ExampleGlideClusterClient_CustomCommandWithRoute() {
 	fmt.Println(result.SingleValue().(string))
 
 	// Output: PONG
+}
+
+func ExampleGlideClusterClient_Scan() {
+	var client *GlideClusterClient = getExampleGlideClusterClient() // example helper function
+
+	keysToSet := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+
+	_, err := client.MSet(keysToSet)
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	cursor := *options.NewClusterScanCursor()
+	allKeys := []string{}
+
+	for !cursor.HasFinished() {
+		var keys []string
+		cursor, keys, err = client.Scan(cursor)
+		if err != nil {
+			fmt.Println("Glide example failed with an error: ", err)
+		}
+		allKeys = append(allKeys, keys...)
+	}
+
+	// Elements will contain values [key1 key2 key3] but because order
+	// can vary, we just check the length
+	fmt.Println(len(allKeys))
+
+	// Output: 3
+}
+
+func ExampleGlideClusterClient_ScanWithOptions_match() {
+	var client *GlideClusterClient = getExampleGlideClusterClient() // example helper function
+
+	keysToSet := map[string]string{
+		"key-1":         "value1",
+		"key-2":         "value2",
+		"key3":          "value3",
+		"nonPatternKey": "value4",
+	}
+
+	_, err := client.MSet(keysToSet)
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	_, err = client.SAdd("someKey", []string{"value"})
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	cursor := *options.NewClusterScanCursor()
+	opts := options.NewClusterScanOptions().SetMatch("key-*")
+	allKeys := []string{}
+
+	for !cursor.HasFinished() {
+		var keys []string
+		cursor, keys, err = client.ScanWithOptions(cursor, *opts)
+		if err != nil {
+			fmt.Println("Glide example failed with an error: ", err)
+		}
+		allKeys = append(allKeys, keys...)
+	}
+
+	// Elements will contain values [key-1 key-2] but because order
+	// can vary, we just check the length
+	fmt.Println(len(allKeys))
+
+	// Output: 2
+}
+
+func ExampleGlideClusterClient_ScanWithOptions_matchNonUTF8() {
+	var client *GlideClusterClient = getExampleGlideClusterClient() // example helper function
+
+	keysToSet := map[string]string{
+		"key\xc0\xc1-1": "value1",
+		"key-2":         "value2",
+		"key\xf9\xc1-3": "value3",
+	}
+
+	_, err := client.MSet(keysToSet)
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	_, err = client.SAdd("someKey", []string{"value"})
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	cursor := *options.NewClusterScanCursor()
+	opts := options.NewClusterScanOptions().SetMatch("key\xc0\xc1-*")
+	allKeys := []string{}
+
+	for !cursor.HasFinished() {
+		var keys []string
+		cursor, keys, err = client.ScanWithOptions(cursor, *opts)
+		if err != nil {
+			fmt.Println("Glide example failed with an error: ", err)
+		}
+		allKeys = append(allKeys, keys...)
+	}
+
+	// Elements will contain value [key\xc0\xc1-1] but since it is
+	// an invalid utf8 character, we just check the length
+	fmt.Println(len(allKeys))
+
+	// Output: 1
+}
+
+func ExampleGlideClusterClient_ScanWithOptions_count() {
+	var client *GlideClusterClient = getExampleGlideClusterClient() // example helper function
+
+	keysToSet := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+
+	_, err := client.MSet(keysToSet)
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	cursor := *options.NewClusterScanCursor()
+	opts := options.NewClusterScanOptions().SetCount(10)
+	allKeys := []string{}
+
+	for !cursor.HasFinished() {
+		var keys []string
+		cursor, keys, err = client.ScanWithOptions(cursor, *opts)
+		if err != nil {
+			fmt.Println("Glide example failed with an error: ", err)
+		}
+		allKeys = append(allKeys, keys...)
+	}
+
+	// Elements will contain values [key1 key2 key3] but because order
+	// can vary, we just check the length
+	fmt.Println(len(allKeys))
+
+	// Output: 3
+}
+
+func ExampleGlideClusterClient_ScanWithOptions_type() {
+	var client *GlideClusterClient = getExampleGlideClusterClient() // example helper function
+
+	keysToSet := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+
+	_, err := client.MSet(keysToSet)
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	_, err = client.SAdd("someKey", []string{"value"})
+	if err != nil {
+		fmt.Println("Glide example failed with an error: ", err)
+	}
+
+	cursor := *options.NewClusterScanCursor()
+	opts := options.NewClusterScanOptions().SetType(options.ObjectTypeSet)
+	allKeys := []string{}
+
+	for !cursor.HasFinished() {
+		var keys []string
+		cursor, keys, err = client.ScanWithOptions(cursor, *opts)
+		if err != nil {
+			fmt.Println("Glide example failed with an error: ", err)
+		}
+		allKeys = append(allKeys, keys...)
+	}
+
+	fmt.Println(allKeys)
+
+	// Output: [someKey]
 }
