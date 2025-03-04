943a2e566456c100e9805ff6f71f02fcc888f1c5
TJ Zhang <tj.zhang@improving.com>
2025-03-03 23:21:36 -0800

    move doc to setter
    Signed-off-by: TJ Zhang <tj.zhang@improving.com>

--- go/api/config.go
+++ go/api/config.go
@@ -389,10 +389,6 @@ func (config *GlideClusterClientConfiguration) WithAdvancedConfiguration(
 // Advanced configuration settings class for creating a client. Shared settings for standalone and
 // cluster clients.
 type AdvancedBaseClientConfiguration struct {
-	// The duration in milliseconds to wait for a TCP/TLS connection to complete. This applies both
-	// during initial client creation and any reconnections that may occur during request processing.
-	// Note: A high connection timeout may lead to prolonged blocking of the entire command
-	// pipeline. If not explicitly set, a default value of 250 milliseconds will be used.
 	connectionTimeout int
 }
 
@@ -407,6 +403,10 @@ func NewAdvancedGlideClientConfiguration() *AdvancedGlideClientConfiguration {
 }
 
 // WithConnectionTimeout sets the duration in milliseconds to wait for a TCP/TLS connection to complete.
+// The duration in milliseconds to wait for a TCP/TLS connection to complete. This applies both
+// during initial client creation and any reconnections that may occur during request processing.
+// Note: A high connection timeout may lead to prolonged blocking of the entire command
+// pipeline. If not explicitly set, a default value of 250 milliseconds will be used.
 func (config *AdvancedGlideClientConfiguration) WithConnectionTimeout(
 	connectionTimeout int,
 ) *AdvancedGlideClientConfiguration {
