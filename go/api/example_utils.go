80152e8a0cf8e249e52afb3e8f074212185584f7
Joseph Brinkman <joe.brinkman@improving.com>
2025-03-04 16:47:31 -0500

    Go: randomize cluster ports for Example tests (#3225)
    * add custom flags for example tests
    
    * update example utils to use randomized ports
    * don't run example tests against Redis 6.2 servers
    
    ---------
    
    Signed-off-by: Joseph Brinkman <joe.brinkman@improving.com>
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- go/api/example_utils.go
+++ go/api/example_utils.go
@@ -3,49 +3,108 @@
 package api
 
 import (
+	"flag"
 	"fmt"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/valkey-io/valkey-glide/go/api/config"
 )
+
+var (
+	clusterNodes   = flag.String("clusternodes", "", "AddressNodes for running Valkey/Redis cluster nodes")
+	standaloneNode = flag.String("standalonenode", "", "AddressNode for running Valkey/Redis standalone node")
+)
+
+var (
+	clusterClient *GlideClusterClient
+	clusterOnce   sync.Once
+)
+
+var (
+	standaloneClient *GlideClient
+	standaloneOnce   sync.Once
+)
+
+var initOnce sync.Once
+
+func initFlags() {
+	// Parse flags only once after the test framework has had a chance to set the flags.
+	// This is necessary because the test framework sets the flags after the init function is called.
+	initOnce.Do(func() {
+		flag.Parse()
+	})
+}
 
 // getExampleGlideClient returns a GlideClient instance for testing purposes.
 // This function is used in the examples of the GlideClient methods.
 func getExampleGlideClient() *GlideClient {
-	config := NewGlideClientConfiguration().
-		WithAddress(new(NodeAddress)) // use default address
+	standaloneOnce.Do(func() {
+		initFlags()
+		addresses := parseHosts(*standaloneNode)
+		config := NewGlideClientConfiguration().
+			WithAddress(&addresses[0]) // use default address
 
-	client, err := NewGlideClient(config)
-	if err != nil {
-		fmt.Println("error connecting to database: ", err)
-	}
+		client, err := NewGlideClient(config)
+		if err != nil {
+			fmt.Println("error connecting to database: ", err)
+		}
 
-	_, err = client.CustomCommand([]string{"FLUSHALL"}) // todo: replace with client.FlushAll() when implemented
+		standaloneClient = client.(*GlideClient)
+	})
+
+	// Flush the database before each test to ensure a clean state.
+	_, err := standaloneClient.CustomCommand([]string{"FLUSHALL"}) // todo: replace with client.FlushAll() when implemented
 	if err != nil {
 		fmt.Println("error flushing database: ", err)
 	}
 
-	return client.(*GlideClient)
+	return standaloneClient
 }
 
 func getExampleGlideClusterClient() *GlideClusterClient {
-	config := NewGlideClusterClientConfiguration().
-		WithAddress(&NodeAddress{Host: "localhost", Port: 7001}).
-		WithRequestTimeout(5000)
+	clusterOnce.Do(func() {
+		initFlags()
+		addresses := parseHosts(*clusterNodes)
+		config := NewGlideClusterClientConfiguration().
+			WithAddress(&addresses[0]).
+			WithRequestTimeout(5000)
 
-	client, err := NewGlideClusterClient(config)
-	if err != nil {
-		fmt.Println("error connecting to database: ", err)
-	}
+		client, err := NewGlideClusterClient(config)
+		if err != nil {
+			fmt.Println("error connecting to database: ", err)
+		}
 
-	_, err = client.CustomCommand([]string{"FLUSHALL"}) // todo: replace with client.FlushAll() when implemented
+		clusterClient = client.(*GlideClusterClient)
+	})
+
+	// Flush the database before each test to ensure a clean state.
+	_, err := clusterClient.CustomCommandWithRoute([]string{"FLUSHALL"}, config.AllPrimaries) // todo: replace with client.FlushAll() when implemented
 	if err != nil {
 		fmt.Println("error flushing database: ", err)
 	}
 
-	return client.(*GlideClusterClient)
+	return clusterClient
 }
 
-// CompareUnorderedSlices compares two unordered slices of structs and returns if both are equal.
-// func CompareUnorderedSlices[T any](slice1, slice2 []T) bool {
-// 	return cmp.Equal(slice1, slice2, cmpopts.SortSlices(func(a, b T) bool {
-// 		return fmt.Sprintf("%v", a) < fmt.Sprintf("%v", b)
-// 	}))
-// }
+func parseHosts(addresses string) []NodeAddress {
+	var result []NodeAddress
+
+	if addresses == "" {
+		result = append(result, *new(NodeAddress))
+	} else {
+		addressList := strings.Split(addresses, ",")
+		for _, address := range addressList {
+			parts := strings.Split(address, ":")
+			port, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Printf("Failed to parse port from string %s: %s", parts[1], err.Error())
+				continue
+			}
+
+			result = append(result, NodeAddress{Host: parts[0], Port: port})
+		}
+	}
+	return result
+}
