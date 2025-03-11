1d9c3d786e5f9b37d9fce9a1766f26e1a3f0a961
Adar Ovadia <adarov@amazon.com>
2025-03-11 08:43:50 +0000

    add prints for ffi calls
    Signed-off-by: Adar Ovadia <adarov@amazon.com>

--- node/tests/OpenTelemetry.test.ts
+++ node/tests/OpenTelemetry.test.ts
@@ -3,7 +3,9 @@
  */
 
 import { afterAll, afterEach, beforeAll, describe } from "@jest/globals";
-import { GlideClient, GlideClusterClient, ProtocolVersion } from "..";
+import { createLeakedOtelSpan } from "glide-rs";
+import Long from "long";
+import { GlideClusterClient, ProtocolVersion } from "..";
 import ValkeyCluster from "../../utils/TestUtils";
 import { flushAndCloseClient, getClientConfigurationOption, getServerVersion, parseEndpoints } from "./TestUtilities";
 
@@ -39,12 +41,24 @@ describe("OpenTelemetry GlideClusterClient", () => {
         }
     });
 
-    it.each([ProtocolVersion.RESP2, ProtocolVersion.RESP3])(
+    it.each([ProtocolVersion.RESP2])(
         `GlideClusterClient test basic openTelemetry_%p`,
         async (protocol) => {
+            if (global.gc) {
+                global.gc(); // Run garbage collection
+            }
+            const startMemory = process.memoryUsage().heapUsed;
             client = await GlideClusterClient.createClient(
                 getClientConfigurationOption(cluster.getAddresses(), protocol),
             );
+
+            if (global.gc) {
+                global.gc(); // Run GC again to clean up
+            }
+            const endMemory = process.memoryUsage().heapUsed;
+
+            console.log(`Memory before: ${startMemory}, after: ${endMemory}`);
+            expect(endMemory).toBeLessThan(startMemory * 1.1); // Allow small fluctuations
         },
         TIMEOUT,
     );
@@ -53,29 +67,51 @@ describe("OpenTelemetry GlideClusterClient", () => {
 
 //standalone tests
 describe("OpenTelemetry GlideClient", () => {
-    const testsFailed = 0;
-    let cluster: ValkeyCluster;
-    let client: GlideClient;
-    beforeAll(async () => {
-        const standaloneAddresses = global.STAND_ALONE_ENDPOINT;
-        cluster = standaloneAddresses
-            ? await ValkeyCluster.initFromExistingCluster(
-                  false,
-                  parseEndpoints(standaloneAddresses),
-                  getServerVersion,
-              )
-            : await ValkeyCluster.createCluster(false, 1, 1, getServerVersion);
-    }, 20000);
+    // const testsFailed = 0;
+    // let cluster: ValkeyCluster;
+    // let client: GlideClient;
+    // beforeAll(async () => {
+    //     const standaloneAddresses = global.STAND_ALONE_ENDPOINT;
+    //     cluster = standaloneAddresses
+    //         ? await ValkeyCluster.initFromExistingCluster(
+    //               false,
+    //               parseEndpoints(standaloneAddresses),
+    //               getServerVersion,
+    //           )
+    //         : await ValkeyCluster.createCluster(false, 1, 1, getServerVersion);
+    // }, 20000);
 
-    afterEach(async () => {
-        await flushAndCloseClient(false, cluster.getAddresses(), client);
-    });
+    // afterEach(async () => {
+    //     await flushAndCloseClient(false, cluster.getAddresses(), client);
+    // });
 
-    afterAll(async () => {
-        if (testsFailed === 0) {
-            await cluster.close();
-        } else {
-            await cluster.close(true);
-        }
-    });
+    // afterAll(async () => {
+    //     if (testsFailed === 0) {
+    //         await cluster.close();
+    //     } else {
+    //         await cluster.close(true);
+    //     }
+    // });
+
+    // it.each([ProtocolVersion.RESP2, ProtocolVersion.RESP3])(
+    //     `GlideClient test basic openTelemetry_%p`,
+    //     async (protocol) => {
+    //         client = await GlideClient.createClient(
+    //             getClientConfigurationOption(cluster.getAddresses(), protocol),
+    //         );
+    //     },
+    //     TIMEOUT,
+    // );
+
+    it.each([ProtocolVersion.RESP2, ProtocolVersion.RESP3])(
+        `GlideClient test basic openTelemetry_%p`,
+        async (protocol) => {
+            let span_ptr = createLeakedOtelSpan("testSpanFFI");
+            console.log(span_ptr);
+            let span_long = new Long(span_ptr[0], span_ptr[1]);
+            console.log(span_long.toString());
+            
+        },
+        TIMEOUT,
+    );
 });
