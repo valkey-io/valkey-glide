fcca50c26c4764e7d99d35148ae4a8ef70f3e75f
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-03-11 19:55:44 -0700

    Fix client config marshalling
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- csharp/lib/ConnectionConfiguration.cs
+++ csharp/lib/ConnectionConfiguration.cs
@@ -12,20 +12,28 @@ public abstract class ConnectionConfiguration
     {
         public nuint AddressCount;
         public IntPtr Addresses; // ** NodeAddress - array pointer
+        [MarshalAs(UnmanagedType.U1)]
         public bool HasTlsMode;
         public TlsMode TlsMode;
+        [MarshalAs(UnmanagedType.U1)]
         public bool ClusterMode;
+        [MarshalAs(UnmanagedType.U1)]
         public bool HasRequestTimeout;
         public uint RequestTimeout;
+        [MarshalAs(UnmanagedType.U1)]
         public bool HasConnectionTimeout;
         public uint ConnectionTimeout;
+        [MarshalAs(UnmanagedType.U1)]
         public bool HasReadFrom;
         public ReadFrom ReadFrom;
+        [MarshalAs(UnmanagedType.U1)]
         public bool HasConnectionRetryStrategy;
         public RetryStrategy ConnectionRetryStrategy;
+        [MarshalAs(UnmanagedType.U1)]
         public bool HasAuthenticationInfo;
         public AuthenticationInfo AuthenticationInfo;
         public uint DatabaseId;
+        [MarshalAs(UnmanagedType.U1)]
         public bool HasProtocol;
         public Protocol Protocol;
         [MarshalAs(UnmanagedType.LPStr)]
