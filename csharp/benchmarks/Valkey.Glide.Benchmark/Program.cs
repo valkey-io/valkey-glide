7613cc8146d9c8f44722d28237a0a662eb4fb5d8
X39 <marco@silipo.de>
2025-03-19 01:21:06 +0100

    fix benchmarkdotnet build

--- csharp/benchmarks/Valkey.Glide.Benchmark/Program.cs
+++ csharp/benchmarks/Valkey.Glide.Benchmark/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using Valkey.Glide.Benchmark;
 
-Summary? summary = BenchmarkRunner.Run<BlockingVsTask>();
+var config = DefaultConfig.Instance;
+var solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(
+    Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(typeof(Program).Assembly.Location))))))
+    ?? throw new NullReferenceException("Failed to get solution directory");
+config = config.AddJob(Job.Default.WithArguments([new MsBuildArgument($"/p:SolutionDir={solutionDir}")]));
+Summary? summary = BenchmarkRunner.Run<BlockingVsTask>(config);
