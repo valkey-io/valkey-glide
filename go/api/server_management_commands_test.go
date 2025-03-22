665680554f2e7ab6177f0531a541dc2a0b8ce144
EdricCua <ecuartero@google.com>
2025-03-22 15:33:46 +0000

    fix unit test
    Signed-off-by: EdricCua <ecuartero@google.com>

--- go/api/server_management_commands_test.go
+++ go/api/server_management_commands_test.go
@@ -184,7 +184,7 @@ func ExampleGlideClient_ConfigRewrite() {
 		if err != nil {
 			fmt.Println("Glide example failed with an error: ", err)
 		}
-		resultRewrite = "response" + response + err
+		resultRewrite = response
 	} else {
 		resultRewrite = "OK"
 	}
