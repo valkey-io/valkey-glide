8864f8ff65d21d8057f0ded795252496c56da32a
Marco Silipo <marco@silipo.de>
2025-04-03 19:13:54 +0200

    C#: Reorganize Structure (#3362)
    Reorganize C# sub-project structure
    
    Signed-off-by: X39 <marco@silipo.de>

--- csharp/sources/Valkey.Glide/GlideClient.cs
+++ csharp/sources/Valkey.Glide/GlideClient.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-using Glide.Commands;
+using Valkey.Glide.Commands;
 
-using static Glide.ConnectionConfiguration;
+using static Valkey.Glide.ConnectionConfiguration;
 
-namespace Glide;
+namespace Valkey.Glide;
 
 public sealed class GlideClient(StandaloneClientConfiguration config) : BaseClient(config), IConnectionManagementCommands, IGenericCommands
 {
