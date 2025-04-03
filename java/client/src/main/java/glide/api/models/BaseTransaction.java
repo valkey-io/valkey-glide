ac27a8d2a4590aea09e825779d8166f7b1ae6d01
Shoham Elias <116083498+shohamazon@users.noreply.github.com>
2025-04-03 22:47:07 +0300

    Pipeline: add support for error handling (#3359)
    ---------
    
    Signed-off-by: Shoham Elias <shohame@amazon.com>

--- java/client/src/main/java/glide/api/models/BaseTransaction.java
+++ java/client/src/main/java/glide/api/models/BaseTransaction.java
@@ -320,6 +320,7 @@ import lombok.NonNull;
 @Getter
 public abstract class BaseTransaction<T extends BaseTransaction<T>> {
     /** Command class to send a single request to Valkey. */
+    // TODO: add support for timeout, raiseOnError and retryStrategy
     protected final Batch.Builder protobufTransaction = Batch.newBuilder().setIsAtomic(true);
 
     /**
