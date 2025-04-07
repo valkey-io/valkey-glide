fb00a276f0590491f14acffbd545da76fe31c4cb
jbrinkman <joe.brinkman@improving.com>
2025-04-07 10:29:02 -0400

    refactor: replace numeric PushKind representation with enum for clarity and safety
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- go/api/pubsub_message.go
+++ go/api/pubsub_message.go
@@ -2,9 +2,11 @@
 
 package api
 
+// #include "../lib.h"
+import "C"
+
 import (
 	"encoding/json"
-	"fmt"
 )
 
 type PubSubMessage struct {
@@ -37,91 +39,8 @@ func (msg *PubSubMessage) ToString() string {
 	return string(jsonBytes)
 }
 
-// PushKind represents the type of push message received from the server
-type PushKind int
-
-// PushKind enum values - these match the numeric values from the Rust side
-const (
-	Disconnection PushKind = iota // 0
-	Other                         // 1
-	Invalidate                    // 2
-	Message                       // 3
-	PMessage                      // 4
-	SMessage                      // 5
-	Unsubscribe                   // 6
-	PUnsubscribe                  // 7
-	SUnsubscribe                  // 8
-	Subscribe                     // 9
-	PSubscribe                    // 10
-	SSubscribe                    // 11
-)
-
-// String returns the string representation of a PushKind
-func (kind PushKind) String() string {
-	switch kind {
-	case Disconnection:
-		return "Disconnection"
-	case Other:
-		return "Other"
-	case Invalidate:
-		return "Invalidate"
-	case Message:
-		return "Message"
-	case PMessage:
-		return "PMessage"
-	case SMessage:
-		return "SMessage"
-	case Unsubscribe:
-		return "Unsubscribe"
-	case PUnsubscribe:
-		return "PUnsubscribe"
-	case SUnsubscribe:
-		return "SUnsubscribe"
-	case Subscribe:
-		return "Subscribe"
-	case PSubscribe:
-		return "PSubscribe"
-	case SSubscribe:
-		return "SSubscribe"
-	default:
-		return fmt.Sprintf("Unknown(%d)", kind)
-	}
-}
-
-// PushKindFromString converts a string representation to a PushKind
-func PushKindFromString(s string) PushKind {
-	switch s {
-	case "Disconnection":
-		return Disconnection
-	case "Other":
-		return Other
-	case "Invalidate":
-		return Invalidate
-	case "Message":
-		return Message
-	case "PMessage":
-		return PMessage
-	case "SMessage":
-		return SMessage
-	case "Unsubscribe":
-		return Unsubscribe
-	case "PUnsubscribe":
-		return PUnsubscribe
-	case "SUnsubscribe":
-		return SUnsubscribe
-	case "Subscribe":
-		return Subscribe
-	case "PSubscribe":
-		return PSubscribe
-	case "SSubscribe":
-		return SSubscribe
-	default:
-		return Other
-	}
-}
-
 // PushInfo represents a message received from the server
 type PushInfo struct {
-	Kind    PushKind
+	Kind    C.PushKind
 	Message *PubSubMessage
 }
