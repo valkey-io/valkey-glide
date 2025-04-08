9c2033888187a60110bed4fb5585b76c0c1d55c0
jbrinkman <joe.brinkman@improving.com>
2025-04-08 16:14:38 -0400

    fix: remove unused PubSub command methods to streamline interface
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- go/api/pubsub_commands.go
+++ go/api/pubsub_commands.go
@@ -6,18 +6,8 @@ package api
 type PubSubCommands interface {
 	// Publish publishes a message to a channel. Returns the number of clients that received the message.
 	Publish(message string, channel string) (int64, error)
-	PubSubChannels() ([]string, error)
-	PubSubChannelsWithPattern(pattern string) ([]string, error)
-	PubSubNumPat() (int64, error)
-	PubSubNumSub(channels []string) (map[string]int64, error)
-	SetMessageHandler(handler *MessageHandler)
-	GetMessageHandler() *MessageHandler
 }
 
 // PubSubClusterCommands defines additional Pub/Sub operations available only in cluster mode.
 type PubSubClusterCommands interface {
-	PublishShard(channel string, message string) (int64, error)
-	PubSubShardChannels() ([]string, error)
-	PubSubShardChannelsWithPattern(pattern string) ([]string, error)
-	PubSubShardNumSub(channels []string) (map[string]int64, error)
 }
