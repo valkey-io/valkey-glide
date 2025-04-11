d4d1949fe200edf221e0bc2191f297bb2f7d5923
Joseph Brinkman <joe.brinkman@improving.com>
2025-04-11 16:20:28 -0400

    Update ffi/src/lib.rs
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -528,8 +528,8 @@ fn create_client_internal(
 
     // Create the client adapter that will be returned and used as conn_ptr
     let core = Arc::new(CommandExecutionCore {
-        client: client.clone(),
-        client_type: client_type.clone(),
+        client: client,
+        client_type: client_type,
     });
     let client_adapter = Arc::new(ClientAdapter { runtime, core });
     // Clone client_adapter before moving it into the async block
