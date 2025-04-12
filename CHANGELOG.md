71d751d8274424806b30fee1e86a3acd89328ecf
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-04-11 18:47:46 -0700

    Docs: Fix docs and examples for client class and constructor. (#3418)
    * Fix docs and examples.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Co-authored-by: Edward Liang <76571219+edlng@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -26,6 +26,7 @@
 * Go: Add `FLUSHDB` ([#3117](https://github.com/valkey-io/valkey-glide/pull/3117))
 * Go: Add `PFMERGE` ([#3082](https://github.com/valkey-io/valkey-glide/pull/3082))
 * Go: Add password update api ([#3346](https://github.com/valkey-io/valkey-glide/pull/3346))
+* Add/update documentation for `GlideClient` and `GlideClusterClient` and for factory constructor for all clients ([#3418](https://github.com/valkey-io/valkey-glide/pull/3418))
 * Go: Add `BITOP` ([#3384](https://github.com/valkey-io/valkey-glide/pull/3384))
 * Go: Add `GeoHash` ([#3439](https://github.com/valkey-io/valkey-glide/pull/3439))
 * Go/Core: Move FFI to a dedicated folder for reusability ([#3372](https://github.com/valkey-io/valkey-glide/pull/3372))
--- java/client/src/main/java/glide/api/GlideClusterClient.java
+++ java/client/src/main/java/glide/api/GlideClusterClient.java
@@ -65,9 +65,12 @@ import glide.api.models.commands.batch.ClusterBatchOptions;
 import glide.api.models.commands.function.FunctionRestorePolicy;
 import glide.api.models.commands.scan.ClusterScanCursor;
 import glide.api.models.commands.scan.ScanOptions;
+import glide.api.models.configuration.BaseClientConfiguration;
+import glide.api.models.configuration.ClusterSubscriptionConfiguration;
 import glide.api.models.configuration.GlideClusterClientConfiguration;
 import glide.api.models.configuration.RequestRoutingConfiguration.Route;
 import glide.api.models.configuration.RequestRoutingConfiguration.SingleNodeRoute;
+import glide.api.models.configuration.ServerCredentials;
 import glide.ffi.resolvers.ClusterScanCursorResolver;
 import glide.managers.CommandManager;
 import glide.utils.ArgsBuilder;
@@ -82,7 +85,14 @@ import java.util.stream.Stream;
 import lombok.NonNull;
 import response.ResponseOuterClass.Response;
 
-/** Async (non-blocking) client for Cluster mode. Use {@link #createClient} to request a client. */
+/**
+ * Client used for connection to cluster servers.<br>
+ * Use {@link #createClient} to request a client.
+ *
+ * @see For full documentation refer to <a
+ *     href="https://github.com/valkey-io/valkey-glide/wiki/Java-Wrapper#cluster">Valkey Glide
+ *     Wiki</a>.
+ */
 public class GlideClusterClient extends BaseClient
         implements ConnectionManagementClusterCommands,
                 GenericClusterCommands,
@@ -97,10 +107,49 @@ public class GlideClusterClient extends BaseClient
     }
 
     /**
-     * Async request for an async (non-blocking) client in Cluster mode.
+     * Creates a new {@link GlideClusterClient} instance and establishes connections to a Valkey
+     * Cluster.
      *
-     * @param config Glide cluster client Configuration.
-     * @return A Future to connect and return a GlideClusterClient.
+     * @param config The configuration options for the client, including cluster addresses,
+     *     authentication credentials, TLS settings, periodic checks, and Pub/Sub subscriptions.
+     * @return A Future that resolves to a connected {@link GlideClusterClient} instance.
+     * @remarks Use this static method to create and connect a {@link GlideClusterClient} to a Valkey
+     *     Cluster. The client will automatically handle connection establishment, including cluster
+     *     topology discovery and handling of authentication and TLS configurations.
+     *     <ul>
+     *       <li><b>Cluster Topology Discovery</b>: The client will automatically discover the cluster
+     *           topology based on the seed addresses provided.
+     *       <li><b>Authentication</b>: If {@link ServerCredentials} are provided, the client will
+     *           attempt to authenticate using the specified username and password.
+     *       <li><b>TLS</b>: If {@link
+     *           BaseClientConfiguration.BaseClientConfigurationBuilder#useTLS(boolean)} is set to
+     *           <code>true</code>, the client will establish secure connections using TLS.
+     *       <li><b>Pub/Sub Subscriptions</b>: Any channels or patterns specified in {@link
+     *           ClusterSubscriptionConfiguration} will be subscribed to upon connection.
+     *     </ul>
+     *
+     * @example
+     *     <pre>{@code
+     * GlideClusterClientConfiguration config =
+     *     GlideClusterClientConfiguration.builder()
+     *         .address(node1address)
+     *         .address(node2address)
+     *         .useTLS(true)
+     *         .readFrom(ReadFrom.PREFER_REPLICA)
+     *         .credentials(credentialsConfiguration)
+     *         .requestTimeout(2000)
+     *         .clientName("GLIDE")
+     *         .subscriptionConfiguration(
+     *             ClusterSubscriptionConfiguration.builder()
+     *                 .subscription(EXACT, "notifications")
+     *                 .subscription(EXACT, "news")
+     *                 .subscription(SHARDED, "data")
+     *                 .callback(callback)
+     *                 .build())
+     *         .inflightRequestsLimit(1000)
+     *         .build();
+     * GlideClusterClient client = GlideClusterClient.createClient(config).get();
+     * }</pre>
      */
     public static CompletableFuture<GlideClusterClient> createClient(
             @NonNull GlideClusterClientConfiguration config) {
