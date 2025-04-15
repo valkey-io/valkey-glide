a95f7a2a29ec4632d9873a9953b21172806659dc
Prateek Kumar <prateek.kumar@improving.com>
2025-04-15 03:03:48 -0700

    Added glide_cluster_client invokeScript methods
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- go/api/glide_cluster_client.go
+++ go/api/glide_cluster_client.go
@@ -1409,6 +1409,117 @@ func (client *GlideClusterClient) FCallReadOnlyWithArgs(function string, args []
 	return client.FCallReadOnlyWithArgsWithRoute(function, args, options.RouteOption{})
 }
 
+// InvokeScript executes a Lua script on the server.
+//
+// Parameters:
+//
+//	script - The script to execute.
+//
+// Return value:
+//
+//	The result of the script execution.
+//
+// See [valkey.io] for details.
+//
+// [valkey.io]: https://valkey.io/commands/eval/
+func (client *GlideClusterClient) InvokeScript(script *options.Script) (ClusterValue[any], error) {
+	return client.InvokeScriptWithRoute(script, options.RouteOption{})
+}
+
+// InvokeScriptWithOptions executes a Lua script on the server with additional options.
+//
+// Parameters:
+//
+//	script - The script to execute.
+//	scriptOptions - Options for script execution including keys and arguments.
+//
+// Return value:
+//
+//	The result of the script execution.
+//
+// See [valkey.io] for details.
+//
+// [valkey.io]: https://valkey.io/commands/eval/
+func (client *GlideClusterClient) InvokeScriptWithOptions(script *options.Script, scriptOptions *options.ScriptOptions) (ClusterValue[any], error) {
+	return client.InvokeScriptWithOptionsAndRoute(script, scriptOptions, options.RouteOption{})
+}
+
+// InvokeScriptWithRoute executes a Lua script on the server with routing information.
+//
+// Parameters:
+//
+//	script - The script to execute.
+//	route - Routing information for the script execution.
+//
+// Return value:
+//
+//	The result of the script execution.
+//
+// See [valkey.io] for details.
+//
+// [valkey.io]: https://valkey.io/commands/eval/
+func (client *GlideClusterClient) InvokeScriptWithRoute(script *options.Script, route options.RouteOption) (ClusterValue[any], error) {
+	response, err := client.baseClient.executeScriptWithRoute(script.GetHash(), []string{}, []string{}, route.Route)
+	if err != nil {
+		return createEmptyClusterValue[any](), err
+	}
+
+	result, err := handleAnyResponse(response)
+	if err != nil {
+		return createEmptyClusterValue[any](), err
+	}
+
+	if route.Route != nil && route.Route.IsMultiNode() {
+		data, err := handleStringToAnyMapResponse(result)
+		if err != nil {
+			return createEmptyClusterValue[any](), err
+		}
+		return createClusterMultiValue[any](data), nil
+	}
+
+	return createClusterSingleValue[any](result), nil
+}
+
+// InvokeScriptWithOptionsAndRoute executes a Lua script on the server with additional options and routing information.
+//
+// Parameters:
+//
+//	script - The script to execute.
+//	scriptOptions - Options for script execution including keys and arguments.
+//	route - Routing information for the script execution.
+//
+// Return value:
+//
+//	The result of the script execution.
+//
+// See [valkey.io] for details.
+//
+// [valkey.io]: https://valkey.io/commands/eval/
+func (client *GlideClusterClient) InvokeScriptWithOptionsAndRoute(script *options.Script, scriptOptions *options.ScriptOptions, route options.RouteOption) (ClusterValue[any], error) {
+	keys := scriptOptions.GetKeys()
+	args := scriptOptions.GetArgs()
+
+	response, err := client.baseClient.executeScriptWithRoute(script.GetHash(), keys, args, route.Route)
+	if err != nil {
+		return createEmptyClusterValue[any](), err
+	}
+
+	result, err := handleAnyResponse(response)
+	if err != nil {
+		return createEmptyClusterValue[any](), err
+	}
+
+	if route.Route != nil && route.Route.IsMultiNode() {
+		data, err := handleStringToAnyMapResponse(result)
+		if err != nil {
+			return createEmptyClusterValue[any](), err
+		}
+		return createClusterMultiValue[any](data), nil
+	}
+
+	return createClusterSingleValue[any](result), nil
+}
+
 // FunctionStats returns information about the function that's currently running and information about the
 // available execution engines.
 // The command will be routed to all nodes by default.
