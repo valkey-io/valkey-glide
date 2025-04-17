ec5d3dd3bf6a9e22fa627eb07cc7b27206c6c2ea
jbrinkman <joe.brinkman@improving.com>
2025-04-17 11:58:46 -0400

    Add Cluster support, refactor PubSub tests, and remove deprecated files
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- go/api/pubsub_commands.go
+++ go/api/pubsub_commands.go
@@ -5,7 +5,7 @@ package api
 // PubSubCommands defines the interface for Pub/Sub operations available in both standalone and cluster modes.
 type PubSubCommands interface {
 	// Publish publishes a message to a channel. Returns the number of clients that received the message.
-	Publish(message string, channel string) (int64, error)
+	Publish(channel string, message string) (int64, error)
 }
 
 // PubSubClusterCommands defines additional Pub/Sub operations available only in cluster mode.
