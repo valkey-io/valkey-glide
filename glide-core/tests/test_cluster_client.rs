f503eee51295e4451a0ff6b4785e44833d4545d9
adarovadya <adarovadya@gmail.com>
2025-04-21 14:32:54 +0300

    Node: add span command to measure command latency (#3391)
    * create ffi call to create and drop a span from node
    
    * handle span transaction and pipeline
    
    * Core/Node: add open telemetry config for metrics and traces
    
    ---------
    
    Signed-off-by: Adar Ovadia <adarov@amazon.com>
    Co-authored-by: Adar Ovadia <adarov@amazon.com>
    Signed-off-by: adarovadya <adarovadya@gmail.com>

--- glide-core/tests/test_cluster_client.rs
+++ glide-core/tests/test_cluster_client.rs
@@ -325,8 +325,9 @@ mod cluster_client_tests {
             connection_request.addresses = addresses.iter().map(get_address_info).collect();
 
             let mut op = OpenTelemetryConfig::new();
-            op.collector_end_point = "http://valid-url.com".into();
-            op.span_flush_interval = Some(300);
+            op.traces_collector_endpoint = "http://valid-url.com".into();
+            op.metrics_collector_endpoint = "http://valid-url.com".into();
+            op.flush_interval = Some(300);
 
             connection_request.opentelemetry_config = protobuf::MessageField::from_option(Some(op));
 
--- node/rust-client/src/lib.rs
+++ node/rust-client/src/lib.rs
@@ -2,6 +2,7 @@
 
 use glide_core::Telemetry;
 use glide_core::errors::error_message;
+use glide_core::{GlideOpenTelemetry, GlideSpan, Telemetry};
 use redis::GlideConnectionOptions;
 
 #[cfg(not(target_env = "msvc"))]
@@ -15,7 +16,6 @@ use byteorder::{LittleEndian, WriteBytesExt};
 use bytes::Bytes;
 use glide_core::MAX_REQUEST_ARGS_LENGTH;
 use glide_core::start_socket_listener;
-#[cfg(feature = "testing_utilities")]
 use napi::bindgen_prelude::BigInt;
 use napi::bindgen_prelude::Either;
 use napi::bindgen_prelude::Uint8Array;
@@ -26,6 +26,7 @@ use redis::{AsyncCommands, Value, aio::MultiplexedConnection};
 #[cfg(feature = "testing_utilities")]
 use std::collections::HashMap;
 use std::ptr::from_mut;
+use std::sync::Arc;
 use tokio::runtime::{Builder, Runtime};
 #[napi]
 pub enum Level {
@@ -415,6 +416,35 @@ pub fn create_leaked_bigint(big_int: BigInt) -> [u32; 2] {
 pub fn create_leaked_double(float: f64) -> [u32; 2] {
     let pointer = from_mut(Box::leak(Box::new(Value::Double(float))));
     split_pointer(pointer)
+}
+
+/// Creates an open telemetry span with the given name and returns a pointer to the span
+#[napi(ts_return_type = "[number, number]")]
+pub fn create_leaked_otel_span(name: String) -> [u32; 2] {
+    let span = GlideOpenTelemetry::new_span(&name);
+    let s = Arc::into_raw(Arc::new(span)) as *mut GlideSpan;
+    split_pointer(s)
+}
+
+#[napi]
+pub fn drop_otel_span(span_ptr: BigInt) {
+    let (is_negative, span_ptr, lossless) = span_ptr.get_u64();
+    let error_msg = if is_negative {
+        "Received a negative pointer value."
+    } else if !lossless {
+        "Some data was lost in the conversion to u64."
+    } else if span_ptr == 0 {
+        "Received a zero pointer value."
+    } else {
+        unsafe { Arc::from_raw(span_ptr as *const GlideSpan) };
+        return;
+    };
+
+    log(
+        Level::Error,
+        "OpenTelemetry".to_string(),
+        format!("Failed to drop span. {}", error_msg),
+    );
 }
 
 #[napi]
