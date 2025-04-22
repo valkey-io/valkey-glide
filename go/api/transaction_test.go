010e23bb0612cbf1dbb1611636957de3024846c7
EdricCua <ecuartero@google.com>
2025-04-22 10:29:27 +0000

    Added transaction_test
    Signed-off-by: EdricCua <ecuartero@google.com>

---
+++ go/api/transaction_test.go
@@ -0,0 +1,25 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+package api
+
+import (
+	"fmt"
+
+	"github.com/valkey-io/valkey-glide/go/api"
+)
+
+func ExampleGlideClient_Del() {
+	var client *GlideClient = getExampleGlideClient() // example helper function
+
+	tx := api.NewTransaction(client)
+	cmd := tx.GlideClient
+	cmd.Set("key123", "Glide")
+	cmd.Get("key123")
+	cmd.Del([]string{"key3"})
+
+	result, err := tx.Exec()
+	fmt.Println(result)
+
+	// Output:
+	// [OK Glide 1]
+}
