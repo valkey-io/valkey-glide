c2bcb291700bd86bbfcc7faa225a91e7d6904501
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-04-22 18:00:08 -0700

    C#: Error handling. (#3411)
    * Error handling.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- benchmarks/csharp/Program.cs
+++ benchmarks/csharp/Program.cs
@@ -258,9 +258,9 @@ public static class MainClass
         return await Task.WhenAll(tasks);
     }
 
-    private static async Task RunWithParameters(int total_commands,
-        int data_size,
-        int num_of_concurrent_tasks,
+    private static async Task RunWithParameters(int totalCommands,
+        int dataSize,
+        int numOfConcurrentTasks,
         string clientsToRun,
         string host,
         ushort port,
@@ -277,13 +277,13 @@ public static class MainClass
                 {
                     StandaloneClientConfiguration config = new StandaloneClientConfigurationBuilder()
                         .WithAddress(host, port).WithTls(useTLS).Build();
-                    glideClient = new GlideClient(config);
+                    glideClient = GlideClient.CreateClient(config).GetAwaiter().GetResult();
                 }
                 else
                 {
                     ClusterClientConfiguration config = new ClusterClientConfigurationBuilder()
                         .WithAddress(host, port).WithTls(useTLS).Build();
-                    glideClient = new GlideClusterClient(config);
+                    glideClient = GlideClusterClient.CreateClient(config).GetAwaiter().GetResult();
                 }
                 return Task.FromResult<(Func<string, Task<string?>>, Func<string, string, Task>, Action)>(
                     (async (key) => await glideClient.Get(key),
@@ -295,9 +295,9 @@ public static class MainClass
                 clients,
                 "glide",
                 isCluster,
-                total_commands,
-                data_size,
-                num_of_concurrent_tasks
+                totalCommands,
+                dataSize,
+                numOfConcurrentTasks
             );
         }
 
@@ -316,9 +316,9 @@ public static class MainClass
                 clients,
                 "StackExchange.Redis",
                 isCluster,
-                total_commands,
-                data_size,
-                num_of_concurrent_tasks
+                totalCommands,
+                dataSize,
+                numOfConcurrentTasks
             );
 
             foreach (ClientWrapper client in clients)
