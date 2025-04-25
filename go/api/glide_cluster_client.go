86186cec7c2c2060f12215b76263d16018104d85
Niharika Bhavaraju <nbhavaraju@google.com>
2025-04-25 15:08:03 +0000

    Added doc strings to the commands
    Signed-off-by: Niharika Bhavaraju <nbhavaraju@google.com>

--- go/api/glide_cluster_client.go
+++ go/api/glide_cluster_client.go
@@ -128,7 +128,8 @@ func (client *GlideClusterClient) Info() (map[string]string, error) {
 
 // Gets information and statistics about the server.
 //
-// The command will be routed to all primary nodes, unless `route` in [ClusterInfoOptions] is provided.
+// In non transaction the command will be routed to all primary nodes, unless `route` in [ClusterInfoOptions] is provided.
+// When executed within a transaction, the command is routed to a single node.
 //
 // See [valkey.io] for details.
 //
@@ -141,6 +142,10 @@ func (client *GlideClusterClient) Info() (map[string]string, error) {
 //	When specifying a route other than a single node or when route is not given,
 //	it returns a map where each address is the key and its corresponding node response is the value.
 //	When a single node route is given, command returns a string containing the information for the sections requested.
+//  In transaction:
+//   The command routes to a single node and returns that node's information containing the information for the
+//   sections requested.
+//   
 //
 // [valkey.io]: https://valkey.io/commands/info/
 func (client *GlideClusterClient) InfoWithOptions(options options.ClusterInfoOptions) (ClusterValue[string], error) {
@@ -219,7 +224,7 @@ func (client *GlideClusterClient) CustomCommandWithRoute(
 }
 
 // Pings the server.
-// The command will be routed to all primary nodes.
+// In non transaction the command will be routed to all primary nodes.
 //
 // Return value:
 //
@@ -323,13 +328,16 @@ func (client *GlideClusterClient) DBSizeWithOptions(opts options.RouteOption) (i
 }
 
 // Deletes all the keys of all the existing databases.
-// The command will be routed to all primary nodes.
+// In non transaction, the command will be routed to all primary nodes.
+// When executed within a transaction, the command is routed to a single node.
 //
 // See [valkey.io] for details.
 //
 // Return value:
 //
 //	`"OK"` response on success.
+//  In Transaction, 
+//	  Upon transaction completion with Exec(), the actual string value will be accessible in the returned results array.
 //
 // [valkey.io]: https://valkey.io/commands/flushall/
 func (client *GlideClusterClient) FlushAll() (string, error) {
