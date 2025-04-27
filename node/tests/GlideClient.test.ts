a34a48bbe39a215cd5388d2c4cae7c468a1945fd
Shoham Elias <shohame@amazon.com>
2025-04-27 13:19:38 +0000

    fix tests
    Signed-off-by: Shoham Elias <shohame@amazon.com>

--- node/tests/GlideClient.test.ts
+++ node/tests/GlideClient.test.ts
@@ -24,6 +24,7 @@ import {
     RequestError,
     Script,
     convertGlideRecordToRecord
+    convertGlideRecordToRecord,
 } from "..";
 import {ValkeyCluster} from "../../utils/TestUtils.js";
 import {command_request} from "../src/ProtobufMessage";
