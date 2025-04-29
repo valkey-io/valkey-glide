49edf4b83e1c422c6f62ef2406b592a044c46ef0
Edward Liang <76571219+edlng@users.noreply.github.com>
2025-04-29 14:39:02 -0700

    Node: Fix `functionLoad` examples (#3710)

--- node/src/GlideClient.ts
+++ node/src/GlideClient.ts
@@ -704,7 +704,7 @@ export class GlideClient extends BaseClient {
      * @example
      * ```typescript
      * const code = "#!lua name=mylib \n redis.register_function('myfunc', function(keys, args) return args[1] end)";
-     * const result = await client.functionLoad(code, true);
+     * const result = await client.functionLoad(code, { replace: true });
      * console.log(result); // Output: 'mylib'
      * ```
      */
