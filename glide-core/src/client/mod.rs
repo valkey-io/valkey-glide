79390465fc37619525fd6dfe5abc36043ab993a3
adarovadya <adarovadya@gmail.com>
2025-05-03 20:31:05 +0300

    Core: OpenTelemetry metrics infrastructure (#3466)
    * Rust: add OpenTelemetry metrics infra
    
    * add timeout_error metric
    
    * add metrics file exporter
    
    Signed-off-by: Adar Ovadia <adarov@amazon.com>
    
    ---------
    
    Signed-off-by: Adar Ovadia <adarov@amazon.com>
    Co-authored-by: Adar Ovadia <adarov@amazon.com>

--- glide-core/src/client/mod.rs
+++ glide-core/src/client/mod.rs
@@ -199,10 +199,19 @@ async fn run_with_timeout<T>(
     future: impl futures::Future<Output = RedisResult<T>> + Send,
 ) -> redis::RedisResult<T> {
     match timeout {
-        Some(duration) => tokio::time::timeout(duration, future)
-            .await
-            .map_err(|_| io::Error::from(io::ErrorKind::TimedOut).into())
-            .and_then(|res| res),
+        Some(duration) => match tokio::time::timeout(duration, future).await {
+            Ok(result) => result,
+            Err(_) => {
+                // Record timeout error metric
+                if let Err(e) = GlideOpenTelemetry::record_timeout_error() {
+                    log_error(
+                        "OpenTelemetry:timeout_error",
+                        format!("Failed to record timeout error: {}", e),
+                    );
+                }
+                Err(io::Error::from(io::ErrorKind::TimedOut).into())
+            }
+        },
         None => future.await,
     }
 }
@@ -1028,7 +1037,7 @@ impl Client {
 
         // initilaize open telemetry traces exporter
         if let Some(endpoint_str) = &request.otel_traces_endpoint {
-            let trace_exporter = GlideOpenTelemetryTraceExporter::from_str(endpoint_str.as_str())
+            let trace_exporter = GlideOpenTelemetrySignalsExporter::from_str(endpoint_str.as_str())
                 .map_err(ConnectionError::IoError)?;
             let config = GlideOpenTelemetryConfigBuilder::default()
                 .with_flush_interval(std::time::Duration::from_millis(
