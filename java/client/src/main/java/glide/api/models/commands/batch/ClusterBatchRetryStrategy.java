68b0a2eb1797f31a3095bc5bbd494b7abcf76b64
Shoham Elias <shohame@amazon.com>
2025-05-06 06:45:53 +0000

    use pair of <code> tags
    Signed-off-by: Shoham Elias <shohame@amazon.com>

--- java/client/src/main/java/glide/api/models/commands/batch/ClusterBatchRetryStrategy.java
+++ java/client/src/main/java/glide/api/models/commands/batch/ClusterBatchRetryStrategy.java
@@ -43,10 +43,11 @@ import lombok.Getter;
  * OK
  * </pre>
  *
- * However, if the slot is migrating, both commands may return an `ASK` error and be redirected.
- * Upon `ASK` redirection, a multi-key command may return a `TRYAGAIN` error (triggering a retry),
- * while the `SET` command succeeds immediately. This can result in an unintended reordering of
- * commands if the first command is retried after the slot stabilizes:
+ * However, if the slot is migrating, both commands may return an <code>ASK</code> error and be
+ * redirected. Upon <code>ASK</code> redirection, a multi-key command may return a <code>TRYAGAIN
+ * </code> error (triggering a retry), while the <code>SET</code> command succeeds immediately. This
+ * can result in an unintended reordering of commands if the first command is retried after the slot
+ * stabilizes:
  *
  * <pre>
  * ["value", null]
