33afc0a1d3393dbf58efafd9f9586c7649d7d142
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-05-05 21:00:14 -0700

    C#: panic handling. (#3560)
    * Handle panics.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- csharp/sources/Valkey.Glide/Internals/MessageContainer.cs
+++ csharp/sources/Valkey.Glide/Internals/MessageContainer.cs
@@ -4,15 +4,14 @@ using System.Collections.Concurrent;
 
 namespace Valkey.Glide.Internals;
 
-
-internal class MessageContainer
+internal class MessageContainer(BaseClient client)
 {
     internal Message GetMessage(int index) => _messages[index];
 
     internal Message GetMessageForCall()
     {
         Message message = GetFreeMessage();
-        message.SetupTask(this);
+        message.SetupTask(_client);
         return message;
     }
 
@@ -35,13 +34,18 @@ internal class MessageContainer
 
     internal void DisposeWithError(Exception? error)
     {
+        int countIncompleted = _messages.Count(message => !message.IsCompleted);
+        if (countIncompleted > 0)
+        {
+            Logger.Log(Level.Error, GetType().Name, $"Client is closing, but there are {countIncompleted} ongoing requests");
+        }
         lock (_messages)
         {
             foreach (Message? message in _messages.Where(message => !message.IsCompleted))
             {
                 try
                 {
-                    message.SetException(new TaskCanceledException("Client closed", error));
+                    message.SetException(new TaskCanceledException($"Client {_client} closed", error));
                 }
                 catch (Exception) { }
             }
@@ -58,4 +62,9 @@ internal class MessageContainer
     /// This queue contains the messages that were created and are currently unused by any task,
     /// so they can be reused y new tasks instead of allocating new messages.
     private readonly ConcurrentQueue<Message> _availableMessages = new();
+
+    // Holding the client prevents it from being GC'd until all operations complete.
+#pragma warning disable IDE0052 // Remove unread private members
+    private readonly BaseClient _client = client;
+#pragma warning restore IDE0052 // Remove unread private members
 }
