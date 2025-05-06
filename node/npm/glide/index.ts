38ed769c09268e9d56d4ab4614a31cc024dfeccf
Avi Fenesh <aviarchi1994@gmail.com>
2025-05-06 07:13:54 +0000

    Add support for x64 architecture in native binding loader
    Signed-off-by: Avi Fenesh <aviarchi1994@gmail.com>

--- node/npm/glide/index.ts
+++ node/npm/glide/index.ts
@@ -56,6 +56,9 @@ function loadNativeBinding() {
                 case "arm64":
                     nativeBinding = require("@scope/valkey-glide-darwin-arm64");
                     break;
+                case "x64":
+                    nativeBinding = require("@scope/valkey-glide-darwin-x64");
+                    break;
                 default:
                     throw new Error(
                         `Unsupported OS: ${platform}, architecture: ${arch}`,
