76d3fc4ed8394dcba08277deeff66e7a1be19f67
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-05-08 10:13:08 -0700

    C#: batch (#3611)
    * C# transaction
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- csharp/tests/Valkey.Glide.IntegrationTests/SharedCommandTests.cs
+++ csharp/tests/Valkey.Glide.IntegrationTests/SharedCommandTests.cs
@@ -1,6 +1,7 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
 namespace Valkey.Glide.IntegrationTests;
+
 public class SharedCommandTests(TestConfiguration config)
 {
     public TestConfiguration Config { get; } = config;
