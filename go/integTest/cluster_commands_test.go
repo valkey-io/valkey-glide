cbbee884433ad3945f74f853d329cef6a4e14c10
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-06-03 16:08:36 -0700

    cleanup
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- go/integTest/cluster_commands_test.go
+++ go/integTest/cluster_commands_test.go
@@ -2602,7 +2602,7 @@ func (suite *GlideTestSuite) TestRetryStrategyIsNotSupportedForTransactions() {
 	assert.IsType(suite.T(), &errors.RequestError{}, err)
 }
 
-func (suite *GlideTestSuite) Test_BatchWithSingleNodeRoute() {
+func (suite *GlideTestSuite) TestBatchWithSingleNodeRoute() {
 	client := suite.defaultClusterClient()
 	opts := pipeline.NewClusterBatchOptions()
 
