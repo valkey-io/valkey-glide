dfbc991706ba0233fca380fb105747a1d9a9be5f
Avi Fenesh <55848801+avifenesh@users.noreply.github.com>
2025-06-04 08:22:24 -0700

    Increase default connection attempt timeout to 2000ms (#4060)
    * Increase default connection attempt timeout to 2000ms and add timeout recommendations in client configuration examples for Java, Node, and Python.
    
    Signed-off-by: Avi Fenesh <aviarchi1994@gmail.com>
    
    * Update default connection timeout to 2000ms and fix documentation across Go, Java, Node, and Python configurations
    
    Signed-off-by: Avi Fenesh <aviarchi1994@gmail.com>
    
    ---------
    
    Signed-off-by: Avi Fenesh <aviarchi1994@gmail.com>

--- java/client/src/main/java/glide/api/models/configuration/AdvancedBaseClientConfiguration.java
+++ java/client/src/main/java/glide/api/models/configuration/AdvancedBaseClientConfiguration.java
@@ -15,9 +15,9 @@ public abstract class AdvancedBaseClientConfiguration {
 
     /**
      * The duration in milliseconds to wait for a TCP/TLS connection to complete. This applies both
-     * during initial client creation and any reconnections that may occur during request processing.
+     * during initial client creation and any reconnection that may occur during request processing.
      *
-     * <p>If not explicitly set, a default value of 250 milliseconds will be used by the Rust core.
+     * <p>If not explicitly set, a default value of 2000 milliseconds will be used by the Rust core.
      *
      * <p>**Note**: A high connection timeout may lead to prolonged blocking of the entire command
      * pipeline.
