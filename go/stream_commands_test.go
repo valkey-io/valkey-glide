02a7b0ea74e2778b1181769206df43ff72393f15
Prateek Kumar <prateek.kumar@improving.com>
2025-06-11 08:57:08 -0700

    Go: Fix review comments
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- go/stream_commands_test.go
+++ go/stream_commands_test.go
@@ -1995,36 +1995,6 @@ func ExampleClusterClient_XInfoStream() {
 		fmt.Println("Glide example failed with an error: ", err)
 	}
 
-	// Create a map to represent the response for JSON output
-	responseMap := map[string]interface{}{
-		"entries-added":           response.EntriesAdded,
-		"groups":                  response.Groups,
-		"last-generated-id":       response.LastGeneratedID,
-		"length":                  response.Length,
-		"max-deleted-entry-id":    response.MaxDeletedEntryID,
-		"radix-tree-keys":         response.RadixTreeKeys,
-		"radix-tree-nodes":        response.RadixTreeNodes,
-		"recorded-first-entry-id": response.FirstEntry.ID,
-	}
-
-	// Add first-entry with its fields
-	firstEntryFields := []interface{}{response.FirstEntry.ID}
-	fieldsArray := []interface{}{}
-	for _, field := range response.FirstEntry.Fields {
-		fieldsArray = append(fieldsArray, field.FieldName, field.Value)
-	}
-	firstEntryFields = append(firstEntryFields, fieldsArray)
-	responseMap["first-entry"] = firstEntryFields
-
-	// Add last-entry with its fields
-	lastEntryFields := []interface{}{response.LastEntry.ID}
-	fieldsArray = []interface{}{}
-	for _, field := range response.LastEntry.Fields {
-		fieldsArray = append(fieldsArray, field.FieldName, field.Value)
-	}
-	lastEntryFields = append(lastEntryFields, fieldsArray)
-	responseMap["last-entry"] = lastEntryFields
-
 	// Response Structure is as follows:
 	// {
 	//   "entries-added": 1,
