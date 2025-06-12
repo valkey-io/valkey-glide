f17e7866b450fdf9dcd33097404a7e621d34321b
Edward Liang <edward.liang@improving.com>
2025-06-12 13:46:54 -0700

    added replace changes for internal files
    Signed-off-by: Edward Liang <edward.liang@improving.com>

--- go/scripts/rc-testing/rc-test.sh
+++ go/scripts/rc-testing/rc-test.sh
@@ -16,22 +16,31 @@ fi
 VERSION=$1
 
 # Validate version format
-if ! [[ $VERSION =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-rc[0-9]+)?$ ]]; then
+if ! [[ $VERSION =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)(-rc[0-9]+)?$ ]]; then
     echo "Error: Version must be in format vX.Y.Z or vX.Y.Z-rcN (e.g., v1.3.4-rc2)"
     exit 1
 else
     MAJOR=${BASH_REMATCH[1]}
-    MINOR=${BASH_REMATCH[2]}
-    BRANCH="release-${MAJOR}.${MINOR}"
-
     echo "Testing release candidate: $VERSION"
-    echo "Determined branch: $BRANCH"
-
-    # Checkout release branch
-    echo "Checking out $BRANCH branch..."
-    git fetch origin $BRANCH
-    git checkout $BRANCH
 fi
+
+# Create testutils directory and copy internal interfaces to avoid internal package restrictions
+echo "Setting up testutils to handle internal package imports..."
+mkdir -p testutils
+cp -r internal/interfaces/* testutils/
+
+# Fix imports in test files to use testutils instead of internal/interfaces
+echo "Updating test file imports..."
+find integTest/ -name "*.go" -type f -exec grep -l "github.com/valkey-io/valkey-glide/go/v2/internal/interfaces" {} \; | while read -r file; do
+    echo "Updating imports in $file"
+    os="$(uname -s)"
+    if [ "$os" = "Linux" ]; then
+        sed -i 's|github.com/valkey-io/valkey-glide/go/v2/internal/interfaces|github.com/valkey-io/valkey-glide/go-test-rc/testutils|g' "$file"
+    else
+        # MacOS
+        sed -i '' 's|github.com/valkey-io/valkey-glide/go/v2/internal/interfaces|github.com/valkey-io/valkey-glide/go-test-rc/testutils|g' "$file"
+    fi
+done
 
 # Modify go.mod file
 echo "Modifying go.mod file..."
@@ -39,16 +48,22 @@ echo "Modifying go.mod file..."
 os="$(uname -s)"
 if [ "$os" = "Linux" ]; then
         # Linux
-        sed -i -e 's|module github.com/valkey-io/valkey-glide/go|module github.com/valkey-io/valkey-glide/go-test-rc|' go.mod
-        sed -i -e '/github.com\/stretchr\/testify/a\\tgithub.com/valkey-io/valkey-glide/go '"$VERSION"'' go.mod
+        sed -i -e 's|module github.com/valkey-io/valkey-glide/go/v2|module github.com/valkey-io/valkey-glide/go-test-rc|' go.mod
+        sed -i -e '/github.com\/stretchr\/testify/a\    github.com/valkey-io/valkey-glide/go/v2 '"$VERSION" go.mod
+        # Add replace directive after the require block
+        sed -i -e '/^)/a\\nreplace github.com/valkey-io/valkey-glide/go/v2 => ./' go.mod
         if [ $MAJOR -lt "2" ]; then # Fix for release version 1 branches
             sed -i -e 's|"redis-cli",|CLI_COMMAND,|g' ../utils/cluster_manager.py
         fi
 else
         # MacOS
-        sed -i '' -e 's|module github.com/valkey-io/valkey-glide/go|module github.com/valkey-io/valkey-glide/go-test-rc|' go.mod
-        sed -i '' -e '/github.com\/stretchr\/testify/ a\
-        github.com\/valkey-io\/valkey-glide\/go '"$VERSION"'' go.mod
+        sed -i '' -e 's|module github.com/valkey-io/valkey-glide/go/v2|module github.com/valkey-io/valkey-glide/go-test-rc|' go.mod
+        sed -i '' -e '/github.com\/stretchr\/testify/a\
+        github.com/valkey-io/valkey-glide/go/v2 '"$VERSION" go.mod
+        # Add replace directive after the require block
+        sed -i '' -e '/^)/a\
+\
+replace github.com/valkey-io/valkey-glide/go/v2 => ./' go.mod
         if [ $MAJOR -lt "2" ]; then # Fix for release version 1 branches
             sed -i '' -e 's|"redis-cli",|CLI_COMMAND,|g' ../utils/cluster_manager.py 
         fi
