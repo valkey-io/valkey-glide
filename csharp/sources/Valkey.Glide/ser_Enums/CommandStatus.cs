adcba479c8828f1da018706dfc396b097eb983ad
jbrinkman <joe.brinkman@improving.com>
2025-06-25 16:46:10 -0400

    chore: migrate basic APITypes and enums from SER to unblock command implementations
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

---
+++ csharp/sources/Valkey.Glide/ser_Enums/CommandStatus.cs
@@ -0,0 +1,27 @@
+﻿namespace Valkey.Glide;
+
+/// <summary>
+/// Track status of a command while communicating with Redis.
+/// </summary>
+public enum CommandStatus
+{
+    /// <summary>
+    /// Command status unknown.
+    /// </summary>
+    Unknown,
+
+    /// <summary>
+    /// ConnectionMultiplexer has not yet started writing this command to Redis.
+    /// </summary>
+    WaitingToBeSent,
+
+    /// <summary>
+    /// Command has been sent to Redis.
+    /// </summary>
+    Sent,
+
+    /// <summary>
+    /// Command is in the backlog, waiting to be processed and written to Redis.
+    /// </summary>
+    WaitingInBacklog,
+}
