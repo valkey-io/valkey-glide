52e8a36108d993d6af4e4ebe1b2f5fba2fa6cfe1
jbrinkman <joe.brinkman@improving.com>
2025-06-25 16:46:10 -0400

    chore: migrate basic APITypes and enums from SER to unblock command implementations
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

---
+++ csharp/sources/Valkey.Glide/ser_Enums/StringIndexType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Valkey.Glide;
+
+/// <summary>
+/// Indicates if we index into a string based on bits or bytes.
+/// </summary>
+public enum StringIndexType
+{
+    /// <summary>
+    /// Indicates the index is the number of bytes into a string.
+    /// </summary>
+    Byte,
+
+    /// <summary>
+    /// Indicates the index is the number of bits into a string.
+    /// </summary>
+    Bit,
+}
+
+internal static class StringIndexTypeExtensions
+{
+    internal static RedisValue ToLiteral(this StringIndexType indexType) => indexType switch
+    {
+        StringIndexType.Bit => RedisLiterals.BIT,
+        StringIndexType.Byte => RedisLiterals.BYTE,
+        _ => throw new ArgumentOutOfRangeException(nameof(indexType)),
+    };
+}
