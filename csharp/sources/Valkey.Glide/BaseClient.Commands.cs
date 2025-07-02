c3ce934db84637da55be746dfd28d918ddd37a86
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-07-02 12:09:31 -0700

    C#: Add response checks for batch for every command (#4236)
    * Add type check for batch
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Co-authored-by: Edward Liang <76571219+edlng@users.noreply.github.com>

---
+++ csharp/sources/Valkey.Glide/BaseClient.Commands.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+using Valkey.Glide.Commands;
+using Valkey.Glide.Internals;
+
+namespace Valkey.Glide;
+
+public abstract partial class BaseClient : IStringBaseCommands
+{
+    public async Task<string> Set(GlideString key, GlideString value)
+        => await Command(Request.Set(key, value));
+
+    public async Task<GlideString?> Get(GlideString key)
+        => await Command(Request.Get(key));
+}
