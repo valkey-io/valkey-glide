9ab7c1effdcf75d1730c3f0fda26edf02c0297d6
Joseph Brinkman <joe.brinkman@improving.com>
2025-07-02 14:44:00 -0400

    C#: Migrate basic APITypes and enums from SER to unblock command implementation (#4254)
    * chore: migrate basic APITypes and enums from SER to unblock command implementations
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * chore: temporarily disable lint checks
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * fix: resolve linting issue in yml
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * Update .github/workflows/csharp.yml
    
    Co-authored-by: Edward Liang <76571219+edlng@users.noreply.github.com>
    Signed-off-by: Joseph Brinkman <joe.brinkman@improving.com>
    
    * fix: resolve linting issue in yml
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * refactor: rename Redis types to Valkey types and update command enums
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * refactor: migrate API types and enums from ser_ to abstract_ namespace
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * refactor: suppress Lint errors for temp files
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * chore: disable additional IDE diagnostics in .editorconfig files
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    * chore: force rebuild
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    
    ---------
    
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>
    Signed-off-by: Joseph Brinkman <joe.brinkman@improving.com>
    Co-authored-by: Edward Liang <76571219+edlng@users.noreply.github.com>

---
+++ csharp/sources/Valkey.Glide/Commands/IHashCommands.cs
@@ -0,0 +1,56 @@
+﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+namespace Valkey.Glide.Commands;
+internal interface IHashCommands
+{
+    Task<long> HashDecrementAsync(ValkeyKey key, ValkeyValue hashField, long value = 1, CommandFlags flags = CommandFlags.None);
+
+    Task<double> HashDecrementAsync(ValkeyKey key, ValkeyValue hashField, double value, CommandFlags flags = CommandFlags.None);
+
+    Task<bool> HashDeleteAsync(ValkeyKey key, ValkeyValue hashField, CommandFlags flags = CommandFlags.None);
+
+    Task<long> HashDeleteAsync(ValkeyKey key, ValkeyValue[] hashFields, CommandFlags flags = CommandFlags.None);
+
+    Task<bool> HashExistsAsync(ValkeyKey key, ValkeyValue hashField, CommandFlags flags = CommandFlags.None);
+
+    Task<ExpireResult[]> HashFieldExpireAsync(ValkeyKey key, ValkeyValue[] hashFields, TimeSpan expiry, ExpireWhen when = ExpireWhen.Always, CommandFlags flags = CommandFlags.None);
+
+    Task<ExpireResult[]> HashFieldExpireAsync(ValkeyKey key, ValkeyValue[] hashFields, DateTime expiry, ExpireWhen when = ExpireWhen.Always, CommandFlags flags = CommandFlags.None);
+
+    Task<long[]> HashFieldGetExpireDateTimeAsync(ValkeyKey key, ValkeyValue[] hashFields, CommandFlags flags = CommandFlags.None);
+
+    Task<PersistResult[]> HashFieldPersistAsync(ValkeyKey key, ValkeyValue[] hashFields, CommandFlags flags = CommandFlags.None);
+
+    Task<long[]> HashFieldGetTimeToLiveAsync(ValkeyKey key, ValkeyValue[] hashFields, CommandFlags flags = CommandFlags.None);
+
+    Task<ValkeyValue> HashGetAsync(ValkeyKey key, ValkeyValue hashField, CommandFlags flags = CommandFlags.None);
+
+    //Task<Lease<byte>?> HashGetLeaseAsync(ValkeyKey key, ValkeyValue hashField, CommandFlags flags = CommandFlags.None);
+
+    Task<ValkeyValue[]> HashGetAsync(ValkeyKey key, ValkeyValue[] hashFields, CommandFlags flags = CommandFlags.None);
+
+    Task<HashEntry[]> HashGetAllAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
+
+    Task<long> HashIncrementAsync(ValkeyKey key, ValkeyValue hashField, long value = 1, CommandFlags flags = CommandFlags.None);
+
+    Task<double> HashIncrementAsync(ValkeyKey key, ValkeyValue hashField, double value, CommandFlags flags = CommandFlags.None);
+
+    Task<ValkeyValue[]> HashKeysAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
+
+    Task<long> HashLengthAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
+
+    Task<ValkeyValue> HashRandomFieldAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
+
+    Task<ValkeyValue[]> HashRandomFieldsAsync(ValkeyKey key, long count, CommandFlags flags = CommandFlags.None);
+
+    Task<HashEntry[]> HashRandomFieldsWithValuesAsync(ValkeyKey key, long count, CommandFlags flags = CommandFlags.None);
+
+    Task HashSetAsync(ValkeyKey key, HashEntry[] hashFields, CommandFlags flags = CommandFlags.None);
+
+    Task<bool> HashSetAsync(ValkeyKey key, ValkeyValue hashField, ValkeyValue value, When when = When.Always, CommandFlags flags = CommandFlags.None);
+
+    Task<long> HashStringLengthAsync(ValkeyKey key, ValkeyValue hashField, CommandFlags flags = CommandFlags.None);
+
+    Task<ValkeyValue[]> HashValuesAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
+
+}
