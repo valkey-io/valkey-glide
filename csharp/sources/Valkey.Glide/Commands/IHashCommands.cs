ce5e1c6679bcbd0e6e749f36d76fc23549fc3a65
Edward Liang <edward.liang@improving.com>
2025-07-03 12:29:06 -0700

    fix spacing
    Signed-off-by: Edward Liang <edward.liang@improving.com>

--- csharp/sources/Valkey.Glide/Commands/IHashCommands.cs
+++ csharp/sources/Valkey.Glide/Commands/IHashCommands.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
 namespace Valkey.Glide.Commands;
-
 internal interface IHashCommands
 {
     Task<long> HashDecrementAsync(ValkeyKey key, ValkeyValue hashField, long value = 1, CommandFlags flags = CommandFlags.None);
