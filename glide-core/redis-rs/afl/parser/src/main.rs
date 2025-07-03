c8a7288a9d4ee02ef5317f91c3416ed802f13dd4
Shachar Langbeheim <nihohit@gmail.com>
2025-07-03 11:39:40 +0300

    Remove unused workspace crates under redis-rs. (#4283)
    Signed-off-by: Shachar Langbeheim <nihohit@gmail.com>
    Co-authored-by: Avi Fenesh <55848801+avifenesh@users.noreply.github.com>

--- glide-core/redis-rs/afl/parser/src/main.rs
+++
@@ -1,9 +0,0 @@
-use afl::fuzz;
-
-use redis::parse_redis_value;
-
-fn main() {
-    fuzz!(|data: &[u8]| {
-        let _ = parse_redis_value(data);
-    });
-}
--- glide-core/redis-rs/redis-test/README.md
+++
@@ -1,4 +0,0 @@
-# redis-test
-
-Testing utilities for the redis-rs crate.
-
