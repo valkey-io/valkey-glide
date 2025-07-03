72c877754368599ff0fde393192c72d3a70fd218
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-07-03 12:20:31 -0700

    C#: UT for commands (#4280)
    * Add command args tests
    * Add test for converters
    
    ---------
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Co-authored-by: Edward Liang <76571219+edlng@users.noreply.github.com>

---
+++ csharp/tests/Valkey.Glide.UnitTests/CommandTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+using Valkey.Glide.Commands.Options;
+using Valkey.Glide.Internals;
+
+using static Valkey.Glide.ConnectionConfiguration;
+
+namespace Valkey.Glide.UnitTests;
+
+public class CommandTests
+{
+    [Fact]
+    public void ValidateCommandArgs()
+    {
+        Assert.Multiple([
+            () => Assert.Equal(["get", "a"], Request.CustomCommand(["get", "a"]).GetArgs()),
+            () => Assert.Equal(["ping", "pong", "pang"], Request.CustomCommand(["ping", "pong", "pang"]).GetArgs()),
+            () => Assert.Equal(["get"], Request.CustomCommand(["get"]).GetArgs()),
+            () => Assert.Equal([], Request.CustomCommand([]).GetArgs()),
+
+            () => Assert.Equal(["SET", "a", "b"], Request.Set("a", "b").GetArgs()),
+            () => Assert.Equal(["GET", "a"], Request.Get("a").GetArgs()),
+            () => Assert.Equal(["INFO"], Request.Info([]).GetArgs()),
+            () => Assert.Equal(["INFO", "CLIENTS", "CPU"], Request.Info([InfoOptions.Section.CLIENTS, InfoOptions.Section.CPU]).GetArgs()),
+        ]);
+    }
+
+    [Fact]
+    public void ValidateCommandConverters()
+    {
+        Assert.Multiple([
+            () => Assert.Equal(1, Request.CustomCommand([]).Converter(1)),
+            () => Assert.Equal(.1, Request.CustomCommand([]).Converter(.1)),
+            () => Assert.Null(Request.CustomCommand([]).Converter(null)),
+
+            () => Assert.Equal("OK", Request.Set("a", "b").Converter("OK")),
+            () => Assert.Equal<GlideString>("OK", Request.Get("a").Converter("OK")),
+            () => Assert.Null(Request.Get("a").Converter(null)),
+            () => Assert.Equal("info", Request.Info([]).Converter("info")),
+        ]);
+    }
+}
--- csharp/tests/Valkey.Glide.UnitTests/GildeStringTests.cs
+++ csharp/tests/Valkey.Glide.UnitTests/GildeStringTests.cs
@@ -7,29 +7,29 @@ public class GildeStringTests
     [Fact]
     public void Sorting()
     {
-        GlideString[] arr = ["abc", "abcd", "abcde", "abd", "abb", "ab1"];
+        gs[] arr = ["abc", "abcd", "abcde", "abd", "abb", "ab1"];
 #if NET8_0_OR_GREATER
-        Assert.Equal(new GlideString[] { "ab1", "abb", "abc", "abd", "abcd", "abcde" }, [.. arr.Order()]);
+        Assert.Equal(new gs[] { "ab1", "abb", "abc", "abd", "abcd", "abcde" }, [.. arr.Order()]);
 #endif
-        Assert.Equal(new GlideString[] { "ab1", "abb", "abc", "abd", "abcd", "abcde" }, [.. arr.OrderBy(s => s)]);
+        Assert.Equal(new gs[] { "ab1", "abb", "abc", "abd", "abcd", "abcde" }, [.. arr.OrderBy(s => s)]);
     }
 
     [Fact]
     public void Comparing()
     {
-        Assert.Equal(new GlideString("abc"), new GlideString("abc"));
-        Assert.NotEqual(new GlideString("abc"), new GlideString("abd"));
-        Assert.Equal(new GlideString("abc"), new GlideString([.. "abc".ToCharArray().Select(c => (byte)c)]));
-        Assert.Equal(new GlideString("abc123"), new GlideString("abc") + "123");
-        Assert.Equal(new GlideString("abc123"), "abc" + new GlideString("123"));
+        Assert.Equal(new gs("abc"), new gs("abc"));
+        Assert.NotEqual(new gs("abc"), new gs("abd"));
+        Assert.Equal(new gs("abc"), new gs([.. "abc".ToCharArray().Select(c => (byte)c)]));
+        Assert.Equal(new gs("abc123"), new gs("abc") + "123");
+        Assert.Equal(new gs("abc123"), "abc" + new gs("123"));
     }
 
     [Fact]
     public void Handling()
     {
-        GlideString gs = new("abc");
+        gs gs = new("abc");
         gs += "123";
-        Assert.Equal(new GlideString("abc123"), gs);
+        Assert.Equal(new gs("abc123"), gs);
         Assert.True(gs.CanConvertToString());
         string str = "שלום hello 汉字";
         gs = str;
@@ -44,10 +44,10 @@ public class GildeStringTests
         Assert.False(gs.CanConvertToString());
         Assert.Contains("Value isn't convertible to string", gs);
 
-        gs = new GlideString("abc") + "123";
+        gs = new gs("abc") + "123";
         Assert.True(gs.CanConvertToString());
         Assert.Equal("abc123", gs.ToString());
 
-        Assert.Equal(new GlideString("abc"), "abc".ToGlideString());
+        Assert.Equal(new gs("abc"), "abc".ToGlideString());
     }
 }
