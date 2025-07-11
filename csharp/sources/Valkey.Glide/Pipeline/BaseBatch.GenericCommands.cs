6fc8cfac97dd61008846123cb743484cf6015f7f
Edward Liang <edward.liang@improving.com>
2025-07-11 12:45:06 -0700

    fix import ordering
    Signed-off-by: Edward Liang <edward.liang@improving.com>

--- csharp/sources/Valkey.Glide/Pipeline/BaseBatch.GenericCommands.cs
+++ csharp/sources/Valkey.Glide/Pipeline/BaseBatch.GenericCommands.cs
@@ -1,7 +1,7 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-using static Valkey.Glide.Internals.Request;
 using Valkey.Glide.Commands.Options;
+using static Valkey.Glide.Internals.Request;
 
 namespace Valkey.Glide.Pipeline;
 
