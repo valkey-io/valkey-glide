848bb6cb749c056848626317ad24409f3c541e09
TJ Zhang <tj.zhang@improving.com>
2025-07-10 17:42:16 -0700

    update doc order
    Signed-off-by: TJ Zhang <tj.zhang@improving.com>

--- csharp/sources/Valkey.Glide/Commands/IStringBaseCommands.cs
+++ csharp/sources/Valkey.Glide/Commands/IStringBaseCommands.cs
@@ -10,8 +10,13 @@ namespace Valkey.Glide.Commands;
 public interface IStringBaseCommands
 {
     /// <summary>
-    /// Set key to hold the string value. If key already holds a value, it is overwritten, regardless of its type.
+    /// Sets the value of a key to a string. If the key already holds a value, it is overwritten, regardless of its type.
     /// </summary>
+    /// <seealso href="https://valkey.io/commands/set/">valkey.io</seealso>
+    /// <param name="key">The key to store.</param>
+    /// <param name="value">The value to store with the given key.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns><see langword="true"/> if the string was set, <see langword="false"/> otherwise.</returns>
     /// <remarks>
     /// <example>
     /// <code>
@@ -20,16 +25,18 @@ public interface IStringBaseCommands
     /// </code>
     /// </example>
     /// </remarks>
-    /// <param name="key">The key to store.</param>
-    /// <param name="value">The value to store with the given key.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns><see langword="true"/> if the string was set, <see langword="false"/> otherwise.</returns>
-    /// <seealso href="https://valkey.io/commands/set/">valkey.io</seealso>
     Task<bool> StringSetAsync(ValkeyKey key, ValkeyValue value, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
     /// Get the value of key. If the key does not exist the special value <see langword="null" /> is returned.
     /// </summary>
+    /// <seealso href="https://valkey.io/commands/get/">valkey.io</seealso>
+    /// <param name="key">The key to retrieve from the database.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>
+    /// If key exists, returns the value of key as a <see cref="ValkeyValue" />.<br/>
+    /// Otherwise, returns <see cref="ValkeyValue.Null" />.
+    /// </returns>
     /// <remarks>
     /// <example>
     /// <code>
@@ -38,18 +45,18 @@ public interface IStringBaseCommands
     /// </code>
     /// </example>
     /// </remarks>
-    /// <param name="key">The key to retrieve from the database.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns>
-    /// If key exists, returns the value of key as a <see cref="ValkeyValue" />.<br/>
-    /// Otherwise, returns <see cref="ValkeyValue.Null" />.
-    /// </returns>
-    /// <seealso href="https://valkey.io/commands/get/">valkey.io</seealso>
     Task<ValkeyValue> StringGetAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
     /// Returns the values of all specified keys.
     /// </summary>
+    /// <seealso href="https://valkey.io/commands/mget/">valkey.io</seealso>
+    /// <param name="keys">A list of keys to retrieve values for.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>
+    /// An array of values corresponding to the provided keys.<br/>
+    /// If a key is not found, its corresponding value in the list will be <see cref="ValkeyValue.Null" />.
+    /// </returns>
     /// <remarks>
     /// <example>
     /// <code>
@@ -60,18 +67,15 @@ public interface IStringBaseCommands
     /// </code>
     /// </example>
     /// </remarks>
-    /// <param name="keys">A list of keys to retrieve values for.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns>
-    /// An array of values corresponding to the provided keys.<br/>
-    /// If a key is not found, its corresponding value in the list will be <see cref="ValkeyValue.Null" />.
-    /// </returns>
-    /// <seealso href="https://valkey.io/commands/mget/">valkey.io</seealso>
     Task<ValkeyValue[]> StringGetAsync(ValkeyKey[] keys, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
     /// Sets the given keys to their respective values.
     /// </summary>
+    /// <seealso href="https://valkey.io/commands/mset/">valkey.io</seealso>
+    /// <param name="values">An array of key-value pairs to set.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns><see langword="true"/> if the strings were set, <see langword="false"/> otherwise.</returns>
     /// <remarks>
     /// <example>
     /// <code>
@@ -84,16 +88,21 @@ public interface IStringBaseCommands
     /// </code>
     /// </example>
     /// </remarks>
-    /// <param name="values">An array of key-value pairs to set.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns><see langword="true"/> if the strings were set, <see langword="false"/> otherwise.</returns>
-    /// <seealso href="https://valkey.io/commands/mset/">valkey.io</seealso>
     Task<bool> StringSetAsync(KeyValuePair<ValkeyKey, ValkeyValue>[] values, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
     /// Returns the substring of the string value stored at key, determined by the offsets 
     /// start and end (both are inclusive).
     /// </summary>
+    /// <seealso href="https://valkey.io/commands/getrange/">valkey.io</seealso>
+    /// <param name="key">The key of the string.</param>
+    /// <param name="start">The starting offset.</param>
+    /// <param name="end">The ending offset.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>
+    /// A substring extracted from the value stored at key.<br/>
+    /// An empty string is returned if the key does not exist or if the start and end offsets are out of range.
+    /// </returns>
     /// <remarks>
     /// <example>
     /// <code>
@@ -103,21 +112,18 @@ public interface IStringBaseCommands
     /// </code>
     /// </example>
     /// </remarks>
-    /// <param name="key">The key of the string.</param>
-    /// <param name="start">The starting offset.</param>
-    /// <param name="end">The ending offset.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns>
-    /// A substring extracted from the value stored at key.<br/>
-    /// An empty string is returned if the key does not exist or if the start and end offsets are out of range.
-    /// </returns>
-    /// <seealso href="https://valkey.io/commands/getrange/">valkey.io</seealso>
     Task<ValkeyValue> StringGetRangeAsync(ValkeyKey key, long start, long end, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
     /// Overwrites part of the string stored at key, starting at the specified offset, 
     /// for the entire length of value.
     /// </summary>
+    /// <seealso href="https://valkey.io/commands/setrange/">valkey.io</seealso>
+    /// <param name="key">The key of the string to update.</param>
+    /// <param name="offset">The position in the string where value should be written.</param>
+    /// <param name="value">The string written with offset.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>The length of the string stored at key after it was modified.</returns>
     /// <remarks>
     /// <example>
     /// <code>
@@ -130,17 +136,18 @@ public interface IStringBaseCommands
     /// </code>
     /// </example>
     /// </remarks>
-    /// <param name="key">The key of the string to update.</param>
-    /// <param name="offset">The position in the string where value should be written.</param>
-    /// <param name="value">The string written with offset.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns>The length of the string stored at key after it was modified.</returns>
-    /// <seealso href="https://valkey.io/commands/setrange/">valkey.io</seealso>
     Task<ValkeyValue> StringSetRangeAsync(ValkeyKey key, long offset, ValkeyValue value, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
     /// Returns the length of the string value stored at key.
     /// </summary>
+    /// <seealso href="https://valkey.io/commands/strlen/">valkey.io</seealso>
+    /// <param name="key">The key to check its length.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>
+    /// The length of the string value stored at key.<br/>
+    /// If key does not exist, it is treated as an empty string, and the command returns <c>0</c>.
+    /// </returns>
     /// <remarks>
     /// <example>
     /// <code>
@@ -150,12 +157,5 @@ public interface IStringBaseCommands
     /// </code>
     /// </example>
     /// </remarks>
-    /// <param name="key">The key to check its length.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns>
-    /// The length of the string value stored at key.<br/>
-    /// If key does not exist, it is treated as an empty string, and the command returns <c>0</c>.
-    /// </returns>
-    /// <seealso href="https://valkey.io/commands/strlen/">valkey.io</seealso>
     Task<long> StringLengthAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
 }
