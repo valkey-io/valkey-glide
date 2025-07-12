918702edbf8639bb7b3f14cb25b7c4c5534a65c5
Yury-Fridlyand <yury.fridlyand@improving.com>
2025-07-11 17:14:20 -0700

    C# compatibility: adding feature to get list of connections (#4310)

--- csharp/sources/Valkey.Glide/Abstract/ConnectionMultiplexer.cs
+++ csharp/sources/Valkey.Glide/Abstract/ConnectionMultiplexer.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
+using System.Net;
+
 using Valkey.Glide.Commands;
+using Valkey.Glide.Internals;
 
 using static Valkey.Glide.Commands.Options.InfoOptions;
 using static Valkey.Glide.ConnectionConfiguration;
@@ -13,19 +16,83 @@ namespace Valkey.Glide;
 /// </summary>
 public sealed class ConnectionMultiplexer : IConnectionMultiplexer
 {
+    public EndPoint[] GetEndPoints(bool configuredOnly) => throw new NotImplementedException();
+
+    public IServer GetServer(string host, int port, object? asyncState = null)
+        => GetServer(Utils.ParseEndPoint(host, port), asyncState);
+
+    public IServer GetServer(string hostAndPort, object? asyncState = null)
+        => Utils.TryParseEndPoint(hostAndPort, out IPEndPoint? ep)
+            ? GetServer(ep, asyncState)
+            : throw new ArgumentException($"The specified host and port could not be parsed: {hostAndPort}", nameof(hostAndPort));
+
+    public IServer GetServer(IPAddress host, int port)
+        => GetServer(new IPEndPoint(host, port));
+
+    public IServer GetServer(EndPoint endpoint, object? asyncState = null)
+    {
+        Utils.Requires<NotImplementedException>(asyncState is null, "Async state is not supported by GLIDE");
+        foreach (IServer server in GetServers())
+        {
+            if (server.EndPoint.Equals(endpoint))
+            {
+                return server;
+            }
+        }
+        throw new ArgumentException("The specified endpoint is not defined", nameof(endpoint));
+    }
+
+    // TODO currently this returns only primary node on standalone
+    // https://github.com/valkey-io/valkey-glide/issues/4293
+    public IServer[] GetServers()
+    {
+        // run INFO on all nodes, but disregard the node responses, we need node addresses only
+        if (_db.IsCluster)
+        {
+            Dictionary<string, string> info = _db.Command(Request.Info([]).ToMultiNodeValue(), Route.AllNodes).GetAwaiter().GetResult();
+            return [.. info.Keys.Select(addr => new ValkeyServer(_db, IPEndPoint.Parse(addr)))];
+        }
+        else
+        {
+            // due to #4293, core ignores route on standalone and always return a single node response
+            string info = _db.Command(Request.Info([]), Route.AllNodes).GetAwaiter().GetResult();
+            // and there is no way to get IP address from server, assuming localhost (127.0.0.1)
+            // we can try to get port only (in some deployments, this info is also missing)
+            int port = 6379;
+            foreach (string line in info.Split("\r\n"))
+            {
+                if (line.Contains("tcp_port:"))
+                {
+                    port = int.Parse(line.Split(':')[1]);
+                }
+            }
+            return [new ValkeyServer(_db, new IPEndPoint(0x100007F, port))];
+        }
+    }
+
+    public bool IsConnected => true;
+
+    public bool IsConnecting => false;
+
     public static async Task<ConnectionMultiplexer> ConnectAsync(string host, ushort port)
     {
         GlideClient standalone = await GlideClient.CreateClient(new StandaloneClientConfigurationBuilder()
             .WithAddress(host, port).Build());
         string info = await standalone.Info([Section.CLUSTER]);
-        return new(await DatabaseImpl.Create(host, port, info.Contains("cluster_enabled:1")));
+        bool isCluster = info.Contains("cluster_enabled:1");
+        return new(await DatabaseImpl.Create(host, port, isCluster));
     }
 
-    public IDatabase GetDatabase() => _db;
+    public IDatabase GetDatabase(int db = -1, object? asyncState = null)
+    {
+        Utils.Requires<NotImplementedException>(db == -1, "To switch the database, please use `SELECT` command.");
+        Utils.Requires<NotImplementedException>(asyncState is null, "Async state is not supported by GLIDE");
+        return _db;
+    }
 
-    private readonly IDatabase _db;
+    private readonly DatabaseImpl _db;
 
-    private ConnectionMultiplexer(IDatabase db)
+    private ConnectionMultiplexer(DatabaseImpl db)
     {
         _db = db;
     }
--- csharp/sources/Valkey.Glide/Abstract/Database.cs
+++ csharp/sources/Valkey.Glide/Abstract/Database.cs
@@ -13,19 +13,19 @@ internal class DatabaseImpl : GlideClient, IDatabase
     public new async Task<string> Info() => await Info([]);
 
     public new async Task<string> Info(InfoOptions.Section[] sections)
-        => _isCluster
+        => IsCluster
             ? await Command(Request.Info(sections), Route.Random)
             : await base.Info(sections);
 
-    private readonly bool _isCluster;
+    internal readonly bool IsCluster;
 
-    private DatabaseImpl(bool isCluster) { _isCluster = isCluster; }
+    private DatabaseImpl(bool isCluster) { IsCluster = isCluster; }
 
     internal static async Task<DatabaseImpl> Create(string host, ushort port, bool isCluster)
     {
         BaseClientConfiguration config = isCluster
-            ? new ClusterClientConfigurationBuilder().WithAddress(host, port).Build()
-            : new StandaloneClientConfigurationBuilder().WithAddress(host, port).Build();
+            ? new ClusterClientConfigurationBuilder().WithAddress(host, port).WithProtocolVersion(ConnectionConfiguration.Protocol.RESP3).Build()
+            : new StandaloneClientConfigurationBuilder().WithAddress(host, port).WithProtocolVersion(ConnectionConfiguration.Protocol.RESP3).Build();
         return await CreateClient(config, () => new DatabaseImpl(isCluster));
     }
 }
