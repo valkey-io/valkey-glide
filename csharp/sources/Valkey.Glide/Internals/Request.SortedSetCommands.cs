d806ca9a31ea1ccc67e5566437e2f2dbaf05b689
Prateek Kumar <prateek.kumar@improving.com>
2025-07-14 16:14:41 -0700

    C#: ZAdd command
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- csharp/sources/Valkey.Glide/Internals/Request.SortedSetCommands.cs
+++ csharp/sources/Valkey.Glide/Internals/Request.SortedSetCommands.cs
@@ -6,15 +6,8 @@ namespace Valkey.Glide.Internals;
 
 internal partial class Request
 {
-    public static Cmd<long, bool> SortedSetAddAsync(ValkeyKey key, ValkeyValue member, double score, SortedSetWhen when = SortedSetWhen.Always, CommandFlags flags = CommandFlags.None)
+    private static void AddSortedSetWhenOptions(List<GlideString> args, SortedSetWhen when)
     {
-        if (flags != CommandFlags.None)
-        {
-            throw new NotImplementedException("Command flags are not supported by GLIDE");
-        }
-
-        List<GlideString> args = [key.ToGlideString()];
-
         // Add conditional options
         if (when.HasFlag(SortedSetWhen.Exists))
         {
@@ -33,6 +26,17 @@ internal partial class Request
         {
             args.Add("LT");
         }
+    }
+
+    public static Cmd<long, bool> SortedSetAddAsync(ValkeyKey key, ValkeyValue member, double score, SortedSetWhen when = SortedSetWhen.Always, CommandFlags flags = CommandFlags.None)
+    {
+        if (flags != CommandFlags.None)
+        {
+            throw new NotImplementedException("Command flags are not supported by GLIDE");
+        }
+
+        List<GlideString> args = [key.ToGlideString()];
+        AddSortedSetWhenOptions(args, when);
 
         // Add score and member
         args.Add(score.ToGlideString());
@@ -49,25 +53,7 @@ internal partial class Request
         }
 
         List<GlideString> args = [key.ToGlideString()];
-
-        // Add conditional options
-        if (when.HasFlag(SortedSetWhen.Exists))
-        {
-            args.Add("XX");
-        }
-        else if (when.HasFlag(SortedSetWhen.NotExists))
-        {
-            args.Add("NX");
-        }
-
-        if (when.HasFlag(SortedSetWhen.GreaterThan))
-        {
-            args.Add("GT");
-        }
-        else if (when.HasFlag(SortedSetWhen.LessThan))
-        {
-            args.Add("LT");
-        }
+        AddSortedSetWhenOptions(args, when);
 
         // Add score-member pairs
         foreach (SortedSetEntry entry in values)
