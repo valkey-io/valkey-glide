c3575ea8e0c192227a13aff37dbce06b0bc6336a
avifenesh <aviarchi1994@gmail.com>
2025-07-15 19:51:23 +0000

    feat: Implement GlideClient and command management with JNI integration
    Signed-off-by: avifenesh <aviarchi1994@gmail.com>

--- java/client/src/main/java/module-info.java
+++ java/client/src/main/java/module-info.java
@@ -1,19 +1,14 @@
-module glide.api {
+module glide.client {
+    // Core JNI classes
+    exports io.valkey.glide.core.client;
+    exports io.valkey.glide.core.commands;
+    exports io.valkey.glide.managers;
+    
+    // Client API classes
     exports glide.api;
-    exports glide.api.commands;
     exports glide.api.models;
-    exports glide.api.models.commands;
-    exports glide.api.models.commands.batch;
-    exports glide.api.models.commands.bitmap;
-    exports glide.api.models.commands.geospatial;
-    exports glide.api.models.commands.scan;
-    exports glide.api.models.commands.stream;
-    exports glide.api.models.configuration;
-    exports glide.api.models.exceptions;
+    exports glide.utils;
 
-    requires java.logging; // required by shadowed protobuf
-    requires static lombok;
-    requires org.apache.commons.lang3;
     requires java.base;
-    requires io.valkey.glide.core;
+    requires java.logging;
 }
--- java/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module io.valkey.glide.core {
-    exports io.valkey.glide.core.client;
-    exports io.valkey.glide.core.commands;
-
-    requires java.base;
-    requires java.logging;
-}
