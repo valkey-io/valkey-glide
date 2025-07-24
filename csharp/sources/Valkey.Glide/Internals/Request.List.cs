f40ddf5bbf0cb852d88603bf536cd8ab7f33d059
James Xin <james.xin@improving.com>
2025-07-24 16:07:38 -0700

    C#: List Commands batch 1 (#4402)
    Co-authored-by: Avi Fenesh <55848801+avifenesh@users.noreply.github.com>
    Co-authored-by: Edward Liang <76571219+edlng@users.noreply.github.com>
    Co-authored-by: Edward Liang <edward.liang@improving.com>

--- csharp/sources/Valkey.Glide/Internals/Request.List.cs
+++ csharp/sources/Valkey.Glide/Internals/Request.List.cs
@@ -6,13 +6,82 @@ namespace Valkey.Glide.Internals;
 
 internal partial class Request
 {
-    public static Cmd<GlideString, ValkeyValue> ListLeftPopAsync(ValkeyKey key)
-        => new(RequestType.LPop, [key], true, gs => (ValkeyValue)gs);
+    public static Cmd<GlideString, ValkeyValue> ListLeftPopAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return new(RequestType.LPop, [key], true, gs => gs is null ? ValkeyValue.Null : (ValkeyValue)gs);
+    }
 
-    public static Cmd<object[], ValkeyValue[]> ListLeftPopAsync(ValkeyKey key, long count)
-        => new(RequestType.LPop, [key, count.ToString()], true, array =>
+    public static Cmd<object[], ValkeyValue[]?> ListLeftPopAsync(ValkeyKey key, long count, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return new(RequestType.LPop, [key, count.ToGlideString()], true, array =>
+            array is null ? null : [.. array.Cast<GlideString>().Select(gs => (ValkeyValue)gs)]);
+    }
+
+    public static Cmd<long, long> ListLeftPushAsync(ValkeyKey key, ValkeyValue value, When when = When.Always, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        Utils.Requires<NotImplementedException>(when == When.Always, "LPUSHX has not been implemented yet.");
+        return Simple<long>(RequestType.LPush, [key, value]);
+    }
+
+    public static Cmd<long, long> ListLeftPushAsync(ValkeyKey key, ValkeyValue[] values, When when = When.Always, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        Utils.Requires<NotImplementedException>(when == When.Always, "LPUSHX has not been implemented yet.");
+        return Simple<long>(RequestType.LPush, [key.ToGlideString(), .. values.ToGlideStrings()]);
+    }
+
+    public static Cmd<GlideString, ValkeyValue> ListRightPopAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return new(RequestType.RPop, [key], true, gs => gs is null ? ValkeyValue.Null : (ValkeyValue)gs);
+    }
+
+    public static Cmd<object[], ValkeyValue[]?> ListRightPopAsync(ValkeyKey key, long count, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return new(RequestType.RPop, [key, count.ToGlideString()], true, array =>
+            array is null ? null : [.. array.Cast<GlideString>().Select(gs => (ValkeyValue)gs)]);
+    }
+
+    public static Cmd<long, long> ListRightPushAsync(ValkeyKey key, ValkeyValue value, When when = When.Always, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        Utils.Requires<NotImplementedException>(when == When.Always, "RPUSHX has not been implemented yet.");
+        return Simple<long>(RequestType.RPush, [key, value]);
+    }
+
+    public static Cmd<long, long> ListRightPushAsync(ValkeyKey key, ValkeyValue[] values, When when = When.Always, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        Utils.Requires<NotImplementedException>(when == When.Always, "RPUSHX has not been implemented yet.");
+        return Simple<long>(RequestType.RPush, [key.ToGlideString(), .. values.ToGlideStrings()]);
+    }
+
+    public static Cmd<long, long> ListLengthAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return Simple<long>(RequestType.LLen, [key]);
+    }
+
+    public static Cmd<long, long> ListRemoveAsync(ValkeyKey key, ValkeyValue value, long count = 0, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return Simple<long>(RequestType.LRem, [key, count.ToGlideString(), value]);
+    }
+
+    public static Cmd<string, string> ListTrimAsync(ValkeyKey key, long start, long stop, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return Simple<string>(RequestType.LTrim, [key, start.ToGlideString(), stop.ToGlideString()]);
+    }
+
+    public static Cmd<object[], ValkeyValue[]> ListRangeAsync(ValkeyKey key, long start = 0, long stop = -1, CommandFlags flags = CommandFlags.None)
+    {
+        Utils.Requires<NotImplementedException>(flags == CommandFlags.None, "Command flags are not supported by GLIDE");
+        return new(RequestType.LRange, [key, start.ToGlideString(), stop.ToGlideString()], false, array =>
             [.. array.Cast<GlideString>().Select(gs => (ValkeyValue)gs)]);
-
-    public static Cmd<long, long> ListLeftPushAsync(ValkeyKey key, ValkeyValue[] values)
-        => Simple<long>(RequestType.LPush, [key.ToGlideString(), .. values.ToGlideStrings()]);
+    }
 }
