a93def2d4dacf82357d01bcd0a12c25c67f72cd3
Prateek Kumar <prateek.kumar@improving.com>
2025-07-29 00:22:24 -0700

    Java: Fix build issue with imports
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- java/client/src/test/java/redis/clients/jedis/JedisWrapperTest.java
+++ java/client/src/test/java/redis/clients/jedis/JedisWrapperTest.java
@@ -1,5 +1,5 @@
 /** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
-package compatibility.clients.jedis;
+package redis.clients.jedis;
 
 import static org.junit.jupiter.api.Assertions.*;
 
