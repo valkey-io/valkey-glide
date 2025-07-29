62092d8a45b08d8e7c52083caae40e0e3fa290cc
Prateek Kumar <prateek.kumar@improving.com>
2025-07-29 10:34:22 -0700

    Java: Add module-info.java for integTests
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

---
+++ java/integTest/src/test/java/module-info.java
@@ -0,0 +1,15 @@
+module glide.integTest {
+    opens glide;
+    opens glide.cluster;
+    opens glide.modules;
+    opens glide.standalone;
+    opens compatibility.jedis;
+
+    requires glide.api;
+    requires com.google.gson;
+    requires static lombok;
+    requires net.bytebuddy;
+    requires org.apache.commons.lang3;
+    requires org.junit.jupiter.params;
+    requires org.semver4j;
+}
