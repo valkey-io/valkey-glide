ecadaeb4cf4252d851dc4772d5e8022b7fd6a5ee
Edward Liang <76571219+edlng@users.noreply.github.com>
2025-07-30 09:02:44 -0700

    C#: String Commands (GA) (#4495)

--- csharp/sources/Valkey.Glide/Commands/IStringCommands.cs
+++ csharp/sources/Valkey.Glide/Commands/IStringCommands.cs
@@ -28,6 +28,35 @@ public interface IStringCommands
     Task<bool> StringSetAsync(ValkeyKey key, ValkeyValue value, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
+    /// Sets multiple keys to multiple values in a single operation based on a specified condition.
+    /// </summary>
+    /// <seealso href="https://valkey.io/commands/mset/">valkey.io</seealso>
+    /// <seealso href="https://valkey.io/commands/msetnx/">valkey.io</seealso>
+    /// <note>In cluster mode, if keys in <paramref name="values"/> map to different hash slots, the command
+    /// will be split across these slots and executed separately for each. This means the command
+    /// is atomic only at the slot level. If one or more slot-specific requests fail, the entire
+    /// call will return the first encountered error, even though some requests may have succeeded
+    /// while others did not. If this behavior impacts your application logic, consider splitting
+    /// the request into sub-requests per slot to ensure atomicity.</note>
+    /// <param name="values">An array of key-value pairs to set.</param>
+    /// <param name="when">The condition to specify. If specified to Not-Exists, sets multiple keys to values only if the key does not exist. Defaults to Always.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns><see langword="true"/> if all the keys were set, <see langword="false"/> if no key was set (at least one key already existed).</returns>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// KeyValuePair&lt;ValkeyKey, ValkeyValue&gt;[] values = [
+    ///     new("key1", "value1"),
+    ///     new("key2", "value2")
+    /// ];
+    /// bool result = await client.StringSetAsync(values);
+    /// Console.WriteLine(result); // Output: true (if neither key existed)
+    /// </code>
+    /// </example>
+    /// </remarks>
+    Task<bool> StringSetAsync(KeyValuePair<ValkeyKey, ValkeyValue>[] values, When when = When.Always, CommandFlags flags = CommandFlags.None);
+
+    /// <summary>
     /// Get the value of key. If the key does not exist the special value <see langword="null" /> is returned.
     /// </summary>
     /// <seealso href="https://valkey.io/commands/get/">valkey.io</seealso>
@@ -74,33 +103,6 @@ public interface IStringCommands
     /// </example>
     /// </remarks>
     Task<ValkeyValue[]> StringGetAsync(ValkeyKey[] keys, CommandFlags flags = CommandFlags.None);
-
-    /// <summary>
-    /// Sets multiple keys to multiple values in a single operation.
-    /// </summary>
-    /// <seealso href="https://valkey.io/commands/mset/">valkey.io</seealso>
-    /// <note>In cluster mode, if keys in <paramref name="values"/> map to different hash slots, the command
-    /// will be split across these slots and executed separately for each. This means the command
-    /// is atomic only at the slot level. If one or more slot-specific requests fail, the entire
-    /// call will return the first encountered error, even though some requests may have succeeded
-    /// while others did not. If this behavior impacts your application logic, consider splitting
-    /// the request into sub-requests per slot to ensure atomicity.</note>
-    /// <param name="values">An array of key-value pairs to set.</param>
-    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
-    /// <returns><see langword="true"/> if the strings were set, <see langword="false"/> otherwise.</returns>
-    /// <remarks>
-    /// <example>
-    /// <code>
-    /// KeyValuePair&lt;ValkeyKey, ValkeyValue&gt;[] values = [
-    ///     new("key1", "value1"),
-    ///     new("key2", "value2")
-    /// ];
-    /// bool result = await client.StringSetAsync(values);
-    /// Console.WriteLine(result); // Output: true
-    /// </code>
-    /// </example>
-    /// </remarks>
-    Task<bool> StringSetAsync(KeyValuePair<ValkeyKey, ValkeyValue>[] values, CommandFlags flags = CommandFlags.None);
 
     /// <summary>
     /// Returns the substring of the string value stored at key, determined by the offsets 
@@ -304,4 +306,145 @@ public interface IStringCommands
     /// </example>
     /// </remarks>
     Task<double> StringIncrementAsync(ValkeyKey key, double increment, CommandFlags flags = CommandFlags.None);
+
+    /// <summary>
+    /// Get the value of key and delete the key. If the key does not exist the special value <see cref="ValkeyValue.Null"/> is returned.
+    /// </summary>
+    /// <seealso href="https://valkey.io/commands/getdel/">valkey.io</seealso>
+    /// <param name="key">The key to get and delete.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>The value of key, or <see cref="ValkeyValue.Null"/> when key does not exist.</returns>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// await client.StringSetAsync("key", "value");
+    /// ValkeyValue result = await client.StringGetDeleteAsync("key");
+    /// Console.WriteLine(result.ToString()); // Output: "value"
+    /// 
+    /// ValkeyValue result2 = await client.StringGetAsync("key");
+    /// Console.WriteLine(result2.IsNull); // Output: true (key was deleted)
+    /// </code>
+    /// </example>
+    /// </remarks>
+    Task<ValkeyValue> StringGetDeleteAsync(ValkeyKey key, CommandFlags flags = CommandFlags.None);
+
+    /// <summary>
+    /// Gets the string value associated with the key and optionally update its (relative) expiry.
+    /// If the key does not exist, the result will be <see cref="ValkeyValue.Null"/>.
+    /// </summary>
+    /// <seealso href="https://valkey.io/commands/getex/">valkey.io</seealso>
+    /// <note>Since Valkey 6.2.0 and above.</note>
+    /// <param name="key">The key to be retrieved from the database.</param>
+    /// <param name="expiry">The expiry to set. <see langword="null"/> will remove expiry.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>The value of key, or <see cref="ValkeyValue.Null"/> when key does not exist.</returns>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// await client.StringSetAsync("key", "value");
+    /// ValkeyValue result = await client.StringGetSetExpiryAsync("key", TimeSpan.FromSeconds(10));
+    /// Console.WriteLine(result.ToString()); // Output: "value"
+    /// </code>
+    /// </example>
+    /// </remarks>
+    Task<ValkeyValue> StringGetSetExpiryAsync(ValkeyKey key, TimeSpan? expiry, CommandFlags flags = CommandFlags.None);
+
+    /// <summary>
+    /// Gets the string value associated with the key and optionally update its (absolute) expiry.
+    /// If the key does not exist, the result will be <see cref="ValkeyValue.Null"/>.
+    /// </summary>
+    /// <seealso href="https://valkey.io/commands/getex/">valkey.io</seealso>
+    /// <note>Since Valkey 6.2.0 and above.</note>
+    /// <param name="key">The key to be retrieved from the database.</param>
+    /// <param name="expiry">The exact date and time to expire at.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>The value of key, or <see cref="ValkeyValue.Null"/> when key does not exist.</returns>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// await client.StringSetAsync("key", "value");
+    /// ValkeyValue result = await client.StringGetSetExpiryAsync("key", DateTime.UtcNow.AddMinutes(5));
+    /// Console.WriteLine(result.ToString()); // Output: "value"
+    /// </code>
+    /// </example>
+    /// </remarks>
+    Task<ValkeyValue> StringGetSetExpiryAsync(ValkeyKey key, DateTime expiry, CommandFlags flags = CommandFlags.None);
+
+    /// <summary>
+    /// Returns the longest common subsequence between the values at <paramref name="first"/> and <paramref name="second"/>,
+    /// returning a string containing the common sequence.
+    /// Note that this is different than the longest common string algorithm,
+    /// since matching characters in the string does not need to be contiguous.
+    /// </summary>
+    /// <seealso href="https://valkey.io/commands/lcs/">valkey.io</seealso>
+    /// <note>Since Valkey 7.0 and above.</note>
+    /// <note>When in cluster mode, both <paramref name="first"/> and <paramref name="second"/> must map to the same hash slot.</note>
+    /// <param name="first">The key that stores the first string.</param>
+    /// <param name="second">The key that stores the second string.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>A string (sequence of characters) of the LCS match.</returns>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// await client.StringSetAsync("key1", "abcdef");
+    /// await client.StringSetAsync("key2", "acef");
+    /// string? result = await client.StringLongestCommonSubsequenceAsync("key1", "key2");
+    /// Console.WriteLine(result); // Output: "acef"
+    /// </code>
+    /// </example>
+    /// </remarks>
+    Task<string?> StringLongestCommonSubsequenceAsync(ValkeyKey first, ValkeyKey second, CommandFlags flags = CommandFlags.None);
+
+    /// <summary>
+    /// Returns the longest common subsequence between the values at <paramref name="first"/> and <paramref name="second"/>,
+    /// returning the length of the common sequence.
+    /// Note that this is different than the longest common string algorithm,
+    /// since matching characters in the string does not need to be contiguous.
+    /// </summary>
+    /// <seealso href="https://valkey.io/commands/lcs/">valkey.io</seealso>
+    /// <note>Since Valkey 7.0 and above.</note>
+    /// <note>When in cluster mode, both <paramref name="first"/> and <paramref name="second"/> must map to the same hash slot.</note>
+    /// <param name="first">The key that stores the first string.</param>
+    /// <param name="second">The key that stores the second string.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>The length of the LCS match.</returns>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// await client.StringSetAsync("key1", "abcdef");
+    /// await client.StringSetAsync("key2", "acef");
+    /// long length = await client.StringLongestCommonSubsequenceLengthAsync("key1", "key2");
+    /// Console.WriteLine(length); // Output: 4
+    /// </code>
+    /// </example>
+    /// </remarks>
+    Task<long> StringLongestCommonSubsequenceLengthAsync(ValkeyKey first, ValkeyKey second, CommandFlags flags = CommandFlags.None);
+
+    /// <summary>
+    /// Returns the longest common subsequence between the values at <paramref name="first"/> and <paramref name="second"/>,
+    /// returning a list of all common sequences with their positions and match information.
+    /// Note that this is different than the longest common string algorithm,
+    /// since matching characters in the string does not need to be contiguous.
+    /// </summary>
+    /// <seealso href="https://valkey.io/commands/lcs/">valkey.io</seealso>
+    /// <param name="first">The key that stores the first string.</param>
+    /// <param name="second">The key that stores the second string.</param>
+    /// <param name="minLength">Can be used to restrict the list of matches to the ones of a given minimum length. Defaults to 0.</param>
+    /// <param name="flags">The flags to use for this operation. Currently flags are ignored.</param>
+    /// <returns>The result of LCS algorithm, containing match positions and lengths based on the given parameters.</returns>
+    /// <remarks>
+    /// <example>
+    /// <code>
+    /// await client.StringSetAsync("key1", "abcdefghijk");
+    /// await client.StringSetAsync("key2", "b1fh");
+    /// LCSMatchResult result = await client.StringLongestCommonSubsequenceWithMatchesAsync("key1", "key2");
+    /// Console.WriteLine($"LCS Length: {result.LongestMatchLength}");
+    /// foreach (var match in result.Matches)
+    /// {
+    ///     Console.WriteLine($"Match at positions: first[{match.FirstStringIndex}], second[{match.SecondStringIndex}], length: {match.Length}");
+    /// }
+    /// </code>
+    /// </example>
+    /// </remarks>
+    Task<LCSMatchResult> StringLongestCommonSubsequenceWithMatchesAsync(ValkeyKey first, ValkeyKey second, long minLength = 0, CommandFlags flags = CommandFlags.None);
 }
