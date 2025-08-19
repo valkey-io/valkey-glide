49b316c267784a6762786d51b20a1c7263297632
jbrinkman <joe.brinkman@improving.com>
2025-08-19 14:49:15 -0400

    fix: resolve issue with paths
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- benchmarks/install_and_test.sh
+++ benchmarks/install_and_test.sh
@@ -106,7 +106,7 @@ function fillDB(){
 }
 
 utilitiesDir=`pwd`/utilities
-script=`pwd`/${BASH_SOURCE[0]}
+script="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 RELATIVE_BENCH_PATH=`dirname ${script}`
 export BENCH_FOLDER=`realpath ${RELATIVE_BENCH_PATH}`
 export PYTHON_FOLDER="${BENCH_FOLDER}/../python"
