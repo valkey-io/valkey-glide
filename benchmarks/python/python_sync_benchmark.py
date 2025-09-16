d8d01742611b8ac97291f7170cc58c3794685342
Lior Sventitzky <liorsve@amazon.com>
2025-09-16 11:46:21 +0000

    addressed comments, added pre-timer pings
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>

--- benchmarks/python/python_sync_benchmark.py
+++ benchmarks/python/python_sync_benchmark.py
@@ -1,21 +1,14 @@
 # Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-import argparse
 import functools
-import json
-import math
-import random
 from threading import Thread
 import time
 from datetime import datetime, timezone
-from enum import Enum
 from pathlib import Path
-from statistics import mean
 from typing import List
 
-from concurrent.futures import ThreadPoolExecutor
-import numpy as np
 import redis as redispy  # type: ignore
+from redis.cluster import RedisCluster
 from glide_sync import (
     GlideClient,
     GlideClientConfiguration,
@@ -25,119 +18,24 @@ from glide_sync import (
     LogLevel,
     NodeAddress,
 )
-
-
-class ChosenAction(Enum):
-    GET_NON_EXISTING = 1
-    GET_EXISTING = 2
-    SET = 3
-
-
-PORT = 6379
-
-arguments_parser = argparse.ArgumentParser()
-arguments_parser.add_argument(
-    "--resultsFile",
-    help="Where to write the results file",
-    required=False,
-    default="../results/python-results.json",
-)
-arguments_parser.add_argument(
-    "--dataSize", help="Size of data to set", required=False, default="100"
-)
-arguments_parser.add_argument(
-    "--concurrentTasks",
-    help="List of number of concurrent threads to run",
-    nargs="+",
-    required=False,
-    default=("1", "10", "100", "1000"),
-)
-arguments_parser.add_argument(
-    "--clients", help="Which clients should run", required=False, default="all"
-)
-arguments_parser.add_argument(
-    "--host", help="What host to target", required=False, default="localhost"
-)
-arguments_parser.add_argument(
-    "--clientCount",
-    help="Number of clients to run concurrently",
-    nargs="+",
-    required=False,
-    default=("1"),
-)
-arguments_parser.add_argument(
-    "--tls",
-    help="Should benchmark a TLS server",
-    action="store_true",
-    required=False,
-    default=False,
-)
-arguments_parser.add_argument(
-    "--clusterModeEnabled",
-    help="Should benchmark a cluster mode enabled cluster",
-    action="store_true",
-    required=False,
-    default=False,
-)
-arguments_parser.add_argument(
-    "--port",
-    default=PORT,
-    type=int,
-    required=False,
-    help="Which port to connect to, defaults to `%(default)s`",
-)
-arguments_parser.add_argument(
-    "--minimal", help="Should run a minimal benchmark", action="store_true"
+from utils import (
+    ChosenAction,
+    choose_action,
+    create_argument_parser,
+    generate_value,
+    generate_key_set,
+    generate_key_get,
+    latency_results,
+    number_of_iterations,
+    process_results,
+    truncate_decimal,
 )
 
+arguments_parser = create_argument_parser()
 args = arguments_parser.parse_args()
 
-
-PROB_GET = 0.8
-PROB_GET_EXISTING_KEY = 0.8
-SIZE_GET_KEYSPACE = 3750000  # 3.75 million
-SIZE_SET_KEYSPACE = 3000000  # 3 million
 started_tasks_counter = 0
 bench_json_results: List[str] = []
-
-
-def truncate_decimal(number: float, digits: int = 3) -> float:
-    stepper = 10**digits
-    return math.floor(number * stepper) / stepper
-
-
-def generate_value(size):
-    return str("0" * size)
-
-
-def generate_key_set():
-    return str(random.randint(1, SIZE_SET_KEYSPACE + 1))
-
-
-def generate_key_get():
-    return str(random.randint(SIZE_SET_KEYSPACE, SIZE_GET_KEYSPACE + 1))
-
-
-def choose_action():
-    if random.random() > PROB_GET:
-        return ChosenAction.SET
-    if random.random() > PROB_GET_EXISTING_KEY:
-        return ChosenAction.GET_NON_EXISTING
-    return ChosenAction.GET_EXISTING
-
-
-def calculate_latency(latency_list, percentile):
-    return round(np.percentile(np.array(latency_list), percentile), 4)
-
-
-def process_results():
-    global bench_json_results
-    global args
-
-    # write json results to a file
-    res_file_path = args.resultsFile
-    with open(res_file_path, "w+") as f:
-        json.dump(bench_json_results, f)
 
 
 def timer(func):
@@ -182,23 +80,13 @@ def create_and_run_concurrent_threads(
         thread = Thread(
             target=execute_commands,
             args=(clients, total_commands, data_size, action_latencies),
-            name=f"Worker-{i}"
+            name=f"Worker-{i}",
         )
         threads.append(thread)
         thread.start()
-    
+
     for thread in threads:
         thread.join()
-
-def latency_results(prefix, latencies):
-    result = {}
-    result[prefix + "_p50_latency"] = calculate_latency(latencies, 50)
-    result[prefix + "_p90_latency"] = calculate_latency(latencies, 90)
-    result[prefix + "_p99_latency"] = calculate_latency(latencies, 9)
-    result[prefix + "_average_latency"] = truncate_decimal(mean(latencies))
-    result[prefix + "_std_dev"] = truncate_decimal(np.std(latencies))
-
-    return result
 
 
 def create_clients(client_count, action):
@@ -272,9 +160,17 @@ def main(
         clients = create_clients(
             client_count,
             lambda: client_class(
-                host=host, port=port, decode_responses=True, ssl=use_tls
+                host=host, port=port, ssl=use_tls
             ),
         )
+        
+        # redis-py starts the connection in the connection pool lazily, so we need to ping the nodes
+        # to not include the connection time in the benchmark
+        for client in clients:
+            if is_cluster:
+                client.ping(target_nodes=RedisCluster.ALL_NODES)
+            else:
+                client.ping()
 
         run_clients(
             clients,
@@ -289,15 +185,18 @@ def main(
             client.close()
 
     if clients_to_run == "all" or clients_to_run == "glide":
-        # Glide Socket
         client_class = GlideClusterClient if is_cluster else GlideClient
         config = (
             GlideClusterClientConfiguration(
-                [NodeAddress(host=host, port=port)], use_tls=use_tls, request_timeout=1000
+                [NodeAddress(host=host, port=port)],
+                use_tls=use_tls,
+                request_timeout=1000,
             )
             if is_cluster
             else GlideClientConfiguration(
-                [NodeAddress(host=host, port=port)], use_tls=use_tls, request_timeout=1000
+                [NodeAddress(host=host, port=port)],
+                use_tls=use_tls,
+                request_timeout=1000,
             )
         )
         clients = create_clients(
@@ -312,10 +211,6 @@ def main(
             data_size,
             is_cluster,
         )
-
-
-def number_of_iterations(num_of_concurrent_threads):
-    return min(max(100000, num_of_concurrent_threads * 10000), 5000000)
 
 
 if __name__ == "__main__":
@@ -343,16 +238,15 @@ if __name__ == "__main__":
         iterations = (
             1000 if args.minimal else number_of_iterations(num_of_concurrent_threads)
         )
-        main (
-        iterations,
-        num_of_concurrent_threads,
-        data_size,
-        clients_to_run,
-        host,
-        number_of_clients,
-        use_tls,
-        is_cluster,
+        main(
+            iterations,
+            num_of_concurrent_threads,
+            data_size,
+            clients_to_run,
+            host,
+            number_of_clients,
+            use_tls,
+            is_cluster,
         )
-        
 
-    process_results()
+    process_results(bench_json_results, args.resultsFile)
