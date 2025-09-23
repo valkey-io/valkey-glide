ce9d7fc343358a536aa21d3b5d5bbcca836cfe4b
James Xin <james.xin@improving.com>
2025-09-23 16:56:49 -0700

    skip isort for test_deprecation_warnings.py
    Signed-off-by: James Xin <james.xin@improving.com>

--- python/tests/async_tests/test_deprecation_warnings.py
+++ python/tests/async_tests/test_deprecation_warnings.py
@@ -1,5 +1,7 @@
 # Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
+# isort:skip_file
+
 import pytest
 
 
@@ -41,10 +43,10 @@ class TestDeprecationWarnings:
         from glide.config import GlideClientConfiguration as DeprecatedConfig
         from glide.constants import OK as DeprecatedOK
         from glide.routes import AllNodes as DeprecatedAllNodes
-        
+
         # Import from preferred paths
         from glide import RequestError, GlideClientConfiguration, OK, AllNodes
-        
+
         # Verify they're the same classes
         assert DeprecatedRequestError is RequestError
         assert DeprecatedConfig is GlideClientConfiguration
