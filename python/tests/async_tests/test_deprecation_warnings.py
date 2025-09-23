a482f85f6ef6e4ddf98cca9c4452d168892e74bb
James Xin <james.xin@improving.com>
2025-09-23 16:30:45 -0700

    linter
    Signed-off-by: James Xin <james.xin@improving.com>

--- python/tests/async_tests/test_deprecation_warnings.py
+++ python/tests/async_tests/test_deprecation_warnings.py
@@ -8,22 +8,30 @@ class TestDeprecationWarnings:
 
     def test_glide_exceptions_deprecation_warning(self):
         """Test that importing from glide.exceptions shows deprecation warning."""
-        with pytest.warns(DeprecationWarning, match="Importing from 'glide.exceptions' is deprecated"):
+        with pytest.warns(
+            DeprecationWarning, match="Importing from 'glide.exceptions' is deprecated"
+        ):
             from glide.exceptions import RequestError
 
     def test_glide_config_deprecation_warning(self):
         """Test that importing from glide.config shows deprecation warning."""
-        with pytest.warns(DeprecationWarning, match="Importing from 'glide.config' is deprecated"):
+        with pytest.warns(
+            DeprecationWarning, match="Importing from 'glide.config' is deprecated"
+        ):
             from glide.config import GlideClientConfiguration
 
     def test_glide_constants_deprecation_warning(self):
         """Test that importing from glide.constants shows deprecation warning."""
-        with pytest.warns(DeprecationWarning, match="Importing from 'glide.constants' is deprecated"):
+        with pytest.warns(
+            DeprecationWarning, match="Importing from 'glide.constants' is deprecated"
+        ):
             from glide.constants import OK
 
     def test_glide_routes_deprecation_warning(self):
         """Test that importing from glide.routes shows deprecation warning."""
-        with pytest.warns(DeprecationWarning, match="Importing from 'glide.routes' is deprecated"):
+        with pytest.warns(
+            DeprecationWarning, match="Importing from 'glide.routes' is deprecated"
+        ):
             from glide.routes import AllNodes
 
     def test_deprecated_imports_still_work(self):
