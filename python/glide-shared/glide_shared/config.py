249aadea9546b7ccb1f5d82d7dacab0f1ffbc7cc
Lior Sventitzky <liorsve@amazon.com>
2025-11-18 09:44:24 +0200

    Dynamic pubsub: added pubsub API in python (#4948)
    * added api pubsub methods
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>
    
    * addressed comments
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>
    
    * addressed comments 2
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>
    
    * exclude SubscriptionStatus symbol from re-export test
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>
    
    * addressed comments 3
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>
    
    * changed types of TO and get_subscriptions
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>
    
    * renamed to param
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>
    
    ---------
    
    Signed-off-by: Lior Sventitzky <liorsve@amazon.com>

--- python/glide-shared/glide_shared/config.py
+++ python/glide-shared/glide_shared/config.py
@@ -873,6 +873,15 @@ class GlideClientConfiguration(BaseClientConfiguration):
         callback: Optional[Callable[[PubSubMsg, Any], None]]
         context: Any
 
+    @dataclass
+    class PubSubState:
+        desired_subscriptions: Dict[
+            GlideClientConfiguration.PubSubChannelModes, Set[str]
+        ]
+        actual_subscriptions: Dict[
+            GlideClientConfiguration.PubSubChannelModes, Set[str]
+        ]
+
     def __init__(
         self,
         addresses: List[NodeAddress],
@@ -1079,6 +1088,15 @@ class GlideClusterClientConfiguration(BaseClientConfiguration):
         ]
         callback: Optional[Callable[[PubSubMsg, Any], None]]
         context: Any
+
+    @dataclass
+    class PubSubState:
+        desired_subscriptions: Dict[
+            GlideClusterClientConfiguration.PubSubChannelModes, Set[str]
+        ]
+        actual_subscriptions: Dict[
+            GlideClusterClientConfiguration.PubSubChannelModes, Set[str]
+        ]
 
     def __init__(
         self,
