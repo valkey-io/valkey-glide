88be4a1f4a0f64b0e8cb1482066ab219e39b559d
Sasidharan Gopal <sasidharan.gopal94@gmail.com>
2026-01-06 21:25:31 +0530

    Update test for script debug
    Signed-off-by: Sasidharan Gopal <sasidharan.gopal94@gmail.com>

--- java/integTest/src/test/java/glide/SharedCommandTests.java
+++ java/integTest/src/test/java/glide/SharedCommandTests.java
@@ -5407,8 +5407,9 @@ public class SharedCommandTests {
         assertEquals("OK", client.scriptDebug(ScriptDebugMode.NO).get());
 
         // Verify we can still execute scripts after changing debug mode
+        // Use regular eval instead of evalReadOnly for Redis 6.2 compatibility
         String simpleScript = "return 'Debug test'";
-        assertEquals("Debug test", client.evalReadOnly(simpleScript).get());
+        assertEquals("Debug test", client.eval(simpleScript).get());
     }
 
     @SneakyThrows
