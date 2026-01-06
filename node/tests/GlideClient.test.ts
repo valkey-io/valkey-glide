946a4ebfbeb5337ae83800816b959ed53beb0be7
James Xin <james.xin@improving.com>
2026-01-06 09:56:00 -0800

    [All clients] Add TCPNoDelay option (#5100)
    * init commit with core and java
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    * linters
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    * node change
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    * python changes
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    * python linter
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    * go changes
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    * address comment, add missing cluster client code
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    * fix python tests
    
    Signed-off-by: James Xin <james.xin@improving.com>
    
    ---------
    
    Signed-off-by: James Xin <james.xin@improving.com>

--- node/tests/GlideClient.test.ts
+++ node/tests/GlideClient.test.ts
@@ -1919,4 +1919,42 @@ describe("GlideClient", () => {
         },
         timeout: TIMEOUT,
     });
+
+    it(
+        "tcp nodelay configuration",
+        async () => {
+            const config = getClientConfigurationOption(
+                cluster.getAddresses(),
+                ProtocolVersion.RESP3,
+            );
+
+            // Test default (undefined - not set)
+            const defaultClient = await GlideClient.createClient(config);
+            expect(await defaultClient.ping()).toBe("PONG");
+            expect(await defaultClient.set("key", "value")).toBe("OK");
+            expect(await defaultClient.get("key")).toBe("value");
+            defaultClient.close();
+
+            // Test explicit true
+            const clientTrue = await GlideClient.createClient({
+                ...config,
+                advancedConfiguration: { tcpNoDelay: true },
+            });
+            expect(await clientTrue.ping()).toBe("PONG");
+            expect(await clientTrue.set("key2", "value2")).toBe("OK");
+            expect(await clientTrue.get("key2")).toBe("value2");
+            clientTrue.close();
+
+            // Test explicit false
+            const clientFalse = await GlideClient.createClient({
+                ...config,
+                advancedConfiguration: { tcpNoDelay: false },
+            });
+            expect(await clientFalse.ping()).toBe("PONG");
+            expect(await clientFalse.set("key3", "value3")).toBe("OK");
+            expect(await clientFalse.get("key3")).toBe("value3");
+            clientFalse.close();
+        },
+        TIMEOUT,
+    );
 });
