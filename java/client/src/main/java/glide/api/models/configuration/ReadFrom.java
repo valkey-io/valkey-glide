8ee2bdfe026f166e86cb57e7908f0da75351dac9
Alex Rehnby-Martin <alex.rehnby-martin@improving.com>
2026-01-22 11:00:38 -0800

    Continued implementation of ALL_NODES
    Signed-off-by: Alex Rehnby-Martin <alex.rehnby-martin@improving.com>

--- java/client/src/main/java/glide/api/models/configuration/ReadFrom.java
+++ java/client/src/main/java/glide/api/models/configuration/ReadFrom.java
@@ -21,4 +21,8 @@ public enum ReadFrom {
      * replica or the primary if needed.
      */
     AZ_AFFINITY_REPLICAS_AND_PRIMARY,
+    /**
+     * Spread the read requests between all nodes (primary and replicas) in a round-robin manner.
+     */
+    ALL_NODES,
 }
