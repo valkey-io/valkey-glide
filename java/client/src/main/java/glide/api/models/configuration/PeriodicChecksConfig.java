a1ad17bf25737f0da4c193a4496d344c6c4124d0
Shoham Elias <116083498+shohamazon@users.noreply.github.com>
2026-01-28 15:02:15 +0200

    [Backport from 2.2] Java: add topology periodic checks config (#5229) (#5247)
    Java: add topology periodic checks config (#5229)
    
    Signed-off-by: Shoham Elias <shohame@amazon.com>

---
+++ java/client/src/main/java/glide/api/models/configuration/PeriodicChecksConfig.java
@@ -0,0 +1,10 @@
+/** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
+package glide.api.models.configuration;
+
+/**
+ * Represents the configuration for periodic checks in cluster mode.
+ *
+ * @see PeriodicChecksStatus
+ * @see PeriodicChecksManualInterval
+ */
+public interface PeriodicChecksConfig {}
