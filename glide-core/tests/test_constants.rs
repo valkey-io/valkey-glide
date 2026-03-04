1c499bc944dc3fc232c20d4094977075be6f3a77
currantw <taylor.curran@improving.com>
2026-03-04 13:53:16 -0800

    Update for consistency with other clients
    Signed-off-by: currantw <taylor.curran@improving.com>

--- glide-core/tests/test_constants.rs
+++ glide-core/tests/test_constants.rs
@@ -3,10 +3,10 @@
 //! Shared test constants for Valkey GLIDE Rust core tests.
 #![allow(dead_code)]
 
-/// Test server IP addresses.
+// Host names and addresses for tests.
+// See 'cluster_manager.py' for details.
+pub const HOSTNAME_TLS: &str = "valkey.glide.test.tls.com";
+pub const HOSTNAME_NO_TLS: &str = "valkey.glide.test.no_tls.com";
 pub const HOST_IPV4: &str = "127.0.0.1";
 pub const HOST_IPV6: &str = "::1";
 
-/// Test server hostnames.
-pub const HOSTNAME_TLS: &str = "valkey.glide.test.tls.com"; // Included in TLS certificate SAN.
-pub const HOSTNAME_NO_TLS: &str = "valkey.glide.test.no_tls.com"; // Not included in TLS certificate SAN.
