0ac2c327fb24173227332aec099c5f8a9cb446b5
currantw <taylor.curran@improving.com>
2026-03-04 17:31:16 -0800

    Fix format
    Signed-off-by: currantw <taylor.curran@improving.com>

--- glide-core/tests/test_constants.rs
+++ glide-core/tests/test_constants.rs
@@ -9,4 +9,3 @@ pub const HOSTNAME_TLS: &str = "valkey.glide.test.tls.com";
 pub const HOSTNAME_NO_TLS: &str = "valkey.glide.test.no_tls.com";
 pub const HOST_IPV4: &str = "127.0.0.1";
 pub const HOST_IPV6: &str = "::1";
-
