e427e9761f35419adeab97a8e758d77850f88fe3
currantw <taylor.curran@improving.com>
2026-03-05 10:49:18 -0800

    style(go): fix constant alignment in integTest/constants.go
    Adjust spacing to match gofumpt formatting requirements
    
    Signed-off-by: currantw <taylor.curran@improving.com>

--- go/integTest/constants.go
+++ go/integTest/constants.go
@@ -6,8 +6,8 @@ package integTest
 const (
 	// Host names and addresses for tests.
 	// See 'cluster_manager.py' for details.
-	HOSTNAME_TLS        = "valkey.glide.test.tls.com"
-	HOSTNAME_NO_TLS     = "valkey.glide.test.no_tls.com"
-	IP_ADDRESS_V4       = "127.0.0.1"
-	IP_ADDRESS_V6       = "::1"
+	HOSTNAME_TLS    = "valkey.glide.test.tls.com"
+	HOSTNAME_NO_TLS = "valkey.glide.test.no_tls.com"
+	IP_ADDRESS_V4   = "127.0.0.1"
+	IP_ADDRESS_V6   = "::1"
 )
