23ff6be492a6f75bf486a60f8b588fa51e48dd87
Pratheep Kumar <pratheepkumar0403@gmail.com>
2026-03-17 19:47:31 +0530

    fixing miri-tests
    Signed-off-by: Pratheep Kumar <pratheepkumar0403@gmail.com>

--- ffi/miri-tests/mock-glide-core/src/connection_request.rs
+++ ffi/miri-tests/mock-glide-core/src/connection_request.rs
@@ -1,3 +1,20 @@
 // Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
 
-pub use glide_core::connection_request::{TlsMode, ConnectionRequest, NodeAddress};
+pub use glide_core::connection_request::{
+    AuthenticationInfo,
+    CompressionBackend,
+    CompressionConfig,
+    ConnectionRequest,
+    ConnectionRetryStrategy,
+    IamCredentials,
+    NodeAddress,
+    PeriodicChecksDisabled,
+    PeriodicChecksManualInterval,
+    ProtocolVersion,
+    PubSubChannelType,
+    PubSubChannelsOrPatterns,
+    PubSubSubscriptions,
+    ReadFrom,
+    ServiceType,
+    TlsMode,
+};
