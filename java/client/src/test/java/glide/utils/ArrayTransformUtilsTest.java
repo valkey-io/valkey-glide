9b7661d35745c392300d150198cd16163d37a8c6
Ariel Zach <ajzach@gmail.com>
2026-03-25 09:07:55 -0300

    fix: correct SPDX Identifier format in ArrayTransformUtilsTest
    Signed-off-by: Ariel Zach <ajzach@gmail.com>
    Signed-off-by: ajzach <ajzach@gmail.com>

--- java/client/src/test/java/glide/utils/ArrayTransformUtilsTest.java
+++ java/client/src/test/java/glide/utils/ArrayTransformUtilsTest.java
@@ -1,4 +1,4 @@
-/** Copyright Valkey GLIDE Project Contributors - SPDX-Identifier: Apache-2.0 */
+/** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
 package glide.utils;
 
 import static glide.api.models.GlideString.gs;
