ed2991da6d9dcdc87198ff6e974fa4088db32e12
currantw <taylor.curran@improving.com>
2026-04-01 13:23:21 -0700

    Fix Go lint error
    Signed-off-by: currantw <taylor.curran@improving.com>

--- go/integTest/constants.go
+++ go/integTest/constants.go
@@ -6,10 +6,10 @@ package integTest
 const (
 	// Host names and addresses for tests.
 	// See 'cluster_manager.py' for details.
-	HostnameTLS    = "valkey.glide.test.tls.com"
-	HostnameNoTLS  = "valkey.glide.test.no_tls.com"
-	IPAddressV4    = "127.0.0.1"
-	IPAddressV6    = "::1"
+	HostnameTLS   = "valkey.glide.test.tls.com"
+	HostnameNoTLS = "valkey.glide.test.no_tls.com"
+	IPAddressV4   = "127.0.0.1"
+	IPAddressV6   = "::1"
 
 	// IAM authentication test constants
 	TestClusterName   = "test-cluster"
