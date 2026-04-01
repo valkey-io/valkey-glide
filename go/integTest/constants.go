3c249ff0d76b94c628231a998e0214cce7b7531b
currantw <taylor.curran@improving.com>
2026-04-01 12:55:05 -0700

    Revert to CamelCase for Go test constants
    Signed-off-by: currantw <taylor.curran@improving.com>

--- go/integTest/constants.go
+++ go/integTest/constants.go
@@ -6,10 +6,10 @@ package integTest
 const (
 	// Host names and addresses for tests.
 	// See 'cluster_manager.py' for details.
-	HOSTNAME_TLS    = "valkey.glide.test.tls.com"
-	HOSTNAME_NO_TLS = "valkey.glide.test.no_tls.com"
-	IP_ADDRESS_V4   = "127.0.0.1"
-	IP_ADDRESS_V6   = "::1"
+	HostnameTLS    = "valkey.glide.test.tls.com"
+	HostnameNoTLS  = "valkey.glide.test.no_tls.com"
+	IPAddressV4    = "127.0.0.1"
+	IPAddressV6    = "::1"
 
 	// IAM authentication test constants
 	TestClusterName   = "test-cluster"
