8d5d485a1c9fdf50d9c290745b1d16fc26207f2b
Edward Liang <edward.liang@improving.com>
2026-04-17 13:31:56 -0700

    change values to be exact
    Signed-off-by: Edward Liang <edward.liang@improving.com>

--- java/integTest/src/test/java/glide/modules/JsonTests.java
+++ java/integTest/src/test/java/glide/modules/JsonTests.java
@@ -412,7 +412,7 @@ public class JsonTests {
         assertEquals(16L, Json.debugMemory(client, gs(key), gs(".key6")).get());
 
         long mem = Json.debugMemory(client, key).get();
-        assertTrue(mem >= 500L && mem <= 600L);
+        assertTrue(mem == 504L || mem == 560L, "Expected debugMemory to be 504 or 560, got " + mem);
         assertEquals(19L, Json.debugFields(client, gs(key)).get());
     }
 
