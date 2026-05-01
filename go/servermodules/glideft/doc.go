ec96564e907f984aa3583b6c127af3392b0342f7
Edward Liang <76571219+edlng@users.noreply.github.com>
2026-05-01 13:19:13 -0700

    Go: Refactor `ft` commands into a `glideft` package (#5855)

---
+++ go/servermodules/glideft/doc.go
@@ -0,0 +1,11 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+
+// Package glideft provides functions for Valkey Search (FT) module commands.
+//
+// These commands require the Search module to be loaded on the Valkey server.
+// Commands are executed via [glide.Client.CustomCommand] or [glide.ClusterClient.CustomCommand].
+//
+// See [valkey.io] for details.
+//
+// [valkey.io]: https://valkey.io/commands/?group=search
+package glideft
