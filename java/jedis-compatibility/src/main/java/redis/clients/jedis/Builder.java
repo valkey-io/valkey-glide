43047c9bc68bec9b5c49e63bce20078013c1d1fd
prashanna-frsh <prashanna.rajendran@freshworks.com>
2026-05-05 11:20:00 +0530

    refactor(jedis-compat): remove duplication on different jedis layers
    Signed-off-by: prashanna-frsh <prashanna.rajendran@freshworks.com>

--- java/jedis-compatibility/src/main/java/redis/clients/jedis/Builder.java
+++
@@ -1,7 +0,0 @@
-/** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
-package redis.clients.jedis;
-
-/** Builder abstract class for compatibility with Valkey GLIDE wrapper. */
-public abstract class Builder<T> {
-    public abstract T build(Object data);
-}
--- java/jedis-compatibility/src/main/java/redis/clients/jedis/args/ListPosition.java
+++
@@ -1,7 +0,0 @@
-/** Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0 */
-package redis.clients.jedis.args;
-
-public enum ListPosition {
-    BEFORE,
-    AFTER
-}
