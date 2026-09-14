7bbc87cb86b27e1c6773b31b39219944f9293bf5
currantw <taylor.curran@improving.com>
2026-09-14 13:08:17 -0700

    chore(rust): add empty cmd module placeholder (#7024)
    Signed-off-by: currantw <taylor.curran@improving.com>

---
+++ rust/src/cmd.rs
@@ -0,0 +1,5 @@
+// Copyright Valkey GLIDE Project Contributors - SPDX Identifier: Apache-2.0
+//! GLIDE's owned command builder.
+//!
+//! The `Cmd` type and its API are added in the following commit; this file is
+//! introduced empty first so that change reviews as a modification.
